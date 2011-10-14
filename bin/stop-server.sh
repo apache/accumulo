717b795de9dbe509bf2fc3232b49ac22f76c23a6
Keith Turner <kturner@apache.org>
2011-10-14 15:51:27 +0000

    ACCUMULO-28 fixed bug in stop-server.sh
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/trunk@1183395 13f79535-47bb-0310-9956-ffa450edef68

--- bin/stop-server.sh
+++ bin/stop-server.sh
@@ -10,7 +10,7 @@ bin=`cd "$bin"; pwd`
 if [ "$1" = "`hostname`" ]; then
 	PID=`ps -ef | grep "$ACCUMULO_HOME" | egrep ${2} | grep "Main ${3}" | grep -v grep | grep -v ssh | grep -v stop-server.sh | awk {'print \$2'} | head -1`
 else
-	PID=`ssh -q -o 'ConnectTimeout 8' $1 "ps -ef | grep \"$ACCUMULO_HOME\" egrep '${2}' | grep 'Main ${3}' | grep -v grep | grep -v ssh | grep -v stop-server.sh" | awk {'print $2'} | head -1`
+	PID=`ssh -q -o 'ConnectTimeout 8' $1 "ps -ef | grep \"$ACCUMULO_HOME\" |  egrep '${2}' | grep 'Main ${3}' | grep -v grep | grep -v ssh | grep -v stop-server.sh" | awk {'print $2'} | head -1`
 fi
 if [ ! -z $PID ]; then
         echo "stopping ${3} on $1";
