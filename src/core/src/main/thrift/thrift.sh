731982fd7ca72eceff590b42b250d304b7ecd4f7
John Vines <vines@apache.org>
2011-10-14 13:42:35 +0000

    Resolves Accumulo-26 - we neglected org/apache for a portion of the directory names.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.3@1183360 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/thrift/thrift.sh
+++ src/core/src/main/thrift/thrift.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 # test to see if we have thrift installed
-VERSION=`thrift -version 2>/dev/null | grep "0.3" |  wc -l`
+VERSION=`thrift -version 2>/dev/null | grep "0.3" |	wc -l`
 if [ "$VERSION" -ne 1 ] ; then 
-   # Nope: bail
-   echo "thrift is not available"
-   exit 0
+	# Nope: bail
+	echo "thrift is not available"
+	exit 0
 fi
 CLOUDTRACE=../trace
 THRIFT_ARGS="-I $CLOUDTRACE/src/main/thrift -o target "
@@ -20,13 +20,13 @@ done
 # copy only files that have changed
 for d in gc master tabletserver security client/impl data
 do
-   mkdir -p src/main/java/accumulo/core/$d/thrift >& /dev/null
-   for f in target/gen-java/accumulo/core/$d/thrift/* 
-   do
-      DEST="src/main/java/accumulo/core/$d/thrift/`basename $f`"
-      if ! cmp -s ${f} ${DEST} ; then
-	echo cp ${f} ${DEST} 
-	cp ${f} ${DEST} 
-      fi
-   done
+	mkdir -p src/main/java/org/apache/accumulo/core/$d/thrift >& /dev/null
+	for f in target/gen-java/org/apache/accumulo/core/$d/thrift/* 
+	do
+		DEST="src/main/java/org/apache/accumulo/core/$d/thrift/`basename $f`"
+		if ! cmp -s ${f} ${DEST} ; then
+			echo cp ${f} ${DEST} 
+			cp ${f} ${DEST} 
+		fi
+	done
 done
