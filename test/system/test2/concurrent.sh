e8a7aed45e2d7dc7d056a609c4f204d044edea57
Keith Turner <kturner@apache.org>
2011-10-18 17:50:01 +0000

    ACCUMULO-42 applied apache license to 1.3
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.3@1185770 13f79535-47bb-0310-9956-ffa450edef68

--- test/system/test2/concurrent.sh
+++ test/system/test2/concurrent.sh
@@ -1,4 +1,20 @@
 #! /usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 echo "ingesting first halves (0 to (500K - 1), 1M to (1.5M - 1), etc)"
 
 ../../../bin/accumulo jar ../../../lib/accumulo.jar org.apache.accumulo.server.test.TestIngest -timestamp 1 -size 50 -random 56 500000 0 1 &
