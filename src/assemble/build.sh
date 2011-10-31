ebd1004cfb335f296ef020dae4999fe3b1b9aee3
Eric C. Newton <ecn@apache.org>
2011-10-31 19:45:01 +0000

    ACCUMULO-86: add rat:check to the build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.3@1195635 13f79535-47bb-0310-9956-ffa450edef68

--- src/assemble/build.sh
+++ src/assemble/build.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 loc=`dirname "$0"`
 loc=`cd "$loc/../.."; pwd`
 
@@ -23,7 +38,14 @@ runAt() {
   ( cd $1 ; echo in `pwd`; shift ; run $@ ) || fail 
 }
 
-run mvn -U -P distclean clean package javadoc:aggregate javadoc:jar source:jar
+run mvn -U -P distclean clean 
+mvn rat:check 
+COUNT=`grep '!????' target/rat.txt | wc -l`
+if [ "$COUNT" -ne 31 ]
+then
+   fail expected 32 files missing licenses, but saw "$COUNT"
+fi
+run mvn package javadoc:aggregate javadoc:jar source:jar
 runAt ./src/server/src/main/c++ make 
 run mvn package source:jar assembly:single
 test -x /usr/bin/rpmbuild && run mvn -N rpm:rpm
