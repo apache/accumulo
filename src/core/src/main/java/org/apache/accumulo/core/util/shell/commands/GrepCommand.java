38ff761d471e83186f89a3d528b98320e574538a
John Vines <vines@apache.org>
2011-11-01 19:22:38 +0000

    re accumulo-107
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/trunk@1196227 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/util/shell/commands/GrepCommand.java
+++ src/core/src/main/java/org/apache/accumulo/core/util/shell/commands/GrepCommand.java
@@ -98,7 +98,7 @@ public class GrepCommand extends ScanCommand {
   @Override
   public Options getOptions() {
     Options opts = super.getOptions();
-    numThreadsOpt = new Option("t", "num-threads", true, "num threads");
+    numThreadsOpt = new Option("nt", "num-threads", true, "num threads");
     tableOpt = new Option(Shell.tableOption, "tableName", true, "table to grep through");
     tableOpt.setArgName("table");
     tableOpt.setRequired(false);
