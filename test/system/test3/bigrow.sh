f2ec438478ba4ee09f2761b5146698e6bf5536fa
Billie Rinaldi <billie@apache.org>
2011-11-18 20:03:33 +0000

    ACCUMULO-157 property changes for 1.4
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.4@1203816 13f79535-47bb-0310-9956-ffa450edef68

--- test/system/test3/bigrow.sh
+++ test/system/test3/bigrow.sh
@@ -21,4 +21,3 @@
 ../../../bin/accumulo jar ../../../lib/accumulo.jar org.apache.accumulo.server.test.VerifyIngest -size 50 -timestamp 1 -random 56 1000000 0 1;
 ../../../bin/accumulo jar ../../../lib/accumulo.jar org.apache.accumulo.server.test.TestIngest -timestamp 1 -size 50 -random 56 1000000 7000000 1;
 ../../../bin/accumulo jar ../../../lib/accumulo.jar org.apache.accumulo.server.test.VerifyIngest -size 50 -timestamp 1 -random 56 1000000 7000000 1;
-
