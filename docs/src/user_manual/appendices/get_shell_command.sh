b76b45627a84120a5da5c5b1e79ee67514552968
Billie Rinaldi <billie@apache.org>
2011-11-18 20:48:23 +0000

    ACCUMULO-157 merged property changes to trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/trunk@1203828 13f79535-47bb-0310-9956-ffa450edef68

--- docs/src/user_manual/appendices/get_shell_command.sh
+++ docs/src/user_manual/appendices/get_shell_command.sh
@@ -16,4 +16,5 @@
 # limitations under the License.
 
 
-echo "help $1quit" | ../../../../bin/accumulo shell -u foo -p foo --fake 2>&1 >/dev/null | sed -E "s/  +/  /g" | perl -ne 'use Text::Wrap; $Text::Wrap::columns=82; $Text::Wrap::break="[ ]"; print wrap("","              ",$_);' | gawk '{print "   ",$0}'
+echo "help $1
+quit" | ../../../../bin/accumulo shell -u foo -p foo --fake 2>&1 >/dev/null | sed -E "s/  +/  /g" | perl -ne 'use Text::Wrap; $Text::Wrap::columns=82; $Text::Wrap::break="[ ]"; print wrap("","              ",$_);' | gawk '{print "   ",$0}'
