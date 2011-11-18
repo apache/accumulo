2d2094737f68b1cb885d1a9fc72f91893e84c1f9
Billie Rinaldi <billie@apache.org>
2011-11-18 16:31:33 +0000

    ACCUMULO-157 fixed property settings
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.3@1203724 13f79535-47bb-0310-9956-ffa450edef68

--- docs/src/user_manual/appendices/get_shell_commands.sh
+++ docs/src/user_manual/appendices/get_shell_commands.sh
@@ -19,7 +19,8 @@
 echo "\\chapter{Shell Commands}"
 echo
 echo "\\begin{alltt}"
-echo "help -npquit" | \
+echo "help -np
+quit" | \
 ../../../../bin/accumulo shell -u foo -p foo --fake | \
 #head -11 | \
 gawk 'BEGIN{count = 0}{if(count > 9 && substr($0,0,4) != "foo@"){print "\\textbf{"$1"}\n"; system("./get_shell_command.sh \""$1"\""); print "";};count += 1}' |\
