c5d5959326b9cf327dc5d5ec08622999734f95f8
Eric C. Newton <ecn@apache.org>
2011-11-21 19:08:51 +0000

    ACCUMULO-86 setting version in release branch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.3.5rc@1204656 13f79535-47bb-0310-9956-ffa450edef68

--- bin/config.sh
+++ bin/config.sh
@@ -47,7 +47,7 @@ mkdir -p $ACCUMULO_LOG_DIR 2>/dev/null
 export ACCUMULO_LOG_DIR
 
 if [ -z ${ACCUMULO_VERSION} ]; then
-        ACCUMULO_VERSION=1.3.5-incubating-SNAPSHOT
+        ACCUMULO_VERSION=1.3.5-incubating
 fi
 
 if [ -z "$HADOOP_HOME" ]
