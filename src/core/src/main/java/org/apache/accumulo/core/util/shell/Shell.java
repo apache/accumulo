f984e181f1e9803fccca36cfe56863dfc13b5ce5
Eric C. Newton <ecn@apache.org>
2012-01-05 19:08:44 +0000

    ACCUMULO-249 turn off verbose when executing a single command; remove shell code that loads member classes: there are none
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/trunk@1227769 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/util/shell/Shell.java
+++ src/core/src/main/java/org/apache/accumulo/core/util/shell/Shell.java
@@ -345,6 +345,7 @@ public class Shell {
     }
     if (cl.hasOption(execCommandOpt.getOpt())) {
       execCommand = cl.getOptionValue(execCommandOpt.getOpt());
+      verbose = false;
     }
     
     rootToken = new Token();
@@ -362,18 +363,6 @@ public class Shell {
         new WhoAmICommand(),};
     for (Command cmd : external) {
       commandFactory.put(cmd.getName(), cmd);
-    }
-    for (Class<?> subclass : Shell.class.getClasses()) {
-      Object t;
-      try {
-        t = subclass.newInstance();
-      } catch (Exception e) {
-        continue;
-      }
-      if (t instanceof Command) {
-        Command sci = (Command) t;
-        commandFactory.put(sci.getName(), sci);
-      }
     }
   }
   
