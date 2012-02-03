705caa03764c543f803775ab1814630f505ad25c
Keith Turner <kturner@apache.org>
2012-02-03 21:14:13 +0000

    ACCUMULO-368 set tablet location in metadata before brining tablet online in tablet server instead of after
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.4@1240342 13f79535-47bb-0310-9956-ffa450edef68

--- src/server/src/main/java/org/apache/accumulo/server/tabletserver/TabletServer.java
+++ src/server/src/main/java/org/apache/accumulo/server/tabletserver/TabletServer.java
@@ -2399,6 +2399,9 @@ public class TabletServer extends AbstractMetricsImpl implements org.apache.accu
             tablet.waitForMinC();
           }
           
+          Assignment assignment = new Assignment(extentToOpen, getTabletSession());
+          TabletStateStore.setLocation(assignment);
+
           synchronized (openingTablets) {
             synchronized (onlineTablets) {
               openingTablets.remove(extentToOpen);
@@ -2445,13 +2448,7 @@ public class TabletServer extends AbstractMetricsImpl implements org.apache.accu
             }
           }, reschedule);
         } else {
-          try {
-            Assignment assignment = new Assignment(extentToOpen, getTabletSession());
-            TabletStateStore.setLocation(assignment);
-            enqueueMasterMessage(new TabletStatusMessage(TabletLoadState.LOADED, extentToOpen));
-          } catch (DistributedStoreException ex) {
-            log.warn("Unable to update storage", ex);
-          }
+          enqueueMasterMessage(new TabletStatusMessage(TabletLoadState.LOADED, extentToOpen));
         }
       }
     }
