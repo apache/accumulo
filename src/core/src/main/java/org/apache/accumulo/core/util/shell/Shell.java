7cc1ffda82094dd0a51bce7f8482e3b4f1d630a3
Billie Rinaldi <billie@apache.org>
2012-02-17 17:06:06 +0000

    ACCUMULO-242 added many references to apache accumulo - merged to 1.4
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.4@1245655 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/util/shell/Shell.java
+++ src/core/src/main/java/org/apache/accumulo/core/util/shell/Shell.java
@@ -155,7 +155,7 @@ public class Shell {
   private static final Logger audit = Logger.getLogger(Shell.class.getName() + ".audit");
   
   public static final int NO_FIXED_ARG_LENGTH_CHECK = -1;
-  private static final String SHELL_DESCRIPTION = "Shell - Accumulo Interactive Shell";
+  private static final String SHELL_DESCRIPTION = "Shell - Apache Accumulo Interactive Shell";
   private static final String DEFAULT_AUTH_TIMEOUT = "60"; // in minutes
   
   private int exitCode = 0;
