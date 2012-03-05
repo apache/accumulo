f2133881a1d2fc6ddda07ad8c233f56e11b21a5e
John Vines <vines@apache.org>
2012-03-05 15:48:32 +0000

    ACCUMULO-442 - improved documentation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/accumulo/branches/1.4@1297093 13f79535-47bb-0310-9956-ffa450edef68

--- src/trace/thrift.sh
+++ src/trace/thrift.sh
@@ -15,12 +15,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script will regenerate the thrift code for cloudtrace. This should be run at least whenever the thrift code is changed. There is no harm, besides wasted
+# cycles, from running it more often then that.
 
+# Generates the cloudtrace thrift code. We're explicitly using thrift0.6 because other versions
+# are not compatible
 thrift0.6 -o target -gen java src/main/thrift/cloudtrace.thrift
 
+# For all generated thrift code, suppress all warnings
 find target/gen-java -name '*.java' -print | xargs sed -i.orig -e 's/public class /@SuppressWarnings("all") public class /'
 
+# Make a directory for said thrift code if does not already exist
 mkdir -p src/main/java/cloudtrace/thrift
+
+# For every file, move it with the appropriate path name IFF they are different
 for f in target/gen-java/cloudtrace/thrift/*
 do
   DEST=src/main/java/cloudtrace/thrift/`basename $f`
