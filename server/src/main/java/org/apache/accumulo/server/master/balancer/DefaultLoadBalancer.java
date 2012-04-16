38a8c823f405b76dca3819071e8ffe9f869000c4
Eric C. Newton <ecn@apache.org>
2012-04-16 13:13:23 +0000

    ACCUMULO-537 needs to use base implementation to capture the configuration
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1326599 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/accumulo/server/master/balancer/DefaultLoadBalancer.java
+++ server/src/main/java/org/apache/accumulo/server/master/balancer/DefaultLoadBalancer.java
@@ -30,7 +30,6 @@ import org.apache.accumulo.core.data.KeyExtent;
 import org.apache.accumulo.core.master.thrift.TableInfo;
 import org.apache.accumulo.core.master.thrift.TabletServerStatus;
 import org.apache.accumulo.core.tabletserver.thrift.TabletStats;
-import org.apache.accumulo.server.conf.ServerConfiguration;
 import org.apache.accumulo.server.master.state.TServerInstance;
 import org.apache.accumulo.server.master.state.TabletMigration;
 import org.apache.log4j.Logger;
@@ -310,12 +309,4 @@ public class DefaultLoadBalancer extends TabletBalancer {
     return 5 * 1000;
   }
   
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.apache.accumulo.server.master.balancer.TabletBalancer#init(org.apache.accumulo.server.conf.ServerConfiguration)
-   */
-  @Override
-  public void init(ServerConfiguration conf) {
-  }
 }
