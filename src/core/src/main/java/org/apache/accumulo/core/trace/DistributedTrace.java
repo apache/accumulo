3e4f75995c0bacbcbfdb77170c704f1d1f8d2ce7
Eric C. Newton <ecn@apache.org>
2012-04-25 12:40:50 +0000

    ACCUMULO-559 use instance.getZooKeepers instead of trying to read the site configuration
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.3@1330246 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/trace/DistributedTrace.java
+++ src/core/src/main/java/org/apache/accumulo/core/trace/DistributedTrace.java
@@ -22,8 +22,6 @@ import java.net.UnknownHostException;
 
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.client.Instance;
-import org.apache.accumulo.core.conf.AccumuloConfiguration;
-import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.zookeeper.ZooUtil;
 import org.apache.zookeeper.KeeperException;
 
@@ -32,7 +30,6 @@ import cloudtrace.instrument.receivers.ZooSpanClient;
 
 public class DistributedTrace {
   public static void enable(Instance instance, String application, String address) throws IOException, KeeperException, InterruptedException {
-    String zookeeper = AccumuloConfiguration.getSystemConfiguration().get(Property.INSTANCE_ZK_HOST);
     String path = ZooUtil.getRoot(instance) + Constants.ZTRACERS;
     if (address == null) {
       try {
@@ -41,6 +38,6 @@ public class DistributedTrace {
         address = "unknown";
       }
     }
-    Tracer.getInstance().addReceiver(new ZooSpanClient(zookeeper, path, address, application, 1000));
+    Tracer.getInstance().addReceiver(new ZooSpanClient(instance.getZooKeepers(), path, address, application, 1000));
   }
 }
