471c0ba64e9ebbea46d387a4adcfb718e138fc71
Billie Rinaldi <billie@apache.org>
2012-04-27 17:40:04 +0000

    ACCUMULO-563 created script and added it to the generate-resources build phase
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.3@1331513 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/assemble/install-thrift-jar.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+fail() {
+  echo '   ' $@
+  exit 1
+}
+
+run() {
+  echo $@
+  eval $@
+  if [ $? -ne 0 ]
+  then
+    fail $@ fails
+  fi
+}
+
+runAt() {
+  ( cd $1 ; echo in `pwd`; shift ; run $@ ) || fail 
+}
+
+run svn export https://svn.apache.org/repos/asf/thrift/tags/thrift-0.3.0
+runAt thrift-0.3.0/lib/java ant
+run mvn install:install-file -Dfile=thrift-0.3.0/lib/java/libthrift.jar -DgroupId=org.apache.accumulo.thrift -DartifactId=libthrift -Dversion=0.3 -Dpackaging=jar
+run rm -rf thrift-0.3.0
