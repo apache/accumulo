a0e817fd075a627999f06d8b90ac8205b948ae42
Eric C. Newton <ecn@apache.org>
2012-06-04 17:51:47 +0000

    ACCUMULO-613 update version numbers
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.3.6@1346053 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/Constants.java
+++ src/core/src/main/java/org/apache/accumulo/core/Constants.java
@@ -27,7 +27,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 
 public class Constants {
-  public static final String VERSION = "1.3.6-SNAPSHOT";
+  public static final String VERSION = "1.3.6";
   public static final int DATA_VERSION = 3;
   
   // Zookeeper locations
