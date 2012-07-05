2b3bfde11cd861c1d39504684ae516070069b0f9
Eric C. Newton <ecn@apache.org>
2012-07-05 12:29:04 +0000

    ACCUMULO-669 fix calls to ThreadPoolExecutor to work the way I think it should work
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1357579 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/accumulo/core/util/SimpleThreadPool.java
+++ core/src/main/java/org/apache/accumulo/core/util/SimpleThreadPool.java
@@ -27,7 +27,8 @@ import java.util.concurrent.TimeUnit;
 public class SimpleThreadPool extends ThreadPoolExecutor {
   
   public SimpleThreadPool(int max, final String name) {
-    super(0, max, 1l, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new NamingThreadFactory(name));
+    super(max, max, 1l, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new NamingThreadFactory(name));
+    allowCoreThreadTimeOut(true);
   }
   
 }
