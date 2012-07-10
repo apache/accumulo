ce75f7946d76de0c97dab4459dc98f488368b83c
Eric C. Newton <ecn@apache.org>
2012-07-10 11:52:43 +0000

    ACCUMULO-678 add administrative shutdown to randomwalk tests
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1359620 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/accumulo/server/test/randomwalk/concurrent/StartAll.java
+++ server/src/main/java/org/apache/accumulo/server/test/randomwalk/concurrent/StartAll.java
@@ -10,7 +10,8 @@ public class StartAll extends Test {
   @Override
   public void visit(State state, Properties props) throws Exception {
     log.info("Starting all servers");
-    Runtime.getRuntime().exec(new String[]{System.getenv().get("ACCUMULO_HOME") + "/bin/start-all.sh"});
+    Process exec = Runtime.getRuntime().exec(new String[]{System.getenv().get("ACCUMULO_HOME") + "/bin/start-all.sh"});
+    exec.waitFor();
   }
   
 }
