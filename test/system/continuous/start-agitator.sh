1859b0ba5e0a35d360c0606d21e58e0baa3de303
Josh Elser <elserj@apache.org>
2013-01-13 04:40:46 +0000

    Ensure that CONTINUOUS_LOG_DIR is created before starting (m)agitator
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1432549 13f79535-47bb-0310-9956-ffa450edef68

--- test/system/continuous/start-agitator.sh
+++ test/system/continuous/start-agitator.sh
@@ -18,6 +18,8 @@
 
 . continuous-env.sh
 
+mkdir -p $CONTINUOUS_LOG_DIR
+
 nohup ./agitator.pl $KILL_SLEEP_TIME $TUP_SLEEP_TIME $MIN_KILL $MAX_KILL >$CONTINUOUS_LOG_DIR/`date +%Y%m%d%H%M%S`_`hostname`_agitator.out 2>$CONTINUOUS_LOG_DIR/`date +%Y%m%d%H%M%S`_`hostname`_agitator.err &
 
 nohup ./magitator.pl $MASTER_KILL_SLEEP_TIME $MASTER_RESTART_SLEEP_TIME >$CONTINUOUS_LOG_DIR/`date +%Y%m%d%H%M%S`_`hostname`_magitator.out 2>$CONTINUOUS_LOG_DIR/`date +%Y%m%d%H%M%S`_`hostname`_magitator.err &
