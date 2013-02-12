44272556c5ff810fc546d2b99778952504f80fb3
Keith Turner <kturner@apache.org>
2013-02-12 16:02:56 +0000

    ACCUMULO-745 updated example to use new continuous ingest options
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1445221 13f79535-47bb-0310-9956-ffa450edef68

--- examples/instamo/src/main/java/org/apache/accumulo/instamo/MapReduceExample.java
+++ examples/instamo/src/main/java/org/apache/accumulo/instamo/MapReduceExample.java
@@ -19,9 +19,9 @@ package org.apache.accumulo.instamo;
 import java.io.File;
 import java.util.UUID;
 
+import org.apache.accumulo.test.MiniAccumuloCluster;
 import org.apache.accumulo.test.continuous.ContinuousIngest;
 import org.apache.accumulo.test.continuous.ContinuousVerify;
-import org.apache.accumulo.test.MiniAccumuloCluster;
 import org.apache.commons.io.FileUtils;
 
 /**
@@ -32,15 +32,15 @@ public class MapReduceExample {
   public static void run(String instanceName, String zookeepers, String rootPassword, String args[]) throws Exception {
     
     // run continuous ingest to create data. This is not a map reduce job
-    ContinuousIngest.main(new String[] {instanceName, zookeepers, "root", rootPassword, "ci", "5000000", "0", Long.MAX_VALUE + "", "1000", "1000", "1000000",
-        "60000", "2", "false"});
+    ContinuousIngest.main(new String[] {"-i", instanceName, "-z", zookeepers, "-u", "root", "-p", rootPassword, "--table", "ci", "--num", "5000000",
+        "--batchMemory", "1000000"});
     
     String outputDir = FileUtils.getTempDirectoryPath() + File.separator + "ci_verify" + UUID.randomUUID().toString();
     
     try {
       // run verify map reduce job locally. This jobs looks for holes in the linked list create by continuous ingest
-      ContinuousVerify.main(new String[] {"-D", "mapred.job.tracker=local", "-D", "fs.default.name=file:///", instanceName, zookeepers, "root", rootPassword,
-          "ci", outputDir, "4", "1", "false"});
+      ContinuousVerify.main(new String[] {"-D", "mapred.job.tracker=local", "-D", "fs.default.name=file:///", "-i", instanceName, "-z", zookeepers, "-u",
+          "root", "-p", rootPassword, "--table", "ci", "--output", outputDir, "--maxMappers", "4", "--reducers", "1"});
     } finally {
       // delete output dir of mapreduce job
       FileUtils.deleteQuietly(new File(outputDir));
