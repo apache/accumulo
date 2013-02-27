888d5b6dc2c9e06b41792aa2ae2242d9a21bce28
Eric C. Newton <ecn@apache.org>
2013-02-27 17:10:48 +0000

    ACCUMULO-1114 store token for root, not serialized bytes
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1450853 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/main/java/org/apache/accumulo/test/randomwalk/security/SecurityFixture.java
+++ test/src/main/java/org/apache/accumulo/test/randomwalk/security/SecurityFixture.java
@@ -21,6 +21,7 @@ import java.util.Set;
 
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.security.Authorizations;
+import org.apache.accumulo.core.security.CredentialHelper;
 import org.apache.accumulo.core.security.SystemPermission;
 import org.apache.accumulo.core.security.TablePermission;
 import org.apache.accumulo.core.security.tokens.PasswordToken;
@@ -53,7 +54,7 @@ public class SecurityFixture extends Fixture {
     conn.securityOperations().createUser(systemUserName, sysUserPass);
     
     WalkingSecurity.get(state).setTableName(secTableName);
-    state.set("rootUserPass", state.getCredentials().getToken());
+    state.set("rootUserPass", CredentialHelper.extractToken(state.getCredentials()));
     
     WalkingSecurity.get(state).setSysUserName(systemUserName);
     WalkingSecurity.get(state).createUser(systemUserName, sysUserPass);
