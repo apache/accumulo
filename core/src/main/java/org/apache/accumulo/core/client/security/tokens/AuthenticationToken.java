845b78e8a5dd351e51bcb53240f66be1aece4dd6
John Vines <vines@apache.org>
2013-02-28 21:45:34 +0000

    ACCUMULO-1133 - moved files... right
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/ACCUMULO-259-polishing@1451368 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/accumulo/core/client/security/tokens/AuthenticationToken.java
+++ core/src/main/java/org/apache/accumulo/core/client/security/tokens/AuthenticationToken.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.core.security.tokens;
+package org.apache.accumulo.core.client.security.tokens;
 
 import javax.security.auth.Destroyable;
 
