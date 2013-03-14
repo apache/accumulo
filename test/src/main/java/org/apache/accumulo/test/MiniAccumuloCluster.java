8e4a6ba9f907feaf21966d6a7007bcb65accbcf0
Eric C. Newton <ecn@apache.org>
2013-03-14 20:09:39 +0000

    ACCUMULO-804 pick up the new location of the hadoop jars
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1456635 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/main/java/org/apache/accumulo/test/MiniAccumuloCluster.java
+++ test/src/main/java/org/apache/accumulo/test/MiniAccumuloCluster.java
@@ -263,7 +263,15 @@ public class MiniAccumuloCluster {
     appendProp(fileWriter, Property.TSERV_NATIVEMAP_ENABLED, "false", siteConfig);
     // since there is a small amount of memory, check more frequently for majc... setting may not be needed in 1.5
     appendProp(fileWriter, Property.TSERV_MAJC_DELAY, "3", siteConfig);
-    String cp = System.getenv("ACCUMULO_HOME")+"/lib/.*.jar,$ZOOKEEPER_HOME/zookeeper[^.].*.jar,$HADOOP_HOME/[^.].*.jar,$HADOOP_HOME/lib/[^.].*.jar"; 
+    String cp = System.getenv("ACCUMULO_HOME")+"/lib/.*.jar,"+
+        "$ZOOKEEPER_HOME/zookeeper[^.].*.jar,"+
+        "$HADOOP_HOME/[^.].*.jar,"+
+        "$HADOOP_HOME/lib/[^.].*.jar,"+
+        "$HADOOP_PREFIX/share/hadoop/common/.*.jar," +
+        "$HADOOP_PREFIX/share/hadoop/common/lib/.*.jar," +
+        "$HADOOP_PREFIX/share/hadoop/hdfs/.*.jar," +
+        "$HADOOP_PREFIX/share/hadoop/mapreduce/.*.jar"
+        ; 
     appendProp(fileWriter, Property.GENERAL_CLASSPATHS, cp, siteConfig);
     appendProp(fileWriter, Property.GENERAL_DYNAMIC_CLASSPATHS, libDir.getAbsolutePath(), siteConfig);
     
