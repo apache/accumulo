742960f1d364a0a195bfd6df886ead18887ed196
Eric C. Newton <ecn@apache.org>
2013-03-18 12:02:58 +0000

    ACCUMULO-1183 applying patch from Corey Nolet
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.5@1457724 13f79535-47bb-0310-9956-ffa450edef68

--- proxy/src/main/java/org/apache/accumulo/proxy/ProxyServer.java
+++ proxy/src/main/java/org/apache/accumulo/proxy/ProxyServer.java
@@ -819,7 +819,17 @@ public class ProxyServer implements AccumuloProxy.Iface {
           }
         }
         scanner.setRanges(ranges);
+
+        if (opts.columns != null) {
+          for (ScanColumn col : opts.columns) {
+            if (col.isSetColQualifier())
+              scanner.fetchColumn(ByteBufferUtil.toText(col.colFamily), ByteBufferUtil.toText(col.colQualifier));
+            else
+              scanner.fetchColumnFamily(ByteBufferUtil.toText(col.colFamily));
+          }
+        }
       }
+
       UUID uuid = UUID.randomUUID();
       
       ScannerPlusIterator spi = new ScannerPlusIterator();
