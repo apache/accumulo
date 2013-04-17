6f229654a87b37921e9c45371f1a904d58be8c37
Eric C. Newton <ecn@apache.org>
2013-04-17 14:30:39 +0000

    ACCUMULO-1199 translate security exceptions for a missing table into TableNotFound exceptions
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.5@1468932 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/accumulo/core/client/admin/TableOperationsImpl.java
+++ core/src/main/java/org/apache/accumulo/core/client/admin/TableOperationsImpl.java
@@ -63,6 +63,7 @@ import org.apache.accumulo.core.client.impl.TabletLocator.TabletLocation;
 import org.apache.accumulo.core.client.impl.thrift.ClientService;
 import org.apache.accumulo.core.client.impl.thrift.ThriftSecurityException;
 import org.apache.accumulo.core.client.impl.thrift.ThriftTableOperationException;
+import org.apache.accumulo.core.client.security.SecurityErrorCode;
 import org.apache.accumulo.core.conf.AccumuloConfiguration;
 import org.apache.accumulo.core.conf.ConfigurationCopy;
 import org.apache.accumulo.core.conf.Property;
@@ -814,6 +815,9 @@ public class TableOperationsImpl extends TableOperationsHelper {
         }
       }
     } catch (ThriftSecurityException e) {
+      if (e.getCode().equals(SecurityErrorCode.TABLE_DOESNT_EXIST)) {
+        throw new TableNotFoundException(tableId, null, e.getMessage(), e);
+      }
       log.debug("flush security exception on table id " + tableId);
       throw new AccumuloSecurityException(e.user, e.code, e);
     } catch (ThriftTableOperationException e) {
