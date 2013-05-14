41cfc5812b448ed6a9ed55221d3b39013688b9eb
John Vines <vines@apache.org>
2013-05-14 20:47:48 +0000

    ACCUMULO-1415 - SystemToken moving to server
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/trunk@1482580 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/accumulo/server/client/security/token/SystemToken.java
+++ server/src/main/java/org/apache/accumulo/server/client/security/token/SystemToken.java
@@ -14,7 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.core.client.security.tokens;
+package org.apache.accumulo.server.client.security.token;
+
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 
 /**
  * @since 1.5.0
