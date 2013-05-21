49201bd2aecbc7fda0adb6a2e5c3f29b62366c44
Corey J. Nolet <cjnolet@apache.org>
2013-05-21 20:17:35 +0000

    ACCUMULO-1438, ACCUMULO-1414 Move minicluster into its own package and add proxy to user manual
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.4@1484935 13f79535-47bb-0310-9956-ffa450edef68

--- src/minicluster/src/main/java/org/apache/accumulo/minicluster/MiniAccumuloCluster.java
+++ src/minicluster/src/main/java/org/apache/accumulo/minicluster/MiniAccumuloCluster.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.server.mini;
+package org.apache.accumulo.minicluster;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- src/minicluster/src/main/java/org/apache/accumulo/minicluster/MiniAccumuloConfig.java
+++ src/minicluster/src/main/java/org/apache/accumulo/minicluster/MiniAccumuloConfig.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.server.mini;
+package org.apache.accumulo.minicluster;
 
 import java.io.File;
 import java.util.Collections;
