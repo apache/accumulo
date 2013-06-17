9b2a56b2f0d538f00e733399396c17fe55322dc8
Keith Turner <kturner@apache.org>
2013-06-17 19:36:52 +0000

    ACCUMULO-1518 Patch from David M. Lyle that allows opening rfiles w/ multiple dots in name.
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.4@1493899 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/file/FileOperations.java
+++ src/core/src/main/java/org/apache/accumulo/core/file/FileOperations.java
@@ -44,14 +44,13 @@ class DispatchingFileFactory extends FileOperations {
     if (name.startsWith(Constants.MAPFILE_EXTENSION + "_")) {
       return new MapFileOperations();
     }
-    
     String[] sp = name.split("\\.");
     
-    if (sp.length != 2) {
+    if (sp.length < 2) {
       throw new IllegalArgumentException("File name " + name + " has no extension");
     }
     
-    String extension = sp[1];
+    String extension = sp[sp.length - 1];
     
     if (extension.equals(Constants.MAPFILE_EXTENSION) || extension.equals(Constants.MAPFILE_EXTENSION + "_tmp")) {
       return new MapFileOperations();
