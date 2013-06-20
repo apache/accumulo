9e44bdf2d5b70d1f0d6c2f31f53e78b9f0aad1ce
Josh Elser <elserj@apache.org>
2013-06-20 00:24:41 +0000

    ACCUMULO-1504 Merge Miguel's changes from 1.4 to 1.5
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.5@1494818 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/accumulo/core/client/IteratorSettingTest.java
@@ -0,0 +1,76 @@
+package org.apache.accumulo.core.client;
+
+import static org.junit.Assert.*;
+
+import org.apache.accumulo.core.iterators.Combiner;
+import org.apache.accumulo.core.iterators.DevNull;
+import org.apache.hadoop.io.Writable;
+import org.junit.Test;
+
+/**
+ * Test cases for the IteratorSetting class
+ */
+public class IteratorSettingTest {
+  
+  IteratorSetting setting1 = new IteratorSetting(500, "combiner", Combiner.class.getName());
+  IteratorSetting setting2 = new IteratorSetting(500, "combiner", Combiner.class.getName());
+  IteratorSetting setting3 = new IteratorSetting(500, "combiner", Combiner.class.getName());
+  IteratorSetting devnull = new IteratorSetting(500, "devNull", DevNull.class.getName());
+  IteratorSetting nullsetting = null;
+  IteratorSetting setting4 = new IteratorSetting(300, "combiner", Combiner.class.getName());
+  IteratorSetting setting5 = new IteratorSetting(500, "foocombiner", Combiner.class.getName());
+  IteratorSetting setting6 = new IteratorSetting(500, "combiner", "MySuperCombiner");
+  
+  @Test
+  public final void testHashCodeSameObject() {
+    assertEquals(setting1.hashCode(), setting1.hashCode());
+  }
+  
+  @Test
+  public final void testHashCodeEqualObjects() {
+    assertEquals(setting1.hashCode(), setting2.hashCode());
+  }
+  
+  @Test
+  public final void testEqualsObjectReflexive() {
+    assertEquals(setting1, setting1);
+  }
+  
+  @Test
+  public final void testEqualsObjectSymmetric() {
+    assertEquals(setting1, setting2);
+    assertEquals(setting2, setting1);
+  }
+  
+  @Test
+  public final void testEqualsObjectTransitive() {
+    assertEquals(setting1, setting2);
+    assertEquals(setting2, setting3);
+    assertEquals(setting1, setting3);
+  }
+  
+  @Test
+  public final void testEqualsNullSetting() {
+    assertNotEquals(setting1, nullsetting);
+  }
+  
+  @Test
+  public final void testEqualsObjectNotEqual() {
+    assertNotEquals(setting1, devnull);
+  }
+  
+  @Test
+  public final void testEqualsObjectProperties() {
+    IteratorSetting mysettings = new IteratorSetting(500, "combiner", Combiner.class.getName());
+    assertEquals(setting1, mysettings);
+    mysettings.addOption("myoption1", "myvalue1");
+    assertNotEquals(setting1, mysettings);
+  }
+  
+  @Test
+  public final void testEqualsDifferentMembers() {
+    assertNotEquals(setting1, setting4);
+    assertNotEquals(setting1, setting5);
+    assertNotEquals(setting1, setting6);
+  }
+}
