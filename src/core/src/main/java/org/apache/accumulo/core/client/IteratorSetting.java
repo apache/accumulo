a91173bcbb71052f730961e842cbc5b713d58104
Josh Elser <elserj@apache.org>
2013-06-20 00:16:14 +0000

    ACCUMULO-1504 Pulling back Miguel's changes for IteratorSetting from trunk to 1.4
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/1.4@1494816 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/src/main/java/org/apache/accumulo/core/client/IteratorSetting.java
+++ src/core/src/main/java/org/apache/accumulo/core/client/IteratorSetting.java
@@ -251,6 +251,52 @@ public class IteratorSetting {
   }
   
   /**
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((iteratorClass == null) ? 0 : iteratorClass.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + priority;
+    result = prime * result + ((properties == null) ? 0 : properties.hashCode());
+    return result;
+  }
+  
+  /**
+   * @see java.lang.Object#equals()
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof IteratorSetting))
+      return false;
+    IteratorSetting other = (IteratorSetting) obj;
+    if (iteratorClass == null) {
+      if (other.iteratorClass != null)
+        return false;
+    } else if (!iteratorClass.equals(other.iteratorClass))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (priority != other.priority)
+      return false;
+    if (properties == null) {
+      if (other.properties != null)
+        return false;
+    } else if (!properties.equals(other.properties))
+      return false;
+    return true;
+  }
+
+  /**
    * @see java.lang.Object#toString()
    */
   @Override
