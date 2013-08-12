d10feb7c7f7ae9eb5affe69ab18959e8bd7c16d7
Sean Hickey <tallirishllama@gmail.com>
2013-08-12 08:11:31 -0400

    ACCUMULO-802 added -l option to namespaces command

--- core/src/main/java/org/apache/accumulo/core/util/shell/commands/NamespacesCommand.java
+++ core/src/main/java/org/apache/accumulo/core/util/shell/commands/NamespacesCommand.java
@@ -17,6 +17,9 @@
 package org.apache.accumulo.core.util.shell.commands;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
@@ -25,14 +28,39 @@ import org.apache.accumulo.core.util.shell.Shell.Command;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.collections.iterators.AbstractIteratorDecorator;
 
 public class NamespacesCommand extends Command {
-  private Option disablePaginationOpt;
+  private Option disablePaginationOpt, namespaceIdOption;
   
+  @SuppressWarnings("unchecked")
   @Override
   public int execute(final String fullCommand, final CommandLine cl, final Shell shellState) throws AccumuloException, AccumuloSecurityException, IOException {
-    shellState.printLines(shellState.getConnector().tableNamespaceOperations().list().iterator(), !cl.hasOption(disablePaginationOpt.getOpt()));
+      Iterator<String> names = shellState.getConnector().tableNamespaceOperations().list().iterator();
+      Iterator<String> ids = new NamespaceIdIterator(new TreeMap<String,String>(shellState.getConnector().tableNamespaceOperations().namespaceIdMap()).entrySet().iterator());
+    
+    if (cl.hasOption(namespaceIdOption.getOpt())) {
+      shellState.printLines(ids, !cl.hasOption(disablePaginationOpt.getOpt()));
+    } else {
+      shellState.printLines(names, !cl.hasOption(disablePaginationOpt.getOpt()));
+    }
     return 0;
+  }
+  
+  /**
+   * Decorator that formats the id and name for display.
+   */
+  private static final class NamespaceIdIterator extends AbstractIteratorDecorator {
+    public NamespaceIdIterator(Iterator<Entry<String,String>> iterator) {
+      super(iterator);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Object next() {
+      Entry entry = (Entry) super.next();
+      return String.format("%-15s => %10s%n", entry.getKey(), entry.getValue());
+    }
   }
   
   @Override
@@ -43,6 +71,8 @@ public class NamespacesCommand extends Command {
   @Override
   public Options getOptions() {
     final Options o = new Options();
+    namespaceIdOption = new Option("l", "list-ids", false, "display internal table namespace ids along with the name");
+    o.addOption(namespaceIdOption);
     disablePaginationOpt = new Option("np", "no-pagination", false, "disable pagination of output");
     o.addOption(disablePaginationOpt);
     return o;
