ccf988546159de0bfb504a33e3f32ee4bdb920d2
Christopher Tubbs <ctubbsii@apache.org>
2013-09-10 16:07:58 -0400

    ACCUMULO-1689 Don't use deprecated methods in gc mac test

--- minicluster/src/test/java/org/apache/accumulo/minicluster/MiniAccumuloClusterGCTest.java
+++ minicluster/src/test/java/org/apache/accumulo/minicluster/MiniAccumuloClusterGCTest.java
@@ -18,12 +18,16 @@ package org.apache.accumulo.minicluster;
 
 import java.io.File;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
@@ -41,73 +45,73 @@ import com.google.common.collect.ImmutableMap;
  * 
  */
 public class MiniAccumuloClusterGCTest {
-  
+
   private static TemporaryFolder tmpDir = new TemporaryFolder();
   private static MiniAccumuloConfig macConfig;
   private static MiniAccumuloCluster accumulo;
   private static final String passwd = "password";
-  
+
   @BeforeClass
   public static void setupMiniCluster() throws Exception {
     tmpDir.create();
     Logger.getLogger("org.apache.zookeeper").setLevel(Level.ERROR);
-    
+
     macConfig = new MiniAccumuloConfig(tmpDir.getRoot(), passwd);
     macConfig.setNumTservers(1);
-    
+
     // Turn on the garbage collector
     macConfig.runGC(true);
-    
+
     // And tweak the settings to make it run often
     Map<String,String> config = ImmutableMap.of(Property.GC_CYCLE_DELAY.getKey(), "1s", Property.GC_CYCLE_START.getKey(), "0s");
     macConfig.setSiteConfig(config);
-    
+
     accumulo = new MiniAccumuloCluster(macConfig);
     accumulo.start();
   }
-  
+
   @AfterClass
   public static void tearDownMiniCluster() throws Exception {
     accumulo.stop();
     tmpDir.delete();
   }
-  
+
   @Test(timeout = 20000)
   public void test() throws Exception {
     ZooKeeperInstance inst = new ZooKeeperInstance(accumulo.getInstanceName(), accumulo.getZooKeepers());
-    Connector c = inst.getConnector("root", passwd);
-    
+    Connector c = inst.getConnector("root", new PasswordToken(passwd));
+
     final String table = "foobar";
     c.tableOperations().create(table);
-    
+
     BatchWriter bw = null;
-    
+
     // Add some data
     try {
-      bw = c.createBatchWriter(table, 1000l, 100l, 1);
+      bw = c.createBatchWriter(table, new BatchWriterConfig().setMaxMemory(1000l).setMaxLatency(100, TimeUnit.MILLISECONDS).setMaxWriteThreads(1));
       Mutation m = new Mutation("a");
       for (int i = 0; i < 50; i++) {
         m.put("colf", Integer.toString(i), "");
       }
-      
+
       bw.addMutation(m);
     } finally {
       if (null != bw) {
         bw.close();
       }
     }
-    
+
     final boolean flush = true, wait = true;
-    
+
     // Compact the tables to get some rfiles which we can gc
     c.tableOperations().compact(table, null, null, flush, wait);
-    c.tableOperations().compact("!METADATA", null, null, flush, wait);
-    
+    c.tableOperations().compact(MetadataTable.NAME, null, null, flush, wait);
+
     File accumuloDir = new File(tmpDir.getRoot().getAbsolutePath(), "accumulo");
     File tables = new File(accumuloDir.getAbsolutePath(), "tables");
-    
+
     int fileCountAfterCompaction = FileUtils.listFiles(tables, new SuffixFileFilter(".rf"), TrueFileFilter.TRUE).size();
-    
+
     // Sleep for 4s to let the GC do its thing
     for (int i = 1; i < 5; i++) {
       Thread.sleep(1000);
@@ -117,8 +121,8 @@ public class MiniAccumuloClusterGCTest {
         return;
       }
     }
-    
+
     Assert.fail("Expected to find less files after compaction and pause for GC");
   }
-  
+
 }
