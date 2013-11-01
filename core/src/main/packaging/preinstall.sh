8f6f6701822edca9b04802497c7566be887d5448
Christopher Tubbs <ctubbsii@apache.org>
2013-11-01 14:36:56 -0400

    ACCUMULO-210 Make native it tests and rat check pass

--- core/src/main/packaging/preinstall.sh
+++
@@ -1,3 +0,0 @@
-getent group accumulo >/dev/null || groupadd -r accumulo
-getent passwd accumulo >/dev/null || useradd -r -g accumulo -d %{_datadir}/accumulo -s /sbin/nologin -c "Apache Accumulo" accumulo
-exit 0
