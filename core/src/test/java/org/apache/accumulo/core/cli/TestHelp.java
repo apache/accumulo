100e75bece15ad2d223070912854430d078e587a
Morgan Haskel <morgan.haskel@gmail.com>
2013-11-24 22:43:36 -0500

    ACCUMULO-1899 exit with non-zero exit code if there's an error parsing the arguments.
    Signed-off-by: Josh Elser <elserj@apache.org>

---
+++ core/src/test/java/org/apache/accumulo/core/cli/TestHelp.java
@@ -0,0 +1,46 @@
+package org.apache.accumulo.core.cli;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestHelp {
+  protected class HelpStub extends Help {
+    @Override
+    public void parseArgs(String programName, String[] args, Object ... others) {
+      super.parseArgs(programName, args, others);
+    }
+
+    @Override
+    public void exit(int status) {
+      throw new RuntimeException(Integer.toString(status));
+    }
+  }
+  
+  @Test
+  public void testInvalidArgs() {
+    String[] args = {"foo"};
+    HelpStub help = new HelpStub();
+    try {
+      help.parseArgs("program", args);
+    } catch (RuntimeException e) {
+      assertEquals("1", e.getMessage());
+    }
+  }
+
+}
