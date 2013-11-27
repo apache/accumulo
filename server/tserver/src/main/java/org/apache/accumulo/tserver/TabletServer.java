edae90f593da385e3a6d58063a2d41fbdbb543aa
Eric Newton <eric.newton@gmail.com>
2013-11-27 09:47:31 -0500

    ACCUMULO-1937 decrease log severity for expected case of a tablet no longer being in the unopened set at the moment it is being loaded

--- server/tserver/src/main/java/org/apache/accumulo/tserver/TabletServer.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/TabletServer.java
@@ -2819,8 +2819,13 @@ public class TabletServer extends AbstractMetricsImpl implements org.apache.accu
 
             if (openingOverlapping.contains(extent) || onlineOverlapping.contains(extent))
               return;
+            
+            if (!unopenedOverlapping.contains(extent)) {
+              log.info("assignment " + extent + " no longer in the unopened set");
+              return;
+            }
 
-            if (!unopenedTablets.contains(extent) || unopenedOverlapping.size() != 1 || openingOverlapping.size() > 0 || onlineOverlapping.size() > 0) {
+            if (unopenedOverlapping.size() != 1 || openingOverlapping.size() > 0 || onlineOverlapping.size() > 0) {
               throw new IllegalStateException("overlaps assigned " + extent + " " + !unopenedTablets.contains(extent) + " " + unopenedOverlapping + " "
                   + openingOverlapping + " " + onlineOverlapping);
             }
