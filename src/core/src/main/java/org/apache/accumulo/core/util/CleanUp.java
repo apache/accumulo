c94a73f478c91a24e35583d41bb39102461c54fa
Jared Winick <jaredwinick@koverse.com>
2014-01-02 22:30:59 -0700

    ACCUMULO-2128 added waitForZooKeeperClientThreads method
    Signed-off-by: Keith Turner <kturner@apache.org>

--- src/core/src/main/java/org/apache/accumulo/core/util/CleanUp.java
+++ src/core/src/main/java/org/apache/accumulo/core/util/CleanUp.java
@@ -16,20 +16,48 @@
  */
 package org.apache.accumulo.core.util;
 
+import java.util.Set;
+
 import org.apache.accumulo.core.client.impl.ThriftTransportPool;
 import org.apache.accumulo.core.zookeeper.ZooSession;
+import org.apache.log4j.Logger;
 
 /**
  * 
  */
 public class CleanUp {
+  
+  private static final Logger log = Logger.getLogger(CleanUp.class);
+  
   /**
    * kills all threads created by internal Accumulo singleton resources. After this method is called, no accumulo client will work in the current classloader.
    */
   public static void shutdownNow() {
     ThriftTransportPool.getInstance().shutdown();
     ZooSession.shutdown();
-    // need to get code from jared w
-    // waitForZooKeeperClientThreads();
+    waitForZooKeeperClientThreads();
+  }
+  
+  /**
+   * As documented in https://issues.apache.org/jira/browse/ZOOKEEPER-1816, ZooKeeper.close()
+   * is a non-blocking call. This method will wait on the ZooKeeper internal threads to exit.
+   */
+  private static void waitForZooKeeperClientThreads() {
+    Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+    for (Thread thread : threadSet) {    
+      // find ZooKeeper threads that were created in the same ClassLoader as the current thread.
+      if (thread.getClass().getName().startsWith("org.apache.zookeeper.ClientCnxn") &&
+          thread.getContextClassLoader().equals(Thread.currentThread().getContextClassLoader())) {
+
+        // wait for the thread the die
+        while (thread.isAlive()) {
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+          }
+        }
+      }
+    }
   }
 }
