db56d8d21dc636e6d271ae4c710ed27196a24506
Josh Elser <elserj@apache.org>
2014-01-10 17:51:29 -0500

    ACCUMULO-2174 Provide a better VFS tmpdir default that won't collide across processes

--- start/src/main/java/org/apache/accumulo/start/classloader/vfs/UniqueFileReplicator.java
+++ start/src/main/java/org/apache/accumulo/start/classloader/vfs/UniqueFileReplicator.java
@@ -30,18 +30,20 @@ import org.apache.commons.vfs2.provider.FileReplicator;
 import org.apache.commons.vfs2.provider.UriParser;
 import org.apache.commons.vfs2.provider.VfsComponent;
 import org.apache.commons.vfs2.provider.VfsComponentContext;
+import org.apache.log4j.Logger;
 
 /**
  * 
  */
 public class UniqueFileReplicator implements VfsComponent, FileReplicator {
-  
+
   private static final char[] TMP_RESERVED_CHARS = new char[] {'?', '/', '\\', ' ', '&', '"', '\'', '*', '#', ';', ':', '<', '>', '|'};
+  private static final Logger log = Logger.getLogger(UniqueFileReplicator.class);
   
   private File tempDir;
   private VfsComponentContext context;
   private List<File> tmpFiles = Collections.synchronizedList(new ArrayList<File>());
-  
+
   public UniqueFileReplicator(File tempDir) {
     this.tempDir = tempDir;
   }
@@ -49,38 +51,38 @@ public class UniqueFileReplicator implements VfsComponent, FileReplicator {
   @Override
   public File replicateFile(FileObject srcFile, FileSelector selector) throws FileSystemException {
     String baseName = srcFile.getName().getBaseName();
-    
+
     try {
       tempDir.mkdirs();
       String safeBasename = UriParser.encode(baseName, TMP_RESERVED_CHARS).replace('%', '_');
       File file = File.createTempFile("vfsr_", "_" + safeBasename, tempDir);
       file.deleteOnExit();
-      
+
       final FileObject destFile = context.toFileObject(file);
       destFile.copyFrom(srcFile, selector);
-      
+
       return file;
     } catch (IOException e) {
       throw new FileSystemException(e);
     }
   }
-  
+
   @Override
   public void setLogger(Log logger) {
     // TODO Auto-generated method stub
-    
+
   }
-  
+
   @Override
   public void setContext(VfsComponentContext context) {
     this.context = context;
   }
-  
+
   @Override
   public void init() throws FileSystemException {
-    
+
   }
-  
+
   @Override
   public void close() {
     synchronized (tmpFiles) {
@@ -88,7 +90,11 @@ public class UniqueFileReplicator implements VfsComponent, FileReplicator {
         tmpFile.delete();
       }
     }
-    
-    tempDir.delete();
+
+    if (tempDir.exists()) {
+      int numChildren = tempDir.list().length;
+      if (0 == numChildren && !tempDir.delete())
+        log.warn("Cannot delete empty directory: " + tempDir);
+    }
   }
 }
