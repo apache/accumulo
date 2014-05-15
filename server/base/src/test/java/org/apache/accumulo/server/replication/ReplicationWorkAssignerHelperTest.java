18432fe36372961a5460d9909b3ca9fcf1f35c1f
Josh Elser <elserj@apache.org>
2014-05-15 19:16:39 -0400

    ACCUMULO-378 Rogue test class not in the right package.

--- server/base/src/test/java/org/apache/accumulo/server/replication/ReplicationWorkAssignerHelperTest.java
+++ server/base/src/test/java/org/apache/accumulo/server/replication/ReplicationWorkAssignerHelperTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ReplicationWorkAssignerHelper;
+package org.apache.accumulo.server.replication;
 
 import java.util.Map.Entry;
 import java.util.UUID;
