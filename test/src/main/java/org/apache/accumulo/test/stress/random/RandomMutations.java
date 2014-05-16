74acc30cafb8be0ee5c7cc457c5e09f0ea56a993
Bill Slacum <ujustgotbilld@apache.org>
2014-05-16 14:06:15 -0400

    ACCUMULO-2789 Add stress test with variable cell sizes
    Added a test that users can run to stress their instances. They can supply
    variable cell sizes (currently row, column family and column qualifier are
    supported), and the write clients will generate random data within those size
    constraints. Users can also scan random tablets to add futher load onto the
    system. A variable number of write and reader clients can be executed to run
    the test under a mixed workload.
    
    This test is an acid test-- it is used to verify that an instance can handle
    the workload provided by it. It doesn't assert an actual state of the system,
    beyond reporting what errors the write and scan clients will see.
    
    See the included package-info.java and/or README.md files for more information.

---
+++ test/src/main/java/org/apache/accumulo/test/stress/random/RandomMutations.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.accumulo.test.stress.random;
+
+import org.apache.accumulo.core.data.Mutation;
+
+public class RandomMutations extends Stream<Mutation> {
+  private final RandomByteArrays rows, column_families, column_qualifiers, values;
+  
+  public RandomMutations(RandomByteArrays rows, RandomByteArrays column_families,
+      RandomByteArrays column_qualifiers, RandomByteArrays values) {
+    this.rows = rows;
+    this.column_families = column_families;
+    this.column_qualifiers = column_qualifiers;
+    this.values = values;
+  }
+  
+  // TODO should we care about timestamps?
+  @Override
+  public Mutation next() {
+    Mutation m = new Mutation(rows.next());
+    m.put(column_families.next(), column_qualifiers.next(), values.next());
+    return m;
+  }
+}
