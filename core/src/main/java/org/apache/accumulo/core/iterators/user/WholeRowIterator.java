73a9e6cfcad6c836ed9eb95421bc7306037739a9
Ryan Leary <rleary@bbn.com>
2014-05-17 21:03:03 -0400

    ACCUMULO-2825 Add RowEncodingIterator
    The WholeRowIterator now extends the abstract RowEncodingIterator

--- core/src/main/java/org/apache/accumulo/core/iterators/user/WholeRowIterator.java
+++ core/src/main/java/org/apache/accumulo/core/iterators/user/WholeRowIterator.java
@@ -21,21 +21,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.PartialKey;
-import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.iterators.IteratorEnvironment;
 import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
-import org.apache.hadoop.io.Text;
 
 /**
  * 
@@ -53,18 +47,28 @@ import org.apache.hadoop.io.Text;
  * 
  * @see RowFilter
  */
-public class WholeRowIterator implements SortedKeyValueIterator<Key,Value> {
-  
-  private SortedKeyValueIterator<Key,Value> sourceIter;
-  private Key topKey = null;
-  private Value topValue = null;
-  
-  public WholeRowIterator() {
-    
-  }
+public class WholeRowIterator extends RowEncodingIterator {
+  public WholeRowIterator() {}
   
   WholeRowIterator(SortedKeyValueIterator<Key,Value> source) {
     this.sourceIter = source;
+  }
+  
+  @Override
+  public SortedKeyValueIterator<Key,Value> deepCopy(IteratorEnvironment env) {
+    if (sourceIter != null)
+      return new WholeRowIterator(sourceIter.deepCopy(env));
+    return new WholeRowIterator();
+  }
+
+  @Override
+  public SortedMap<Key, Value> rowDecoder(Key rowKey, Value rowValue) throws IOException {
+	return decodeRow(rowKey, rowValue);
+  }
+
+  @Override
+  public Value rowEncoder(List<Key> keys, List<Value> values) throws IOException {
+	return encodeRow(keys, values);
   }
 
   /**
@@ -139,100 +143,4 @@ public class WholeRowIterator implements SortedKeyValueIterator<Key,Value> {
     
     return new Value(out.toByteArray());
   }
-  
-  List<Key> keys = new ArrayList<Key>();
-  List<Value> values = new ArrayList<Value>();
-  
-  private void prepKeys() throws IOException {
-    if (topKey != null)
-      return;
-    Text currentRow;
-    do {
-      if (sourceIter.hasTop() == false)
-        return;
-      currentRow = new Text(sourceIter.getTopKey().getRow());
-      keys.clear();
-      values.clear();
-      while (sourceIter.hasTop() && sourceIter.getTopKey().getRow().equals(currentRow)) {
-        keys.add(new Key(sourceIter.getTopKey()));
-        values.add(new Value(sourceIter.getTopValue()));
-        sourceIter.next();
-      }
-    } while (!filter(currentRow, keys, values));
-    
-    topKey = new Key(currentRow);
-    topValue = encodeRow(keys, values);
-    
-  }
-  
-  /**
-   * 
-   * @param currentRow
-   *          All keys have this in their row portion (do not modify!).
-   * @param keys
-   *          One key for each key in the row, ordered as they are given by the source iterator (do not modify!).
-   * @param values
-   *          One value for each key in keys, ordered to correspond to the ordering in keys (do not modify!).
-   * @return true if we want to keep the row, false if we want to skip it
-   */
-  protected boolean filter(Text currentRow, List<Key> keys, List<Value> values) {
-    return true;
-  }
-  
-  @Override
-  public SortedKeyValueIterator<Key,Value> deepCopy(IteratorEnvironment env) {
-    if (sourceIter != null)
-      return new WholeRowIterator(sourceIter.deepCopy(env));
-    return new WholeRowIterator();
-  }
-  
-  @Override
-  public Key getTopKey() {
-    return topKey;
-  }
-  
-  @Override
-  public Value getTopValue() {
-    return topValue;
-  }
-  
-  @Override
-  public boolean hasTop() {
-    return topKey != null;
-  }
-  
-  @Override
-  public void init(SortedKeyValueIterator<Key,Value> source, Map<String,String> options, IteratorEnvironment env) throws IOException {
-    sourceIter = source;
-  }
-  
-  @Override
-  public void next() throws IOException {
-    topKey = null;
-    topValue = null;
-    prepKeys();
-  }
-  
-  @Override
-  public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
-    topKey = null;
-    topValue = null;
-    
-    Key sk = range.getStartKey();
-    
-    if (sk != null && sk.getColumnFamilyData().length() == 0 && sk.getColumnQualifierData().length() == 0 && sk.getColumnVisibilityData().length() == 0
-        && sk.getTimestamp() == Long.MAX_VALUE && !range.isStartKeyInclusive()) {
-      // assuming that we are seeking using a key previously returned by this iterator
-      // therefore go to the next row
-      Key followingRowKey = sk.followingKey(PartialKey.ROW);
-      if (range.getEndKey() != null && followingRowKey.compareTo(range.getEndKey()) > 0)
-        return;
-      
-      range = new Range(sk.followingKey(PartialKey.ROW), true, range.getEndKey(), range.isEndKeyInclusive());
-    }
-    
-    sourceIter.seek(range, columnFamilies, inclusive);
-    prepKeys();
-  }
-  
 }
