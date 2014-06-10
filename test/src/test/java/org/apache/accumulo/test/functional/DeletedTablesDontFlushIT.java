cff58191413c7ae1bdaa7c5bd897f302586b9241
Eric C. Newton <eric.newton@gmail.com>
2014-06-10 09:45:01 -0400

    ACCUMULO-2880 verify that deleted tables do not flush on unload

---
+++ test/src/test/java/org/apache/accumulo/test/functional/DeletedTablesDontFlushIT.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.accumulo.test.functional;
+
+import java.util.EnumSet;
+
+import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.BatchWriterConfig;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.IteratorUtil.IteratorScope;
+import org.apache.accumulo.fate.util.UtilWaitThread;
+import org.junit.Test;
+
+// ACCUMULO-2880
+public class DeletedTablesDontFlushIT extends SimpleMacIT {
+  
+  @Test(timeout = 60 * 1000)
+  public void test() throws Exception {
+    Connector c = getConnector();
+    String tableName = getUniqueNames(1)[0];
+    c.tableOperations().create(tableName);
+    IteratorSetting setting = new IteratorSetting(100, SlowIterator.class);
+    SlowIterator.setSleepTime(setting, 1000);
+    c.tableOperations().attachIterator(tableName, setting, EnumSet.of(IteratorScope.minc));
+    // let the configuration change propagate through zookeeper
+    UtilWaitThread.sleep(1000);
+    
+    Mutation m = new Mutation("xyzzy");
+    for (int i = 0; i < 100; i++) {
+      m.put("cf", "" + i, new Value(new byte[]{}));
+    }
+    BatchWriter bw = c.createBatchWriter(tableName, new BatchWriterConfig());
+    bw.addMutation(m);
+    bw.close();
+    // should go fast
+    c.tableOperations().delete(tableName);
+  }
+  
+}
