a31bf1c4245817c21e0d07a574edcd335fbee7b8
Christopher Tubbs <ctubbsii@apache.org>
2014-08-11 16:49:33 -0400

    ACCUMULO-3013 Remove unused import left behind.

--- test/src/main/java/org/apache/accumulo/test/functional/RunTests.java
+++ test/src/main/java/org/apache/accumulo/test/functional/RunTests.java
@@ -34,7 +34,6 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.Counters.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
