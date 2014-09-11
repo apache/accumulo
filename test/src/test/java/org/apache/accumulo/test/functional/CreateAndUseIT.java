c566e931b2bad8cb7667b4f8e67992e2a5f9a060
Josh Elser <elserj@apache.org>
2014-09-10 22:28:49 -0700

    ACCUMULO-3110 ACCUMULO-3108 Fix defaultTimeoutSeconds to be class-level, not method-level

--- test/src/test/java/org/apache/accumulo/test/functional/CreateAndUseIT.java
+++ test/src/test/java/org/apache/accumulo/test/functional/CreateAndUseIT.java
@@ -43,7 +43,8 @@ public class CreateAndUseIT extends SimpleMacIT {
 
   @Override
   protected int defaultTimeoutSeconds() {
-    return 4 * 60;
+    // class level timeout
+    return 3 * 4 * 60;
   }
 
   private static SortedSet<Text> splits;
