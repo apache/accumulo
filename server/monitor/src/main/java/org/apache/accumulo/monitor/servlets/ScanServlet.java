d23cb34edff1a3fdd8487d27bbe6fe5db2983f70
Christopher Tubbs <ctubbsii@apache.org>
2014-09-12 17:32:08 -0400

    ACCUMULO-2645 Remove unused imports

--- server/monitor/src/main/java/org/apache/accumulo/monitor/servlets/ScanServlet.java
+++ server/monitor/src/main/java/org/apache/accumulo/monitor/servlets/ScanServlet.java
@@ -18,21 +18,18 @@ package org.apache.accumulo.monitor.servlets;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.accumulo.core.master.thrift.TabletServerStatus;
-
-import org.apache.accumulo.monitor.util.celltypes.TServerLinkType;
 import org.apache.accumulo.core.tabletserver.thrift.ActiveScan;
 import org.apache.accumulo.monitor.Monitor;
 import org.apache.accumulo.monitor.Monitor.ScanStats;
 import org.apache.accumulo.monitor.util.Table;
 import org.apache.accumulo.monitor.util.TableRow;
 import org.apache.accumulo.monitor.util.celltypes.DurationType;
-import org.apache.accumulo.monitor.util.celltypes.StringType;
+import org.apache.accumulo.monitor.util.celltypes.TServerLinkType;
 
 public class ScanServlet extends BasicServlet {
 
@@ -42,7 +39,7 @@ public class ScanServlet extends BasicServlet {
   protected String getTitle(HttpServletRequest req) {
     return "Scans";
   }
-  
+
   @Override
   protected void pageBody(HttpServletRequest req, HttpServletResponse response, StringBuilder sb) throws IOException {
     Map<String,ScanStats> scans = Monitor.getScans();
@@ -68,5 +65,5 @@ public class ScanServlet extends BasicServlet {
     }
     scanTable.generate(req, sb);
   }
-  
+
 }
