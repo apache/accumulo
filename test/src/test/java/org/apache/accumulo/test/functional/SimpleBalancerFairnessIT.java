44bf5ed2b175cbc93ea0daf6c7c80c14b48c9fb3
Josh Elser <elserj@apache.org>
2014-10-05 21:13:53 -0400

    ACCUMULO-3146 Also give the tserver a bit more heap to make sure the tserver doesn't die.

--- test/src/test/java/org/apache/accumulo/test/functional/SimpleBalancerFairnessIT.java
+++ test/src/test/java/org/apache/accumulo/test/functional/SimpleBalancerFairnessIT.java
@@ -36,6 +36,8 @@ import org.apache.accumulo.core.master.thrift.TableInfo;
 import org.apache.accumulo.core.master.thrift.TabletServerStatus;
 import org.apache.accumulo.core.security.Credentials;
 import org.apache.accumulo.core.util.UtilWaitThread;
+import org.apache.accumulo.minicluster.MemoryUnit;
+import org.apache.accumulo.minicluster.ServerType;
 import org.apache.accumulo.minicluster.impl.MiniAccumuloConfigImpl;
 import org.apache.accumulo.test.TestIngest;
 import org.apache.accumulo.trace.instrument.Tracer;
@@ -51,6 +53,7 @@ public class SimpleBalancerFairnessIT extends ConfigurableMacIT {
     siteConfig.put(Property.TSERV_MAXMEM.getKey(), "10K");
     siteConfig.put(Property.TSERV_MAJC_DELAY.getKey(), "0");
     cfg.setSiteConfig(siteConfig);
+    cfg.setMemory(ServerType.TABLET_SERVER, cfg.getMemory(ServerType.TABLET_SERVER) * 3, MemoryUnit.BYTE);
   }
 
   @Override
