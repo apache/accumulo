54d09bb16a16217701ce92a40f00f3a241d16975
Josh Elser <elserj@apache.org>
2014-10-29 14:17:56 -0400

    ACCUMULO-3276 More logging about what the client is doing for shard and multitable.

--- test/src/main/java/org/apache/accumulo/test/randomwalk/shard/ShardFixture.java
+++ test/src/main/java/org/apache/accumulo/test/randomwalk/shard/ShardFixture.java
@@ -31,69 +31,83 @@ import org.apache.hadoop.io.Text;
 import org.apache.log4j.Logger;
 
 public class ShardFixture extends Fixture {
-  
+
   static SortedSet<Text> genSplits(long max, int numTablets, String format) {
-    
+
     int numSplits = numTablets - 1;
     long distance = max / numTablets;
     long split = distance;
-    
+
     TreeSet<Text> splits = new TreeSet<Text>();
-    
+
     for (int i = 0; i < numSplits; i++) {
       splits.add(new Text(String.format(format, split)));
       split += distance;
     }
-    
+
     return splits;
   }
-  
+
   static void createIndexTable(Logger log, State state, String suffix, Random rand) throws Exception {
     Connector conn = state.getConnector();
     String name = (String) state.get("indexTableName") + suffix;
     int numPartitions = (Integer) state.get("numPartitions");
     boolean enableCache = (Boolean) state.get("cacheIndex");
     conn.tableOperations().create(name);
-    conn.tableOperations().addSplits(name, genSplits(numPartitions, rand.nextInt(numPartitions) + 1, "%06x"));
+
+    String tableId = conn.tableOperations().tableIdMap().get(name);
+    log.info("Created index table " + name + "(id:" + tableId + ")");
     
+    SortedSet<Text> splits = genSplits(numPartitions, rand.nextInt(numPartitions) + 1, "%06x");
+    conn.tableOperations().addSplits(name, splits);
+
+    log.info("Added " + splits.size() + " splits to " + name);
+
     if (enableCache) {
       conn.tableOperations().setProperty(name, Property.TABLE_INDEXCACHE_ENABLED.getKey(), "true");
       conn.tableOperations().setProperty(name, Property.TABLE_BLOCKCACHE_ENABLED.getKey(), "true");
-      
+
       log.info("Enabled caching for table " + name);
     }
   }
-  
+
   @Override
   public void setUp(State state) throws Exception {
     String hostname = InetAddress.getLocalHost().getHostName().replaceAll("[-.]", "_");
     String pid = state.getPid();
-    
+
     Random rand = new Random();
-    
+
     int numPartitions = rand.nextInt(90) + 10;
-    
+
     state.set("indexTableName", String.format("ST_index_%s_%s_%d", hostname, pid, System.currentTimeMillis()));
     state.set("docTableName", String.format("ST_docs_%s_%s_%d", hostname, pid, System.currentTimeMillis()));
     state.set("numPartitions", Integer.valueOf(numPartitions));
     state.set("cacheIndex", rand.nextDouble() < .5);
     state.set("rand", rand);
     state.set("nextDocID", Long.valueOf(0));
-    
+
     Connector conn = state.getConnector();
-    
+
     createIndexTable(this.log, state, "", rand);
-    
+
     String docTableName = (String) state.get("docTableName");
     conn.tableOperations().create(docTableName);
-    conn.tableOperations().addSplits(docTableName, genSplits(0xff, rand.nextInt(32) + 1, "%02x"));
-    
+
+    String tableId = conn.tableOperations().tableIdMap().get(docTableName);
+    log.info("Created doc table " + docTableName + " (id:" + tableId + ")");
+
+    SortedSet<Text> splits = genSplits(0xff, rand.nextInt(32) + 1, "%02x");
+    conn.tableOperations().addSplits(docTableName, splits);
+
+    log.info("Added " + splits.size() + " splits to " + docTableName);
+
     if (rand.nextDouble() < .5) {
       conn.tableOperations().setProperty((String) state.get("docTableName"), Property.TABLE_BLOOM_ENABLED.getKey(), "true");
       log.info("Enabled bloom filters for table " + (String) state.get("docTableName"));
     }
   }
-  
+
   @Override
   public void tearDown(State state) throws Exception {
     // We have resources we need to clean up
@@ -104,17 +118,19 @@ public class ShardFixture extends Fixture {
       } catch (MutationsRejectedException e) {
         log.error("Ignoring mutations that weren't flushed", e);
       }
-      
+
       // Reset the MTBW on the state to null
       state.resetMultiTableBatchWriter();
     }
-    
+
     Connector conn = state.getConnector();
-    
+
+    log.info("Deleting index and doc tables");
+
     conn.tableOperations().delete((String) state.get("indexTableName"));
     conn.tableOperations().delete((String) state.get("docTableName"));
-    
+
     log.debug("Exiting shard test");
   }
-  
+
 }
