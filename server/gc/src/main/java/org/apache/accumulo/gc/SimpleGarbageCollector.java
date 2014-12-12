1a944c06c7b54cdca943d29af65bc8b4bf728128
Eric C. Newton <eric.newton@gmail.com>
2014-12-12 18:20:51 -0500

    ACCUMULO-3397 decrease the percentage

--- server/gc/src/main/java/org/apache/accumulo/gc/SimpleGarbageCollector.java
+++ server/gc/src/main/java/org/apache/accumulo/gc/SimpleGarbageCollector.java
@@ -122,7 +122,7 @@ public class SimpleGarbageCollector implements Iface {
   /**
    * A fraction representing how much of the JVM's available memory should be used for gathering candidates.
    */
-  static final float CANDIDATE_MEMORY_PERCENTAGE = 0.75f;
+  static final float CANDIDATE_MEMORY_PERCENTAGE = 0.50f;
 
   private static final Logger log = Logger.getLogger(SimpleGarbageCollector.class);
 
