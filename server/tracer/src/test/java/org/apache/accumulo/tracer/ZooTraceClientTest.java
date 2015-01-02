9022987e093842eb465113f5a3a80f3ef2855444
Josh Elser <elserj@apache.org>
2015-01-02 16:49:54 -0500

    ACCUMULO-3461 Fix race condition in ZooTraceClientTest
    The 2nd call to ZooTraceClient.updateHostsFromKeeper is made
    asynchronously (after waiting for 0ms). This will sometimes
    pass and sometimes fail. Add a delegate to the 2nd call and
    make a simple latch that the test case can wait on to ensure
    both calls happened before we verify the mock's state.
    
    Also fixed a bad log message that [~ecn] noticed.

--- server/tracer/src/test/java/org/apache/accumulo/tracer/ZooTraceClientTest.java
+++ server/tracer/src/test/java/org/apache/accumulo/tracer/ZooTraceClientTest.java
@@ -16,25 +16,50 @@
  */
 package org.apache.accumulo.tracer;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.easymock.EasyMock;
 import org.junit.Test;
 
 public class ZooTraceClientTest {
 
+  /**
+   * An extension on ZooTraceClient which acts as a latch on updateHostsFromZooKeeper using the provided {@link AtomicBoolean}
+   */
+  private static class UpdateHostsDelegate extends ZooTraceClient {
+    private final AtomicBoolean done;
+
+    private UpdateHostsDelegate(AtomicBoolean done) {
+      this.done = done;
+    }
+
+    @Override
+    public void updateHostsFromZooKeeper() {
+      this.done.set(true);
+    }
+  }
+
   @Test
   public void testConnectFailureRetries() throws Exception {
     ZooTraceClient client = EasyMock.createMockBuilder(ZooTraceClient.class).addMockedMethod("updateHostsFromZooKeeper").createStrictMock();
     client.setRetryPause(0l);
+    AtomicBoolean done = new AtomicBoolean(false);
 
     client.updateHostsFromZooKeeper();
     EasyMock.expectLastCall().andThrow(new RuntimeException()).once();
     client.updateHostsFromZooKeeper();
-    EasyMock.expectLastCall();
+    // Expect the second call to updateHostsFromZooKeeper, but wait for it to fire before verification
+    EasyMock.expectLastCall().andDelegateTo(new UpdateHostsDelegate(done));
 
     EasyMock.replay(client);
 
     client.setInitialTraceHosts();
-    
+
+    while (!done.get()) {
+      // The 2nd call to updateHostsFromZooKeeper is async. Wait for it for fire before verifying it was called.
+      Thread.sleep(200);
+    }
+
     EasyMock.verify(client);
 
   }
