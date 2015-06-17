9026ff8082b3b954b6323c769bfc9f8a05a845a4
Christopher Tubbs <ctubbsii@apache.org>
2015-06-17 18:09:21 -0400

    ACCUMULO-3871 Roll mrit module into test
    * Makes the mrit module an optional shaded artifact for the test module
    * Doesn't build mrit by default (activate with -Dmrit or -Pmrit)
    * Deletes the mrit module
    * Minor pom cleanup

--- test/src/main/java/org/apache/accumulo/test/mrit/IntegrationTestMapReduce.java
+++ test/src/main/java/org/apache/accumulo/test/mrit/IntegrationTestMapReduce.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.mrit;
+package org.apache.accumulo.test.mrit;
 
 import java.io.IOException;
 
