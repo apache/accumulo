9a3e35190cb26807fd8835183baf1236f458a8a0
Eric C. Newton <eric.newton@gmail.com>
2015-06-23 16:39:37 -0400

    ACCUMULO-3747 port 1.7 changes to master

--- test/src/main/java/org/apache/accumulo/test/DetectDeadTabletServers.java
+++ test/src/main/java/org/apache/accumulo/test/DetectDeadTabletServers.java
@@ -32,13 +32,13 @@ import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.trace.Tracer;
 import org.apache.accumulo.fate.util.UtilWaitThread;
 import org.apache.accumulo.minicluster.impl.MiniAccumuloConfigImpl;
-import org.apache.accumulo.test.functional.ConfigurableMacIT;
+import org.apache.accumulo.test.functional.ConfigurableMacBase;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
 
 import com.google.common.collect.Iterators;
 
-public class DetectDeadTabletServers extends ConfigurableMacIT {
+public class DetectDeadTabletServers extends ConfigurableMacBase {
 
   @Override
   protected void configure(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
