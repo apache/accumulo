265f08a7776e0788f624d9188f14be15c70c0ae4
Eric Newton <eric.newton@gmail.com>
2015-06-25 10:09:51 -0400

    Revert "ACCUMULO-3423 Fix FindBugs issue"
    This reverts commit 880e3386c355c2daac4a71ae6ec2775cf00a7e45.

--- test/src/main/java/org/apache/accumulo/test/DumpConfigIT.java
+++ test/src/main/java/org/apache/accumulo/test/DumpConfigIT.java
@@ -49,7 +49,7 @@ public class DumpConfigIT extends ConfigurableMacBase {
   @Test
   public void test() throws Exception {
     File target = new File(System.getProperty("user.dir"), "target");
-    assertTrue("target directory should exist", target.exists());
+    target.mkdirs();
     TemporaryFolder folder = new TemporaryFolder(target);
     folder.create();
     File siteFileBackup = new File(folder.getRoot(), "accumulo-site.xml.bak");
