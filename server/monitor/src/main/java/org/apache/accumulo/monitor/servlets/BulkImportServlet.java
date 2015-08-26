2522d643601b5bc32833fa72ed22374f1c39d239
Josh Elser <elserj@apache.org>
2015-08-25 23:03:43 -0400

    ACCUMULO-3735 Fix the checkstyle/formatting and license header violations.

--- server/monitor/src/main/java/org/apache/accumulo/monitor/servlets/BulkImportServlet.java
+++ server/monitor/src/main/java/org/apache/accumulo/monitor/servlets/BulkImportServlet.java
@@ -40,26 +40,26 @@ public class BulkImportServlet extends BasicServlet {
   protected String getTitle(HttpServletRequest req) {
     return "Bulk Imports";
   }
-  
+
   static private long duration(long start) {
     return (System.currentTimeMillis() - start) / 1000L;
   }
 
   @Override
   protected void pageBody(HttpServletRequest req, HttpServletResponse response, StringBuilder sb) throws IOException {
-	  Table table = new Table("masterBulkImportStatus", "Bulk&nbsp;Import&nbsp;Status");
+    Table table = new Table("masterBulkImportStatus", "Bulk&nbsp;Import&nbsp;Status");
     table.addSortableColumn("Directory");
-	  table.addSortableColumn("Age", new DurationType(0l, 5 * 60 * 1000l), "The age the import.");
-	  table.addSortableColumn("State", new BulkImportStateType(), "The current state of the bulk import");
-	  for (BulkImportStatus bulk : Monitor.getMmi().bulkImports) {
-	    TableRow row = table.prepareRow();
-	    row.add(bulk.filename);
-	    row.add(duration(bulk.startTime));
-	    row.add(bulk.state);
-	    table.addRow(row);
-	  }
-	  table.generate(req, sb);
-	      
+    table.addSortableColumn("Age", new DurationType(0l, 5 * 60 * 1000l), "The age the import.");
+    table.addSortableColumn("State", new BulkImportStateType(), "The current state of the bulk import");
+    for (BulkImportStatus bulk : Monitor.getMmi().bulkImports) {
+      TableRow row = table.prepareRow();
+      row.add(bulk.filename);
+      row.add(duration(bulk.startTime));
+      row.add(bulk.state);
+      table.addRow(row);
+    }
+    table.generate(req, sb);
+
     table = new Table("bulkImportStatus", "TabletServer&nbsp;Bulk&nbsp;Import&nbsp;Status");
     table.addSortableColumn("Server", new TServerLinkType(), null);
     table.addSortableColumn("#", new PreciseNumberType(0, 20, 0, 100), "Number of imports presently running");
@@ -72,16 +72,16 @@ public class BulkImportServlet extends BasicServlet {
         row.add(stats.size());
         long oldest = Long.MAX_VALUE;
         for (BulkImportStatus bulk : stats) {
-        	oldest = Math.min(oldest, bulk.startTime);
+          oldest = Math.min(oldest, bulk.startTime);
         }
         if (oldest != Long.MAX_VALUE) {
-        	row.add(duration(oldest));
+          row.add(duration(oldest));
         } else {
-        	row.add(0L);
+          row.add(0L);
         }
       } else {
-    	row.add(0);
-    	row.add(0L);
+        row.add(0);
+        row.add(0L);
       }
       table.addRow(row);
     }
