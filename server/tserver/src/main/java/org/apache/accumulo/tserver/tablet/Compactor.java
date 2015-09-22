758abd6518958c6c7ef22f06e23e41113007200b
Eric C. Newton <eric.newton@gmail.com>
2015-09-22 14:59:07 -0400

    ACCUMULO-4008 remove the file if it fails to close cleanly

--- server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Compactor.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Compactor.java
@@ -218,14 +218,14 @@ public class Compactor implements Callable<CompactionStats> {
       FileSKVWriter mfwTmp = mfw;
       mfw = null; // set this to null so we do not try to close it again in finally if the close fails
       try {
-    	  mfwTmp.close(); // if the close fails it will cause the compaction to fail
+        mfwTmp.close(); // if the close fails it will cause the compaction to fail
       } catch (IOException ex) {
-    	  if (!fs.deleteRecursively(outputFile.path())) {
-              if (fs.exists(outputFile.path())) {
-                log.error("Unable to delete " + outputFile);
-              }
-    	  }
-    	  throw ex;
+        if (!fs.deleteRecursively(outputFile.path())) {
+          if (fs.exists(outputFile.path())) {
+            log.error("Unable to delete " + outputFile);
+          }
+        }
+        throw ex;
       }
 
       log.debug(String.format("Compaction %s %,d read | %,d written | %,6d entries/sec | %,6.3f secs | %,12d bytes | %9.3f byte/sec", extent,
