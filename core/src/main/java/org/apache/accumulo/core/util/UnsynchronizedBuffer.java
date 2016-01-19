a2c2d38aa248056c1cf592e8a2a0ada17eb518e2
Keith Turner <kturner@apache.org>
2016-01-19 15:55:34 -0500

    ACCUMULO-4098 Fixed bug with ByteBuffers thats do not start at 0

--- core/src/main/java/org/apache/accumulo/core/util/UnsynchronizedBuffer.java
+++ core/src/main/java/org/apache/accumulo/core/util/UnsynchronizedBuffer.java
@@ -118,8 +118,8 @@ public class UnsynchronizedBuffer {
     }
 
     public Reader(ByteBuffer buffer) {
-      if (buffer.hasArray()) {
-        offset = buffer.arrayOffset();
+      if (buffer.hasArray() && buffer.array().length == buffer.arrayOffset() + buffer.limit()) {
+        offset = buffer.arrayOffset() + buffer.position();
         data = buffer.array();
       } else {
         data = new byte[buffer.remaining()];
