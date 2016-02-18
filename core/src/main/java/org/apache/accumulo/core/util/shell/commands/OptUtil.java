50db442b04f615ce698d0f63545917be6d4fb13c
Michael Wall <mjwall@gmail.com>
2016-02-18 15:22:09 -0500

    ACCUMULO-4138 Fix the description of -b options
    It was wrong for FlushCommand and CompactCommand.  In the process of
    refactoring, the following was accomplished:
    
    - Change the description in the OptUtil.startRowOpt to be (NOT) inclusive.
    - Use OptUtil.startRowOpt in CompactCommand, MergeCommand and FlushCommand.
    - Refactor the DeleteRowCommand to use the OptUtil.startRowOpt method.
    - Refactor ScanCommand to override the description to say it is inclusive.
    
    Also updated the javadoc from a potential cut and paste on TableOperations.
    
    A little mixed on the usefulness of the Unit Tests for these.  They may be
    brittle, but perhaps they would alert if someone changed the way one command
    worked without looking at the others.

--- core/src/main/java/org/apache/accumulo/core/util/shell/commands/OptUtil.java
+++ core/src/main/java/org/apache/accumulo/core/util/shell/commands/OptUtil.java
@@ -117,7 +117,7 @@ public abstract class OptUtil {
   }
 
   public static Option startRowOpt() {
-    final Option o = new Option(START_ROW_OPT, "begin-row", true, "begin row (inclusive)");
+    final Option o = new Option(START_ROW_OPT, "begin-row", true, "begin row (NOT) inclusive");
     o.setArgName("begin-row");
     return o;
   }
