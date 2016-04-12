584b8121288c109945da62c97ea8727956de38bf
Christopher Tubbs <ctubbsii@apache.org>
2016-04-12 17:15:23 -0400

    ACCUMULO-4188 Fix PowerMock issue in ShellSetInstanceTest
    Instruct PowerMock to ignore javax.security.* classes in its classloader

--- shell/src/test/java/org/apache/accumulo/shell/ShellSetInstanceTest.java
+++ shell/src/test/java/org/apache/accumulo/shell/ShellSetInstanceTest.java
@@ -55,10 +55,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.security.*")
 @PrepareForTest({Shell.class, ZooUtil.class, ConfigSanityCheck.class})
 public class ShellSetInstanceTest {
   public static class TestOutputStream extends OutputStream {
