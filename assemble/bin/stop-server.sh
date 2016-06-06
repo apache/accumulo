58b0ac106900ede40a1831a90bfbfd1c791d1e35
Dave Marion <dlmarion@apache.org>
2016-06-06 19:35:09 -0400

    ACCUMULO-4328: Remove quotes around file pattern, test for file

--- assemble/bin/stop-server.sh
+++ assemble/bin/stop-server.sh
@@ -41,8 +41,8 @@ fi
 
 # only stop if there's not one already running
 if [[ $HOST == localhost || $HOST = "$(hostname -s)" || $HOST = "$(hostname -f)" || $HOST = "$IP" ]] ; then
-   for PID_FILE in "${ACCUMULO_PID_DIR}/accumulo-${ACCUMULO_IDENT_STRING}-${3}*.pid"; do
-      if [[ ! -z ${PID_FILE} ]]; then
+   for PID_FILE in ${ACCUMULO_PID_DIR}/accumulo-${ACCUMULO_IDENT_STRING}-${3}*.pid; do
+      if [[ -f ${PID_FILE} ]]; then
          echo "Stopping $3 on $1";
          kill -s "$4" `cat ${PID_FILE}` 2>/dev/null
          rm -f ${PID_FILE} 2>/dev/null
