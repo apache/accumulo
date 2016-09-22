6a677adafe0fe03a6a331d281c1a27b4854f94e7
Sean Busbey <busbey@cloudera.com>
2016-09-22 13:01:52 -0500

    ACCUMULO-4467 Ensure commons-math3 is on the classpath for RandomWalk MR jobs.

--- test/src/main/java/org/apache/accumulo/test/randomwalk/Node.java
+++ test/src/main/java/org/apache/accumulo/test/randomwalk/Node.java
@@ -91,7 +91,7 @@ public abstract class Node {
     }
 
     File libdir = new File(acuHome + "/lib");
-    for (String jar : "accumulo-core accumulo-server-base accumulo-fate accumulo-trace libthrift htrace-core".split(" ")) {
+    for (String jar : "accumulo-core accumulo-server-base accumulo-fate accumulo-trace commons-math3 libthrift htrace-core".split(" ")) {
       retval += String.format(",%s/%s.jar", libdir.getAbsolutePath(), jar);
     }
 
