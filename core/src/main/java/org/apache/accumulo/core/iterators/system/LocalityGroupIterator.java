06ea98717c7a0f1329eb99fbfe653a28349b41cc
Ivan Bella <ivan@bella.name>
2017-06-28 10:00:27 -0400

    ACCUMULO-4667 Inefficient LocalityGroupIterator
    Reworked the LocalityGroupIterator to more efficiently determine which groups to search when seeking.

--- core/src/main/java/org/apache/accumulo/core/iterators/system/LocalityGroupIterator.java
+++ core/src/main/java/org/apache/accumulo/core/iterators/system/LocalityGroupIterator.java
@@ -17,14 +17,18 @@
 package org.apache.accumulo.core.iterators.system;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import com.google.common.collect.ImmutableSet;
 import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Range;
@@ -65,14 +69,69 @@ public class LocalityGroupIterator extends HeapIterator implements Interruptible
     private InterruptibleIterator iterator;
   }
 
-  private LocalityGroup groups[];
-  private Set<ByteSequence> nonDefaultColumnFamilies;
+  public static class LocalityGroupContext {
+    final List<LocalityGroup> groups;
+    final LocalityGroup defaultGroup;
+    final Map<ByteSequence,LocalityGroup> groupByCf;
+
+    public LocalityGroupContext(LocalityGroup[] groups) {
+      this.groups = Collections.unmodifiableList(Arrays.asList(groups));
+      this.groupByCf = new HashMap<ByteSequence,LocalityGroup>();
+      LocalityGroup foundDefault = null;
+
+      for (LocalityGroup group : groups) {
+        if (group.isDefaultLocalityGroup && group.columnFamilies == null) {
+          if (foundDefault != null) {
+            throw new IllegalStateException("Found multiple default locality groups");
+          }
+          foundDefault = group;
+        } else {
+          for (Entry<ByteSequence,MutableLong> entry : group.columnFamilies.entrySet()) {
+            if (entry.getValue().longValue() > 0) {
+              if (groupByCf.containsKey(entry.getKey())) {
+                throw new IllegalStateException("Found the same cf in multiple locality groups");
+              }
+              groupByCf.put(entry.getKey(), group);
+            }
+          }
+        }
+      }
+      defaultGroup = foundDefault;
+    }
+  }
+
+  /**
+   * This will cache the arguments used in the seek call along with the locality groups seeked.
+   */
+  public static class LocalityGroupSeekCache {
+    private ImmutableSet<ByteSequence> lastColumnFamilies;
+    private volatile boolean lastInclusive;
+    private Collection<LocalityGroup> lastUsed;
+
+    public ImmutableSet<ByteSequence> getLastColumnFamilies() {
+      return lastColumnFamilies;
+    }
+
+    public boolean isLastInclusive() {
+      return lastInclusive;
+    }
+
+    public Collection<LocalityGroup> getLastUsed() {
+      return lastUsed;
+    }
+
+    public int getNumLGSeeked() {
+      return (lastUsed == null ? 0 : lastUsed.size());
+    }
+  }
+
+  private final LocalityGroupContext lgContext;
+  private LocalityGroupSeekCache lgCache;
   private AtomicBoolean interruptFlag;
 
-  public LocalityGroupIterator(LocalityGroup groups[], Set<ByteSequence> nonDefaultColumnFamilies) {
+  public LocalityGroupIterator(LocalityGroup groups[]) {
     super(groups.length);
-    this.groups = groups;
-    this.nonDefaultColumnFamilies = nonDefaultColumnFamilies;
+    this.lgContext = new LocalityGroupContext(groups);
   }
 
   @Override
@@ -80,11 +139,27 @@ public class LocalityGroupIterator extends HeapIterator implements Interruptible
     throw new UnsupportedOperationException();
   }
 
-  public static final int seek(HeapIterator hiter, LocalityGroup[] groups, Set<ByteSequence> nonDefaultColumnFamilies, Range range,
-      Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
+  /**
+   * This is the seek work horse for a HeapIterator with locality groups (uses by the InMemory and RFile mechanisms). This method will find the locality groups
+   * to use in the LocalityGroupContext, and will seek those groups.
+   *
+   * @param hiter
+   *          The heap iterator
+   * @param lgContext
+   *          The locality groups
+   * @param range
+   *          The range to seek
+   * @param columnFamilies
+   *          The column fams to seek
+   * @param inclusive
+   *          The inclusiveness of the column fams
+   * @return The locality groups seeked
+   * @throws IOException
+   *           thrown if an locality group seek fails
+   */
+  static final Collection<LocalityGroup> _seek(HeapIterator hiter, LocalityGroupContext lgContext, Range range, Collection<ByteSequence> columnFamilies,
+      boolean inclusive) throws IOException {
     hiter.clear();
-
-    int numLGSeeked = 0;
 
     Set<ByteSequence> cfSet;
     if (columnFamilies.size() > 0)
@@ -97,75 +172,140 @@ public class LocalityGroupIterator extends HeapIterator implements Interruptible
     else
       cfSet = Collections.emptySet();
 
-    for (LocalityGroup lgr : groups) {
-      // when include is set to true it means this locality groups contains
-      // wanted column families
-      boolean include = false;
+    // determine the set of groups to use
+    Collection<LocalityGroup> groups = Collections.emptyList();
 
-      if (cfSet.size() == 0) {
-        include = !inclusive;
-      } else if (lgr.isDefaultLocalityGroup && lgr.columnFamilies == null) {
-        // do not know what column families are in the default locality group,
-        // only know what column families are not in it
+    // if no column families specified, then include all groups unless !inclusive
+    if (cfSet.size() == 0) {
+      if (!inclusive) {
+        groups = lgContext.groups;
+      }
+    } else {
+      groups = new HashSet<LocalityGroup>();
 
+      // do not know what column families are in the default locality group,
+      // only know what column families are not in it
+      if (lgContext.defaultGroup != null) {
         if (inclusive) {
-          if (!nonDefaultColumnFamilies.containsAll(cfSet)) {
+          if (!lgContext.groupByCf.keySet().containsAll(cfSet)) {
             // default LG may contain wanted and unwanted column families
-            include = true;
+            groups.add(lgContext.defaultGroup);
           }// else - everything wanted is in other locality groups, so nothing to do
         } else {
-          // must include, if all excluded column families are in other locality groups
-          // then there are not unwanted column families in default LG
-          include = true;
+          // must include the default group as it may include cfs not in our cfSet
+          groups.add(lgContext.defaultGroup);
         }
-      } else {
-        /*
-         * Need to consider the following cases for inclusive and exclusive (lgcf:locality group column family set, cf:column family set) lgcf and cf are
-         * disjoint lgcf and cf are the same cf contains lgcf lgcf contains cf lgccf and cf intersect but neither is a subset of the other
-         */
-
-        for (Entry<ByteSequence,MutableLong> entry : lgr.columnFamilies.entrySet())
-          if (entry.getValue().longValue() > 0)
-            if (cfSet.contains(entry.getKey())) {
-              if (inclusive)
-                include = true;
-            } else if (!inclusive) {
-              include = true;
-            }
       }
 
-      if (include) {
-        lgr.getIterator().seek(range, EMPTY_CF_SET, false);
-        hiter.addSource(lgr.getIterator());
-        numLGSeeked++;
-      }// every column family is excluded, zero count, or not present
+      /*
+       * Need to consider the following cases for inclusive and exclusive (lgcf:locality group column family set, cf:column family set) lgcf and cf are disjoint
+       * lgcf and cf are the same cf contains lgcf lgcf contains cf lgccf and cf intersect but neither is a subset of the other
+       */
+      if (!inclusive) {
+        for (Entry<ByteSequence,LocalityGroup> entry : lgContext.groupByCf.entrySet()) {
+          if (!cfSet.contains(entry.getKey())) {
+            groups.add(entry.getValue());
+          }
+        }
+      } else if (lgContext.groupByCf.size() <= cfSet.size()) {
+        for (Entry<ByteSequence,LocalityGroup> entry : lgContext.groupByCf.entrySet()) {
+          if (cfSet.contains(entry.getKey())) {
+            groups.add(entry.getValue());
+          }
+        }
+      } else {
+        for (ByteSequence cf : cfSet) {
+          LocalityGroup group = lgContext.groupByCf.get(cf);
+          if (group != null) {
+            groups.add(group);
+          }
+        }
+      }
     }
 
-    return numLGSeeked;
+    for (LocalityGroup lgr : groups) {
+      lgr.getIterator().seek(range, EMPTY_CF_SET, false);
+      hiter.addSource(lgr.getIterator());
+    }
+
+    return groups;
+  }
+
+  /**
+   * This seek method will reuse the supplied LocalityGroupSeekCache if it can. Otherwise it will delegate to the _seek method.
+   *
+   * @param hiter
+   *          The heap iterator
+   * @param lgContext
+   *          The locality groups
+   * @param range
+   *          The range to seek
+   * @param columnFamilies
+   *          The column fams to seek
+   * @param inclusive
+   *          The inclusiveness of the column fams
+   * @param lgSeekCache
+   *          A cache returned by the previous call to this method
+   * @return A cache for this seek call
+   * @throws IOException
+   *           thrown if an locality group seek fails
+   */
+  public static LocalityGroupSeekCache seek(HeapIterator hiter, LocalityGroupContext lgContext, Range range, Collection<ByteSequence> columnFamilies,
+      boolean inclusive, LocalityGroupSeekCache lgSeekCache) throws IOException {
+    if (lgSeekCache == null)
+      lgSeekCache = new LocalityGroupSeekCache();
+
+    // determine if the arguments have changed since the last time
+    boolean sameArgs = false;
+    ImmutableSet<ByteSequence> cfSet = null;
+    if (lgSeekCache.lastUsed != null && inclusive == lgSeekCache.lastInclusive) {
+      if (columnFamilies instanceof Set) {
+        sameArgs = lgSeekCache.lastColumnFamilies.equals(columnFamilies);
+      } else {
+        cfSet = ImmutableSet.copyOf(columnFamilies);
+        sameArgs = lgSeekCache.lastColumnFamilies.equals(cfSet);
+      }
+    }
+
+    // if the column families and inclusiveness have not changed, then we can simply re-seek the
+    // locality groups we discovered last round and rebuild the heap.
+    if (sameArgs) {
+      hiter.clear();
+      for (LocalityGroup lgr : lgSeekCache.lastUsed) {
+        lgr.getIterator().seek(range, EMPTY_CF_SET, false);
+        hiter.addSource(lgr.getIterator());
+      }
+    } else { // otherwise capture the parameters, and use the static seek method to locate the locality groups to use.
+      lgSeekCache.lastColumnFamilies = (cfSet == null ? ImmutableSet.copyOf(columnFamilies) : cfSet);
+      lgSeekCache.lastInclusive = inclusive;
+      lgSeekCache.lastUsed = _seek(hiter, lgContext, range, columnFamilies, inclusive);
+    }
+
+    return lgSeekCache;
   }
 
   @Override
   public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
-    seek(this, groups, nonDefaultColumnFamilies, range, columnFamilies, inclusive);
+    lgCache = seek(this, lgContext, range, columnFamilies, inclusive, lgCache);
   }
 
   @Override
   public SortedKeyValueIterator<Key,Value> deepCopy(IteratorEnvironment env) {
-    LocalityGroup[] groupsCopy = new LocalityGroup[groups.length];
+    LocalityGroup[] groupsCopy = new LocalityGroup[lgContext.groups.size()];
 
-    for (int i = 0; i < groups.length; i++) {
-      groupsCopy[i] = new LocalityGroup(groups[i], env);
+    for (int i = 0; i < lgContext.groups.size(); i++) {
+      groupsCopy[i] = new LocalityGroup(lgContext.groups.get(i), env);
       if (interruptFlag != null)
         groupsCopy[i].getIterator().setInterruptFlag(interruptFlag);
     }
 
-    return new LocalityGroupIterator(groupsCopy, nonDefaultColumnFamilies);
+    return new LocalityGroupIterator(groupsCopy);
   }
 
   @Override
   public void setInterruptFlag(AtomicBoolean flag) {
     this.interruptFlag = flag;
-    for (LocalityGroup lgr : groups) {
+    for (LocalityGroup lgr : lgContext.groups) {
       lgr.getIterator().setInterruptFlag(flag);
     }
   }
