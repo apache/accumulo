ef1b6033cb3cd9928722abed7a01f684c1460db0
Adam J. Shook <adamjshook@gmail.com>
2017-10-11 13:56:25 -0400

    [ACCUMULO-4591] Add test case for replication latency metrics

--- server/master/src/main/java/org/apache/accumulo/master/metrics/Metrics2ReplicationMetrics.java
+++ server/master/src/main/java/org/apache/accumulo/master/metrics/Metrics2ReplicationMetrics.java
@@ -38,6 +38,8 @@ import org.apache.hadoop.metrics2.lib.Interns;
 import org.apache.hadoop.metrics2.lib.MetricsRegistry;
 import org.apache.hadoop.metrics2.lib.MutableQuantiles;
 import org.apache.hadoop.metrics2.lib.MutableStat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -47,6 +49,8 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
       RECORD = "MasterReplication";
   public static final String PENDING_FILES = "filesPendingReplication", NUM_PEERS = "numPeers", MAX_REPLICATION_THREADS = "maxReplicationThreads",
       REPLICATION_QUEUE_TIME_QUANTILES = "replicationQueue10m", REPLICATION_QUEUE_TIME = "replicationQueue";
+
+  private final static Logger log = LoggerFactory.getLogger(Metrics2ReplicationMetrics.class);
 
   private final Master master;
   private final MetricsSystem system;
@@ -64,16 +68,22 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
 
     registry = new MetricsRegistry(Interns.info(NAME, DESCRIPTION));
     replicationUtil = new ReplicationUtil(master);
-    replicationQueueTimeQuantiles = registry.newQuantiles(REPLICATION_QUEUE_TIME_QUANTILES, "replication queue time quantiles in milliseconds", "ops",
+    replicationQueueTimeQuantiles = registry.newQuantiles(REPLICATION_QUEUE_TIME_QUANTILES, "Replication queue time quantiles in milliseconds", "ops",
         "latency", 600);
-    replicationQueueTimeStat = registry.newStat(REPLICATION_QUEUE_TIME, "replication queue time stat in milliseconds", "ops", "latency", true);
+    replicationQueueTimeStat = registry.newStat(REPLICATION_QUEUE_TIME, "Replication queue time statistics in milliseconds", "ops", "latency", true);
   }
 
   protected void snapshot() {
-    registry.add(PENDING_FILES, getNumFilesPendingReplication());
+    // Only add these metrics if the replication table is online and there are peers
+    if (TableState.ONLINE == Tables.getTableState(master.getInstance(), ReplicationTable.ID) && !replicationUtil.getPeers().isEmpty()) {
+      registry.add(PENDING_FILES, getNumFilesPendingReplication());
+      addReplicationQueueTimeMetrics();
+    } else {
+      registry.add(PENDING_FILES, 0);
+    }
+
     registry.add(NUM_PEERS, getNumConfiguredPeers());
     registry.add(MAX_REPLICATION_THREADS, getMaxReplicationThreads());
-    addReplicationQueueTimeMetrics();
   }
 
   @Override
@@ -103,18 +113,6 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
   }
 
   protected int getNumFilesPendingReplication() {
-    if (TableState.ONLINE != Tables.getTableState(master.getInstance(), ReplicationTable.ID)) {
-      return 0;
-    }
-
-    // Get all of the configured replication peers
-    Map<String,String> peers = replicationUtil.getPeers();
-
-    // A quick lookup to see if have any replication peer configured
-    if (peers.isEmpty()) {
-      return 0;
-    }
-
     // The total set of configured targets
     Set<ReplicationTarget> allConfiguredTargets = replicationUtil.getReplicationTargets();
 
@@ -144,22 +142,12 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
   }
 
   protected void addReplicationQueueTimeMetrics() {
-    // Exit early if replication table is offline
-    if (TableState.ONLINE != Tables.getTableState(master.getInstance(), ReplicationTable.ID)) {
-      return;
-    }
-
-    // Exit early if we have no replication peers configured
-    if (replicationUtil.getPeers().isEmpty()) {
-      return;
-    }
-
     Set<Path> paths = replicationUtil.getPendingReplicationPaths();
 
     // We'll take a snap of the current time and use this as a diff between any deleted
     // file's modification time and now. The reported latency will be off by at most a
     // number of seconds equal to the metric polling period
-    long currentTime = System.currentTimeMillis();
+    long currentTime = getCurrentTime();
 
     // Iterate through all the pending paths and update the mod time if we don't know it yet
     for (Path path : paths) {
@@ -170,6 +158,7 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
           // Ignore all IOExceptions
           // Either the system is unavailable or the file was deleted
           // since the initial scan and this check
+          log.trace("Failed to get file status for {}, file system is unavailable or it does not exist", path);
         }
       }
     }
@@ -187,10 +176,16 @@ public class Metrics2ReplicationMetrics implements Metrics, MetricsSource {
 
     for (Path path : deletedPaths) {
       // Remove this path and add the latency
-      long modTime = pathModTimes.remove(path);
-      long diff = Math.max(0, currentTime - modTime);
-      replicationQueueTimeQuantiles.add(diff);
-      replicationQueueTimeStat.add(diff);
+      Long modTime = pathModTimes.remove(path);
+      if (modTime != null) {
+        long diff = Math.max(0, currentTime - modTime);
+        replicationQueueTimeQuantiles.add(diff);
+        replicationQueueTimeStat.add(diff);
+      }
     }
+  }
+
+  protected long getCurrentTime() {
+    return System.currentTimeMillis();
   }
 }
