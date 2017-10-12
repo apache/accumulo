5a818d0bcd7e8203c222c730b872729626a0f89d
Mark Owens <jmarkowe@gmail.com>
2017-10-12 17:12:39 -0400

    ACCUMULO-4170
    Re-added in some loadDefaults() information that had been inadvertently
    removed when previously modifying the javadoc.

--- core/src/main/java/org/apache/accumulo/core/client/ClientConfiguration.java
+++ core/src/main/java/org/apache/accumulo/core/client/ClientConfiguration.java
@@ -213,6 +213,12 @@ public class ClientConfiguration extends CompositeConfiguration {
    * <li>/etc/accumulo/client.conf
    * <li>/etc/accumulo/conf/client.conf
    * </ul>
+   *<p>
+   * A client configuration will then be read from each location using <em>PropertiesConfiguration</em> to construct a configuration.
+   * That means the latest item will be the one in the configuration.
+   *<p>
+   * @see PropertiesConfiguration
+   * @see File#pathSeparator
    */
   public static ClientConfiguration loadDefault() {
     return loadFromSearchPath(getDefaultSearchPath());
