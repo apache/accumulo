d3e2b41936255f7f76a9d65a3324fa5b58793c4c
jkrdev <jkrushdev@gmail.com>
2017-11-06 15:05:24 +0000

    WIP

---
+++ core/src/test/java/org/apache/accumulo/core/client/summary/summarizers/EntryLengthSummarizersTest.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.accumulo.core.client.summary.summarizers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.apache.accumulo.core.client.summary.CounterSummary;
+import org.apache.accumulo.core.client.summary.summarizers.EntryLengthSummarizer;
+import org.apache.accumulo.core.client.summary.Summarizer;
+import org.apache.accumulo.core.client.summary.Summarizer.Collector;
+import org.apache.accumulo.core.client.summary.SummarizerConfiguration;
+import org.apache.accumulo.core.client.summary.summarizers.FamilySummarizer;
+import org.apache.accumulo.core.client.summary.summarizers.VisibilitySummarizer;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EntryLengthSummarizersTest {
+  
+  @Test
+  public void testMinMaxKey() {
+    
+  }
+  
+  @Test
+  public void testKeySum() {
+    
+  }
+  
+  @Test
+  public void testMinMaxRow() {
+    
+  }
+  
+  @Test
+  public void testRowSum() {
+    
+  }
+  
+  @Test
+  public void testMinMaxFamily() {
+    
+  }
+  
+  @Test
+  public void testFamilySum() {
+    
+  }
+  
+  @Test
+  public void testMinMaxQualifier() {
+    
+  }
+  
+  @Test
+  public void testQualifierSum() {
+    
+  }
+  
+  @Test
+  public void testMinMaxVisibility() {
+    
+  }
+  
+  @Test
+  public void testVisbilitySum() {
+    
+  }
+  
+  @Test
+  public void testMinMaxValue() {
+    
+  }
+  
+  @Test
+  public void testValueSum() {
+    
+  }
+  
+  @Test
+  public void testTotal() {
+    
+  }
+}
