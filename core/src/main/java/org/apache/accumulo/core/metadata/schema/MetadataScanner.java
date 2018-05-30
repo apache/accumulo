apache/accumulo

commit 9feb5e1dce0082b188a70753f3587bc1ba085c0f

fixes #469 made bulk import scan split tolerant (#488)
 * Made MetadataScanner check structure and retry when linked list broken
 * Removed TabletIterator
 * Made code that used TabletIterator use MetadataScanner

core/src/main/java/org/apache/accumulo/core/metadata/schema/MetadataScanner.java
@@ -17,6 +17,8 @@
 
 package org.apache.accumulo.core.metadata.schema;
 
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.TIME_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.PREV_ROW_COLUMN;
 
 import java.util.ArrayList;
@@ -24,58 +26,89 @@ import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.IsolatedScanner;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.impl.ClientContext;
 import org.apache.accumulo.core.client.impl.Table;
+import org.apache.accumulo.core.client.impl.Table.ID;
+import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.impl.KeyExtent;
 import org.apache.accumulo.core.metadata.MetadataTable;
-import org.apache.accumulo.core.metadata.RootTable;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ClonedColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.CurrentLocationColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.DataFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.FutureLocationColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.LastLocationColumnFamily;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ScanFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.TabletMetadata.FetchedColumns;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.util.ColumnFQ;
 import org.apache.hadoop.io.Text;
 
-import com.google.common.base.Preconditions;
-
-public class MetadataScanner {
+public class MetadataScanner implements Iterable<TabletMetadata>, AutoCloseable {
 
   public interface SourceOptions {
-    TableOptions from(Scanner scanner);
-
     TableOptions from(ClientContext ctx);
+
+    TableOptions from(Connector conn);
   }
 
-  public interface TableOptions {
-    ColumnOptions overRootTable();
-
-    ColumnOptions overMetadataTable();
-
-    ColumnOptions overUserTableId(Table.ID tableId);
-
-    ColumnOptions overUserTableId(Table.ID tableId, Text startRow, Text endRow);
+  public interface TableOptions extends RangeOptions {
+    /**
+     * Optionally set a table name, defaults to {@value MetadataTable#NAME}
+     */
+    RangeOptions scanTable(String tableName);
   }
 
-  public interface ColumnOptions {
-    ColumnOptions fetchFiles();
+  public interface RangeOptions {
+    Options overTabletRange();
 
-    ColumnOptions fetchLoaded();
+    Options overRange(Range range);
 
-    ColumnOptions fetchLocation();
+    Options overRange(Table.ID tableId);
 
-    ColumnOptions fetchPrev();
+    Options overRange(Table.ID tableId, Text startRow, Text endRow);
+  }
 
-    ColumnOptions fetchLast();
+  public interface Options {
+    /**
+     * Checks that the metadata table forms a linked list and automatically backs up until it does.
+     */
+    Options checkConsistency();
 
-    Iterable<TabletMetadata> build()
+    /**
+     * Saves the key values seen in the metadata table for each tablet.
+     */
+    Options saveKeyValues();
+
+    Options fetchFiles();
+
+    Options fetchLoaded();
+
+    Options fetchLocation();
+
+    Options fetchPrev();
+
+    Options fetchLast();
+
+    Options fetchScans();
+
+    Options fetchDir();
+
+    Options fetchTime();
+
+    Options fetchCloned();
+
+    MetadataScanner build()
         throws TableNotFoundException, AccumuloException, AccumuloSecurityException;
   }
 
@@ -108,34 +141,41 @@ public class MetadataScanner {
     }
   }
 
-  private static class Builder implements SourceOptions, TableOptions, ColumnOptions {
+  private static class Builder implements SourceOptions, TableOptions, RangeOptions, Options {
 
     private List<Text> families = new ArrayList<>();
     private List<ColumnFQ> qualifiers = new ArrayList<>();
-    private Scanner scanner;
-    private ClientContext ctx;
-    private String table;
-    private Table.ID userTableId;
+    private Connector conn;
+    private String table = MetadataTable.NAME;
+    private Range range;
     private EnumSet<FetchedColumns> fetchedCols = EnumSet.noneOf(FetchedColumns.class);
-    private Text startRow;
     private Text endRow;
+    private boolean checkConsistency = false;
+    private boolean saveKeyValues;
 
     @Override
-    public ColumnOptions fetchFiles() {
+    public Options fetchFiles() {
       fetchedCols.add(FetchedColumns.FILES);
       families.add(DataFileColumnFamily.NAME);
       return this;
     }
 
     @Override
-    public ColumnOptions fetchLoaded() {
-      fetchedCols.add(FetchedColumns.LOADED);
-      families.add(MetadataSchema.TabletsSection.BulkFileColumnFamily.NAME);
+    public Options fetchScans() {
+      fetchedCols.add(FetchedColumns.SCANS);
+      families.add(ScanFileColumnFamily.NAME);
       return this;
     }
 
     @Override
-    public ColumnOptions fetchLocation() {
+    public Options fetchLoaded() {
+      fetchedCols.add(FetchedColumns.LOADED);
+      families.add(BulkFileColumnFamily.NAME);
+      return this;
+    }
+
+    @Override
+    public Options fetchLocation() {
       fetchedCols.add(FetchedColumns.LOCATION);
       families.add(CurrentLocationColumnFamily.NAME);
       families.add(FutureLocationColumnFamily.NAME);
@@ -143,31 +183,49 @@ public class MetadataScanner {
     }
 
     @Override
-    public ColumnOptions fetchPrev() {
+    public Options fetchPrev() {
       fetchedCols.add(FetchedColumns.PREV_ROW);
       qualifiers.add(PREV_ROW_COLUMN);
       return this;
     }
 
     @Override
-    public ColumnOptions fetchLast() {
+    public Options fetchDir() {
+      fetchedCols.add(FetchedColumns.DIR);
+      qualifiers.add(DIRECTORY_COLUMN);
+      return this;
+    }
+
+    @Override
+    public Options fetchLast() {
       fetchedCols.add(FetchedColumns.LAST);
       families.add(LastLocationColumnFamily.NAME);
       return this;
     }
 
     @Override
-    public Iterable<TabletMetadata> build()
-        throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
-      if (ctx != null) {
-        scanner = new IsolatedScanner(
-            ctx.getConnector().createScanner(table, Authorizations.EMPTY));
-      } else if (!(scanner instanceof IsolatedScanner)) {
-        scanner = new IsolatedScanner(scanner);
-      }
+    public Options fetchTime() {
+      fetchedCols.add(FetchedColumns.TIME);
+      qualifiers.add(TIME_COLUMN);
+      return this;
+    }
 
-      if (userTableId != null) {
-        scanner.setRange(new KeyExtent(userTableId, null, startRow).toMetadataRange());
+    @Override
+    public Options fetchCloned() {
+      fetchedCols.add(FetchedColumns.CLONED);
+      families.add(ClonedColumnFamily.NAME);
+      return this;
+    }
+
+    @Override
+    public MetadataScanner build()
+        throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
+
+      Scanner scanner = new IsolatedScanner(conn.createScanner(table, Authorizations.EMPTY));
+      scanner.setRange(range);
+
+      if (checkConsistency && !fetchedCols.contains(FetchedColumns.PREV_ROW)) {
+        fetchPrev();
       }
 
       for (Text fam : families) {
@@ -182,69 +240,101 @@ public class MetadataScanner {
         fetchedCols = EnumSet.allOf(FetchedColumns.class);
       }
 
-      Iterable<TabletMetadata> tmi = TabletMetadata.convert(scanner, fetchedCols);
+      Iterable<TabletMetadata> tmi = TabletMetadata.convert(scanner, fetchedCols, checkConsistency,
+          saveKeyValues);
 
       if (endRow != null) {
         // create an iterable that will stop at the tablet which contains the endRow
-        return new Iterable<TabletMetadata>() {
-          @Override
-          public Iterator<TabletMetadata> iterator() {
-            return new TabletMetadataIterator(tmi.iterator(), endRow);
-          }
-        };
+        return new MetadataScanner(scanner,
+            () -> new TabletMetadataIterator(tmi.iterator(), endRow));
       } else {
-        return tmi;
+        return new MetadataScanner(scanner, tmi);
       }
-
-    }
-
-    @Override
-    public ColumnOptions overRootTable() {
-      this.table = RootTable.NAME;
-      return this;
-    }
-
-    @Override
-    public ColumnOptions overMetadataTable() {
-      this.table = MetadataTable.NAME;
-      return this;
-    }
-
-    @Override
-    public ColumnOptions overUserTableId(Table.ID tableId) {
-      Preconditions
-          .checkArgument(!tableId.equals(RootTable.ID) && !tableId.equals(MetadataTable.ID));
-
-      this.table = MetadataTable.NAME;
-      this.userTableId = tableId;
-      return this;
-    }
-
-    @Override
-    public TableOptions from(Scanner scanner) {
-      this.scanner = scanner;
-      return this;
     }
 
     @Override
     public TableOptions from(ClientContext ctx) {
-      this.ctx = ctx;
+      try {
+        this.conn = ctx.getConnector();
+      } catch (AccumuloException | AccumuloSecurityException e) {
+        throw new RuntimeException(e);
+      }
       return this;
     }
 
     @Override
-    public ColumnOptions overUserTableId(Table.ID tableId, Text startRow, Text endRow) {
-      this.table = MetadataTable.NAME;
-      this.userTableId = tableId;
-      this.startRow = startRow;
+    public TableOptions from(Connector conn) {
+      this.conn = conn;
+      return this;
+    }
+
+    @Override
+    public Options checkConsistency() {
+      this.checkConsistency = true;
+      return this;
+    }
+
+    @Override
+    public Options saveKeyValues() {
+      this.saveKeyValues = true;
+      return this;
+    }
+
+    @Override
+    public Options overTabletRange() {
+      this.range = TabletsSection.getRange();
+      return this;
+    }
+
+    @Override
+    public Options overRange(Range range) {
+      this.range = range;
+      return this;
+    }
+
+    @Override
+    public Options overRange(ID tableId) {
+      this.range = TabletsSection.getRange(tableId);
+      return this;
+    }
+
+    @Override
+    public Options overRange(ID tableId, Text startRow, Text endRow) {
+      this.range = new KeyExtent(tableId, null, startRow).toMetadataRange();
       this.endRow = endRow;
       return this;
     }
 
+    @Override
+    public RangeOptions scanTable(String tableName) {
+      this.table = tableName;
+      return this;
+    }
+  }
+
+  private Scanner scanner;
+  private Iterable<TabletMetadata> tablets;
+
+  private MetadataScanner(Scanner scanner, Iterable<TabletMetadata> tmi) {
+    this.scanner = scanner;
+    this.tablets = tmi;
   }
 
   public static SourceOptions builder() {
     return new Builder();
   }
 
+  @Override
+  public Iterator<TabletMetadata> iterator() {
+    return tablets.iterator();
+  }
+
+  public Stream<TabletMetadata> stream() {
+    return StreamSupport.stream(tablets.spliterator(), false);
+  }
+
+  @Override
+  public void close() {
+    scanner.close();
+  }
 }

core/src/main/java/org/apache/accumulo/core/summary/Gatherer.java
@@ -38,7 +38,6 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
@@ -162,11 +161,12 @@ public class Gatherer {
       throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
 
     Iterable<TabletMetadata> tmi = MetadataScanner.builder().from(ctx)
-        .overUserTableId(tableId, startRow, endRow).fetchFiles().fetchLocation().fetchLast()
-        .fetchPrev().build();
+        .overRange(tableId, startRow, endRow).fetchFiles().fetchLocation().fetchLast().fetchPrev()
+        .build();
 
     // get a subset of files
     Map<String,List<TabletMetadata>> files = new HashMap<>();
+
     for (TabletMetadata tm : tmi) {
       for (String file : tm.getFiles()) {
         if (fileSelector.test(file)) {
@@ -522,10 +522,8 @@ public class Gatherer {
   private int countFiles()
       throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
     // TODO use a batch scanner + iterator to parallelize counting files
-    Iterable<TabletMetadata> tmi = MetadataScanner.builder().from(ctx)
-        .overUserTableId(tableId, startRow, endRow).fetchFiles().fetchPrev().build();
-    return StreamSupport.stream(tmi.spliterator(), false).mapToInt(tm -> tm.getFiles().size())
-        .sum();
+    return MetadataScanner.builder().from(ctx).overRange(tableId, startRow, endRow).fetchFiles()
+        .fetchPrev().build().stream().mapToInt(tm -> tm.getFiles().size()).sum();
   }
 
   private class GatherRequest implements Supplier<SummaryCollection> {

server/gc/src/main/java/org/apache/accumulo/gc/SimpleGarbageCollector.java
@@ -30,6 +30,7 @@ import java.util.SortedMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.client.AccumuloException;
@@ -58,10 +59,9 @@ import org.apache.accumulo.core.gc.thrift.GcCycleStats;
 import org.apache.accumulo.core.master.state.tables.TableState;
 import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.accumulo.core.metadata.RootTable;
+import org.apache.accumulo.core.metadata.schema.MetadataScanner;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.DataFileColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ScanFileColumnFamily;
+import org.apache.accumulo.core.metadata.schema.TabletMetadata;
 import org.apache.accumulo.core.replication.ReplicationSchema.StatusSection;
 import org.apache.accumulo.core.replication.ReplicationTable;
 import org.apache.accumulo.core.replication.ReplicationTableOfflineException;
@@ -102,7 +102,6 @@ import org.apache.accumulo.server.rpc.ThriftServerType;
 import org.apache.accumulo.server.security.SecurityUtil;
 import org.apache.accumulo.server.tables.TableManager;
 import org.apache.accumulo.server.util.Halt;
-import org.apache.accumulo.server.util.TabletIterator;
 import org.apache.accumulo.server.zookeeper.ZooLock;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
@@ -297,18 +296,23 @@ public class SimpleGarbageCollector extends AccumuloServerContext implements Ifa
     }
 
     @Override
-    public Iterator<Entry<Key,Value>> getReferenceIterator()
+    public Stream<Reference> getReferences()
         throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
-      IsolatedScanner scanner = new IsolatedScanner(
-          getConnector().createScanner(tableName, Authorizations.EMPTY));
-      scanner.fetchColumnFamily(DataFileColumnFamily.NAME);
-      scanner.fetchColumnFamily(ScanFileColumnFamily.NAME);
-      TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN.fetch(scanner);
-      TabletIterator tabletIterator = new TabletIterator(scanner,
-          MetadataSchema.TabletsSection.getRange(), false, true);
 
-      return Iterators
-          .concat(Iterators.transform(tabletIterator, input -> input.entrySet().iterator()));
+      Stream<TabletMetadata> tabletStream = MetadataScanner.builder().from(getConnector())
+          .overTabletRange().checkConsistency().fetchDir().fetchFiles().fetchScans().build()
+          .stream();
+
+      Stream<Reference> refStream = tabletStream.flatMap(tm -> {
+        Stream<Reference> refs = Stream.concat(tm.getFiles().stream(), tm.getScans().stream())
+            .map(f -> new Reference(tm.getTableId(), f, false));
+        if (tm.getDir() != null) {
+          refs = Stream.concat(refs, Stream.of(new Reference(tm.getTableId(), tm.getDir(), true)));
+        }
+        return refs;
+      });
+
+      return refStream;
     }
 
     @Override

server/master/src/main/java/org/apache/accumulo/master/tableOps/bulkVer2/LoadFiles.java
@@ -107,11 +107,10 @@ class LoadFiles extends MasterRepo {
 
     Text startRow = loadMapEntry.getKey().getPrevEndRow();
 
-    Iterable<TabletMetadata> tableMetadata = MetadataScanner.builder().from(master)
-        .overUserTableId(tableId, startRow, null).fetchPrev().fetchLocation().fetchLoaded().build();
-
     long timeInMillis = master.getConfiguration().getTimeInMillis(Property.MASTER_BULK_TIMEOUT);
-    Iterator<TabletMetadata> tabletIter = tableMetadata.iterator();
+    Iterator<TabletMetadata> tabletIter = MetadataScanner.builder().from(master)
+        .overRange(tableId, startRow, null).checkConsistency().fetchPrev().fetchLocation()
+        .fetchLoaded().build().iterator();
 
     List<TabletMetadata> tablets = new ArrayList<>();
     TabletMetadata currentTablet = tabletIter.next();

server/master/src/main/java/org/apache/accumulo/master/tableOps/bulkVer2/PrepBulkImport.java
@@ -166,9 +166,9 @@ public class PrepBulkImport extends MasterRepo {
       Iterators.transform(lmi, entry -> entry.getKey());
 
       TabletIterFactory tabletIterFactory = startRow -> {
-        Iterable<TabletMetadata> tableMetadata = MetadataScanner.builder().from(master)
-            .overUserTableId(bulkInfo.tableId, startRow, null).build();
-        return Iterators.transform(tableMetadata.iterator(), tm -> tm.getExtent());
+        return MetadataScanner.builder().from(master).overRange(bulkInfo.tableId, startRow, null)
+            .checkConsistency().fetchPrev().build().stream().map(TabletMetadata::getExtent)
+            .iterator();
       };
 
       checkForMerge(bulkInfo.tableId.canonicalID(),

core/src/main/java/org/apache/accumulo/core/metadata/schema/LinkingIterator.java
@@ -1,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.accumulo.core.metadata.schema;
+
+import static org.apache.accumulo.fate.util.UtilWaitThread.sleepUninterruptibly;
+
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.PartialKey;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.impl.KeyExtent;
+import org.apache.hadoop.io.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Iterators;
+
+/**
+ * Tablets for a table in the metadata table should form a linked list. This iterator detects when
+ * tablets do not form a linked list and backs up when this happens.
+ *
+ * <p>
+ * The purpose of this is to hide inconsistencies caused by splits and detect anomalies in the
+ * metadata table.
+ *
+ * <p>
+ * If a tablet that was returned by this iterator is subsequently deleted from the metadata table,
+ * then this iterator will throw a TabletDeletedException. This could occur when a table is merged.
+ */
+public class LinkingIterator implements Iterator<TabletMetadata> {
+
+  private static final Logger log = LoggerFactory.getLogger(LinkingIterator.class);
+
+  private Range range;
+  private Function<Range,Iterator<TabletMetadata>> iteratorFactory;
+  private Iterator<TabletMetadata> source;
+  private TabletMetadata prevTablet = null;
+
+  LinkingIterator(Function<Range,Iterator<TabletMetadata>> iteratorFactory, Range range) {
+    this.range = range;
+    this.iteratorFactory = iteratorFactory;
+    source = iteratorFactory.apply(range);
+  }
+
+  @Override
+  public boolean hasNext() {
+    return source.hasNext();
+  }
+
+  static boolean goodTransition(TabletMetadata prev, TabletMetadata curr) {
+    if (!curr.sawPrevEndRow()) {
+      log.warn("Tablet {} had no prev end row.", curr.getExtent());
+      return false;
+    }
+
+    if (!curr.getTableId().equals(prev.getTableId())) {
+      if (prev.getEndRow() != null) {
+        log.debug("Non-null end row for last tablet in table: " + prev.getExtent() + " "
+            + curr.getExtent());
+        return false;
+      }
+
+      if (curr.getPrevEndRow() != null) {
+        log.debug("First tablet for table had prev end row {} {} ", prev.getExtent(),
+            curr.getExtent());
+        return false;
+      }
+    } else {
+      if (prev.getEndRow() == null) {
+        throw new IllegalStateException("Null end row for tablet in middle of table: "
+            + prev.getExtent() + " " + curr.getExtent());
+      }
+
+      if (curr.getPrevEndRow() == null || !prev.getEndRow().equals(curr.getPrevEndRow())) {
+        log.debug("Tablets end row and prev end row not equals {} {} ", prev.getExtent(),
+            curr.getExtent());
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private void resetSource() {
+    if (prevTablet == null) {
+      source = iteratorFactory.apply(range);
+    } else {
+      // get the metadata table row for the previous tablet
+      Text prevMetaRow = KeyExtent.getMetadataEntry(prevTablet.getTableId(),
+          prevTablet.getEndRow());
+
+      // ensure the previous tablet still exists in the metadata table
+      if (Iterators.size(iteratorFactory.apply(new Range(prevMetaRow))) == 0) {
+        throw new TabletDeletedException("Tablet " + prevMetaRow + " was deleted while iterating");
+      }
+
+      // start scanning at next possible row in metadata table
+      Range seekRange = new Range(new Key(prevMetaRow).followingKey(PartialKey.ROW), true,
+          range.getEndKey(), range.isEndKeyInclusive());
+
+      log.info("Resetting scanner to {}", seekRange);
+
+      source = iteratorFactory.apply(seekRange);
+    }
+  }
+
+  @Override
+  public TabletMetadata next() {
+
+    long sleepTime = 250;
+
+    TabletMetadata currTablet = null;
+    while (currTablet == null) {
+      TabletMetadata tmp = source.next();
+
+      if (prevTablet == null) {
+        if (tmp.sawPrevEndRow()) {
+          currTablet = tmp;
+        } else {
+          log.warn("Tablet has no prev end row " + tmp.getTableId() + " " + tmp.getEndRow());
+        }
+      } else if (goodTransition(prevTablet, tmp)) {
+        currTablet = tmp;
+      }
+
+      if (currTablet == null) {
+        sleepUninterruptibly(sleepTime, TimeUnit.MILLISECONDS);
+        resetSource();
+        sleepTime = Math.min(2 * sleepTime, 5000);
+      }
+    }
+
+    prevTablet = currTablet;
+    return currTablet;
+  }
+}

commit 780c4cab06597547d028ed59abdaa967a1c7900a

#469 Fix bug with GC not scanning root table for file refs (#503)
This bug was introduced in 9feb5e1. In addition to fixing the bug,
specifying the table was made mandatory for the metadata scanner
builder.

The bug was found by MetadataMaxFilesIT

core/src/main/java/org/apache/accumulo/core/metadata/schema/MetadataScanner.java
@@ -41,6 +41,7 @@ import org.apache.accumulo.core.client.impl.Table.ID;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.impl.KeyExtent;
 import org.apache.accumulo.core.metadata.MetadataTable;
+import org.apache.accumulo.core.metadata.RootTable;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ClonedColumnFamily;
@@ -62,10 +63,11 @@ public class MetadataScanner implements Iterable<TabletMetadata>, AutoCloseable 
     TableOptions from(Connector conn);
   }
 
-  public interface TableOptions extends RangeOptions {
-    /**
-     * Optionally set a table name, defaults to {@value MetadataTable#NAME}
-     */
+  public interface TableOptions {
+    RangeOptions scanRootTable();
+
+    RangeOptions scanMetadataTable();
+
     RangeOptions scanTable(String tableName);
   }
 
@@ -309,6 +311,16 @@ public class MetadataScanner implements Iterable<TabletMetadata>, AutoCloseable 
     public RangeOptions scanTable(String tableName) {
       this.table = tableName;
       return this;
+    }
+
+    @Override
+    public RangeOptions scanMetadataTable() {
+      return scanTable(MetadataTable.NAME);
+    }
+
+    @Override
+    public RangeOptions scanRootTable() {
+      return scanTable(RootTable.NAME);
     }
   }
 

core/src/main/java/org/apache/accumulo/core/summary/Gatherer.java
@@ -160,7 +160,7 @@ public class Gatherer {
       Predicate<String> fileSelector)
       throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
 
-    Iterable<TabletMetadata> tmi = MetadataScanner.builder().from(ctx)
+    Iterable<TabletMetadata> tmi = MetadataScanner.builder().from(ctx).scanMetadataTable()
         .overRange(tableId, startRow, endRow).fetchFiles().fetchLocation().fetchLast().fetchPrev()
         .build();
 
@@ -522,8 +522,9 @@ public class Gatherer {
   private int countFiles()
       throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
     // TODO use a batch scanner + iterator to parallelize counting files
-    return MetadataScanner.builder().from(ctx).overRange(tableId, startRow, endRow).fetchFiles()
-        .fetchPrev().build().stream().mapToInt(tm -> tm.getFiles().size()).sum();
+    return MetadataScanner.builder().from(ctx).scanMetadataTable()
+        .overRange(tableId, startRow, endRow).fetchFiles().fetchPrev().build().stream()
+        .mapToInt(tm -> tm.getFiles().size()).sum();
   }
 
   private class GatherRequest implements Supplier<SummaryCollection> {

server/gc/src/main/java/org/apache/accumulo/gc/SimpleGarbageCollector.java
@@ -300,8 +300,8 @@ public class SimpleGarbageCollector extends AccumuloServerContext implements Ifa
         throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
 
       Stream<TabletMetadata> tabletStream = MetadataScanner.builder().from(getConnector())
-          .overTabletRange().checkConsistency().fetchDir().fetchFiles().fetchScans().build()
-          .stream();
+          .scanTable(tableName).overTabletRange().checkConsistency().fetchDir().fetchFiles()
+          .fetchScans().build().stream();
 
       Stream<Reference> refStream = tabletStream.flatMap(tm -> {
         Stream<Reference> refs = Stream.concat(tm.getFiles().stream(), tm.getScans().stream())

server/master/src/main/java/org/apache/accumulo/master/tableOps/bulkVer2/LoadFiles.java
@@ -108,7 +108,7 @@ class LoadFiles extends MasterRepo {
     Text startRow = loadMapEntry.getKey().getPrevEndRow();
 
     long timeInMillis = master.getConfiguration().getTimeInMillis(Property.MASTER_BULK_TIMEOUT);
-    Iterator<TabletMetadata> tabletIter = MetadataScanner.builder().from(master)
+    Iterator<TabletMetadata> tabletIter = MetadataScanner.builder().from(master).scanMetadataTable()
         .overRange(tableId, startRow, null).checkConsistency().fetchPrev().fetchLocation()
         .fetchLoaded().build().iterator();
 

server/master/src/main/java/org/apache/accumulo/master/tableOps/bulkVer2/PrepBulkImport.java
@@ -166,9 +166,9 @@ public class PrepBulkImport extends MasterRepo {
       Iterators.transform(lmi, entry -> entry.getKey());
 
       TabletIterFactory tabletIterFactory = startRow -> {
-        return MetadataScanner.builder().from(master).overRange(bulkInfo.tableId, startRow, null)
-            .checkConsistency().fetchPrev().build().stream().map(TabletMetadata::getExtent)
-            .iterator();
+        return MetadataScanner.builder().from(master).scanMetadataTable()
+            .overRange(bulkInfo.tableId, startRow, null).checkConsistency().fetchPrev().build()
+            .stream().map(TabletMetadata::getExtent).iterator();
       };
 
       checkForMerge(bulkInfo.tableId.canonicalID(),