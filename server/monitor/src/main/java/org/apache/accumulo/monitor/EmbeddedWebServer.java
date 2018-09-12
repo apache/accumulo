d23754f87227b9b5426dd52884a332d2b26b3e66
Romil Choksi <rchoksi@hortonworks.com>
2018-09-12 15:07:20 -0400

    Do not require a password on the truststore JKS
    A password on a truststore provides no security value, only validation
    that the JKS is the JKS that the user expects.
    
    Signed-off-by: Josh Elser <elserj@apache.org>

--- server/monitor/src/main/java/org/apache/accumulo/monitor/EmbeddedWebServer.java
+++ server/monitor/src/main/java/org/apache/accumulo/monitor/EmbeddedWebServer.java
@@ -48,8 +48,7 @@ public class EmbeddedWebServer {
     final AccumuloConfiguration conf = Monitor.getContext().getConfiguration();
     if (EMPTY.equals(conf.get(Property.MONITOR_SSL_KEYSTORE))
         || EMPTY.equals(conf.get(Property.MONITOR_SSL_KEYSTOREPASS))
-        || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTORE))
-        || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTOREPASS))) {
+        || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTORE))) {
       connector = new ServerConnector(server, new HttpConnectionFactory());
       usingSsl = false;
     } else {
