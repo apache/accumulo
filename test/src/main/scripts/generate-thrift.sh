8493a104b53ee638337abcd890d642e494701e81
Christopher Tubbs <ctubbsii@apache.org>
2018-10-09 10:24:44 -0400

    Revert "Work around compilation bug in Eclipse Oxygen"
    Fixes #485; workaround not needed in latest version of Eclipse Photon
    
    This reverts commit b49865d08f3edb425306c3e48689c8836d947ad5.

--- test/src/main/scripts/generate-thrift.sh
+++ test/src/main/scripts/generate-thrift.sh
@@ -22,20 +22,3 @@ PACKAGES_TO_GENERATE=(test.rpc)
 
 . ../core/src/main/scripts/generate-thrift.sh
 
-# fix SimpleThriftService compilation error in Eclipse Oxygen
-# see https://bugs.eclipse.org/bugs/show_bug.cgi?id=534559
-patch -p2 <<'EOF'
-diff --git a/test/src/main/java/org/apache/accumulo/test/rpc/thrift/SimpleThriftService.java b/test/src/main/java/org/apache/accumulo/test/rpc/thrift/SimpleThriftService.java
-index d76a789e0..fa0ea29e6 100644
---- a/test/src/main/java/org/apache/accumulo/test/rpc/thrift/SimpleThriftService.java
-+++ b/test/src/main/java/org/apache/accumulo/test/rpc/thrift/SimpleThriftService.java
-@@ -405,7 +405,7 @@ public class SimpleThriftService {
-       super(iface, getProcessMap(processMap));
-     }
- 
--    private static <I extends Iface> java.util.Map<java.lang.String,  org.apache.thrift.ProcessFunction<I, ? extends org.apache.thrift.TBase>> getProcessMap(java.util.Map<java.lang.String, org.apache.thrift.ProcessFunction<I, ? extends  org.apache.thrift.TBase>> processMap) {
-+    private static <I extends Iface> java.util.Map<java.lang.String,  org.apache.thrift.ProcessFunction<I, ?>> getProcessMap(java.util.Map<java.lang.String, org.apache.thrift.ProcessFunction<I, ? extends  org.apache.thrift.TBase>> processMap) {
-       processMap.put("echoPass", new echoPass());
-       processMap.put("onewayPass", new onewayPass());
-       processMap.put("echoFail", new echoFail());
-EOF
