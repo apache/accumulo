c8d19d45f361f49a4d42b07310393b7bdf68b3b4
Mike Walch <mwalch@apache.org>
2018-12-11 11:41:19 -0500

    Auto-formatting fix (#814)

--- core/src/main/java/org/apache/accumulo/core/clientImpl/mapreduce/lib/ConfiguratorBase.java
+++ core/src/main/java/org/apache/accumulo/core/clientImpl/mapreduce/lib/ConfiguratorBase.java
@@ -304,7 +304,8 @@ public class ConfiguratorBase {
    * @param clientConfig
    *          client configuration for specifying connection timeouts, SSL connection options, etc.
    * @since 1.6.0
-   * @deprecated since 2.0.0; use {@link #setClientProperties(Class, Configuration, Properties)} instead
+   * @deprecated since 2.0.0; use {@link #setClientProperties(Class, Configuration, Properties)}
+   *             instead
    */
   @Deprecated
   public static void setZooKeeperInstance(Class<?> implementingClass, Configuration conf,
