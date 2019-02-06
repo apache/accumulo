787590acc3470738d6b7db52e71579d31976ed39
Mike Walch <mwalch@apache.org>
2019-02-06 15:06:49 -0500

    CR updates

--- core/src/main/java/org/apache/accumulo/core/client/ScannerBase.java
+++ core/src/main/java/org/apache/accumulo/core/client/ScannerBase.java
@@ -105,6 +105,7 @@ public interface ScannerBase extends Iterable<Entry<Key,Value>>, AutoCloseable {
    *
    * @param colFam
    *          the column family to be fetched
+   * @since 2.0.0
    */
   void fetchColumnFamily(CharSequence colFam);
 
@@ -142,31 +143,14 @@ public interface ScannerBase extends Iterable<Entry<Key,Value>>, AutoCloseable {
   /**
    * Adds a column to the list of columns that will be fetched by this scanner. The column is
    * identified by family and qualifier. By default when no columns have been added the scanner
-   * fetches all columns.
+   * fetches all columns. See the warning on {@link #fetchColumn(Text, Text)}
    *
-   * <p>
-   * <b>WARNING</b>. Using this method with custom iterators may have unexpected results. Iterators
-   * have control over which column families are fetched. However iterators have no control over
-   * which column qualifiers are fetched. When this method is called it activates a system iterator
-   * that only allows the requested family/qualifier pairs through. This low level filtering
-   * prevents custom iterators from requesting additional column families when calling seek.
-   *
-   * <p>
-   * For an example, assume fetchColumns(A, Q1) and fetchColumns(B,Q1) is called on a scanner and a
-   * custom iterator is configured. The families (A,B) will be passed to the seek method of the
-   * custom iterator. If the custom iterator seeks its source iterator using the families (A,B,C),
-   * it will never see any data from C because the system iterator filtering A:Q1 and B:Q1 will
-   * prevent the C family from getting through. ACCUMULO-3905 also has an example of the type of
-   * problem this method can cause.
-   *
-   * <p>
-   * tl;dr If using a custom iterator with a seek method that adds column families, then may want to
-   * avoid using this method.
    *
    * @param colFam
    *          the column family of the column to be fetched
    * @param colQual
    *          the column qualifier of the column to be fetched
+   * @since 2.0.0
    */
   void fetchColumn(CharSequence colFam, CharSequence colQual);
 
