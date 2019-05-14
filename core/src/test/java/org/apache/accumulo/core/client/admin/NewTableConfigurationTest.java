9ac579c7765d585fb8e04a3e10de3c3cf2e449ac
Elina Wise <elinaawise@gmail.com>
2019-05-14 10:31:10 -0400

    issue #576: Removed .* import

--- core/src/test/java/org/apache/accumulo/core/client/admin/NewTableConfigurationTest.java
+++ core/src/test/java/org/apache/accumulo/core/client/admin/NewTableConfigurationTest.java
@@ -16,6 +16,16 @@
  */
 package org.apache.accumulo.core.client.admin;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.apache.accumulo.core.client.sample.RowSampler;
 import org.apache.accumulo.core.client.sample.SamplerConfiguration;
 import org.apache.accumulo.core.client.summary.Summarizer;
@@ -25,15 +35,10 @@ import org.apache.hadoop.io.Text;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class NewTableConfigurationTest {
 
   private SortedSet<Text> splits;
-  private Map<String, String> options;
+  private Map<String,String> options;
 
   @Before
   public void setup() {
@@ -123,7 +128,8 @@ public class NewTableConfigurationTest {
     SamplerConfiguration sha1SamplerConfig = new SamplerConfiguration(rowSampler.getClass());
     sha1SamplerConfig.setOptions(options);
     rowSampler.init(sha1SamplerConfig);
-    NewTableConfiguration ntcSample2 = new NewTableConfiguration().enableSampling(sha1SamplerConfig);
+    NewTableConfiguration ntcSample2 =
+        new NewTableConfiguration().enableSampling(sha1SamplerConfig);
     assertTrue(ntcSample2.getProperties().containsValue("murmur3_32"));
   }
 
@@ -134,10 +140,11 @@ public class NewTableConfigurationTest {
   @Test
   public void testEnableSummarization() {
     SummarizerConfiguration summarizerConfig1 = SummarizerConfiguration.builder("test").build();
-    NewTableConfiguration ntcSummarization1 = new NewTableConfiguration().enableSummarization(summarizerConfig1);
+    NewTableConfiguration ntcSummarization1 =
+        new NewTableConfiguration().enableSummarization(summarizerConfig1);
 
     Object summarizerValue = null;
-    for(Map.Entry<String, String> e : ntcSummarization1.getProperties().entrySet()) {
+    for (Map.Entry<String,String> e : ntcSummarization1.getProperties().entrySet()) {
       if (e.getKey().contains("table.summarizer")) {
         summarizerValue = e.getValue();
       }
@@ -148,10 +155,12 @@ public class NewTableConfigurationTest {
     assertTrue(Summarizer.class.isAssignableFrom(builderClass));
 
     summarizerValue = null;
-    SummarizerConfiguration summarizerConfig2 = SummarizerConfiguration.builder(builderClass).build();
-    NewTableConfiguration ntcSummarization2 = new NewTableConfiguration().enableSummarization(summarizerConfig2);
+    SummarizerConfiguration summarizerConfig2 =
+        SummarizerConfiguration.builder(builderClass).build();
+    NewTableConfiguration ntcSummarization2 =
+        new NewTableConfiguration().enableSummarization(summarizerConfig2);
 
-    for(Map.Entry<String, String> e : ntcSummarization2.getProperties().entrySet()) {
+    for (Map.Entry<String,String> e : ntcSummarization2.getProperties().entrySet()) {
       if (e.getKey().contains("table.summarizer")) {
         summarizerValue = e.getValue();
       }
