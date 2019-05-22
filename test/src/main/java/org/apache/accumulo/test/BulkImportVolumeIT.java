a1b1ae6bfef2ca11d92b86ab60930b3c56971f6e
Mike Miller <mmiller@apache.org>
2019-05-22 11:56:46 -0400

    Formatting

--- test/src/main/java/org/apache/accumulo/test/BulkImportVolumeIT.java
+++ test/src/main/java/org/apache/accumulo/test/BulkImportVolumeIT.java
@@ -70,7 +70,8 @@ public class BulkImportVolumeIT extends AccumuloClusterHarness {
     try (AccumuloClient client = Accumulo.newClient().from(getClientProps()).build()) {
       client.tableOperations().create(tableName);
       FileSystem fs = getFileSystem();
-      Path rootPath = new Path(fs.getUri().toString() + cluster.getTemporaryPath(), getClass().getName());
+      Path rootPath =
+          new Path(fs.getUri().toString() + cluster.getTemporaryPath(), getClass().getName());
       fs.deleteOnExit(rootPath);
 
       Path bulk = new Path(rootPath, "bulk");
