0ac42c876d9406aa657548004cd5ca5b5b57e08e
Jeffrey Manno <jeffreymanno15@gmail.com>
2019-05-24 10:07:14 -0400

    Fixes #1159. Refactor DurabilityIT (#1165)

--- test/src/main/java/org/apache/accumulo/test/functional/DurabilityIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/DurabilityIT.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -44,14 +43,11 @@ import org.apache.hadoop.fs.RawLocalFileSystem;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Iterators;
 
 @Category({MiniClusterOnlyTests.class, PerformanceTests.class})
 public class DurabilityIT extends ConfigurableMacBase {
-  private static final Logger log = LoggerFactory.getLogger(DurabilityIT.class);
 
   @Override
   public void configure(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
@@ -89,32 +85,6 @@ public class DurabilityIT extends ConfigurableMacBase {
 
   private void createTable(AccumuloClient c, String tableName) throws Exception {
     c.tableOperations().create(tableName);
-  }
-
-  @Test(timeout = 2 * 60 * 1000)
-  public void testWriteSpeed() throws Exception {
-    try (AccumuloClient client = Accumulo.newClient().from(getClientProperties()).build()) {
-      TableOperations tableOps = client.tableOperations();
-      String[] tableNames = init(client);
-      // write some gunk, delete the table to keep that table from messing with the performance
-      // numbers of successive calls
-      // sync
-      long t0 = writeSome(client, tableNames[0], N);
-      tableOps.delete(tableNames[0]);
-      // flush
-      long t1 = writeSome(client, tableNames[1], N);
-      tableOps.delete(tableNames[1]);
-      // log
-      long t2 = writeSome(client, tableNames[2], N);
-      tableOps.delete(tableNames[2]);
-      // none
-      long t3 = writeSome(client, tableNames[3], N);
-      tableOps.delete(tableNames[3]);
-      System.out.println(String.format("sync %d flush %d log %d none %d", t0, t1, t2, t3));
-      assertTrue("flush should be faster than sync", t0 > t1);
-      assertTrue("log should be faster than flush", t1 > t2);
-      assertTrue("no durability should be faster than log", t2 > t3);
-    }
   }
 
   @Test(timeout = 4 * 60 * 1000)
@@ -218,27 +188,17 @@ public class DurabilityIT extends ConfigurableMacBase {
     cluster.start();
   }
 
-  private long writeSome(AccumuloClient c, String table, long count) throws Exception {
-    int iterations = 5;
-    long[] attempts = new long[iterations];
-    for (int attempt = 0; attempt < iterations; attempt++) {
-      long now = System.currentTimeMillis();
-      try (BatchWriter bw = c.createBatchWriter(table)) {
-        for (int i = 1; i < count + 1; i++) {
-          Mutation m = new Mutation("" + i);
-          m.put("", "", "");
-          bw.addMutation(m);
-          if (i % (Math.max(1, count / 100)) == 0) {
-            bw.flush();
-          }
+  private void writeSome(AccumuloClient c, String table, long count) throws Exception {
+    try (BatchWriter bw = c.createBatchWriter(table)) {
+      for (int i = 1; i < count + 1; i++) {
+        Mutation m = new Mutation("" + i);
+        m.put("", "", "");
+        bw.addMutation(m);
+        if (i % (Math.max(1, count / 100)) == 0) {
+          bw.flush();
         }
       }
-      attempts[attempt] = System.currentTimeMillis() - now;
     }
-    Arrays.sort(attempts);
-    log.info("Attempt durations: {}", Arrays.toString(attempts));
-    // Return the median duration
-    return attempts[2];
   }
 
 }
