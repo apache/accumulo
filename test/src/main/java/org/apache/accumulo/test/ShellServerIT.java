0e89ab238b14d257f9f901a36a9b87a7491c1ea9
Christopher Tubbs <ctubbsii@apache.org>
2019-06-16 17:48:01 -0400

    Fix more tests building 2.0 with Java 11 (#1215)
    * Fix more tests building 2.0 with Java 11
    
    * Update monitor dependencies to release versions (instead of beta
      versions) for WebViewsIT
    * Simplify parameters to TabletServerResourceManager (everything it
      needs is in the ServerContext; other params were redundant)
    * Ensure TabletServerResourceManager updates the dynamic thread pools in
      the correct order (cannot increase the corePoolSize before increasing
      the maxPoolSize)
    * Make ShellServerIT.classpath test handle output under Java 11
    * Convert dubious DynamicThreadPoolsIT from an IT to a unit test
      (TabletServerResourceManagerDynamicCompactionPoolTest)
    * Give SimpleGarbageCollector 16MB of memory instead of 10MB for its
      gcLotsOfCandidatesIT test, so it doesn't crash under Java 11 before the
      test runs (Java 11 needs slightly more base memory, it appears)

--- test/src/main/java/org/apache/accumulo/test/ShellServerIT.java
+++ test/src/main/java/org/apache/accumulo/test/ShellServerIT.java
@@ -834,8 +834,8 @@ public class ShellServerIT extends SharedMiniClusterBase {
   @Test
   public void classpath() throws Exception {
     // classpath
-    ts.exec("classpath", true, "Level 2: Java Classloader (loads everything"
-        + " defined by java classpath) URL classpath items are", true);
+    ts.exec("classpath", true,
+        "Level 2: Java Classloader (loads everything" + " defined by java classpath)", true);
   }
 
   @Test
