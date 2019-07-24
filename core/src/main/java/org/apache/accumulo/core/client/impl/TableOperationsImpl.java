fece0bbf44dcbd986854353927ea50cffaf8cb00
Mike Miller <mmiller@apache.org>
2019-07-24 18:30:52 -0400

    Fix misspelling

--- core/src/main/java/org/apache/accumulo/core/client/impl/TableOperationsImpl.java
+++ core/src/main/java/org/apache/accumulo/core/client/impl/TableOperationsImpl.java
@@ -1698,13 +1698,13 @@ public class TableOperationsImpl extends TableOperationsHelper {
     return sci.toSamplerConfiguration();
   }
 
-  private static class LoctionsImpl implements Locations {
+  private static class LocationsImpl implements Locations {
 
     private Map<Range,List<TabletId>> groupedByRanges;
     private Map<TabletId,List<Range>> groupedByTablets;
     private Map<TabletId,String> tabletLocations;
 
-    public LoctionsImpl(Map<String,Map<KeyExtent,List<Range>>> binnedRanges) {
+    public LocationsImpl(Map<String,Map<KeyExtent,List<Range>>> binnedRanges) {
       groupedByTablets = new HashMap<>();
       groupedByRanges = null;
       tabletLocations = new HashMap<>();
@@ -1808,6 +1808,6 @@ public class TableOperationsImpl extends TableOperationsHelper {
       locator.invalidateCache();
     }
 
-    return new LoctionsImpl(binnedRanges);
+    return new LocationsImpl(binnedRanges);
   }
 }
