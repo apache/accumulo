2d0091d7136c216e1e043d462c34870f4e68ba55
EdColeman <dev1@etcoleman.com>
2019-08-15 15:41:15 -0400

    Add optional -t tablename to importdirectory shell command. (#1299)
    * Add optional -t tablename to importdirectory shell command.
    
    the -t tablename is optional to preserve original behaviour. Without
    the option, the current table is used.
    
    * added getOptions
    
    * wip - tests cmd format, need to evaluate testing functionality

--- shell/src/main/java/org/apache/accumulo/shell/commands/ImportDirectoryCommand.java
+++ shell/src/main/java/org/apache/accumulo/shell/commands/ImportDirectoryCommand.java
@@ -24,37 +24,50 @@ import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.shell.Shell;
 import org.apache.accumulo.shell.Shell.Command;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 
 public class ImportDirectoryCommand extends Command {
 
   @Override
   public String description() {
-    return "bulk imports an entire directory of data files to the current"
-        + " table. The boolean argument determines if accumulo sets the time.";
+    return "bulk imports an entire directory of data files into an existing table."
+        + " The table is either passed with the -t tablename opt, or into to the current"
+        + " table if the -t option is not provided. The boolean argument determines if accumulo sets the time.";
   }
 
   @Override
   public int execute(final String fullCommand, final CommandLine cl, final Shell shellState)
       throws IOException, AccumuloException, AccumuloSecurityException, TableNotFoundException {
-    shellState.checkTableState();
+
+    final String tableName = OptUtil.getTableOpt(cl, shellState);
 
     String dir = cl.getArgs()[0];
     String failureDir = cl.getArgs()[1];
+
     final boolean setTime = Boolean.parseBoolean(cl.getArgs()[2]);
 
-    shellState.getConnector().tableOperations().importDirectory(shellState.getTableName(), dir,
-        failureDir, setTime);
+    shellState.getConnector().tableOperations().importDirectory(tableName, dir, failureDir,
+        setTime);
+
     return 0;
   }
 
   @Override
   public int numArgs() {
+    // arg count for args not handled with Options
     return 3;
   }
 
   @Override
   public String usage() {
-    return getName() + " <directory> <failureDirectory> true|false";
+    return getName() + "[-t tablename] <directory> <failureDirectory> true|false";
+  }
+
+  @Override
+  public Options getOptions() {
+    final Options opts = super.getOptions();
+    opts.addOption(OptUtil.tableOpt("name of the table to import files into"));
+    return opts;
   }
 
 }
