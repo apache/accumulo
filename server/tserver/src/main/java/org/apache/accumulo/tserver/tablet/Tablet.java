apache/accumulo

commit 57ed0ec503159e23d608628aa856ca8a3f164465

Always choose volume for new tablet files. (#1389)
This commit changes Accumulo to always call the volume chooser every time a
tablet creates a new file.  It also changes the interpretation of the srv:dir
column in the metadata table.  This column used to contain a URI to a
directory on a specific volume that was used for all new tablet files. Now the
srv:dir column only contains a directory name.  This directory name will be
used for new tablet files across all volumes.

This change necessitated to ~del markers in the metadata table used for
garbage collection.  When a table is cloned or tablets are merged out of
existence it can result in ~del markers for tablet dirs being placed in
the metadata table.  These ~del markers used to reference a specific volume.
With this change, the ~del marker now use a special URI  of the form

  agcav:/tables/<tableId>/<dir name>

When the Accumulo GC sees this, it will delete the dir on all configured
volumes when its no longer used.

This change superceded #642.  These changes are possible because of the
changes made in #936.

server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Tablet.java
@@ -39,6 +39,7 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -62,7 +63,6 @@ import org.apache.accumulo.core.data.ColumnUpdate;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
-import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.dataImpl.KeyExtent;
 import org.apache.accumulo.core.dataImpl.thrift.IterInfo;
@@ -78,6 +78,7 @@ import org.apache.accumulo.core.master.thrift.BulkImportState;
 import org.apache.accumulo.core.master.thrift.TabletLoadState;
 import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.accumulo.core.metadata.schema.DataFileValue;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataTime;
 import org.apache.accumulo.core.protobuf.ProtobufUtil;
 import org.apache.accumulo.core.replication.ReplicationConfigurationUtil;
@@ -91,6 +92,7 @@ import org.apache.accumulo.core.util.LocalityGroupUtil;
 import org.apache.accumulo.core.util.Pair;
 import org.apache.accumulo.core.util.ShutdownUtil;
 import org.apache.accumulo.core.util.ratelimit.RateLimiter;
+import org.apache.accumulo.core.volume.Volume;
 import org.apache.accumulo.server.ServerConstants;
 import org.apache.accumulo.server.ServerContext;
 import org.apache.accumulo.server.conf.TableConfiguration;
@@ -98,7 +100,6 @@ import org.apache.accumulo.server.fs.FileRef;
 import org.apache.accumulo.server.fs.VolumeChooserEnvironment;
 import org.apache.accumulo.server.fs.VolumeChooserEnvironmentImpl;
 import org.apache.accumulo.server.fs.VolumeManager;
-import org.apache.accumulo.server.fs.VolumeManager.FileType;
 import org.apache.accumulo.server.fs.VolumeUtil;
 import org.apache.accumulo.server.fs.VolumeUtil.TabletFiles;
 import org.apache.accumulo.server.master.state.TServerInstance;
@@ -170,8 +171,7 @@ public class Tablet {
   private final TabletResourceManager tabletResources;
   private final DatafileManager datafileManager;
   private final TableConfiguration tableConfiguration;
-  private final String tabletDirectory;
-  private final Path location; // absolute path of this tablets dir
+  private final String dirName;
 
   private final TabletMemory tabletMemory;
 
@@ -180,7 +180,7 @@ public class Tablet {
   private long persistedTime;
 
   private TServerInstance lastLocation = null;
-  private volatile boolean tableDirChecked = false;
+  private volatile Set<Path> checkedTabletDirs = new ConcurrentSkipListSet<>();
 
   private final AtomicLong dataSourceDeletions = new AtomicLong(0);
 
@@ -274,32 +274,38 @@ public class Tablet {
     public boolean closed = false;
   }
 
-  FileRef getNextMapFilename(String prefix) throws IOException {
-    String extension = FileOperations.getNewFileExtension(tableConfiguration);
-    checkTabletDir();
-    return new FileRef(
-        location + "/" + prefix + context.getUniqueNameAllocator().getNextName() + "." + extension);
+  private String chooseTabletDir() throws IOException {
+    VolumeChooserEnvironment chooserEnv =
+        new VolumeChooserEnvironmentImpl(extent.getTableId(), extent.getEndRow(), context);
+    String dirUri =
+        tabletServer.getFileSystem().choose(chooserEnv, ServerConstants.getBaseUris(context))
+            + Constants.HDFS_TABLES_DIR + Path.SEPARATOR + extent.getTableId() + Path.SEPARATOR
+            + dirName;
+    checkTabletDir(new Path(dirUri));
+    return dirUri;
   }
 
-  private void checkTabletDir() throws IOException {
-    if (!tableDirChecked) {
+  FileRef getNextMapFilename(String prefix) throws IOException {
+    String extension = FileOperations.getNewFileExtension(tableConfiguration);
+    return new FileRef(chooseTabletDir() + "/" + prefix
+        + context.getUniqueNameAllocator().getNextName() + "." + extension);
+  }
+
+  private void checkTabletDir(Path path) throws IOException {
+    if (!checkedTabletDirs.contains(path)) {
       FileStatus[] files = null;
       try {
-        files = getTabletServer().getFileSystem().listStatus(location);
+        files = getTabletServer().getFileSystem().listStatus(path);
       } catch (FileNotFoundException ex) {
         // ignored
       }
 
       if (files == null) {
-        if (location.getName().startsWith(Constants.CLONE_PREFIX)) {
-          log.debug("Tablet {} had no dir, creating {}", extent, location); // its a clone dir...
-        } else {
-          log.warn("Tablet {} had no dir, creating {}", extent, location);
-        }
+        log.debug("Tablet {} had no dir, creating {}", extent, path);
 
-        getTabletServer().getFileSystem().mkdirs(location);
+        getTabletServer().getFileSystem().mkdirs(path);
       }
-      tableDirChecked = true;
+      checkedTabletDirs.add(path);
     }
   }
 
@@ -333,20 +339,12 @@ public class Tablet {
     boolean replicationEnabled =
         ReplicationConfigurationUtil.isEnabled(extent, this.tableConfiguration);
     TabletFiles tabletPaths =
-        new TabletFiles(data.getDirectory(), data.getLogEntries(), data.getDataFiles());
+        new TabletFiles(data.getDirectoryName(), data.getLogEntries(), data.getDataFiles());
     tabletPaths = VolumeUtil.updateTabletVolumes(tabletServer.getContext(), tabletServer.getLock(),
         fs, extent, tabletPaths, replicationEnabled);
 
-    // deal with relative path for the directory
-    Path locationPath;
-    if (tabletPaths.dir.contains(":")) {
-      locationPath = new Path(tabletPaths.dir);
-    } else {
-      locationPath = tabletServer.getFileSystem().getFullPath(FileType.TABLE,
-          extent.getTableId() + tabletPaths.dir);
-    }
-    this.location = locationPath;
-    this.tabletDirectory = tabletPaths.dir;
+    this.dirName = data.getDirectoryName();
+
     for (Entry<Long,List<FileRef>> entry : data.getBulkImported().entrySet()) {
       this.bulkImported.put(entry.getKey(), new ArrayList<>(entry.getValue()));
     }
@@ -487,17 +485,22 @@ public class Tablet {
   private void removeOldTemporaryFiles() {
     // remove any temporary files created by a previous tablet server
     try {
-      for (FileStatus tmp : getTabletServer().getFileSystem()
-          .globStatus(new Path(location, "*_tmp"))) {
-        try {
-          log.debug("Removing old temp file {}", tmp.getPath());
-          getTabletServer().getFileSystem().delete(tmp.getPath());
-        } catch (IOException ex) {
-          log.error("Unable to remove old temp file " + tmp.getPath() + ": " + ex);
+      Collection<Volume> volumes = getTabletServer().getFileSystem().getVolumes();
+      for (Volume volume : volumes) {
+        String dirUri = volume.getBasePath() + Constants.HDFS_TABLES_DIR + Path.SEPARATOR + dirName;
+
+        for (FileStatus tmp : getTabletServer().getFileSystem()
+            .globStatus(new Path(dirUri, "*_tmp"))) {
+          try {
+            log.debug("Removing old temp file {}", tmp.getPath());
+            getTabletServer().getFileSystem().delete(tmp.getPath());
+          } catch (IOException ex) {
+            log.error("Unable to remove old temp file " + tmp.getPath() + ": " + ex);
+          }
         }
       }
     } catch (IOException ex) {
-      log.error("Error scanning for old temp files in {}", location);
+      log.error("Error scanning for old temp files", ex);
     }
   }
 
@@ -1453,15 +1456,6 @@ public class Tablet {
     // TODO check lastFlushID and lostCompactID - ACCUMULO-1290
   }
 
-  /**
-   * Returns a Path object representing the tablet's location on the DFS.
-   *
-   * @return location
-   */
-  public Path getLocation() {
-    return location;
-  }
-
   public synchronized void initiateMajorCompaction(MajorCompactionReason reason) {
 
     if (isClosing() || isClosed() || !needsMajorCompaction(reason) || isMajorCompactionRunning()
@@ -1532,7 +1526,7 @@ public class Tablet {
 
     try {
       // we should make .25 below configurable
-      keys = FileUtil.findMidPoint(context, tabletDirectory, extent.getPrevEndRow(),
+      keys = FileUtil.findMidPoint(context, chooseTabletDir(), extent.getPrevEndRow(),
           extent.getEndRow(), FileUtil.toPathStrings(files), .25);
     } catch (IOException e) {
       log.error("Failed to find midpoint {}", e.getMessage());
@@ -2210,7 +2204,7 @@ public class Tablet {
       } else {
         Text tsp = new Text(sp);
         splitPoint = new SplitRowSpec(
-            FileUtil.estimatePercentageLTE(context, tabletDirectory, extent.getPrevEndRow(),
+            FileUtil.estimatePercentageLTE(context, chooseTabletDir(), extent.getPrevEndRow(),
                 extent.getEndRow(), FileUtil.toPathStrings(getDatafileManager().getFiles()), tsp),
             tsp);
       }
@@ -2230,8 +2224,7 @@ public class Tablet {
       KeyExtent low = new KeyExtent(extent.getTableId(), midRow, extent.getPrevEndRow());
       KeyExtent high = new KeyExtent(extent.getTableId(), extent.getEndRow(), midRow);
 
-      String lowDirectory = createTabletDirectory(context, getTabletServer().getFileSystem(),
-          extent.getTableId(), midRow);
+      String lowDirectoryName = createTabletDirectoryName(context, midRow);
 
       // write new tablet information to MetadataTable
       SortedMap<FileRef,DataFileValue> lowDatafileSizes = new TreeMap<>();
@@ -2249,7 +2242,7 @@ public class Tablet {
 
       MetadataTableUtil.splitTablet(high, extent.getPrevEndRow(), splitRatio,
           getTabletServer().getContext(), getTabletServer().getLock());
-      MasterMetadataUtil.addNewTablet(getTabletServer().getContext(), low, lowDirectory,
+      MasterMetadataUtil.addNewTablet(getTabletServer().getContext(), low, lowDirectoryName,
           getTabletServer().getTabletSession(), lowDatafileSizes, bulkImported, time, lastFlushID,
           lastCompactID, getTabletServer().getLock());
       MetadataTableUtil.finishSplit(high, highDatafileSizes, highDatafilesToRemove,
@@ -2257,9 +2250,9 @@ public class Tablet {
 
       log.debug("TABLET_HIST {} split {} {}", extent, low, high);
 
-      newTablets.put(high, new TabletData(tabletDirectory, highDatafileSizes, time, lastFlushID,
+      newTablets.put(high, new TabletData(dirName, highDatafileSizes, time, lastFlushID,
           lastCompactID, lastLocation, bulkImported));
-      newTablets.put(low, new TabletData(lowDirectory, lowDatafileSizes, time, lastFlushID,
+      newTablets.put(low, new TabletData(lowDirectoryName, lowDatafileSizes, time, lastFlushID,
           lastCompactID, lastLocation, bulkImported));
 
       long t2 = System.currentTimeMillis();
@@ -2806,49 +2799,12 @@ public class Tablet {
     return scannedCount;
   }
 
-  private static String createTabletDirectory(ServerContext context, VolumeManager fs,
-      TableId tableId, Text endRow) {
-    String lowDirectory;
-
-    UniqueNameAllocator namer = context.getUniqueNameAllocator();
-    VolumeChooserEnvironment chooserEnv =
-        new VolumeChooserEnvironmentImpl(tableId, endRow, context);
-    String volume = fs.choose(chooserEnv, ServerConstants.getBaseUris(context))
-        + Constants.HDFS_TABLES_DIR + Path.SEPARATOR;
-
-    while (true) {
-      try {
-        if (endRow == null) {
-          lowDirectory = Constants.DEFAULT_TABLET_LOCATION;
-          Path lowDirectoryPath = new Path(volume + "/" + tableId + "/" + lowDirectory);
-          if (fs.exists(lowDirectoryPath) || fs.mkdirs(lowDirectoryPath)) {
-            FileSystem pathFs = fs.getVolumeByPath(lowDirectoryPath).getFileSystem();
-            return lowDirectoryPath.makeQualified(pathFs.getUri(), pathFs.getWorkingDirectory())
-                .toString();
-          }
-          log.warn("Failed to create {} for unknown reason", lowDirectoryPath);
-        } else {
-          lowDirectory = "/" + Constants.GENERATED_TABLET_DIRECTORY_PREFIX + namer.getNextName();
-          Path lowDirectoryPath = new Path(volume + "/" + tableId + "/" + lowDirectory);
-          if (fs.exists(lowDirectoryPath)) {
-            throw new IllegalStateException("Attempting to create tablet dir for tableID " + tableId
-                + " and dir exists when it should not: " + lowDirectoryPath);
-          }
-          if (fs.mkdirs(lowDirectoryPath)) {
-            FileSystem lowDirectoryFs = fs.getVolumeByPath(lowDirectoryPath).getFileSystem();
-            return lowDirectoryPath
-                .makeQualified(lowDirectoryFs.getUri(), lowDirectoryFs.getWorkingDirectory())
-                .toString();
-          }
-        }
-      } catch (IOException e) {
-        log.warn("{}", e.getMessage(), e);
-      }
-
-      log.warn("Failed to create dir for tablet in table {} in volume {} will retry ...", tableId,
-          volume);
-      sleepUninterruptibly(3, TimeUnit.SECONDS);
-
+  private static String createTabletDirectoryName(ServerContext context, Text endRow) {
+    if (endRow == null) {
+      return ServerColumnFamily.DEFAULT_TABLET_DIR_NAME;
+    } else {
+      UniqueNameAllocator namer = context.getUniqueNameAllocator();
+      return Constants.GENERATED_TABLET_DIRECTORY_PREFIX + namer.getNextName();
     }
   }
 
@@ -2860,4 +2816,7 @@ public class Tablet {
     bulkImported.keySet().removeAll(tids);
   }
 
+  public String getDirName() {
+    return dirName;
+  }
 }

core/src/main/java/org/apache/accumulo/core/Constants.java
@@ -81,11 +81,6 @@ public class Constants {
    */
   public static final String ZDELEGATION_TOKEN_KEYS = "/delegation_token_keys";
 
-  /**
-   * Initial tablet directory name for the default tablet in all tables
-   */
-  public static final String DEFAULT_TABLET_LOCATION = "/default_tablet";
-
   public static final String ZTABLE_LOCKS = "/table_locks";
 
   public static final String BULK_PREFIX = "b-";

core/src/main/java/org/apache/accumulo/core/metadata/RootTable.java
@@ -30,12 +30,12 @@ public class RootTable {
   /**
    * DFS location relative to the Accumulo directory
    */
-  public static final String ROOT_TABLET_LOCATION = "/root_tablet";
+  public static final String ROOT_TABLET_DIR_NAME = "root_tablet";
 
   /**
    * ZK path relative to the zookeeper node where the root tablet metadata is stored.
    */
-  public static final String ZROOT_TABLET = ROOT_TABLET_LOCATION;
+  public static final String ZROOT_TABLET = "/" + ROOT_TABLET_DIR_NAME;
 
   /**
    * ZK path relative to the zookeeper node where the root tablet gc candidates are stored.

core/src/main/java/org/apache/accumulo/core/metadata/schema/Ample.java
@@ -193,7 +193,7 @@ public interface Ample {
 
     public TabletMutator putZooLock(ZooLock zooLock);
 
-    public TabletMutator putDir(String dir);
+    public TabletMutator putDirName(String dirName);
 
     public TabletMutator putWal(LogEntry logEntry);
 

core/src/main/java/org/apache/accumulo/core/metadata/schema/MetadataSchema.java
@@ -31,6 +31,8 @@ import org.apache.accumulo.core.util.ColumnFQ;
 import org.apache.accumulo.fate.FateTxId;
 import org.apache.hadoop.io.Text;
 
+import com.google.common.base.Preconditions;
+
 /**
  * Describes the table schema used for metadata tables
  */
@@ -110,6 +112,27 @@ public class MetadataSchema {
        */
       public static final String DIRECTORY_QUAL = "dir";
       public static final ColumnFQ DIRECTORY_COLUMN = new ColumnFQ(NAME, new Text(DIRECTORY_QUAL));
+      /**
+       * Initial tablet directory name for the default tablet in all tables
+       */
+      public static final String DEFAULT_TABLET_DIR_NAME = "default_tablet";
+
+      /**
+       * @return true if dirName is a valid value for the {@link #DIRECTORY_COLUMN} in the metadata
+       *         table. Returns false otherwise.
+       */
+      public static boolean isValidDirCol(String dirName) {
+        return !dirName.contains("/");
+      }
+
+      /**
+       * @throws IllegalArgumentException
+       *           when {@link #isValidDirCol(String)} returns false.
+       */
+      public static void validateDirCol(String dirName) {
+        Preconditions.checkArgument(isValidDirCol(dirName), "Invalid dir name %s", dirName);
+      }
+
       /**
        * Holds the {@link TimeType}
        */

commit 0aa865cf4e9a72db0526a3476a59b5231fd94556

fix #1408 remedy the ITs broken by #1389
Most of the broken ITs had issues with the change from dir uri to dir
name.  However the problem with VolumeChooserFailureIT was different, it
expected the created table Fate op to call the volume chooser.  The
volume chooser is no longer called in create table master code after
called until a tablet was assigned to a tserver.

Some of the failure behavior in VolumeChooserFailureIT is covered by the
PreferredVolumeChooserTest unit test.  However the behavior of ensuring
that when a volume chooser fails that the system does not fall back to
something else is not.   I could not think of a way to ensure the
fallback behavior does not happen, so I deleted the test. The lack of
any fallback behavior was just causing the create table call to hang
forever (it used to throw an exception).  I don't think we want to check
that a create table calls hangs for some minimum time.

server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Tablet.java
@@ -487,7 +487,8 @@ public class Tablet {
     try {
       Collection<Volume> volumes = getTabletServer().getFileSystem().getVolumes();
       for (Volume volume : volumes) {
-        String dirUri = volume.getBasePath() + Constants.HDFS_TABLES_DIR + Path.SEPARATOR + dirName;
+        String dirUri = volume.getBasePath() + Constants.HDFS_TABLES_DIR + Path.SEPARATOR
+            + extent.getTableId() + Path.SEPARATOR + dirName;
 
         for (FileStatus tmp : getTabletServer().getFileSystem()
             .globStatus(new Path(dirUri, "*_tmp"))) {

test/src/main/java/org/apache/accumulo/test/SplitRecoveryIT.java
@@ -109,8 +109,7 @@ public class SplitRecoveryIT extends AccumuloClusterHarness {
 
               KeyExtent extent2 = new KeyExtent(tableId, new Text("b"), null);
               m = extent2.getPrevRowUpdateMutation();
-              TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN.put(m,
-                  new Value("/t2".getBytes()));
+              TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN.put(m, new Value("t2".getBytes()));
               TabletsSection.ServerColumnFamily.TIME_COLUMN.put(m, new Value("M0".getBytes()));
 
               for (Entry<Key,Value> entry : scanner) {

test/src/main/java/org/apache/accumulo/test/VolumeChooserFailureIT.java
@@ -1,167 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.apache.accumulo.test;
-
-import static org.apache.accumulo.test.VolumeChooserIT.PERTABLE_CHOOSER_PROP;
-import static org.apache.accumulo.test.VolumeChooserIT.PREFERRED_CHOOSER_PROP;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-import org.apache.accumulo.core.client.Accumulo;
-import org.apache.accumulo.core.client.AccumuloClient;
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.conf.Property;
-import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
-import org.apache.accumulo.server.fs.PerTableVolumeChooser;
-import org.apache.accumulo.server.fs.PreferredVolumeChooser;
-import org.apache.accumulo.server.fs.VolumeChooserEnvironment.ChooserScope;
-import org.apache.accumulo.test.functional.ConfigurableMacBase;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RawLocalFileSystem;
-import org.apache.hadoop.io.Text;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-public class VolumeChooserFailureIT extends ConfigurableMacBase {
-
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
-
-  private File volDirBase;
-  private Path v1, v2, v3, v4;
-  private static String[] rows = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
-  private String namespace1;
-
-  @Override
-  protected int defaultTimeoutSeconds() {
-    return 60;
-  }
-
-  @Override
-  public void configure(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
-    // Get 2 tablet servers
-    cfg.setNumTservers(2);
-    namespace1 = "ns_" + getUniqueNames(1)[0];
-
-    // Set the general volume chooser to the PerTableVolumeChooser so that different choosers can be
-    // specified
-    Map<String,String> siteConfig = new HashMap<>();
-    siteConfig.put(Property.GENERAL_VOLUME_CHOOSER.getKey(), PerTableVolumeChooser.class.getName());
-    // if a table doesn't have a volume chooser, use the preferred volume chooser
-    siteConfig.put(VolumeChooserIT.PERTABLE_CHOOSER_PROP, PreferredVolumeChooser.class.getName());
-
-    // Set up 4 different volume paths
-    File baseDir = cfg.getDir();
-    volDirBase = new File(baseDir, "volumes");
-    File v1f = new File(volDirBase, "v1");
-    File v2f = new File(volDirBase, "v2");
-    File v3f = new File(volDirBase, "v3");
-    File v4f = new File(volDirBase, "v4");
-    v1 = new Path("file://" + v1f.getAbsolutePath());
-    v2 = new Path("file://" + v2f.getAbsolutePath());
-    v3 = new Path("file://" + v3f.getAbsolutePath());
-    v4 = new Path("file://" + v4f.getAbsolutePath());
-
-    cfg.setSiteConfig(siteConfig);
-
-    siteConfig.put(VolumeChooserIT.getPerTableProp(ChooserScope.LOGGER),
-        PreferredVolumeChooser.class.getName());
-    // do not set preferred volumes
-    cfg.setSiteConfig(siteConfig);
-
-    // Only add volumes 1, 2, and 4 to the list of instance volumes to have one volume that isn't in
-    // the options list when they are choosing
-    cfg.setProperty(Property.INSTANCE_VOLUMES, v1 + "," + v2 + "," + v4);
-    // no not set preferred volumes
-
-    // use raw local file system so walogs sync and flush will work
-    hadoopCoreSite.set("fs.file.impl", RawLocalFileSystem.class.getName());
-
-    super.configure(cfg, hadoopCoreSite);
-
-  }
-
-  public static void addSplits(AccumuloClient accumuloClient, String tableName)
-      throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
-    // Add 10 splits to the table
-    SortedSet<Text> partitions = new TreeSet<>();
-    for (String s : rows)
-      partitions.add(new Text(s));
-    accumuloClient.tableOperations().addSplits(tableName, partitions);
-  }
-
-  // Test that uses one table with 10 split points each. It uses the PreferredVolumeChooser, but no
-  // preferred volume is specified.
-  // This means that the volume chooser will fail and no instance volumes will be assigned.
-  @Test
-  public void missingVolumePreferredVolumeChooser() throws Exception {
-    log.info("Starting missingVolumePreferredVolumeChooser");
-
-    // Create namespace
-    try (AccumuloClient client = Accumulo.newClient().from(getClientProperties()).build()) {
-      client.namespaceOperations().create(namespace1);
-
-      // Set properties on the namespace
-      client.namespaceOperations().setProperty(namespace1, PERTABLE_CHOOSER_PROP,
-          PreferredVolumeChooser.class.getName());
-      // deliberately do not set preferred volumes
-
-      // Create table1 on namespace1 (will fail)
-      String tableName = namespace1 + ".1";
-      thrown.expect(AccumuloException.class);
-      client.tableOperations().create(tableName);
-    }
-  }
-
-  // Test that uses one table with 10 split points each. It uses the PreferredVolumeChooser, but
-  // preferred volume is not an instance volume.
-  // This should fail.
-  @Test
-  public void notInstancePreferredVolumeChooser() throws Exception {
-    log.info("Starting notInstancePreferredVolumeChooser");
-
-    // Create namespace
-    try (AccumuloClient client = Accumulo.newClient().from(getClientProperties()).build()) {
-      client.namespaceOperations().create(namespace1);
-
-      // Set properties on the namespace
-      String propertyName = PERTABLE_CHOOSER_PROP;
-      String volume = PreferredVolumeChooser.class.getName();
-      client.namespaceOperations().setProperty(namespace1, propertyName, volume);
-
-      // set to v3 which is not included in the list of instance volumes, so it should go to the
-      // system default preferred volumes
-      propertyName = PREFERRED_CHOOSER_PROP;
-      volume = v3.toString();
-      client.namespaceOperations().setProperty(namespace1, propertyName, volume);
-
-      // Create table1 on namespace1 (will fail)
-      String tableName = namespace1 + ".1";
-      thrown.expect(AccumuloException.class);
-      client.tableOperations().create(tableName);
-    }
-  }
-
-}

test/src/main/java/org/apache/accumulo/test/functional/CloneTestIT.java
@@ -51,7 +51,6 @@ import org.apache.accumulo.core.metadata.schema.MetadataSchema;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
 import org.apache.accumulo.miniclusterImpl.MiniAccumuloClusterImpl;
-import org.apache.accumulo.server.ServerConstants;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -152,11 +151,10 @@ public class CloneTestIT extends AccumuloClusterHarness {
               MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN
                   .getColumnQualifier(),
               cq);
-          Path tabletDir = new Path(entry.getValue().toString());
-          Path tableDir = tabletDir.getParent();
-          Path tablesDir = tableDir.getParent();
 
-          assertEquals(ServerConstants.TABLE_DIR, tablesDir.getName());
+          String dirName = entry.getValue().toString();
+
+          assertTrue("Bad dir name " + dirName, dirName.matches("[tc]-[0-9a-z]+"));
         } else {
           fail("Got unexpected key-value: " + entry);
           throw new RuntimeException();

test/src/main/java/org/apache/accumulo/test/functional/SplitRecoveryIT.java
@@ -147,9 +147,10 @@ public class SplitRecoveryIT extends ConfigurableMacBase {
     for (int i = 0; i < extents.length; i++) {
       KeyExtent extent = extents[i];
 
+      String dirName = "dir_" + i;
       String tdir =
-          ServerConstants.getTablesDirs(context)[0] + "/" + extent.getTableId() + "/dir_" + i;
-      MetadataTableUtil.addTablet(extent, tdir, context, TimeType.LOGICAL, zl);
+          ServerConstants.getTablesDirs(context)[0] + "/" + extent.getTableId() + "/" + dirName;
+      MetadataTableUtil.addTablet(extent, dirName, context, TimeType.LOGICAL, zl);
       SortedMap<FileRef,DataFileValue> mapFiles = new TreeMap<>();
       mapFiles.put(new FileRef(tdir + "/" + RFile.EXTENSION + "_000_000"),
           new DataFileValue(1000017 + i, 10000 + i));
@@ -205,8 +206,8 @@ public class SplitRecoveryIT extends ConfigurableMacBase {
     if (steps >= 1) {
       Map<Long,List<FileRef>> bulkFiles = getBulkFilesLoaded(context, extent);
 
-      MasterMetadataUtil.addNewTablet(context, low, "/lowDir", instance, lowDatafileSizes,
-          bulkFiles, new MetadataTime(0, TimeType.LOGICAL), -1L, -1L, zl);
+      MasterMetadataUtil.addNewTablet(context, low, "lowDir", instance, lowDatafileSizes, bulkFiles,
+          new MetadataTime(0, TimeType.LOGICAL), -1L, -1L, zl);
     }
     if (steps >= 2) {
       MetadataTableUtil.finishSplit(high, highDatafileSizes, highDatafilesToRemove, context, zl);