3fd5cad92f9b63ac19e4466f3f2d5237b905262c
Christopher Tubbs <ctubbsii@apache.org>
2019-11-15 14:56:24 -0500

    Standardize license headers (#1433)
    * Add license header automation
    
    Configure license-maven-plugin to automatically apply standardized
    formatting for license headers
      https://github.com/mycila/license-maven-plugin
    
    Standard formatter taken directly from:
      https://www.apache.org/legal/src-headers.html#headers
    
    * Regenerate license headers
    
    Regenerate license headers based on configuration in previous commit
    
    * Remove duplicate license headers and fix whitespace
    
    * Remove duplicate license headers that don't conform to the convention
      used by the license-maven-plugin
      (specifically, *.thrift, *.proto, *.css, *.js, *.ftl, *.h, *.c, and *.cc)
      The license-maven-plugin probably didn't replace these, because it
      didn't recognize their indentation style; subsequent executions of the
      plugin do not re-add these
    * Fix license headers in bloomfilter code (remove ASF license, leaving
      original "BSD 3-clause" license header)
    * Format the HostAndPort license header based on the ALv2 appendix for
      copied and modified class from Guava
    * Replace tabs with spaces, and remove trailing spaces in edited C/C++ code
    
    * PR feedback
    
    * Move module-specific mappings of plugin to their respective pom.xml
    * Remove file deleted in master, causing merge-conflict
    * Reorder rat to run after license-maven-plugin
    * Remove rat exclusions from test module for files that don't exist

--- core/src/main/java/org/apache/accumulo/core/bloomfilter/Filter.java
+++ core/src/main/java/org/apache/accumulo/core/bloomfilter/Filter.java
@@ -1,8 +1,6 @@
 /*
- *
  * Copyright (c) 2005, European Commission project OneLab under contract 034819
  * (http://www.one-lab.org)
- *
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following
@@ -29,24 +27,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.apache.accumulo.core.bloomfilter;
 
