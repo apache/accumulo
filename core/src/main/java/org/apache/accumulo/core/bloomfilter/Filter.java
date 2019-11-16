2f4ad2d310b8388d88446f95a6d7e5ce16ff35c0
Christopher Tubbs <ctubbsii@apache.org>
2019-11-15 22:23:07 -0500

    Backport license and typo fixes for bloomfilter code
    * Remove incorrect redundant license headers from bloomfilter code
    * Add exceptions for rat (because it doesn't recognize the style of the
      BSD license header for the bloomfilter code)
    * Backport any spelling typos fixed in bloomfilter code in master branch
      back to 1.9

--- core/src/main/java/org/apache/accumulo/core/bloomfilter/Filter.java
+++ core/src/main/java/org/apache/accumulo/core/bloomfilter/Filter.java
@@ -1,8 +1,6 @@
 /*
- *
  * Copyright (c) 2005, European Commission project OneLab under contract 034819
  * (http://www.one-lab.org)
- *
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following
@@ -29,24 +27,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.apache.accumulo.core.bloomfilter;
 
@@ -126,7 +106,7 @@ public abstract class Filter implements Writable {
   public abstract boolean membershipTest(Key key);
 
   /**
-   * Peforms a logical AND between <i>this</i> filter and a specified filter.
+   * Performs a logical AND between <i>this</i> filter and a specified filter.
    * <p>
    * <b>Invariant</b>: The result is assigned to <i>this</i> filter.
    *
@@ -136,7 +116,7 @@ public abstract class Filter implements Writable {
   public abstract void and(Filter filter);
 
   /**
-   * Peforms a logical OR between <i>this</i> filter and a specified filter.
+   * Performs a logical OR between <i>this</i> filter and a specified filter.
    * <p>
    * <b>Invariant</b>: The result is assigned to <i>this</i> filter.
    *
@@ -146,7 +126,7 @@ public abstract class Filter implements Writable {
   public abstract void or(Filter filter);
 
   /**
-   * Peforms a logical XOR between <i>this</i> filter and a specified filter.
+   * Performs a logical XOR between <i>this</i> filter and a specified filter.
    * <p>
    * <b>Invariant</b>: The result is assigned to <i>this</i> filter.
    *
@@ -188,7 +168,7 @@ public abstract class Filter implements Writable {
       this.nbHash = ver;
       this.hashType = Hash.JENKINS_HASH;
 
-    } else if (ver == VERSION | ver == VERSION + 1) { // Support for directly serialzing the bitset
+    } else if (ver == VERSION | ver == VERSION + 1) { // Support for directly serializing the bitset
       this.nbHash = in.readInt();
       this.hashType = in.readByte();
     } else {
