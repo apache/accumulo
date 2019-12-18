0c478696466c8c078ee2b5eca4701be51bc341ba
Keith Turner <kturner@apache.org>
2019-12-18 13:41:48 -0500

    Remove useless log message

--- server/master/src/main/java/org/apache/accumulo/master/recovery/RecoveryManager.java
+++ server/master/src/main/java/org/apache/accumulo/master/recovery/RecoveryManager.java
@@ -183,7 +183,6 @@ public class RecoveryManager {
         String filename = master.getFileSystem().getFullPath(FileType.WAL, walog).toString();
         String dest =
             RecoveryPath.getRecoveryPath(master.getFileSystem(), new Path(filename)).toString();
-        log.debug("Recovering " + filename + " to " + dest);
 
         boolean sortQueued;
         synchronized (this) {
