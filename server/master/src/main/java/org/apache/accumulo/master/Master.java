828a3219fd4234dfa045c8a63e02285170afda96
Keith Turner <kturner@apache.org>
2020-01-02 15:04:17 -0500

    Temporarily cache existence of recovery wals (#1462)
    For the case where a lot of tablet servers died, the master was
    frequently checking for the existence of a recovery log for each tablet.
    Its very likely that many tablets point to the same recovery logs and
    the existence checks are redundant.  This patch caches the result of
    existence checks for a short period.

--- server/master/src/main/java/org/apache/accumulo/master/Master.java
+++ server/master/src/main/java/org/apache/accumulo/master/Master.java
@@ -178,6 +178,8 @@ public class Master extends AccumuloServerContext
 
   final static int ONE_SECOND = 1000;
   final static long TIME_TO_WAIT_BETWEEN_SCANS = 60 * ONE_SECOND;
+  // made this less than TIME_TO_WAIT_BETWEEN_SCANS, so that the cache is cleared between cycles
+  final static long TIME_TO_CACHE_RECOVERY_WAL_EXISTENCE = (3 * TIME_TO_WAIT_BETWEEN_SCANS) / 4;
   final private static long TIME_BETWEEN_MIGRATION_CLEANUPS = 5 * 60 * ONE_SECOND;
   final static long WAIT_BETWEEN_ERRORS = ONE_SECOND;
   final private static long DEFAULT_WAIT_FOR_WATCHER = 10 * ONE_SECOND;
@@ -1242,7 +1244,7 @@ public class Master extends AccumuloServerContext
 
     getMasterLock(zroot + Constants.ZMASTER_LOCK);
 
-    recoveryManager = new RecoveryManager(this);
+    recoveryManager = new RecoveryManager(this, TIME_TO_CACHE_RECOVERY_WAL_EXISTENCE);
 
     TableManager.getInstance().addObserver(this);
 
