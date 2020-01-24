2fea1a65f31916f327cebf6e7804ad95474cd40b
Keith Turner <kturner@apache.org>
2020-01-24 12:59:23 -0500

    Attempt to document raison d'être

--- core/src/main/java/org/apache/accumulo/core/logging/TabletLogger.java
+++ core/src/main/java/org/apache/accumulo/core/logging/TabletLogger.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.accumulo.core.logging;
 
@@ -29,7 +31,12 @@ import org.apache.accumulo.core.util.HostAndPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// logs messages about a tablets internal state, like its location, set of files, metadata... operations that cut across multiple tablets should go elsewhere
+/**
+ * This class contains source for logs messages about a tablets internal state, like its location,
+ * set of files, metadata.
+ *
+ * @see org.apache.accumulo.core.logging
+ */
 public class TabletLogger {
 
   private static final String PREFIX = Logging.PREFIX + "tablet.";
@@ -37,7 +44,7 @@ public class TabletLogger {
   private static final Logger locLog = LoggerFactory.getLogger(PREFIX + "location");
   private static final Logger fileLog = LoggerFactory.getLogger(PREFIX + "files");
   private static final Logger recoveryLog = LoggerFactory.getLogger(PREFIX + "recovery");
-  private static final Logger walsLog = LoggerFactory.getLogger(PREFIX + "wals");
+  private static final Logger walsLog = LoggerFactory.getLogger(PREFIX + "walogs");
 
   /**
    * A decision was made to assign a tablet to a tablet server process. Accumulo will stick to this
