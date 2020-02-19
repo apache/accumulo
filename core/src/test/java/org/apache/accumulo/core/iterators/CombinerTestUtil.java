6a2b94987548ecbcd2655d0c3016d809877fd94c
Mike Miller <mmiller@apache.org>
2020-02-19 09:17:42 -0500

    Remove log4j from CombinerTest. Fixes #1512

--- core/src/test/java/org/apache/accumulo/core/iterators/CombinerTestUtil.java
+++ core/src/test/java/org/apache/accumulo/core/iterators/CombinerTestUtil.java
@@ -22,4 +22,8 @@ public class CombinerTestUtil {
   public static void clearLogCache() {
     Combiner.loggedMsgCache.invalidateAll();
   }
+
+  public static long cacheSize() {
+    return Combiner.loggedMsgCache.size();
+  }
 }
