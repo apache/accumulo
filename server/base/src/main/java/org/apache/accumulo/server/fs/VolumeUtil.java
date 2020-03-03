105c4f2ef77d396d6e6caf5695368b8f3f6b2779
Jeffrey Zeiberg <jzeiberg@gmail.com>
2020-03-03 11:58:41 -0500

    Fixes #1525 - remove lines added in b67d3f9 in VolumeUtil (#1539)
    * Revert optimization that broke special tablet directory volume code 
    that is still needed in 1.X

--- server/base/src/main/java/org/apache/accumulo/server/fs/VolumeUtil.java
+++ server/base/src/main/java/org/apache/accumulo/server/fs/VolumeUtil.java
@@ -187,8 +187,7 @@ public class VolumeUtil {
       VolumeManager vm, KeyExtent extent, TabletFiles tabletFiles, boolean replicate)
       throws IOException {
     List<Pair<Path,Path>> replacements = ServerConstants.getVolumeReplacements();
-    if (replacements.isEmpty())
-      return tabletFiles;
+
     log.trace("Using volume replacements: " + replacements);
 
     List<LogEntry> logsToRemove = new ArrayList<>();
