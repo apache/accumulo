e90a3713f44d4072bc3570aec44a2b45618cb763
Jeffrey Manno <jeffreymanno@gmail.com>
2020-03-04 13:55:47 -0500

    revert to original change

--- server/base/src/main/java/org/apache/accumulo/server/master/state/MetaDataStateStore.java
+++ server/base/src/main/java/org/apache/accumulo/server/master/state/MetaDataStateStore.java
@@ -67,6 +67,7 @@ class MetaDataStateStore implements TabletStateStore {
         Mutation m = new Mutation(assignment.tablet.getMetadataEntry());
         assignment.server.putLocation(m);
         assignment.server.clearLastLocation(m);
+        assignment.server.putLastLocation(m);
         assignment.server.clearFutureLocation(m);
         SuspendingTServer.clearSuspension(m);
         writer.addMutation(m);
@@ -135,7 +136,6 @@ class MetaDataStateStore implements TabletStateStore {
       for (TabletLocationState tls : tablets) {
         Mutation m = new Mutation(tls.extent.getMetadataEntry());
         if (tls.current != null) {
-          tls.last.putLastLocation(m);
           tls.current.clearLocation(m);
           if (logsForDeadServers != null) {
             List<Path> logs = logsForDeadServers.get(tls.current);
