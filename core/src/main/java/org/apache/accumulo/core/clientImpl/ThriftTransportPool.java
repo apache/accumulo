25981cd92be2a6f20b457e097dd9ca37876f8dce
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2020-03-05 14:14:58 -0500

    Fixes #1542 - Created a randomIndex member of ThriftTransportPool for testing

--- core/src/main/java/org/apache/accumulo/core/clientImpl/ThriftTransportPool.java
+++ core/src/main/java/org/apache/accumulo/core/clientImpl/ThriftTransportPool.java
@@ -19,8 +19,17 @@
 package org.apache.accumulo.core.clientImpl;
 
 import java.security.SecureRandom;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
