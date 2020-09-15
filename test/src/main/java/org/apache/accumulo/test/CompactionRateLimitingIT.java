7dab4afcf500805c0016e611196d3e93d8c733a3
Keith Turner <kturner@apache.org>
2020-09-15 10:28:48 -0400

    Update test/src/main/java/org/apache/accumulo/test/CompactionRateLimitingIT.java
    Co-authored-by: EdColeman <dev1@etcoleman.com>

--- test/src/main/java/org/apache/accumulo/test/CompactionRateLimitingIT.java
+++ test/src/main/java/org/apache/accumulo/test/CompactionRateLimitingIT.java
@@ -104,7 +104,7 @@ public class CompactionRateLimitingIT extends ConfigurableMacBase {
         // (allow for 20% under the maximum expected duration)
         assertTrue(String.format(
             "Expected a compaction rate of no more than %,d bytes/sec, but saw a rate of %,f bytes/sec",
-            (int) 0.8d * RATE, 1000.0 * bytesWritten / duration),
+            (int) (0.8d * RATE), 1000.0 * bytesWritten / duration),
             duration > 1000L * 0.8 * BYTES_TO_WRITE / RATE);
       }
     }
