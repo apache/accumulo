126c53fb1168faa72a1587bf2682663a3b10b12c
cradal <cdschei2@gmail.com>
2020-11-10 14:24:56 -0500

    Issue #1742 - Updated test.

--- core/src/test/java/org/apache/accumulo/core/clientImpl/ScannerBaseTest.java
+++ core/src/test/java/org/apache/accumulo/core/clientImpl/ScannerBaseTest.java
@@ -21,6 +21,7 @@ package org.apache.accumulo.core.clientImpl;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
@@ -32,18 +33,9 @@ import org.apache.accumulo.core.client.ScannerBase;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.hadoop.io.Text;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ScannerBaseTest {
-
-  private ScannerBase s;
-  private Map<Key,Value> scannerMap;
-  private Iterator<Map.Entry<Key,Value>> it;
-  private Key key;
-  private Value val;
-  private BiConsumer<Key,Value> keyValueConsumer;
-  private forEachTester fet;
 
   private static class forEachTester {
 
@@ -60,13 +52,18 @@ public class ScannerBaseTest {
     }
   }
 
-  @Before
-  public void setUp() throws Exception {
-    s = createMock(ScannerBase.class);
-  }
-
   @Test
   public void testScannerBaseForEach() throws Exception {
+
+    ScannerBase s;
+    Map<Key,Value> scannerMap;
+    Iterator<Map.Entry<Key,Value>> it;
+    Key key;
+    Value val;
+    BiConsumer<Key,Value> keyValueConsumer;
+    forEachTester fet;
+
+    s = createMock(ScannerBase.class);
     key = new Key(new Text("a"), new Text("cf1"), new Text("cq1"));
     val = new Value(new Text("v1"));
     scannerMap = new HashMap<>();
@@ -77,41 +74,17 @@ public class ScannerBaseTest {
 
     it = scannerMap.entrySet().iterator();
 
-    expect(s.iterator()).andReturn(it).times(3);
+    expect(s.iterator()).andReturn(it).times(1);
     replay(s);
 
     Map<Key,Value> map = new HashMap<>();
-
-    class MyBiConsumer implements BiConsumer<Key,Value> {
-      @Override
-      public void accept(Key key, Value value) {
-        map.put(key, value);
-      }
-    }
-
-    keyValueConsumer = new MyBiConsumer();
-
-    fet.forEach(keyValueConsumer);
-
+    fet.forEach(map::put);
     // Test the Scanner values put into the map via keyValueConsumer
-
     for (Map.Entry<Key,Value> entry : map.entrySet()) {
       Map.Entry<Key,Value> expectedEntry = s.iterator().next();
-      Key expectedKey = expectedEntry.getKey();
-      Value expectedValue = expectedEntry.getValue();
-
-      String expectedCf = expectedKey.getColumnFamily().toString();
-      String actualCf = entry.getKey().getColumnFamily().toString();
-
-      String expectedCq = expectedKey.getColumnQualifier().toString();
-      String actualCq = entry.getKey().getColumnQualifier().toString();
-
-      String expectedVal = expectedValue.toString();
-      String actualVal = entry.getValue().toString();
-
-      assertEquals(expectedCf, actualCf);
-      assertEquals(expectedCq, actualCq);
-      assertEquals(expectedVal, actualVal);
+      assertEquals(expectedEntry.getKey(), entry.getKey());
+      assertEquals(expectedEntry.getValue(), entry.getValue());
     }
+    verify(s);
   }
 }
