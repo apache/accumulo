5590320ebe6878568b979ca35b9967945bee810a
Ed Coleman etcoleman <edcoleman@apache.org>
2021-04-15 18:29:06 +0000

    added micrometer fate metrics

--- server/manager/src/main/java/org/apache/accumulo/manager/metrics/fate/FateMetrics.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/metrics/fate/FateMetrics.java
@@ -18,15 +18,17 @@
  */
 package org.apache.accumulo.manager.metrics.fate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import org.apache.accumulo.core.Constants;
+import org.apache.accumulo.core.util.threads.ThreadPools;
 import org.apache.accumulo.fate.ReadOnlyTStore;
 import org.apache.accumulo.fate.ZooStore;
 import org.apache.accumulo.manager.metrics.ManagerMetrics;
@@ -38,25 +40,26 @@ import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.micrometer.core.instrument.MultiGauge;
+import io.micrometer.core.instrument.Tags;
+
 public class FateMetrics extends ManagerMetrics {
 
   private static final Logger log = LoggerFactory.getLogger(FateMetrics.class);
 
   // limit calls to update fate counters to guard against hammering zookeeper.
-  private static final long DEFAULT_MIN_REFRESH_DELAY = TimeUnit.SECONDS.toMillis(30);
+  private static final long DEFAULT_MIN_REFRESH_DELAY = TimeUnit.SECONDS.toMillis(5);
   private static final String FATE_TX_STATE_METRIC_PREFIX = "FateTxState_";
   private static final String FATE_OP_TYPE_METRIC_PREFIX = "FateTxOpType_";
 
   private final ServerContext context;
   private final ReadOnlyTStore<FateMetrics> zooStore;
   private final String fateRootPath;
-  private final AtomicLong mmCurrentFateOps;
+  private final MicrometerFormatter mmFormatter;
   private final FateHadoopMetrics hadoopMetrics;
-  private long minimumRefreshDelay;
-  private AtomicReference<FateMetricValues> metricValues;
+  private final long minimumRefreshDelay;
+  private final AtomicReference<FateMetricValues> metricValues;
   private volatile long lastUpdate = 0;
-
-  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
   public FateMetrics(final ServerContext context, final long minimumRefreshDelay,
       MicrometerMetricsFactory micrometerMetrics) {
@@ -65,7 +68,11 @@ public class FateMetrics extends ManagerMetrics {
     this.context = context;
     fateRootPath = context.getZooKeeperRoot() + Constants.ZFATE;
 
-    mmCurrentFateOps = micrometerMetrics.getRegistry().gauge("current.fateOps", new AtomicLong(0));
+    MultiGauge mmFateMultiGauge =
+        MultiGauge.builder("fate.status").description("Fate transaction and operation counts")
+            .baseUnit("Counts").register(micrometerMetrics.getRegistry());
+
+    mmFormatter = new MicrometerFormatter(mmFateMultiGauge);
 
     try {
 
@@ -85,16 +92,19 @@ public class FateMetrics extends ManagerMetrics {
     metricValues =
         new AtomicReference<>(FateMetricValues.getFromZooKeeper(context, fateRootPath, zooStore));
 
+    mmFormatter.update(metricValues.get());
+
     // get fate status is read only operation - no reason to be nice on shutdown.
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> scheduler.shutdownNow()));
+    ScheduledExecutorService scheduler =
+        ThreadPools.createScheduledExecutorService(1, "fateMetricsPoller", false);
+    Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdownNow));
 
     scheduler.scheduleAtFixedRate(() -> {
       try {
         var fates = FateMetricValues.getFromZooKeeper(context, fateRootPath, zooStore);
         metricValues.set(fates);
 
-        // TODO this is a stub - replace. Maybe with multiguage?
-        mmCurrentFateOps.set(fates.getCurrentFateOps());
+        mmFormatter.update(metricValues.get());
 
       } catch (Exception ex) {
         log.info("Failed to update fate metrics due to exception", ex);
@@ -110,6 +120,7 @@ public class FateMetrics extends ManagerMetrics {
   public void overrideRefresh() {
     var fates = FateMetricValues.getFromZooKeeper(context, fateRootPath, zooStore);
     metricValues.set(fates);
+    mmFormatter.update(fates);
   }
 
   @Override
@@ -125,8 +136,62 @@ public class FateMetrics extends ManagerMetrics {
     }
   }
 
-  private String metricNameHelper(final String prefix, final String name) {
-    return prefix + name;
+  private static class MicrometerFormatter {
+
+    final MultiGauge mmFateMultiGauge;
+
+    // ops are dynamic from a finite set.
+    private final Map<String,Long> fateOpCounts = new TreeMap<>();
+
+    public MicrometerFormatter(final MultiGauge mmFateMultiGauge) {
+
+      this.mmFateMultiGauge = mmFateMultiGauge;
+
+      for (ReadOnlyTStore.TStatus t : ReadOnlyTStore.TStatus.values()) {
+        // types are from a fixed set
+        Map<String,Long> fateTypeCounts = new TreeMap<>();
+        fateTypeCounts.put(t.name(), 0L);
+      }
+
+    }
+
+    void update(final FateMetricValues values) {
+
+      List<MMWrapper> results = new ArrayList<>(5 + values.getTxStateCounters().size()
+          + values.getOpTypeCounters().size() + fateOpCounts.size());
+
+      // clear current ops, keep keys.
+      fateOpCounts.replaceAll((key, value) -> 0L);
+
+      results.add(new MMWrapper("currentFateOps", values.getCurrentFateOps()));
+
+      results.add(new MMWrapper("zkChildFateOpsTotal;", values.getZkFateChildOpsTotal()));
+      results.add(new MMWrapper("zkConnectionErrorsTotal", values.getZkConnectionErrors()));
+      values.getTxStateCounters()
+          .forEach((n, v) -> results.add(new MMWrapper(FATE_TX_STATE_METRIC_PREFIX + n, v)));
+
+      fateOpCounts.putAll(values.getOpTypeCounters());
+
+      fateOpCounts.forEach((n, v) -> results.add(new MMWrapper(FATE_OP_TYPE_METRIC_PREFIX + n, v)));
+
+      mmFateMultiGauge.register(results.stream().map(MMWrapper::toRow).collect(Collectors.toList()),
+          true);
+
+    }
+  }
+
+  private static class MMWrapper {
+    private final String name;
+    private final Long value;
+
+    public MMWrapper(final String name, final Long value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    public MultiGauge.Row<MMWrapper> toRow() {
+      return MultiGauge.Row.of(Tags.of("status", name), this, v -> this.value);
+    }
   }
 
   private static class FateHadoopMetrics {
