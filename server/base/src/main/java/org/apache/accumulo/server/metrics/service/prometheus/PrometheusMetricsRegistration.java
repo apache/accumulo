4d39c59c5b06da624e8b077149dea7696b0de564
Ed Coleman <dev1@etcoleman.com>
2021-04-18 11:28:27 -0400

    inital port of prometheus exporter (non-functional)

--- server/base/src/main/java/org/apache/accumulo/server/metrics/service/prometheus/PrometheusMetricsRegistration.java
+++ server/base/src/main/java/org/apache/accumulo/server/metrics/service/prometheus/PrometheusMetricsRegistration.java
@@ -19,9 +19,14 @@
 package org.apache.accumulo.server.metrics.service.prometheus;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.accumulo.server.ServerContext;
 import org.apache.accumulo.server.metrics.service.MetricsRegistrationService;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,11 +39,60 @@ public class PrometheusMetricsRegistration implements MetricsRegistrationService
 
   private static final Logger log = LoggerFactory.getLogger(PrometheusMetricsRegistration.class);
 
+  private static Exporter exporter;
+
+  public static Exporter getExporter() {
+    return exporter;
+  }
+
   @Override
   public void register(final ServerContext context, final String serviceName,
       final Map<String,String> properties, final CompositeMeterRegistry registry) {
     log.info("Loading metrics service name: {}, to {}, with props: {}", serviceName, registry,
         properties);
     log.warn("ZZZ - Init prometheus metrics");
+    exporter = Exporter.getInstance();
+  }
+
+  private static class MetricsHttpServer implements AutoCloseable {
+
+    private final Server jetty;
+
+    public MetricsHttpServer(final int port) {
+
+      jetty = new Server();
+
+      ServerConnector connector = new ServerConnector(jetty);
+      connector.setPort(port);
+      jetty.setConnectors(new Connector[] {connector});
+
+      ServletHandler servletHandler = new ServletHandler();
+      jetty.setHandler(servletHandler);
+
+      servletHandler.addServletWithMapping(HealthCheckServlet.class, "/status");
+      servletHandler.addServletWithMapping(PrometheusExporterServlet.class, "/metrics");
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+  }
+
+  static class Exporter {
+    private static Exporter _instance;
+
+    private Exporter() {}
+
+    public static synchronized Exporter getInstance() {
+      if (Objects.isNull(_instance)) {
+        _instance = new Exporter();
+      }
+      return _instance;
+    }
+
+    public String prometheusScrape() {
+      return "return something";
+    }
   }
 }
