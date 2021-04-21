98b4dc7e2238cfb5d607a5eb17a734c896965b34
Mike Miller <mmiller@apache.org>
2021-04-21 07:54:30 -0400

    Format

--- core/src/test/java/org/apache/accumulo/core/constraints/DeprecatedConstraintExtendTest.java
+++ core/src/test/java/org/apache/accumulo/core/constraints/DeprecatedConstraintExtendTest.java
@@ -19,7 +19,6 @@
 package org.apache.accumulo.core.constraints;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.Collections;
 import java.util.List;
