apache/accumulo

commit f2b0936029fcaadb925bd9c229403c82f0283211

Improve client table exceptions (#2195)
Improve client-side table exception handling by providing tableNames
from the client to avoid looking them up later to format an exception
message.

* Add TableOfflineException constructor to provide a formatted message
  without requiring a ClientContext to look up the name or id
* Reuse code to check for TableNotFoundException, TableDeletedException,
  and TableOfflineException using convenience methods in ClientContext
* Pass along tableName alongside tableId, in some internal client-side
  code, for use in exception messages, if needed later
* Make check for requiring that a table not be offline explicit, rather
  than entangled with the tableId lookup in ClientContext
* Update ConnectorImpl to proxy to ClientContext in a few cases where
  it previously wasn't, to ensure consistent behavior and argument
  validations
* Use TableId from Thrift exception to avoid another client-side lookup
  when a fate operation throws an exception because the table is offline
* Simplify private addSplit method by passing SplitEnv, rather than
  sending env.tableId and env.tableName as separate parameters
* Remove some redundant tableName validations when they are already
  validated by context.getTableId
* Simplify and inline a private updateAuthorizationsFailures method in
  TabletServerBatchWriter that was used to gather a set of
  authorizations failures whose tables were then checked for being
  deleted
* Fix a few TableState enum comparisons from `.equals()` to `==`

core/src/main/java/org/apache/accumulo/core/clientImpl/TableOperationsImpl.java
@@ -72,7 +72,6 @@ import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.NamespaceExistsException;
 import org.apache.accumulo.core.client.NamespaceNotFoundException;
 import org.apache.accumulo.core.client.Scanner;
-import org.apache.accumulo.core.client.TableDeletedException;
 import org.apache.accumulo.core.client.TableExistsException;
 import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.TableOfflineException;
@@ -395,7 +394,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
           throw new NamespaceNotFoundException(e);
         case OFFLINE:
           throw new TableOfflineException(
-              Tables.getTableOfflineMsg(context, Tables.getTableId(context, tableOrNamespaceName)));
+              e.getTableId() == null ? null : TableId.of(e.getTableId()), tableOrNamespaceName);
         case BULK_CONCURRENT_MERGE:
           throw new AccumuloBulkMergeException(e);
         default:
@@ -416,11 +415,11 @@ public class TableOperationsImpl extends TableOperationsHelper {
   }
 
   private static class SplitEnv {
-    private String tableName;
-    private TableId tableId;
-    private ExecutorService executor;
-    private CountDownLatch latch;
-    private AtomicReference<Exception> exception;
+    private final String tableName;
+    private final TableId tableId;
+    private final ExecutorService executor;
+    private final CountDownLatch latch;
+    private final AtomicReference<Exception> exception;
 
     SplitEnv(String tableName, TableId tableId, ExecutorService executor, CountDownLatch latch,
         AtomicReference<Exception> exception) {
@@ -449,7 +448,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
           return;
 
         if (splits.size() <= 2) {
-          addSplits(env.tableName, new TreeSet<>(splits), env.tableId);
+          addSplits(env, new TreeSet<>(splits));
           splits.forEach(s -> env.latch.countDown());
           return;
         }
@@ -458,7 +457,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
 
         // split the middle split point to ensure that child task split
         // different tablets and can therefore run in parallel
-        addSplits(env.tableName, new TreeSet<>(splits.subList(mid, mid + 1)), env.tableId);
+        addSplits(env, new TreeSet<>(splits.subList(mid, mid + 1)));
         env.latch.countDown();
 
         env.executor.execute(new SplitTask(env, splits.subList(0, mid)));
@@ -504,7 +503,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
           } else if (excep instanceof TableOfflineException) {
             log.debug("TableOfflineException occurred in background thread. Throwing new exception",
                 excep);
-            throw new TableOfflineException(Tables.getTableOfflineMsg(context, tableId));
+            throw new TableOfflineException(tableId, tableName);
           } else if (excep instanceof AccumuloSecurityException) {
             // base == background accumulo security exception
             AccumuloSecurityException base = (AccumuloSecurityException) excep;
@@ -526,12 +525,10 @@ public class TableOperationsImpl extends TableOperationsHelper {
     }
   }
 
-  private void addSplits(String tableName, SortedSet<Text> partitionKeys, TableId tableId)
-      throws AccumuloException, AccumuloSecurityException, TableNotFoundException,
-      AccumuloServerException {
-    EXISTING_TABLE_NAME.validate(tableName);
+  private void addSplits(SplitEnv env, SortedSet<Text> partitionKeys) throws AccumuloException,
+      AccumuloSecurityException, TableNotFoundException, AccumuloServerException {
 
-    TabletLocator tabLocator = TabletLocator.getLocator(context, tableId);
+    TabletLocator tabLocator = TabletLocator.getLocator(context, env.tableId);
     for (Text split : partitionKeys) {
       boolean successful = false;
       int attempt = 0;
@@ -547,10 +544,8 @@ public class TableOperationsImpl extends TableOperationsHelper {
         TabletLocation tl = tabLocator.locateTablet(context, split, false, false);
 
         if (tl == null) {
-          if (!Tables.exists(context, tableId))
-            throw new TableNotFoundException(tableId.canonical(), tableName, null);
-          else if (Tables.getTableState(context, tableId) == TableState.OFFLINE)
-            throw new TableOfflineException(Tables.getTableOfflineMsg(context, tableId));
+          context.requireTableExists(env.tableId, env.tableName);
+          context.requireNotOffline(env.tableId, env.tableName);
           continue;
         }
 
@@ -591,15 +586,14 @@ public class TableOperationsImpl extends TableOperationsHelper {
           continue;
         } catch (ThriftSecurityException e) {
           Tables.clearCache(context);
-          if (!Tables.exists(context, tableId))
-            throw new TableNotFoundException(tableId.canonical(), tableName, null);
+          context.requireTableExists(env.tableId, env.tableName);
           throw new AccumuloSecurityException(e.user, e.code, e);
         } catch (NotServingTabletException e) {
           // Do not silently spin when we repeatedly fail to get the location for a tablet
           locationFailures++;
           if (locationFailures == 5 || locationFailures % 50 == 0) {
             log.warn("Having difficulty locating hosting tabletserver for split {} on table {}."
-                + " Seen {} failures.", split, tableName, locationFailures);
+                + " Seen {} failures.", split, env.tableName, locationFailures);
           }
 
           tabLocator.invalidateCache(tl.tablet_extent);
@@ -661,9 +655,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
 
   private List<Text> _listSplits(String tableName)
       throws TableNotFoundException, AccumuloSecurityException {
-    EXISTING_TABLE_NAME.validate(tableName);
-
-    TableId tableId = Tables.getTableId(context, tableName);
+    TableId tableId = context.getTableId(tableName);
     TreeMap<KeyExtent,String> tabletLocations = new TreeMap<>();
     while (true) {
       try {
@@ -674,9 +666,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
       } catch (AccumuloSecurityException ase) {
         throw ase;
       } catch (Exception e) {
-        if (!Tables.exists(context, tableId)) {
-          throw new TableNotFoundException(tableId.canonical(), tableName, null);
-        }
+        context.requireTableExists(tableId, tableName);
 
         if (e instanceof RuntimeException && e.getCause() instanceof AccumuloSecurityException) {
           throw (AccumuloSecurityException) e.getCause();
@@ -751,10 +741,9 @@ public class TableOperationsImpl extends TableOperationsHelper {
   public void clone(String srcTableName, String newTableName, CloneConfiguration config)
       throws AccumuloSecurityException, TableNotFoundException, AccumuloException,
       TableExistsException {
-    EXISTING_TABLE_NAME.validate(srcTableName);
     NEW_TABLE_NAME.validate(newTableName);
 
-    TableId srcTableId = Tables.getTableId(context, srcTableName);
+    TableId srcTableId = context.getTableId(srcTableName);
 
     if (config.isFlush())
       _flush(srcTableId, null, null, true);
@@ -811,9 +800,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
   @Override
   public void flush(String tableName, Text start, Text end, boolean wait)
       throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
-    EXISTING_TABLE_NAME.validate(tableName);
-    TableId tableId = Tables.getTableId(context, tableName);
-    _flush(tableId, start, end, wait);
+    _flush(context.getTableId(tableName), start, end, wait);
   }
 
   @Override
@@ -1148,10 +1135,8 @@ public class TableOperationsImpl extends TableOperationsHelper {
     // tablets... so clear it
     tl.invalidateCache();
     while (!tl.binRanges(context, Collections.singletonList(range), binnedRanges).isEmpty()) {
-      if (!Tables.exists(context, tableId))
-        throw new TableDeletedException(tableId.canonical());
-      if (Tables.getTableState(context, tableId) == TableState.OFFLINE)
-        throw new TableOfflineException(Tables.getTableOfflineMsg(context, tableId));
+      context.requireNotDeleted(tableId);
+      context.requireNotOffline(tableId, tableName);
 
       log.warn("Unable to locate bins for specified range. Retrying.");
       // sleep randomly between 100 and 200ms
@@ -1257,8 +1242,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
         Tables.clearCache(context);
         TableState currentState = Tables.getTableState(context, tableId);
         if (currentState != expectedState) {
-          if (!Tables.exists(context, tableId))
-            throw new TableDeletedException(tableId.canonical());
+          context.requireNotDeleted(tableId);
           if (currentState == TableState.DELETING)
             throw new TableNotFoundException(tableId.canonical(), "", "Table is being deleted.");
           throw new AccumuloException("Unexpected table state " + tableId + " "
@@ -1832,20 +1816,14 @@ public class TableOperationsImpl extends TableOperationsHelper {
         .logInterval(3, TimeUnit.MINUTES).createRetry();
 
     while (!locator.binRanges(context, rangeList, binnedRanges).isEmpty()) {
-
-      if (!Tables.exists(context, tableId))
-        throw new TableNotFoundException(tableId.canonical(), tableName, null);
-      if (Tables.getTableState(context, tableId) == TableState.OFFLINE)
-        throw new TableOfflineException(Tables.getTableOfflineMsg(context, tableId));
-
+      context.requireTableExists(tableId, tableName);
+      context.requireNotOffline(tableId, tableName);
       binnedRanges.clear();
-
       try {
         retry.waitForNextAttempt();
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
-
       locator.invalidateCache();
     }
 
@@ -1854,7 +1832,14 @@ public class TableOperationsImpl extends TableOperationsHelper {
 
   @Override
   public SummaryRetriever summaries(String tableName) {
-    EXISTING_TABLE_NAME.validate(tableName);
+    TableId tableId;
+    try {
+      tableId = context.getTableId(tableName);
+    } catch (TableNotFoundException e) {
+      // this has to be a runtime exception, because TableNotFoundException wasn't put on the
+      // interface in 2.0 and adding it now would break the API contract
+      throw new IllegalArgumentException(e);
+    }
 
     return new SummaryRetriever() {
       private Text startRow = null;
@@ -1882,9 +1867,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
       @Override
       public List<Summary> retrieve()
           throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
-        TableId tableId = Tables.getTableId(context, tableName);
-        if (Tables.getTableState(context, tableId) == TableState.OFFLINE)
-          throw new TableOfflineException(Tables.getTableOfflineMsg(context, tableId));
+        context.requireNotOffline(tableId, tableName);
 
         TRowRange range =
             new TRowRange(TextUtil.getByteBuffer(startRow), TextUtil.getByteBuffer(endRow));

core/src/main/java/org/apache/accumulo/core/client/TableOfflineException.java
@@ -18,6 +18,8 @@
  */
 package org.apache.accumulo.core.client;
 
+import org.apache.accumulo.core.data.TableId;
+
 public class TableOfflineException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
@@ -35,6 +37,17 @@ public class TableOfflineException extends RuntimeException {
    */
   public TableOfflineException(String msg) {
     super(msg);
+  }
+
+  /**
+   * @since 2.1.0
+   */
+  public TableOfflineException(TableId tableId, String tableName) {
+    // @formatter:off
+    super(String.format("Table %s (%s) is offline",
+        tableName == null ? "<unknown table>" : tableName,
+        tableId == null ? "<unknown id>" : tableId));
+    // @formatter:on
   }
 
   /**

core/src/main/java/org/apache/accumulo/core/client/mapred/AbstractInputFormat.java
@@ -41,9 +41,7 @@ import org.apache.accumulo.core.client.IsolatedScanner;
 import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.ScannerBase;
-import org.apache.accumulo.core.client.TableDeletedException;
 import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.client.TableOfflineException;
 import org.apache.accumulo.core.client.admin.DelegationTokenConfig;
 import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.sample.SamplerConfiguration;
@@ -64,7 +62,6 @@ import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.dataImpl.KeyExtent;
-import org.apache.accumulo.core.manager.state.tables.TableState;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.util.Pair;
 import org.apache.hadoop.io.Text;
@@ -698,11 +695,8 @@ public abstract class AbstractInputFormat<K,V> implements InputFormat<K,V> {
           tl.invalidateCache();
 
           while (!tl.binRanges(client, ranges, binnedRanges).isEmpty()) {
-            String tableIdStr = tableId.canonical();
-            if (!Tables.exists(client, tableId))
-              throw new TableDeletedException(tableIdStr);
-            if (Tables.getTableState(client, tableId) == TableState.OFFLINE)
-              throw new TableOfflineException(Tables.getTableOfflineMsg(client, tableId));
+            client.requireNotDeleted(tableId);
+            client.requireNotOffline(tableId, tableName);
             binnedRanges.clear();
             log.warn("Unable to locate bins for specified ranges. Retrying.");
             // sleep randomly between 100 and 200 ms

core/src/main/java/org/apache/accumulo/core/client/mapreduce/AbstractInputFormat.java
@@ -41,9 +41,7 @@ import org.apache.accumulo.core.client.IsolatedScanner;
 import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.ScannerBase;
-import org.apache.accumulo.core.client.TableDeletedException;
 import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.client.TableOfflineException;
 import org.apache.accumulo.core.client.admin.DelegationTokenConfig;
 import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.sample.SamplerConfiguration;
@@ -64,7 +62,6 @@ import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.dataImpl.KeyExtent;
-import org.apache.accumulo.core.manager.state.tables.TableState;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.util.Pair;
 import org.apache.hadoop.io.Text;
@@ -729,11 +726,8 @@ public abstract class AbstractInputFormat<K,V> extends InputFormat<K,V> {
           tl.invalidateCache();
 
           while (!tl.binRanges(client, ranges, binnedRanges).isEmpty()) {
-            String tableIdStr = tableId.canonical();
-            if (!Tables.exists(client, tableId))
-              throw new TableDeletedException(tableIdStr);
-            if (Tables.getTableState(client, tableId) == TableState.OFFLINE)
-              throw new TableOfflineException(Tables.getTableOfflineMsg(client, tableId));
+            client.requireNotDeleted(tableId);
+            client.requireNotOffline(tableId, tableName);
             binnedRanges.clear();
             log.warn("Unable to locate bins for specified ranges. Retrying.");
             // sleep randomly between 100 and 200 ms

commit 7d6fd926c316d40494242a8dbd7f15556456d228

Fix and improve SummaryIT
Fix SummaryIT, broken by #2195; restore late table existence checks, so
table doesn't need to exist until you call retrieve() on the
SummaryRetriever

Improve SummaryIT by making it share a cluster, so it runs faster, and
by using assertThrows() instead of try-catch blocks and fail()

core/src/main/java/org/apache/accumulo/core/clientImpl/TableOperationsImpl.java
@@ -1832,14 +1832,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
 
   @Override
   public SummaryRetriever summaries(String tableName) {
-    TableId tableId;
-    try {
-      tableId = context.getTableId(tableName);
-    } catch (TableNotFoundException e) {
-      // this has to be a runtime exception, because TableNotFoundException wasn't put on the
-      // interface in 2.0 and adding it now would break the API contract
-      throw new IllegalArgumentException(e);
-    }
+    EXISTING_TABLE_NAME.validate(tableName);
 
     return new SummaryRetriever() {
       private Text startRow = null;
@@ -1867,6 +1860,7 @@ public class TableOperationsImpl extends TableOperationsHelper {
       @Override
       public List<Summary> retrieve()
           throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
+        TableId tableId = context.getTableId(tableName);
         context.requireNotOffline(tableId, tableName);
 
         TRowRange range =

test/src/main/java/org/apache/accumulo/test/functional/SummaryIT.java
@@ -31,6 +31,7 @@ import static org.apache.accumulo.test.functional.BasicSummarizer.MAX_TIMESTAMP_
 import static org.apache.accumulo.test.functional.BasicSummarizer.MIN_TIMESTAMP_STAT;
 import static org.apache.accumulo.test.functional.BasicSummarizer.TOTAL_STAT;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -67,6 +68,7 @@ import org.apache.accumulo.core.client.TableOfflineException;
 import org.apache.accumulo.core.client.admin.CompactionConfig;
 import org.apache.accumulo.core.client.admin.NewTableConfiguration;
 import org.apache.accumulo.core.client.admin.PluginConfig;
+import org.apache.accumulo.core.client.admin.SummaryRetriever;
 import org.apache.accumulo.core.client.admin.compaction.CompactionSelector;
 import org.apache.accumulo.core.client.security.SecurityErrorCode;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
@@ -86,14 +88,26 @@ import org.apache.accumulo.core.iterators.Filter;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.security.TablePermission;
 import org.apache.accumulo.fate.util.UtilWaitThread;
-import org.apache.accumulo.harness.AccumuloClusterHarness;
+import org.apache.accumulo.harness.SharedMiniClusterBase;
 import org.apache.hadoop.io.Text;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
-public class SummaryIT extends AccumuloClusterHarness {
+public class SummaryIT extends SharedMiniClusterBase {
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    SharedMiniClusterBase.startMiniCluster();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    SharedMiniClusterBase.stopMiniCluster();
+  }
 
   private LongSummaryStatistics getTimestampStats(final String table, AccumuloClient c)
       throws TableNotFoundException {
@@ -380,11 +394,9 @@ public class SummaryIT extends AccumuloClusterHarness {
       checkSummary(summaries, sc2, "len=14", 100_000L);
 
       // Ensure a bad regex fails fast.
-      try {
-        summaries = c.tableOperations().summaries(table)
-            .withMatchingConfiguration(".*KeySizeSummarizer {maxLen=256}.*").retrieve();
-        fail("Bad regex should have caused exception");
-      } catch (PatternSyntaxException e) {}
+      assertThrows("Bad regex should have caused exception", PatternSyntaxException.class,
+          () -> c.tableOperations().summaries(table)
+              .withMatchingConfiguration(".*KeySizeSummarizer {maxLen=256}.*").retrieve());
     }
   }
 
@@ -524,7 +536,7 @@ public class SummaryIT extends AccumuloClusterHarness {
     compactionTest(compactConfig);
   }
 
-  public void compactionTest(CompactionConfig compactConfig) throws Exception {
+  private void compactionTest(CompactionConfig compactConfig) throws Exception {
     final String table = getUniqueNames(1)[0];
     try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
       NewTableConfiguration ntc = new NewTableConfiguration();
@@ -606,11 +618,8 @@ public class SummaryIT extends AccumuloClusterHarness {
       }
 
       c.tableOperations().flush(table, null, null, true);
-      try {
-        c.tableOperations().summaries(table).retrieve();
-        fail("Expected server side failure and did not see it");
-      } catch (AccumuloServerException ase) {}
-
+      assertThrows("Expected server side failure and did not see it", AccumuloServerException.class,
+          () -> c.tableOperations().summaries(table).retrieve());
     }
   }
 
@@ -636,12 +645,11 @@ public class SummaryIT extends AccumuloClusterHarness {
 
       try (AccumuloClient c2 =
           Accumulo.newClient().from(c.properties()).as("user1", passTok).build()) {
-        try {
-          c2.tableOperations().summaries(table).retrieve();
-          fail("Expected operation to fail because user does not have permssion to get summaries");
-        } catch (AccumuloSecurityException ase) {
-          assertEquals(SecurityErrorCode.PERMISSION_DENIED, ase.getSecurityErrorCode());
-        }
+        var e = assertThrows(
+            "Expected operation to fail because user does not have permssion to get summaries",
+            AccumuloSecurityException.class,
+            () -> c2.tableOperations().summaries(table).retrieve());
+        assertEquals(SecurityErrorCode.PERMISSION_DENIED, e.getSecurityErrorCode());
 
         c.securityOperations().grantTablePermission("user1", table, TablePermission.GET_SUMMARIES);
 
@@ -826,58 +834,42 @@ public class SummaryIT extends AccumuloClusterHarness {
 
   @Test
   public void testExceptions() throws Exception {
+    String testTableName = getUniqueNames(1)[0];
     try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
 
-      try {
-        c.tableOperations().summaries("foo").retrieve();
-        fail();
-      } catch (TableNotFoundException e) {}
-
-      try {
-        c.tableOperations().addSummarizers("foo",
-            SummarizerConfiguration.builder(VisibilitySummarizer.class).build());
-        fail();
-      } catch (TableNotFoundException e) {}
-
-      try {
-        c.tableOperations().listSummarizers("foo");
-        fail();
-      } catch (TableNotFoundException e) {}
-
-      try {
-        c.tableOperations().removeSummarizers("foo", sc -> true);
-        fail();
-      } catch (TableNotFoundException e) {}
+      SummaryRetriever summaryRetriever = c.tableOperations().summaries(testTableName);
+      assertThrows(TableNotFoundException.class, () -> summaryRetriever.retrieve());
+      var summarizerConf = SummarizerConfiguration.builder(VisibilitySummarizer.class).build();
+      assertThrows(TableNotFoundException.class,
+          () -> c.tableOperations().addSummarizers(testTableName, summarizerConf));
+      assertThrows(TableNotFoundException.class,
+          () -> c.tableOperations().listSummarizers(testTableName));
+      assertThrows(TableNotFoundException.class,
+          () -> c.tableOperations().removeSummarizers(testTableName, sc -> true));
 
       SummarizerConfiguration sc1 =
           SummarizerConfiguration.builder(FamilySummarizer.class).setPropertyId("p1").build();
       SummarizerConfiguration sc2 =
           SummarizerConfiguration.builder(VisibilitySummarizer.class).setPropertyId("p1").build();
 
-      c.tableOperations().create("foo");
-      c.tableOperations().addSummarizers("foo", sc1);
-      c.tableOperations().addSummarizers("foo", sc1);
-      try {
-        // adding second summarizer with same id should fail
-        c.tableOperations().addSummarizers("foo", sc2);
-        fail();
-      } catch (IllegalArgumentException e) {}
+      c.tableOperations().create(testTableName);
+      c.tableOperations().addSummarizers(testTableName, sc1);
+      c.tableOperations().addSummarizers(testTableName, sc1);
+      assertThrows("adding second summarizer with same id should fail",
+          IllegalArgumentException.class,
+          () -> c.tableOperations().addSummarizers(testTableName, sc2));
 
-      c.tableOperations().removeSummarizers("foo", sc -> true);
-      assertEquals(0, c.tableOperations().listSummarizers("foo").size());
+      c.tableOperations().removeSummarizers(testTableName, sc -> true);
+      assertEquals(0, c.tableOperations().listSummarizers(testTableName).size());
 
-      try {
-        // adding two summarizers at the same time with same id should fail
-        c.tableOperations().addSummarizers("foo", sc1, sc2);
-        fail();
-      } catch (IllegalArgumentException e) {}
-      assertEquals(0, c.tableOperations().listSummarizers("foo").size());
+      assertThrows("adding two summarizers at the same time with same id should fail",
+          IllegalArgumentException.class,
+          () -> c.tableOperations().addSummarizers(testTableName, sc1, sc2));
+      assertEquals(0, c.tableOperations().listSummarizers(testTableName).size());
 
-      c.tableOperations().offline("foo", true);
-      try {
-        c.tableOperations().summaries("foo").retrieve();
-        fail();
-      } catch (TableOfflineException e) {}
+      c.tableOperations().offline(testTableName, true);
+      assertThrows(TableOfflineException.class,
+          () -> c.tableOperations().summaries(testTableName).retrieve());
     }
   }
 