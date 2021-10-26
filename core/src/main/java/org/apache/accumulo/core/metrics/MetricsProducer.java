79b60c8735568104838bb6da17c9a17dc24977d2
Dave Marion <dlmarion@apache.org>
2021-10-26 12:05:43 +0000

    Removed modifiers from MetricsProducer interface constants

--- core/src/main/java/org/apache/accumulo/core/metrics/MetricsProducer.java
+++ core/src/main/java/org/apache/accumulo/core/metrics/MetricsProducer.java
@@ -500,87 +500,78 @@ import io.micrometer.core.instrument.MeterRegistry;
  */
 public interface MetricsProducer {
 
-  public static final Logger LOG = LoggerFactory.getLogger(MetricsProducer.class);
+  Logger LOG = LoggerFactory.getLogger(MetricsProducer.class);
 
-  public static final String METRICS_FATE_PREFIX = "accumulo.fate.";
-  public static final String METRICS_FATE_CURRENT_OPS = METRICS_FATE_PREFIX + "ops.current";
-  public static final String METRICS_FATE_TOTAL_OPS = METRICS_FATE_PREFIX + "ops.total";
-  public static final String METRICS_FATE_ERRORS = METRICS_FATE_PREFIX + "errors";
-  public static final String METRICS_FATE_TX = METRICS_FATE_PREFIX + "tx";
+  String METRICS_FATE_PREFIX = "accumulo.fate.";
+  String METRICS_FATE_CURRENT_OPS = METRICS_FATE_PREFIX + "ops.current";
+  String METRICS_FATE_TOTAL_OPS = METRICS_FATE_PREFIX + "ops.total";
+  String METRICS_FATE_ERRORS = METRICS_FATE_PREFIX + "errors";
+  String METRICS_FATE_TX = METRICS_FATE_PREFIX + "tx";
 
-  public static final String METRICS_GC_PREFIX = "accumulo.gc.";
-  public static final String METRICS_GC_STARTED = METRICS_GC_PREFIX + "started";
-  public static final String METRICS_GC_FINISHED = METRICS_GC_PREFIX + "finished";
-  public static final String METRICS_GC_CANDIDATES = METRICS_GC_PREFIX + "candidates";
-  public static final String METRICS_GC_IN_USE = METRICS_GC_PREFIX + "in.use";
-  public static final String METRICS_GC_DELETED = METRICS_GC_PREFIX + "deleted";
-  public static final String METRICS_GC_ERRORS = METRICS_GC_PREFIX + "errors";
-  public static final String METRICS_GC_WAL_STARTED = METRICS_GC_PREFIX + "wal.started";
-  public static final String METRICS_GC_WAL_FINISHED = METRICS_GC_PREFIX + "wal.finished";
-  public static final String METRICS_GC_WAL_CANDIDATES = METRICS_GC_PREFIX + "wal.candidates";
-  public static final String METRICS_GC_WAL_IN_USE = METRICS_GC_PREFIX + "wal.in.use";
-  public static final String METRICS_GC_WAL_DELETED = METRICS_GC_PREFIX + "wal.deleted";
-  public static final String METRICS_GC_WAL_ERRORS = METRICS_GC_PREFIX + "wal.errors";
-  public static final String METRICS_GC_POST_OP_DURATION = METRICS_GC_PREFIX + "post.op.duration";
-  public static final String METRICS_GC_RUN_CYCLE = METRICS_GC_PREFIX + "run.cycle";
+  String METRICS_GC_PREFIX = "accumulo.gc.";
+  String METRICS_GC_STARTED = METRICS_GC_PREFIX + "started";
+  String METRICS_GC_FINISHED = METRICS_GC_PREFIX + "finished";
+  String METRICS_GC_CANDIDATES = METRICS_GC_PREFIX + "candidates";
+  String METRICS_GC_IN_USE = METRICS_GC_PREFIX + "in.use";
+  String METRICS_GC_DELETED = METRICS_GC_PREFIX + "deleted";
+  String METRICS_GC_ERRORS = METRICS_GC_PREFIX + "errors";
+  String METRICS_GC_WAL_STARTED = METRICS_GC_PREFIX + "wal.started";
+  String METRICS_GC_WAL_FINISHED = METRICS_GC_PREFIX + "wal.finished";
+  String METRICS_GC_WAL_CANDIDATES = METRICS_GC_PREFIX + "wal.candidates";
+  String METRICS_GC_WAL_IN_USE = METRICS_GC_PREFIX + "wal.in.use";
+  String METRICS_GC_WAL_DELETED = METRICS_GC_PREFIX + "wal.deleted";
+  String METRICS_GC_WAL_ERRORS = METRICS_GC_PREFIX + "wal.errors";
+  String METRICS_GC_POST_OP_DURATION = METRICS_GC_PREFIX + "post.op.duration";
+  String METRICS_GC_RUN_CYCLE = METRICS_GC_PREFIX + "run.cycle";
 
-  public static final String METRICS_MAJC_PREFIX = "accumulo.tserver.compactions.majc.";
-  public static final String METRICS_MAJC_QUEUED = METRICS_MAJC_PREFIX + "queued";
-  public static final String METRICS_MAJC_RUNNING = METRICS_MAJC_PREFIX + "running";
+  String METRICS_MAJC_PREFIX = "accumulo.tserver.compactions.majc.";
+  String METRICS_MAJC_QUEUED = METRICS_MAJC_PREFIX + "queued";
+  String METRICS_MAJC_RUNNING = METRICS_MAJC_PREFIX + "running";
 
-  public static final String METRICS_MINC_PREFIX = "accumulo.tserver.compactions.minc.";
-  public static final String METRICS_MINC_QUEUED = METRICS_MINC_PREFIX + "queued";
-  public static final String METRICS_MINC_RUNNING = METRICS_MINC_PREFIX + "running";
+  String METRICS_MINC_PREFIX = "accumulo.tserver.compactions.minc.";
+  String METRICS_MINC_QUEUED = METRICS_MINC_PREFIX + "queued";
+  String METRICS_MINC_RUNNING = METRICS_MINC_PREFIX + "running";
 
-  public static final String METRICS_REPLICATION_PREFIX = "accumulo.replication.";
-  public static final String METRICS_REPLICATION_QUEUE = METRICS_REPLICATION_PREFIX + "queue";
-  public static final String METRICS_REPLICATION_PENDING_FILES =
-      METRICS_REPLICATION_PREFIX + "files.pending";
-  public static final String METRICS_REPLICATION_PEERS = METRICS_REPLICATION_PREFIX + "peers";
-  public static final String METRICS_REPLICATION_THREADS = METRICS_REPLICATION_PREFIX + "threads";
+  String METRICS_REPLICATION_PREFIX = "accumulo.replication.";
+  String METRICS_REPLICATION_QUEUE = METRICS_REPLICATION_PREFIX + "queue";
+  String METRICS_REPLICATION_PENDING_FILES = METRICS_REPLICATION_PREFIX + "files.pending";
+  String METRICS_REPLICATION_PEERS = METRICS_REPLICATION_PREFIX + "peers";
+  String METRICS_REPLICATION_THREADS = METRICS_REPLICATION_PREFIX + "threads";
 
-  public static final String METRICS_SCAN = "accumulo.tserver.scans";
-  public static final String METRICS_SCAN_RESULTS = METRICS_SCAN + ".result";
-  public static final String METRICS_SCAN_YIELDS = METRICS_SCAN + ".yields";
+  String METRICS_SCAN = "accumulo.tserver.scans";
+  String METRICS_SCAN_RESULTS = METRICS_SCAN + ".result";
+  String METRICS_SCAN_YIELDS = METRICS_SCAN + ".yields";
 
-  public static final String METRICS_TSERVER_PREFIX = "accumulo.tserver.";
-  public static final String METRICS_TSERVER_ENTRIES = METRICS_TSERVER_PREFIX + "entries";
-  public static final String METRICS_TSERVER_MEM_ENTRIES = METRICS_TSERVER_PREFIX + "entries.mem";
-  public static final String METRICS_TSERVER_MAJC_QUEUED = METRICS_TSERVER_PREFIX + "majc.queued";
-  public static final String METRICS_TSERVER_MAJC_RUNNING = METRICS_TSERVER_PREFIX + "majc.running";
-  public static final String METRICS_TSERVER_MINC_QUEUED = METRICS_TSERVER_PREFIX + "minc.queued";
-  public static final String METRICS_TSERVER_MINC_RUNNING = METRICS_TSERVER_PREFIX + "minc.running";
-  public static final String METRICS_TSERVER_MINC_TOTAL = METRICS_TSERVER_PREFIX + "minc.total";
-  public static final String METRICS_TSERVER_TABLETS_ONLINE =
-      METRICS_TSERVER_PREFIX + "tablets.online";
-  public static final String METRICS_TSERVER_TABLETS_OPENING =
-      METRICS_TSERVER_PREFIX + "tablets.opening";
-  public static final String METRICS_TSERVER_TABLETS_UNOPENED =
-      METRICS_TSERVER_PREFIX + "tablets.unopened";
-  public static final String METRICS_TSERVER_QUERIES = METRICS_TSERVER_PREFIX + "queries";
-  public static final String METRICS_TSERVER_TABLETS_FILES =
-      METRICS_TSERVER_PREFIX + "tablets.files";
-  public static final String METRICS_TSERVER_HOLD = METRICS_TSERVER_PREFIX + "hold";
-  public static final String METRICS_TSERVER_INGEST_MUTATIONS =
-      METRICS_TSERVER_PREFIX + "ingest.mutations";
-  public static final String METRICS_TSERVER_INGEST_BYTES = METRICS_TSERVER_PREFIX + "ingest.bytes";
-  public static final String METRICS_TSERVER_SCAN_RESULTS = METRICS_TSERVER_PREFIX + "scan.results";
-  public static final String METRICS_TSERVER_SCAN_RESULTS_BYTES =
-      METRICS_TSERVER_PREFIX + "scan.results.bytes";
-  public static final String METRICS_TSERVER_SCANNED_ENTRIES =
-      METRICS_TSERVER_PREFIX + "scan.scanned.entries";
+  String METRICS_TSERVER_PREFIX = "accumulo.tserver.";
+  String METRICS_TSERVER_ENTRIES = METRICS_TSERVER_PREFIX + "entries";
+  String METRICS_TSERVER_MEM_ENTRIES = METRICS_TSERVER_PREFIX + "entries.mem";
+  String METRICS_TSERVER_MAJC_QUEUED = METRICS_TSERVER_PREFIX + "majc.queued";
+  String METRICS_TSERVER_MAJC_RUNNING = METRICS_TSERVER_PREFIX + "majc.running";
+  String METRICS_TSERVER_MINC_QUEUED = METRICS_TSERVER_PREFIX + "minc.queued";
+  String METRICS_TSERVER_MINC_RUNNING = METRICS_TSERVER_PREFIX + "minc.running";
+  String METRICS_TSERVER_MINC_TOTAL = METRICS_TSERVER_PREFIX + "minc.total";
+  String METRICS_TSERVER_TABLETS_ONLINE = METRICS_TSERVER_PREFIX + "tablets.online";
+  String METRICS_TSERVER_TABLETS_OPENING = METRICS_TSERVER_PREFIX + "tablets.opening";
+  String METRICS_TSERVER_TABLETS_UNOPENED = METRICS_TSERVER_PREFIX + "tablets.unopened";
+  String METRICS_TSERVER_QUERIES = METRICS_TSERVER_PREFIX + "queries";
+  String METRICS_TSERVER_TABLETS_FILES = METRICS_TSERVER_PREFIX + "tablets.files";
+  String METRICS_TSERVER_HOLD = METRICS_TSERVER_PREFIX + "hold";
+  String METRICS_TSERVER_INGEST_MUTATIONS = METRICS_TSERVER_PREFIX + "ingest.mutations";
+  String METRICS_TSERVER_INGEST_BYTES = METRICS_TSERVER_PREFIX + "ingest.bytes";
+  String METRICS_TSERVER_SCAN_RESULTS = METRICS_TSERVER_PREFIX + "scan.results";
+  String METRICS_TSERVER_SCAN_RESULTS_BYTES = METRICS_TSERVER_PREFIX + "scan.results.bytes";
+  String METRICS_TSERVER_SCANNED_ENTRIES = METRICS_TSERVER_PREFIX + "scan.scanned.entries";
 
-  public static final String METRICS_THRIFT_PREFIX = "accumulo.thrift.";
-  public static final String METRICS_THRIFT_EXECUTE = METRICS_THRIFT_PREFIX + "execute";
-  public static final String METRICS_THRIFT_IDLE = METRICS_THRIFT_PREFIX + "idle";
+  String METRICS_THRIFT_PREFIX = "accumulo.thrift.";
+  String METRICS_THRIFT_EXECUTE = METRICS_THRIFT_PREFIX + "execute";
+  String METRICS_THRIFT_IDLE = METRICS_THRIFT_PREFIX + "idle";
 
-  public static final String METRICS_UPDATE_PREFIX = "accumulo.tserver.updates.";
-  public static final String METRICS_UPDATE_ERRORS = METRICS_UPDATE_PREFIX + "error";
-  public static final String METRICS_UPDATE_COMMIT = METRICS_UPDATE_PREFIX + "commit";
-  public static final String METRICS_UPDATE_COMMIT_PREP = METRICS_UPDATE_COMMIT + ".prep";
-  public static final String METRICS_UPDATE_WALOG_WRITE = METRICS_UPDATE_PREFIX + "walog.write";
-  public static final String METRICS_UPDATE_MUTATION_ARRAY_SIZE =
-      METRICS_UPDATE_PREFIX + "mutation.arrays.size";
+  String METRICS_UPDATE_PREFIX = "accumulo.tserver.updates.";
+  String METRICS_UPDATE_ERRORS = METRICS_UPDATE_PREFIX + "error";
+  String METRICS_UPDATE_COMMIT = METRICS_UPDATE_PREFIX + "commit";
+  String METRICS_UPDATE_COMMIT_PREP = METRICS_UPDATE_COMMIT + ".prep";
+  String METRICS_UPDATE_WALOG_WRITE = METRICS_UPDATE_PREFIX + "walog.write";
+  String METRICS_UPDATE_MUTATION_ARRAY_SIZE = METRICS_UPDATE_PREFIX + "mutation.arrays.size";
 
   /**
    * Build Micrometer Meter objects and register them with the registry
