ff4e542a7ef1a244826888c572bedc713fe8183b
Mike Miller <mmiller@apache.org>
2021-12-13 10:53:05 -0500

    Create GenerateSplits utility (#2368)
    * New command line utility to generate splits from 1 or more rfiles
    * Uses new Apache Datasketches library
    * Create new GenerateSplitsTest
    * Closes #2361
    
    Co-authored-by: Christopher Tubbs <ctubbsii@apache.org>
    Co-authored-by: Dom G. <47725857+DomGarguilo@users.noreply.github.com>
    Co-authored-by: Keith Turner <kturner@apache.org>

--- core/src/main/java/org/apache/accumulo/core/file/rfile/PrintInfo.java
+++ core/src/main/java/org/apache/accumulo/core/file/rfile/PrintInfo.java
@@ -181,10 +181,6 @@ public class PrintInfo implements KeywordExecutable {
       log.debug("Adding Hadoop configuration file {}", confFile);
       conf.addResource(new Path(confFile));
     }
-
-    FileSystem hadoopFs = FileSystem.get(conf);
-    FileSystem localFs = FileSystem.getLocal(conf);
-
     LogHistogram kvHistogram = new LogHistogram();
 
     KeyStats dataKeyStats = new KeyStats();
@@ -192,14 +188,7 @@ public class PrintInfo implements KeywordExecutable {
 
     for (String arg : opts.files) {
       Path path = new Path(arg);
-      FileSystem fs;
-      if (arg.contains(":")) {
-        fs = path.getFileSystem(conf);
-      } else {
-        log.warn(
-            "Attempting to find file across filesystems. Consider providing URI instead of path");
-        fs = hadoopFs.exists(path) ? hadoopFs : localFs; // fall back to local
-      }
+      FileSystem fs = resolveFS(log, conf, path);
       System.out
           .println("Reading file: " + path.makeQualified(fs.getUri(), fs.getWorkingDirectory()));
 
@@ -311,6 +300,20 @@ public class PrintInfo implements KeywordExecutable {
         return;
       }
     }
+  }
+
+  public static FileSystem resolveFS(Logger log, Configuration conf, Path file) throws IOException {
+    FileSystem hadoopFs = FileSystem.get(conf);
+    FileSystem localFs = FileSystem.getLocal(conf);
+    FileSystem fs;
+    if (file.toString().contains(":")) {
+      fs = file.getFileSystem(conf);
+    } else {
+      log.warn(
+          "Attempting to find file across filesystems. Consider providing URI instead of path");
+      fs = hadoopFs.exists(file) ? hadoopFs : localFs; // fall back to local
+    }
+    return fs;
   }
 
   /**
