1a8fb714a89fa2771617f3d5fe6c63eb8fddbab8
Christopher Tubbs <ctubbsii@apache.org>
2022-02-22 19:13:45 -0500

    Bump ZooKeeper and Hadoop dependencies
    Bump Hadoop to latest bugfix release (3.3.0 to 3.3.1)
    
    Bump ZooKeeper from 3.5 to 3.7
    
    Recent conversations on the ZooKeeper mailing lists suggest that
    community will soon mark 3.5 as EOL, and may also move to make 3.6 EOL
    after 3.8 is released.
    
    This change updates the default ZooKeeper used to build Accumulo to
    3.7.0, but updates the GitHub Actions to test that 3.5 still works.
    
    Additionally, since a new event type (PersistentWatchRemoved) was added
    after 3.5, all unhandled event types in switch statements in
    DistributedWorkQueue are collapsed into a single default case to ensure
    it includes the new event type without directly referencing it, which
    would cause a compile failure against 3.5.

--- server/base/src/main/java/org/apache/accumulo/server/zookeeper/DistributedWorkQueue.java
+++ server/base/src/main/java/org/apache/accumulo/server/zookeeper/DistributedWorkQueue.java
@@ -210,12 +210,7 @@ public class DistributedWorkQueue {
             else
               log.info("Unexpected path for NodeChildrenChanged event {}", event.getPath());
             break;
-          case NodeCreated:
-          case NodeDataChanged:
-          case NodeDeleted:
-          case ChildWatchRemoved:
-          case DataWatchRemoved:
-          case None:
+          default:
             log.info("Got unexpected zookeeper event: {} for {}", event.getType(), path);
             break;
         }
@@ -274,12 +269,7 @@ public class DistributedWorkQueue {
               condVar.notify();
             }
             break;
-          case NodeCreated:
-          case NodeDataChanged:
-          case NodeDeleted:
-          case ChildWatchRemoved:
-          case DataWatchRemoved:
-          case None:
+          default:
             log.info("Got unexpected zookeeper event: {} for {}", event.getType(), path);
             break;
         }
