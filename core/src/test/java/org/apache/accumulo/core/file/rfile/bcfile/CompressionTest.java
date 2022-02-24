6fcf9c6a76e5bba6e3ac14cc0ebb30fc9531222e
Dom G <dominic.garguilo@gmail.com>
2022-02-23 23:38:48 -0500

    Change timeouts to correct unit (#2521)

--- core/src/test/java/org/apache/accumulo/core/file/rfile/bcfile/CompressionTest.java
+++ core/src/test/java/org/apache/accumulo/core/file/rfile/bcfile/CompressionTest.java
@@ -159,7 +159,7 @@ public class CompressionTest {
   }
 
   @Test
-  @Timeout(60_000)
+  @Timeout(60)
   public void testManyStartNotNull() throws InterruptedException, ExecutionException {
 
     for (final Algorithm al : Algorithm.values()) {
@@ -202,7 +202,7 @@ public class CompressionTest {
 
   // don't start until we have created the codec
   @Test
-  @Timeout(60_000)
+  @Timeout(60)
   public void testManyDontStartUntilThread() throws InterruptedException, ExecutionException {
 
     for (final Algorithm al : Algorithm.values()) {
@@ -239,7 +239,7 @@ public class CompressionTest {
   }
 
   @Test
-  @Timeout(60_000)
+  @Timeout(60)
   public void testThereCanBeOnlyOne() throws InterruptedException, ExecutionException {
 
     for (final Algorithm al : Algorithm.values()) {
