cefb7f784df9508c2e513140962948221b1ddfcd
domgarguilo <dominic.garguilo@gmail.com>
2022-03-03 14:09:09 -0500

    Automatic import fix

--- test/src/main/java/org/apache/accumulo/test/compaction/UserCompactionStrategyIT.java
+++ test/src/main/java/org/apache/accumulo/test/compaction/UserCompactionStrategyIT.java
@@ -20,7 +20,6 @@ package org.apache.accumulo.test.compaction;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
