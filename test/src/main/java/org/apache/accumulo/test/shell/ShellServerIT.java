ff0bdfdd1f298a5de693c2d11a6fc9f6dae0b7a3
Mark Owens <jmark99@apache.org>
2022-03-21 13:00:22 -0400

    Add import for AtomicInteger in ShellServerIT

--- test/src/main/java/org/apache/accumulo/test/shell/ShellServerIT.java
+++ test/src/main/java/org/apache/accumulo/test/shell/ShellServerIT.java
@@ -43,6 +43,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 import org.apache.accumulo.core.Constants;
