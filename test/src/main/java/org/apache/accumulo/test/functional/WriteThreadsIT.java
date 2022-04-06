1651027d96157c635022c61e561d2a86e8654c93
Mike Miller <mmiller@apache.org>
2022-04-06 11:16:51 -0400

    Update test/src/main/java/org/apache/accumulo/test/functional/WriteThreadsIT.java
    Co-authored-by: Dom G. <domgarguilo@apache.org>

--- test/src/main/java/org/apache/accumulo/test/functional/WriteThreadsIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/WriteThreadsIT.java
@@ -58,7 +58,7 @@ import org.slf4j.LoggerFactory;
 public class WriteThreadsIT extends AccumuloClusterHarness {
 
   ThreadPoolExecutor tpe;
-  private static final Logger log = LoggerFactory.getLogger(TabletServerBatchWriter.class);
+  private static final Logger log = LoggerFactory.getLogger(WriteThreadsIT.class);
 
   @Override
   public void configureMiniCluster(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
