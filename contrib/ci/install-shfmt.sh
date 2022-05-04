9d5ad6b27b930d756a6a3175ece4b8b40a4f9806
Christopher Tubbs <ctubbsii@apache.org>
2022-05-04 18:44:25 -0400

    Fix install script formatting

--- contrib/ci/install-shfmt.sh
+++ contrib/ci/install-shfmt.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 shfmt_version=3.4.3
-sudo wget "https://github.com/mvdan/sh/releases/download/v${shfmt_version}/shfmt_v${shfmt_version}_linux_amd64" -O /usr/local/bin/shfmt \
-    && sudo chmod +x /usr/local/bin/shfmt
+sudo wget "https://github.com/mvdan/sh/releases/download/v${shfmt_version}/shfmt_v${shfmt_version}_linux_amd64" -O /usr/local/bin/shfmt &&
+  sudo chmod +x /usr/local/bin/shfmt
