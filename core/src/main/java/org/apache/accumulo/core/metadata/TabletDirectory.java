75fcca5caae80d41847bbb6b9bdfc5e4a4463141
Mike Miller <mmiller@apache.org>
2022-05-10 12:29:16 -0400

    Create TabletDirectory classes
    * Create TabletDirectory and RelativeTabletDirectory
    * Move Reference class and improve it so other classes can be children
    * Add validate method to compare what was in the metadata to what is
    being used in the GC
    * Use TabletDirectory in TabletFile
    * Refactor some GC code

---
+++ core/src/main/java/org/apache/accumulo/core/metadata/TabletDirectory.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.core.metadata;
+
+import org.apache.accumulo.core.data.TableId;
+
+/**
+ * The Tablet directory that may exist in the metadata table.
+ */
+public class TabletDirectory extends RelativeTabletDirectory {
+  // parts of an absolute URI, like "hdfs://1.2.3.4/accumulo/tables/2a/t-0003"
+  private final String volume; // hdfs://1.2.3.4/accumulo
+
+  public TabletDirectory(String volume, TableId tableId, String tabletDir) {
+    super(tableId, tabletDir);
+    this.volume = volume;
+  }
+
+  public String getVolume() {
+    return volume;
+  }
+
+  public TableId getTableId() {
+    return tableId;
+  }
+
+  public String getTabletDir() {
+    return tabletDir;
+  }
+}
