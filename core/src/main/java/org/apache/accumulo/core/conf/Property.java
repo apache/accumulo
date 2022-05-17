b04ea5f37b255afab7fc98c7e6c76e9ce7555c34
kitswas <90329875+kitswas@users.noreply.github.com>
2022-05-17 18:34:14 +0530

    Updated annotations, fixed versions

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -244,12 +244,13 @@ public enum Property {
   GENERAL_OPENTELEMETRY_ENABLED("general.opentelemetry.enabled", "false", PropertyType.BOOLEAN,
       "Enables tracing functionality using OpenTelemetry (assuming OpenTelemetry is configured).",
       "2.1.0"),
-  @Deprecated
+  GENERAL_THREADPOOL_SIZE("general.server.threadpool.size", "1", PropertyType.COUNT,
+      "The number of threads to use for server-internal scheduled tasks", "2.1.0"),
+  @Deprecated(since = "2.1.0")
+  @ReplacedBy(property = GENERAL_THREADPOOL_SIZE)
   GENERAL_SIMPLETIMER_THREADPOOL_SIZE("general.server.simpletimer.threadpool.size", "1",
       PropertyType.COUNT, "The number of threads to use for server-internal scheduled tasks",
       "1.7.0"),
-  GENERAL_THREADPOOL_SIZE("general.server.threadpool.size", "1", PropertyType.COUNT,
-      "The number of threads to use for server-internal scheduled tasks", "3.0.0"),
   // If you update the default type, be sure to update the default used for initialization failures
   // in VolumeManagerImpl
   @Experimental
