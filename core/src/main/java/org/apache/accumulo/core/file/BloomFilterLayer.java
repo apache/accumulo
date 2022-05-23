a8505ddfe9eb927470b1d9bd01f199e7dcb040c0
Christopher Tubbs <ctubbsii@apache.org>
2022-05-23 16:45:17 -0400

    Update core/src/main/java/org/apache/accumulo/core/file/BloomFilterLayer.java
    Co-authored-by: Luke Foster <84727868+foster33@users.noreply.github.com>

--- core/src/main/java/org/apache/accumulo/core/file/BloomFilterLayer.java
+++ core/src/main/java/org/apache/accumulo/core/file/BloomFilterLayer.java
@@ -280,7 +280,7 @@ public class BloomFilterLayer {
     }
 
     /**
-     * Prevent potential CRLF injection into logs from read in user data See
+     * Prevent potential CRLF injection into logs from read in user data. See
      * <a href="https://find-sec-bugs.github.io/bugs.htm#CRLF_INJECTION_LOGS">bug description</a>
      */
     private String sanitize(String msg) {
