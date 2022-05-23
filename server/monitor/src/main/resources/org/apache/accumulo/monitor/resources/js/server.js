d2d81a47bfc83407f68c369e2b1762471163e3f5
domgarguilo <dominic.garguilo@gmail.com>
2022-05-23 16:26:07 -0400

    All tables converted, working through bug

--- server/monitor/src/main/resources/org/apache/accumulo/monitor/resources/js/server.js
+++ server/monitor/src/main/resources/org/apache/accumulo/monitor/resources/js/server.js
@@ -18,18 +18,20 @@
  */
 "use strict";
 
-var serv;
-var tabletResults;
+var serv, tabletResults, detailTable, historyTable, currentTable, resultsTable;
+
+var currentServer = window.location.search.split('=')[1];
+var url = '/rest/tservers/' + currentServer;
+console.debug('REST url for fetching data: ' + url);
+
 /**
  * Makes the REST calls, generates the tables with the new information
  */
 function refreshServer() {
-  getTServer(serv).then(function() {
-    refreshDetailTable();
-    refreshHistoryTable();
-    refreshCurrentTable();
-    refreshResultsTable();
-  });
+  ajaxReloadTable(detailTable);
+  ajaxReloadTable(historyTable);
+  ajaxReloadTable(currentTable);
+  ajaxReloadTable(resultsTable);
 }
 
 /**
@@ -39,95 +41,263 @@ function refresh() {
   refreshServer();
 }
 
-/**
- * Populates the server details table
- */
-function refreshDetailTable() {
-    var data = sessionStorage.server === undefined ?
-        [] : JSON.parse(sessionStorage.server);
-    if (data.length === 0 || data.details === undefined) {
-        clearAllTableCells("tServerDetail");
-    } else {
-        $("#hostedTablets").text(bigNumberForQuantity(data.details.hostedTablets));
-        $("#entries").text(bigNumberForQuantity(data.details.entries));
-        $("#minors").text(bigNumberForQuantity(data.details.minors));
-        $("#majors").text(bigNumberForQuantity(data.details.majors));
-        $("#splits").text(bigNumberForQuantity(data.details.splits));
-    }
-}
 
-/**
- * Populates the All Time Tablet Operations table
- */
-function refreshHistoryTable() {
-  var data = sessionStorage.server === undefined ?
-      [] : JSON.parse(sessionStorage.server);
+$(document).ready(function () {
 
-  if (data.length === 0 || data.allTimeTabletResults === undefined) {
-    clearAllTableCells("opHistoryDetails");
-  } else {
-    var totalTimeSpent = 0;
-    $.each(data.allTimeTabletResults, function(key, val) {
-      totalTimeSpent += val.timeSpent;
-    });
+  // Create a table for details on the current server
+  detailTable = $('#tServerDetail').DataTable({
+    "ajax": {
+      "url": url,
+      "dataSrc": function (data) {
+        // the data needs to be in an array to work with DataTables
+        var arr = [];
+        if (data.details === undefined) {
+          console.warn('the value of "details" is undefined');
+        } else {
+          arr = [data.details];
+        }
 
-    $.each(data.allTimeTabletResults, function(key, val) {
-      // use the first 5 characters of the operation for the jquery selector
-      var rowId = "#" + val.operation.slice(0, 5) + "Row";
-      console.log("Populating rowId " + rowId + " with " + val.operation + " data");
+        return arr;
+      }
+    },
+    "stateSave": true,
+    "searching": false,
+    "paging": false,
+    "info": false,
+    "columnDefs": [{
+      "targets": "big-num",
+      "render": function (data, type) {
+        if (type === 'display') {
+          data = bigNumberForQuantity(data);
+        }
+        return data;
+      }
+    }],
+    "columns": [{
+        "data": "hostedTablets"
+      },
+      {
+        "data": "entries"
+      },
+      {
+        "data": "minors"
+      },
+      {
+        "data": "majors"
+      },
+      {
+        "data": "splits"
+      }
+    ]
+  });
 
-      $(rowId).append("<td>" + val.operation + "</td>");
-      $(rowId).append("<td>" + bigNumberForQuantity(val.success) + "</td>");
-      $(rowId).append("<td>" + bigNumberForQuantity(val.failure) + "</td>");
+  // Create a table for all time tablet operations
+  historyTable = $('#opHistoryDetails').DataTable({
+    "ajax": {
+      "url": url,
+      "dataSrc": function (data, type) {
+        if (type === 'display' && data.allTimeTabletResults !== undefined) {
+          var totalTimeSpent = 0;
+          for (var i = 0; i < data.allTimeTabletResults.length; i++) {
+            totalTimeSpent += parseInt(data.allTimeTabletResults[i].timeSpent, 10);
+            console.debug('new total:' + totalTimeSpent);
+          }
+          for (var i = 0; i < data.allTimeTabletResults.length; i++) {
+            var percent = Math.floor((data.allTimeTabletResults[i].timeSpent / totalTimeSpent) * 100);
+            console.debug('new percent:' + percent);
+            data.allTimeTabletResults[i].percentTime = percent;
+            console.debug('new percent1:' + data.allTimeTabletResults[i].percentTime);
+          }
+        }
+        return data;
+      }
+    },
+    "stateSave": true,
+    "searching": false,
+    "paging": false,
+    "info": false,
+    "columnDefs": [{
+        "targets": "big-num",
+        "render": function (data, type) {
+          if (type === 'display') {
+            data = bigNumberForQuantity(data);
+          }
+          return data;
+        }
+      },
+      {
+        "targets": "duration",
+        "render": function (data, type) {
+          if (type === 'display') {
+            if (data === null) {
+              data = '—';
+            } else {
+              data = timeDuration(data * 1000.0);
+            }
+          }
+          return data;
+        }
+      }
+    ],
+    "columns": [{
+        "data": "operation"
+      },
+      {
+        "data": "success"
+      },
+      {
+        "data": "failure"
+      },
+      {
+        "data": "avgQueueTime"
+      },
+      {
+        "data": "queueStdDev"
+      },
+      {
+        "data": "avgTime"
+      },
+      {
+        "data": "stdDev"
+      },
+      {
+        "data": "queueStdDev"
+      },
+      {
+        "data": "percentTime",
+        "type": "html",
+        "render": function (data, type) {
+          if (type === 'display') {
+            console.debug('data: ' + data);
+            data = `<div class="progress"><div class="progress-bar" role="progressbar" style="min-width: 2em; width:${data}%;">${data}%</div>`
+          }
+          console.debug('data before return: ' + data);
+          return data;
+        }
+      }
+    ]
+  });
 
-      appendDurationToRow(rowId, val.avgQueueTime);
-      appendDurationToRow(rowId, val.queueStdDev);
-      appendDurationToRow(rowId, val.avgTime);
-      appendDurationToRow(rowId, val.stdDev);
+  // Create a table for tablet operations on the current server
+  currentTable = $('#currentTabletOps').DataTable({
+    "ajax": {
+      "url": url,
+      "dataSrc": function (data) {
+        // the data needs to be in an array to work with DataTables
+        var arr = [];
+        if (data.currentTabletOperationResults === undefined) {
+          console.warn('the value of "currentTabletOperationResults" is undefined');
+        } else {
+          arr = [data.currentTabletOperationResults];
+        }
 
-      var percent = Math.floor((val.timeSpent / totalTimeSpent) * 100);
-      var progressBarCell = '<td><div class="progress"><div class="progress-bar"' +
-         ' role="progressbar" style="min-width: 2em; width:' + percent + '%;">' +
-         percent + '%</div></div></td>';
-      console.log("Time spent percent = " + val.timeSpent + "/" + totalTimeSpent + " " + percent);
+        return arr;
+      }
+    },
+    "stateSave": true,
+    "searching": false,
+    "paging": false,
+    "info": false,
+    "columnDefs": [{
+      "targets": "duration",
+      "render": function (data, type) {
+        if (type === 'display') {
+          data = timeDuration(data * 1000.0);
+        }
+        return data;
+      }
+    }],
+    "columns": [{
+        "data": "currentMinorAvg"
+      },
+      {
+        "data": "currentMinorStdDev"
+      },
+      {
+        "data": "currentMajorAvg"
+      },
+      {
+        "data": "currentMajorStdDev"
+      }
+    ]
+  });
 
-      $(rowId).append(progressBarCell);
-    });
-  }
-}
+  // Create a table for detailed tablet operations
+  resultsTable = $('#perTabletResults').DataTable({
+    "ajax": {
+      "url": url,
+      "dataSrc": "currentOperations"
+    },
+    "stateSave": true,
+    "dom": 't<"align-left"l>p',
+    "columnDefs": [{
+        "targets": "big-num",
+        "render": function (data, type) {
+          if (type === 'display') {
+            data = bigNumberForQuantity(data);
+          }
+          return data;
+        }
+      },
+      {
+        "targets": "duration",
+        "render": function (data, type) {
+          if (type === 'display') {
+            data = timeDuration(data);
+          }
+          return data;
+        }
+      }
+    ],
+    "columns": [{
+        "data": "name",
+        "type": "html",
+        "render": function (data, type, row) {
+          if (type === 'display') {
+            data = `<a href="/tables/${row.tableID}">${data}</a>`;
+          }
+          return data;
+        }
+      },
+      {
+        "data": "tablet",
+        "type": "html",
+        "render": function (data, type) {
+          if (type === 'display') {
+            data = `<code>${data}</code>`;
+          }
+          return data;
+        }
+      },
+      {
+        "data": "entries"
+      },
+      {
+        "data": "ingest"
+      },
+      {
+        "data": "query"
+      },
+      {
+        "data": "minorAvg"
+      },
+      {
+        "data": "minorStdDev"
+      },
+      {
+        "data": "minorAvgES"
+      },
+      {
+        "data": "majorAvg"
+      },
+      {
+        "data": "majorStdDev"
+      },
+      {
+        "data": "majorAvgES"
+      }
+    ]
+  });
 
-/**
- * Populates the current tablet operations results table
- */
-function refreshCurrentTable() {
-  var data = sessionStorage.server === undefined ?
-      [] : JSON.parse(sessionStorage.server);
-
-  if (data.length === 0 || data.currentTabletOperationResults === undefined) {
-      clearAllTableCells("currentTabletOps");
-  } else {
-    var current = data.currentTabletOperationResults;
-    $("#currentMinorAvg").html(timeDuration(current.currentMinorAvg * 1000.0));
-    $("#currentMinorStdDev").html(timeDuration(current.currentMinorStdDev * 1000.0));
-    $("#currentMajorAvg").html(timeDuration(current.currentMajorAvg * 1000.0));
-    $("#currentMajorStdDev").html(timeDuration(current.currentMajorStdDev * 1000.0));
-  }
-}
-
-/**
- * Generates the server results table
- */
-function refreshResultsTable() {
-  tabletResults.ajax.reload(null, false ); // user paging is not reset on reload
-}
-
-/*
- * Appends a table cell containing value to the rowId, if value is not null
- */
-function appendDurationToRow(rowId, value) {
-  let v = EMPTY_CELL;
-  if (value != null) {
-    v = "<td>" + timeDuration(value * 1000.0) + "</td>";
-  }
-  $(rowId).append(v);
-}
+  refreshServer();
+});
