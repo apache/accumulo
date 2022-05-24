apache/accumulo

commit 0982c1ec3bd189c67c7d2b5660ab3e42d10c44f7

Add ZooKeeper ACLs to encoded propety nodes. (#2721)
* Add ACLs to encoded prop nodes
* update test to get ACLs using ZooReaderWriter method
* add doc update to specify ZooReader needs authenticated session to read props

server/base/src/main/java/org/apache/accumulo/server/conf/store/impl/ZooPropStore.java
@@ -153,7 +153,7 @@ public class ZooPropStore implements PropStore, PropChangeListener {
     try {
       VersionedProperties vProps = new VersionedProperties(props);
       String path = propCacheKey.getPath();
-      zrw.putPersistentData(path, codec.toBytes(vProps), ZooUtil.NodeExistsPolicy.FAIL);
+      zrw.putPrivatePersistentData(path, codec.toBytes(vProps), ZooUtil.NodeExistsPolicy.FAIL);
     } catch (IOException | KeeperException | InterruptedException ex) {
       throw new IllegalStateException("Failed to serialize properties for " + propCacheKey, ex);
     }
@@ -198,7 +198,7 @@ public class ZooPropStore implements PropStore, PropChangeListener {
    * @param watcher
    *          a prop store watcher that will receive / handle ZooKeeper events.
    * @param zooReader
-   *          a ZooReader
+   *          a ZooReader with an authenticated session.
    * @return the versioned properties or null if the node does not exist.
    * @throws IOException
    *           if the underlying data from the ZooKeeper node cannot be decoded.

server/base/src/main/java/org/apache/accumulo/server/conf/util/ConfigTransformer.java
@@ -286,7 +286,7 @@ public class ConfigTransformer {
     String path = propCacheKey.getPath();
     try {
       try {
-        zrw.putPersistentData(path, codec.toBytes(vProps), ZooUtil.NodeExistsPolicy.FAIL);
+        zrw.putPrivatePersistentData(path, codec.toBytes(vProps), ZooUtil.NodeExistsPolicy.FAIL);
       } catch (KeeperException.NodeExistsException ex) {
         vProps = ZooPropStore.readFromZk(propCacheKey, propStoreWatcher, zrw);
       }

server/base/src/main/java/org/apache/accumulo/server/init/ZooKeeperInitializer.java
@@ -76,7 +76,7 @@ class ZooKeeperInitializer {
       if (zoo.exists(sysPropPath)) {
         return;
       }
-      var created = zoo.putPersistentData(sysPropPath,
+      var created = zoo.putPrivatePersistentData(sysPropPath,
           VersionedPropCodec.getDefault().toBytes(vProps), ZooUtil.NodeExistsPolicy.FAIL);
       if (!created) {
         throw new IllegalStateException(

server/base/src/test/java/org/apache/accumulo/server/conf/store/impl/ZooPropStoreTest.java
@@ -88,7 +88,7 @@ public class ZooPropStoreTest {
     var propCacheKey = TablePropKey.of(instanceId, TableId.of("propCacheKey"));
 
     Capture<byte[]> bytes = newCapture();
-    expect(zrw.putPersistentData(eq(propCacheKey.getPath()), capture(bytes), anyObject()))
+    expect(zrw.putPrivatePersistentData(eq(propCacheKey.getPath()), capture(bytes), anyObject()))
         .andReturn(true).once();
 
     replay(context, zrw);

test/src/main/java/org/apache/accumulo/test/conf/PropStoreConfigIT.java
@@ -22,12 +22,22 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.accumulo.harness.AccumuloITBase.MINI_CLUSTER_ONLY;
 import static org.apache.accumulo.harness.AccumuloITBase.SUNNY_DAY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 
 import org.apache.accumulo.core.client.Accumulo;
 import org.apache.accumulo.core.conf.Property;
+import org.apache.accumulo.core.data.NamespaceId;
+import org.apache.accumulo.core.data.TableId;
+import org.apache.accumulo.fate.zookeeper.ZooReaderWriter;
+import org.apache.accumulo.fate.zookeeper.ZooUtil;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
+import org.apache.accumulo.server.ServerContext;
+import org.apache.accumulo.server.conf.store.NamespacePropKey;
+import org.apache.accumulo.server.conf.store.SystemPropKey;
+import org.apache.accumulo.server.conf.store.TablePropKey;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -61,6 +71,59 @@ public class PropStoreConfigIT extends AccumuloClusterHarness {
           log.info("after bloom property: {}={}", e.getKey(), e.getValue());
           assertEquals("true", e.getValue());
         }
+      }
+    }
+  }
+
+  /**
+   * Validate that property nodes have an ACL set to restrict world access.
+   */
+  @Test
+  public void permissionsTest() throws Exception {
+    var names = getUniqueNames(3);
+    String namespace = names[0];
+    String table1 = namespace + "." + names[1];
+    String table2 = names[2];
+
+    try (var client = Accumulo.newClient().from(getClientProps()).build()) {
+
+      client.namespaceOperations().create(namespace);
+      client.tableOperations().create(table1);
+      client.tableOperations().create(table2);
+
+      client.instanceOperations().setProperty(Property.TABLE_BLOOM_ENABLED.getKey(), "true");
+      client.tableOperations().setProperty(table1, Property.TABLE_BLOOM_ENABLED.getKey(), "true");
+
+      Thread.sleep(SECONDS.toMillis(3L));
+
+      ServerContext serverContext = cluster.getServerContext();
+      ZooReaderWriter zrw = serverContext.getZooReaderWriter();
+
+      // validate that a world-readable node has expected perms to validate test method
+      var noAcl = zrw.getACL(ZooUtil.getRoot(serverContext.getInstanceID()));
+      assertTrue(noAcl.size() > 1);
+      assertTrue(
+          noAcl.get(0).toString().contains("world") || noAcl.get(1).toString().contains("world"));
+
+      var sysAcl = zrw.getACL(SystemPropKey.of(serverContext).getNodePath());
+      assertEquals(1, sysAcl.size());
+      assertFalse(sysAcl.get(0).toString().contains("world"));
+
+      for (Map.Entry<String,String> nsEntry : client.namespaceOperations().namespaceIdMap()
+          .entrySet()) {
+        log.debug("Check acl on namespace name: {}, id: {}", nsEntry.getKey(), nsEntry.getValue());
+        var namespaceAcl = zrw.getACL(
+            NamespacePropKey.of(serverContext, NamespaceId.of(nsEntry.getValue())).getNodePath());
+        assertEquals(1, namespaceAcl.size());
+        assertFalse(namespaceAcl.get(0).toString().contains("world"));
+      }
+
+      for (Map.Entry<String,String> tEntry : client.tableOperations().tableIdMap().entrySet()) {
+        log.debug("Check acl on table name: {}, id: {}", tEntry.getKey(), tEntry.getValue());
+        var tableAcl =
+            zrw.getACL(TablePropKey.of(serverContext, TableId.of(tEntry.getValue())).getNodePath());
+        assertEquals(1, tableAcl.size());
+        assertFalse(tableAcl.get(0).toString().contains("world"));
       }
     }
   }

commit 1bf913ba84c909b4d4378451cee42e1dcdf0e807

Fix recently broken ZooKeeper ITs
Fix ZooKeeper-related ITs broken by #2721, which added authentication to
configuration nodes stored in ZooKeeper. This adds the auth info to the
ZooKeeper object used by the ITs, matching the ZooKeeperTestingServer's
SECRET.

test/src/main/java/org/apache/accumulo/test/conf/store/PropStoreZooKeeperIT.java
@@ -87,6 +87,7 @@ public class PropStoreZooKeeperIT {
     // using default zookeeper port - we don't have a full configuration
     testZk = new ZooKeeperTestingServer(tempDir);
     zooKeeper = testZk.getZooKeeper();
+    ZooUtil.digestAuth(zooKeeper, ZooKeeperTestingServer.SECRET);
   }
 
   @AfterAll

test/src/main/java/org/apache/accumulo/test/conf/store/ZooBasedConfigIT.java
@@ -104,6 +104,7 @@ public class ZooBasedConfigIT {
     // using default zookeeper port - we don't have a full configuration
     testZk = new ZooKeeperTestingServer(tempDir);
     zooKeeper = testZk.getZooKeeper();
+    ZooUtil.digestAuth(zooKeeper, ZooKeeperTestingServer.SECRET);
     zrw = testZk.getZooReaderWriter();
   }
 

test/src/main/java/org/apache/accumulo/test/conf/util/ConfigTransformerIT.java
@@ -22,8 +22,10 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.accumulo.harness.AccumuloITBase.ZOOKEEPER_TESTING_SERVER;
+import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -82,6 +84,7 @@ public class ConfigTransformerIT {
     // using default zookeeper port - we don't have a full configuration
     testZk = new ZooKeeperTestingServer(tempDir);
     zooKeeper = testZk.getZooKeeper();
+    ZooUtil.digestAuth(zooKeeper, ZooKeeperTestingServer.SECRET);
     zrw = testZk.getZooReaderWriter();
   }
 
@@ -106,6 +109,8 @@ public class ConfigTransformerIT {
     expect(context.getPropStore()).andReturn(propStore).anyTimes();
 
     watcher = createMock(PropStoreWatcher.class);
+    watcher.process(anyObject());
+    expectLastCall().anyTimes();
 
     replay(context, watcher);
 

test/src/main/java/org/apache/accumulo/test/upgrade/ConfigPropertyUpgraderIT.java
@@ -18,7 +18,6 @@
  */
 package org.apache.accumulo.test.upgrade;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.accumulo.harness.AccumuloITBase.ZOOKEEPER_TESTING_SERVER;
 import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.expect;
@@ -74,7 +73,7 @@ public class ConfigPropertyUpgraderIT {
     // using default zookeeper port - we don't have a full configuration
     testZk = new ZooKeeperTestingServer(tempDir);
     zooKeeper = testZk.getZooKeeper();
-    zooKeeper.addAuthInfo("digest", "accumulo:test".getBytes(UTF_8));
+    ZooUtil.digestAuth(zooKeeper, ZooKeeperTestingServer.SECRET);
 
     zrw = testZk.getZooReaderWriter();
 

test/src/main/java/org/apache/accumulo/test/zookeeper/ZooKeeperTestingServer.java
@@ -42,6 +42,7 @@ import com.google.common.base.Preconditions;
 public class ZooKeeperTestingServer implements AutoCloseable {
 
   private static final Logger log = LoggerFactory.getLogger(ZooKeeperTestingServer.class);
+  public static final String SECRET = "secret";
 
   private TestingServer zkServer;
   private final ZooKeeper zoo;
@@ -101,7 +102,7 @@ public class ZooKeeperTestingServer implements AutoCloseable {
   }
 
   public ZooReaderWriter getZooReaderWriter() {
-    return new ZooReader(getConn(), 30000).asWriter("secret");
+    return new ZooReader(getConn(), 30000).asWriter(SECRET);
   }
 
   public String getConn() {