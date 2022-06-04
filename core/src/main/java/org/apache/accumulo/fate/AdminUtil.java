b5cf66fc2659eb5e6413e1d63fb2bad51f192e96
Ed Coleman <dev1@etcoleman.com>
2022-06-04 01:19:07 +0000

    auto format corrections

--- core/src/main/java/org/apache/accumulo/fate/AdminUtil.java
+++ core/src/main/java/org/apache/accumulo/fate/AdminUtil.java
@@ -389,12 +389,13 @@ public class AdminUtil<T> {
       long timeCreated = zs.timeCreated(tid);
 
       zs.unreserve(tid, 0);
-      TransactionStatus txStatus = new TransactionStatus(tid, status, debug, hlocks, wlocks, top, timeCreated);
+      TransactionStatus txStatus =
+          new TransactionStatus(tid, status, debug, hlocks, wlocks, top, timeCreated);
 
-      if((filterTxid == null) || filterTxid.contains(tid)){
+      if ((filterTxid == null) || filterTxid.contains(tid)) {
         statuses.add(txStatus);
       } else {
-        if((filterStatus == null) || filterStatus.contains(status)){
+        if ((filterStatus == null) || filterStatus.contains(status)) {
           statuses.add(txStatus);
         }
       }
