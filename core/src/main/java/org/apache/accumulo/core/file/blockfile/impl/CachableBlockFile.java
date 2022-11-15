f180381655b45c97e29d51c7418ea9b33359ae9c
Dave Marion <dlmarion@apache.org>
2022-11-15 16:24:31 +0000

    Wire up FSDataInputStream.setDropBehind, set to true in FileCompactor
    Added CachableBlockFile.CachableBuilder.fsPath override method that allows
    the caller to specify whether or not setDropBehind should be called on
    the FSDataInputStream. Wired up FileOptions and RFileOperations for this
    new setting. Modified FileCompactor only to enable this setting. Any scans
    that are running on the same Tablet that is compacting will likely have
    their blocks cached, so this should not impact scans.
    
    I looked at setting FSDataInputStream.setDropBehind on the FileCompactors
    input files after the compaction was completed, in
    DatafileManager.bringMajorCompactionOnline, but the way this is wired up
    in Hadoop you have to read from the file to make this setting active (see
    org.apache.hadoop.hdfs.server.datanode.BlockSender.close().
    
    I believe that the effect of this change is that this will set
    POSIX_FADV_DONTNEED on the DataNodes that serve up a copy of each HDFS file
    block that is read. This means that DataNodes that serve up different
    block replicas may not have the POSIX_FADV_DONTNEED set on the file
    descriptor.
    
    The impact of this change is likely inconsequential when looking at a
    single file or on a system that is not actively compacting. This change
    will have a larger impact on systems that are constantly compacting.

--- core/src/main/java/org/apache/accumulo/core/file/blockfile/impl/CachableBlockFile.java
+++ core/src/main/java/org/apache/accumulo/core/file/blockfile/impl/CachableBlockFile.java
@@ -41,6 +41,7 @@ import org.apache.accumulo.core.spi.cache.CacheEntry;
 import org.apache.accumulo.core.spi.crypto.CryptoService;
 import org.apache.accumulo.core.util.ratelimit.RateLimiter;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.Seekable;
@@ -83,8 +84,24 @@ public class CachableBlockFile {
     }
 
     public CachableBuilder fsPath(FileSystem fs, Path dataFile) {
+      return fsPath(fs, dataFile, false);
+    }
+
+    public CachableBuilder fsPath(FileSystem fs, Path dataFile, boolean dropCacheBehind) {
       this.cacheId = pathToCacheId(dataFile);
-      this.inputSupplier = () -> fs.open(dataFile);
+      this.inputSupplier = () -> {
+        FSDataInputStream is = fs.open(dataFile);
+        if (dropCacheBehind) {
+          // Tell the DataNode that the write ahead log does not need to be cached in the OS page
+          // cache
+          try {
+            is.setDropBehind(Boolean.TRUE);
+          } catch (IOException | UnsupportedOperationException e) {
+            log.debug("setDropBehind not enabled for wal file: {}", dataFile);
+          }
+        }
+        return is;
+      };
       this.lengthSupplier = () -> fs.getFileStatus(dataFile).getLen();
       return this;
     }
