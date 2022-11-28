f86d39c400330d8fd2147fca924dd5efe54f34db
Dave Marion <dlmarion@apache.org>
2022-11-28 08:31:28 -0500

    Update test/src/main/java/org/apache/accumulo/test/ScanServerIT.java
    Co-authored-by: Keith Turner <kturner@apache.org>

--- test/src/main/java/org/apache/accumulo/test/ScanServerIT.java
+++ test/src/main/java/org/apache/accumulo/test/ScanServerIT.java
@@ -221,7 +221,9 @@ public class ScanServerIT extends SharedMiniClusterBase {
       try (BatchScanner scanner = client.createBatchScanner(tableName, Authorizations.EMPTY)) {
         scanner.setRanges(Collections.singletonList(new Range()));
         scanner.setConsistencyLevel(ConsistencyLevel.EVENTUAL);
+        assertEquals(0, Iterables.size(scanner), "Unexpected entries seen when scanning empty table");
       } // when the scanner is closed, all open sessions should be closed
+      assertFalse(client.tableOperations().isOnline(tableName));
     }
   }
 
