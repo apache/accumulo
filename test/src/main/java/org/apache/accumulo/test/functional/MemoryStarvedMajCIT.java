6afbb39777720ac3d85b8d7e9c3d7686bb3e678c
Dave Marion <dlmarion@apache.org>
2023-01-18 21:14:37 +0000

    fix junit imports

--- test/src/main/java/org/apache/accumulo/test/functional/MemoryStarvedMajCIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/MemoryStarvedMajCIT.java
@@ -18,8 +18,8 @@
  */
 package org.apache.accumulo.test.functional;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
