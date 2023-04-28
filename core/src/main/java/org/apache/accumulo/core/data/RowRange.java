adfc5cd388071f6d59c72376a2df448a9d7fe7ae
DomGarguilo <dominic.garguilo@gmail.com>
2023-04-28 14:55:24 -0400

    Add hashCode() equals() compareTo() contains() afterEndRow() toRange()

--- core/src/main/java/org/apache/accumulo/core/data/RowRange.java
+++ core/src/main/java/org/apache/accumulo/core/data/RowRange.java
@@ -23,10 +23,13 @@ import java.nio.ByteBuffer;
 import org.apache.accumulo.core.dataImpl.thrift.TRowRange;
 import org.apache.accumulo.core.util.ByteBufferUtil;
 import org.apache.accumulo.core.util.TextUtil;
+import org.apache.hadoop.io.BinaryComparable;
 import org.apache.hadoop.io.Text;
 
 /**
  * This class is used to specify a range of rows.
+ *
+ * @since 3.0.0
  */
 public class RowRange {
   final private Text startRow;
@@ -290,6 +293,13 @@ public class RowRange {
   }
 
   /**
+   * Converts this row range to a {@link Range} object.
+   */
+  public Range toRange() {
+    return new Range(startRow, startRowInclusive, endRow, endRowInclusive);
+  }
+
+  /**
    * Converts this row range to Thrift.
    *
    * @return Thrift row range
@@ -322,6 +332,23 @@ public class RowRange {
   }
 
   /**
+   * Determines if the given row is after the end row of this row range.
+   *
+   * @param row row to check
+   * @return true if the given row is after the row range, otherwise false
+   */
+  public boolean afterEndRow(Text row) {
+    if (infiniteEndRow) {
+      return false;
+    }
+
+    if (endRowInclusive) {
+      return row.compareTo(endRow) < 0;
+    }
+    return row.compareTo(endRow) <= 0;
+  }
+
+  /**
    * Implements logic of {@link #beforeStartRow(Text)}, but in a private method, so that it can be
    * safely used by constructors if a subclass overrides that {@link #beforeStartRow(Text)}
    */
@@ -343,5 +370,80 @@ public class RowRange {
     final String endRow = this.endRow == null ? "+inf" : this.endRow.toString();
     final String endRangeSymbol = (endRowInclusive && this.endRow != null) ? "]" : ")";
     return startRangeSymbol + startRow + "," + endRow + endRangeSymbol;
+  }
+
+  @Override
+  public int hashCode() {
+    int startHash = infiniteStartRow ? 0 : startRow.hashCode() + (startRowInclusive ? 1 : 0);
+    int stopHash = infiniteEndRow ? 0 : endRow.hashCode() + (endRowInclusive ? 1 : 0);
+
+    return startHash + stopHash;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof RowRange)) {
+      return false;
+    }
+    return equals((RowRange) o);
+  }
+
+  /**
+   * Determines if this row range equals another.
+   *
+   * @param other range to compare
+   * @return true if row ranges are equals, false otherwise
+   */
+  public boolean equals(RowRange other) {
+    return compareTo(other) == 0;
+  }
+
+  /**
+   * Compares this row range to another row range. Compares in order: start row, inclusiveness of
+   * start row, end row, inclusiveness of end row. Infinite rows sort first, and non-infinite rows
+   * are compared with {@link Text#compareTo(BinaryComparable)}. Inclusive sorts before
+   * non-inclusive.
+   *
+   * @param o range row to compare
+   * @return comparison result
+   */
+  public int compareTo(RowRange o) {
+    // Compare infinite start rows
+    int comp = Boolean.compare(infiniteStartRow, o.infiniteStartRow);
+
+    if (comp == 0) {
+      // Compare non-infinite start rows and start row inclusiveness
+      if (!infiniteStartRow) {
+        comp = startRow.compareTo(o.startRow);
+        if (comp == 0) {
+          comp = Boolean.compare(o.startRowInclusive, startRowInclusive);
+        }
+      }
+    }
+
+    if (comp == 0) {
+      // Compare infinite end rows
+      comp = Boolean.compare(infiniteEndRow, o.infiniteEndRow);
+
+      // Compare non-infinite end rows and end row inclusiveness
+      if (comp == 0 && !infiniteEndRow) {
+        comp = endRow.compareTo(o.endRow);
+        if (comp == 0) {
+          comp = Boolean.compare(endRowInclusive, o.endRowInclusive);
+        }
+      }
+    }
+
+    return comp;
+  }
+
+  /**
+   * Determines if this row range contains the given row.
+   *
+   * @param row row to check
+   * @return true if the row is contained in the row range, false otherwise
+   */
+  public boolean contains(Text row) {
+    return !beforeStartRowImpl(row) && !afterEndRow(row);
   }
 }
