8db76fa4794af3fb9e18d2fbc9f0054dcd036e6d
Keith Turner <kturner@apache.org>
2023-06-06 11:52:16 -0400

    adds missing import

--- core/src/main/java/org/apache/accumulo/core/conf/cluster/ClusterConfigParser.java
+++ core/src/main/java/org/apache/accumulo/core/conf/cluster/ClusterConfigParser.java
@@ -29,6 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.yaml.snakeyaml.Yaml;
