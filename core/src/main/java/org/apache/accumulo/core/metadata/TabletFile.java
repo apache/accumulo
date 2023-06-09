37c4aa553d44963e60c3d4c0ab93e1c3d350644f
Christopher L. Shannon <christopher.l.shannon@gmail.com>
2023-06-09 12:37:31 -0400

    Update docs
    Co-authored-by: EdColeman <dev1@etcoleman.com>

--- core/src/main/java/org/apache/accumulo/core/metadata/TabletFile.java
+++ core/src/main/java/org/apache/accumulo/core/metadata/TabletFile.java
@@ -37,7 +37,7 @@ public interface TabletFile {
 
   /**
    * Returns the full path for the TabletFile on the file system. The path may be normalized
-   * depending on the specific implementation.
+   * depending on the specific implementation.  For example, a path in hdfs would be returned as `hdfs://nn1/accumulo/tables/5a/t-0001/F0002.rf`
    */
   Path getPath();
 
