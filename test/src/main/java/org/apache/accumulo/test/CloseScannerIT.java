fcc91a9fcc531d041ce02d3d1690b3a53d7f8642
Dave Marion <dlmarion@apache.org>
2023-07-03 11:16:29 +0000

    updates

--- test/src/main/java/org/apache/accumulo/test/CloseScannerIT.java
+++ test/src/main/java/org/apache/accumulo/test/CloseScannerIT.java
@@ -19,6 +19,7 @@
 package org.apache.accumulo.test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Collections;
 import java.util.List;
@@ -30,21 +31,24 @@ import java.util.concurrent.Executors;
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.client.Accumulo;
 import org.apache.accumulo.core.client.AccumuloClient;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.IsolatedScanner;
 import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.admin.Locations;
 import org.apache.accumulo.core.clientImpl.ClientContext;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.fate.zookeeper.ServiceLock;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
 import org.apache.accumulo.core.rpc.clients.ThriftClientTypes;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.trace.TraceUtil;
 import org.apache.accumulo.core.util.HostAndPort;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
 import org.apache.accumulo.minicluster.ServerType;
-import org.apache.accumulo.miniclusterImpl.MiniAccumuloClusterControl;
 import org.apache.accumulo.test.functional.ReadWriteIT;
 import org.apache.accumulo.test.util.Wait;
 import org.junit.jupiter.api.Test;
@@ -92,13 +96,18 @@ public class CloseScannerIT extends AccumuloClusterHarness {
   public void testIsolatedScannerPreventsTServerShutdown() throws Exception {
 
     // 2 TabletServers started for this test, shut them down so we only have 1.
-    getClusterControl().stopAllServers(ServerType.TABLET_SERVER);
-    ((MiniAccumuloClusterControl) getClusterControl()).start(ServerType.TABLET_SERVER,
-        Collections.emptyMap(), 1);
+//    getClusterControl().stopAllServers(ServerType.TABLET_SERVER);
+//    ((MiniAccumuloClusterControl) getClusterControl()).start(ServerType.TABLET_SERVER,
+//        Collections.emptyMap(), 1);
 
     String tableName = getUniqueNames(1)[0];
 
     try (AccumuloClient client = Accumulo.newClient().from(getClientProps()).build()) {
+
+//      Wait.waitFor(() -> client.instanceOperations().getTabletServers().size() == 1);
+      
+//      Thread.sleep(120_000);
+      
       client.tableOperations().create(tableName);
 
       // wait for everything to be hosted and balanced
@@ -142,7 +151,6 @@ public class CloseScannerIT extends AccumuloClusterHarness {
       for (int i = 0; i < 10; i++) {
         service.execute(task);
       }
-      ;
 
       // Wait until all threads are reading some data
       latch.await();
@@ -150,8 +158,10 @@ public class CloseScannerIT extends AccumuloClusterHarness {
       // getClusterControl().stopAllServers(ServerType.TABLET_SERVER)
       // could potentially send a kill -9 to the process. Shut the tablet
       // servers down in a more graceful way.
-      for (String server : client.instanceOperations().getTabletServers()) {
-        HostAndPort address = HostAndPort.fromString(server);
+      
+      Locations locs = client.tableOperations().locate(tableName, Collections.singletonList(TabletsSection.getRange()));
+      locs.groupByTablet().keySet().stream().map(tid -> locs.getTabletLocation(tid)).forEach(location -> {
+        HostAndPort address = HostAndPort.fromString(location);
         String addressWithSession = address.toString();
         var zLockPath = ServiceLock.path(getCluster().getServerContext().getZooKeeperRoot()
             + Constants.ZTSERVERS + "/" + address.toString());
@@ -163,12 +173,17 @@ public class CloseScannerIT extends AccumuloClusterHarness {
 
         final String finalAddress = addressWithSession;
         System.out.println("Attempting to shutdown TabletServer at: " + address.toString());
-        ThriftClientTypes.MANAGER.executeVoid((ClientContext) client,
-            c -> c.shutdownTabletServer(TraceUtil.traceInfo(),
-                getCluster().getServerContext().rpcCreds(), finalAddress, false));
-      }
+        try {
+          ThriftClientTypes.MANAGER.executeVoid((ClientContext) client,
+              c -> c.shutdownTabletServer(TraceUtil.traceInfo(),
+                  getCluster().getServerContext().rpcCreds(), finalAddress, false));
+        } catch (AccumuloException | AccumuloSecurityException e) {
+          fail("Error shutting down TabletServer", e);
+        }
+        
+      });
 
-      Wait.waitFor(() -> client.instanceOperations().getTabletServers().size() == 0);
+      Wait.waitFor(() -> client.instanceOperations().getTabletServers().size() == 1);
 
     }
   }
