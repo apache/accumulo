00310990593f1ed476192974d8bee75e36f89e9f
Ed Coleman <edcoleman@apache.org>
2023-07-06 00:21:51 +0000

    address PR comments

--- test/src/main/java/org/apache/accumulo/test/util/Wait.java
+++ test/src/main/java/org/apache/accumulo/test/util/Wait.java
@@ -31,35 +31,68 @@ public class Wait {
 
   /**
    * Wait for the provided condition - will throw an IllegalStateException is the wait exceeds the
-   * wait period.
+   * default wait period of 30 seconds and a retry period of 1 second.
+   *
+   * @param condition when condition evaluates ture, return from wait
    */
-  public static void waitFor(Condition condition) throws Exception {
+  public static void waitFor(Condition condition) {
     waitFor(condition, MAX_WAIT_MILLIS);
   }
 
   /**
    * Wait for the provided condition - will throw an IllegalStateException is the wait exceeds the
-   * wait period.
+   * wait duration with a default retry period of 1 second.
+   *
+   * @param condition when condition evaluates ture, return from wait
+   * @param duration maximum total time to wait (milliseconds)
    */
-  public static void waitFor(final Condition condition, final long duration) throws Exception {
+  public static void waitFor(final Condition condition, final long duration) {
     waitFor(condition, duration, SLEEP_MILLIS);
   }
 
   /**
    * Wait for the provided condition - will throw an IllegalStateException is the wait exceeds the
    * wait period.
+   *
+   * @param condition when condition evaluates ture, return from wait
+   * @param duration maximum total time to wait (milliseconds)
+   * @param sleepMillis time to sleep between condition checks
    */
-  public static void waitFor(final Condition condition, final long duration, final long sleepMillis)
-      throws Exception {
+  public static void waitFor(final Condition condition, final long duration,
+      final long sleepMillis) {
+    waitFor(condition, duration, sleepMillis, "");
+  }
+
+  /**
+   * Wait for the provided condition - will throw an IllegalStateException is the wait exceeds the
+   * wait period.
+   *
+   * @param condition when condition evaluates ture, return from wait
+   * @param duration maximum total time to wait (milliseconds)
+   * @param sleepMillis time to sleep between condition checks
+   * @param failMessage optional message to include in IllegalStateException if condition not met
+   *        before expiration.
+   */
+  public static void waitFor(final Condition condition, final long duration, final long sleepMillis,
+      final String failMessage) {
 
     final long expiry = System.currentTimeMillis() + duration;
-    boolean success = condition.isSatisfied();
-    while (!success && System.currentTimeMillis() < expiry) {
-      TimeUnit.MILLISECONDS.sleep(sleepMillis);
+    boolean success;
+    try {
       success = condition.isSatisfied();
+      while (!success && System.currentTimeMillis() < expiry) {
+        TimeUnit.MILLISECONDS.sleep(sleepMillis);
+        success = condition.isSatisfied();
+      }
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+      throw new IllegalStateException("Interrupted during wait");
+    } catch (Exception ex) {
+      throw new IllegalStateException(failMessage + ". Failed because of exception in condition",
+          ex);
     }
     if (!success) {
-      throw new IllegalStateException("Failed to satisfy condition before timeout exceeded");
+      throw new IllegalStateException(failMessage + ". Timeout exceeded");
     }
   }
 }
