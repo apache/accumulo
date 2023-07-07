9437761a7065f98273838abf5b6c69e8cafcc23a
DomGarguilo <dominic.garguilo@gmail.com>
2023-07-07 18:09:46 -0400

    Ensure serialzed verion of SelectedFiles is comparable

--- core/src/main/java/org/apache/accumulo/core/metadata/schema/SelectedFiles.java
+++ core/src/main/java/org/apache/accumulo/core/metadata/schema/SelectedFiles.java
@@ -19,8 +19,9 @@
 package org.apache.accumulo.core.metadata.schema;
 
 import static java.util.stream.Collectors.toList;
-import static org.apache.accumulo.core.util.LazySingletons.GSON;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -29,6 +30,11 @@ import org.apache.accumulo.core.fate.FateTxId;
 import org.apache.accumulo.core.metadata.StoredTabletFile;
 
 import com.google.common.base.Preconditions;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 /**
  * This class is used to manage the set of files selected for a user compaction for a tablet.
@@ -40,6 +46,9 @@ public class SelectedFiles {
   private final boolean initiallySelectedAll;
   private final long fateTxId;
   private final String metadataValue;
+
+  private static final Gson GSON = new GsonBuilder()
+      .registerTypeAdapter(SelectedFiles.class, new SelectedFilesTypeAdapter()).create();
 
   // This class is used to serialize and deserialize this class using GSon. Any changes to this
   // class must consider persisted data.
@@ -63,23 +72,63 @@ public class SelectedFiles {
     jData.selAll = initiallySelectedAll;
     // ELASITICITY_TODO need the produced json to always be the same when the input data is the same
     // as its used for comparison. Need unit test to ensure this behavior.
-    metadataValue = GSON.get().toJson(jData);
+    metadataValue = GSON.toJson(jData);
   }
 
-  private SelectedFiles(Set<StoredTabletFile> files, boolean initiallySelectedAll, long fateTxId,
-      String metaVal) {
-    Preconditions.checkArgument(files != null && !files.isEmpty());
-    this.files = files;
-    this.initiallySelectedAll = initiallySelectedAll;
-    this.fateTxId = fateTxId;
-    this.metadataValue = metaVal;
+  private static class SelectedFilesTypeAdapter extends TypeAdapter<SelectedFiles> {
+
+    @Override
+    public void write(JsonWriter out, SelectedFiles selectedFiles) throws IOException {
+      out.beginObject();
+      out.name("txid").value(FateTxId.formatTid(selectedFiles.getFateTxId()));
+      out.name("selAll").value(selectedFiles.initiallySelectedAll());
+      List<String> sortedFiles = selectedFiles.getFiles().stream()
+          .map(StoredTabletFile::getMetaUpdateDelete).sorted().collect(Collectors.toList());
+      out.name("files").beginArray();
+      for (String file : sortedFiles) {
+        out.value(file);
+      }
+      out.endArray();
+      out.endObject();
+    }
+
+    @Override
+    public SelectedFiles read(JsonReader in) throws IOException {
+      long fateTxId = 0L;
+      boolean selAll = false;
+      List<String> files = new ArrayList<>();
+
+      in.beginObject();
+      while (in.hasNext()) {
+        String name = in.nextName();
+        switch (name) {
+          case "txid":
+            fateTxId = FateTxId.fromString(in.nextString());
+            break;
+          case "selAll":
+            selAll = in.nextBoolean();
+            break;
+          case "files":
+            in.beginArray();
+            while (in.hasNext()) {
+              files.add(in.nextString());
+            }
+            in.endArray();
+            break;
+        }
+      }
+      in.endObject();
+
+      Set<StoredTabletFile> tabletFiles =
+          files.stream().map(StoredTabletFile::new).collect(Collectors.toSet());
+
+      return new SelectedFiles(tabletFiles, selAll, fateTxId);
+    }
+
   }
 
   public static SelectedFiles from(String json) {
-    GSonData jData = GSON.get().fromJson(json, GSonData.class);
-    return new SelectedFiles(
-        jData.files.stream().map(StoredTabletFile::new).collect(Collectors.toSet()), jData.selAll,
-        FateTxId.fromString(jData.txid), json);
+    return GSON.fromJson(json, SelectedFiles.class);
   }
 
   public Set<StoredTabletFile> getFiles() {
