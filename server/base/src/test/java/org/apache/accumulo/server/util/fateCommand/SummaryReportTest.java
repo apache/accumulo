955e7185a70c84c0d001aa8d04b0e3eef474fb9e
Taher Ghaleb <taher.a.ghaleb@gmail.com>
2023-08-21 10:09:53 -0400

    Remove unneeded import related to assertTrue

--- server/base/src/test/java/org/apache/accumulo/server/util/fateCommand/SummaryReportTest.java
+++ server/base/src/test/java/org/apache/accumulo/server/util/fateCommand/SummaryReportTest.java
@@ -25,7 +25,6 @@ import static org.easymock.EasyMock.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Map;
