a3f6df0d67eec117160375e3ff528a28ae03277a
Jeffrey Schmidt jjschm4 <jjschm4@ip-10-113-15-208.evoforge.org>
2023-08-28 16:43:00 +0000

    Remove prefix

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -228,8 +228,6 @@ public enum Property {
       "2.1.0"),
   GENERAL_RPC_TIMEOUT("general.rpc.timeout", "120s", PropertyType.TIMEDURATION,
       "Time to wait on I/O for simple, short RPC calls", "1.3.5"),
-  GENERAL_FILENAME_PREFIX("general.filename.", null, PropertyType.PREFIX,
-      "Properties related to filename allocations.", "2.1.3"),
   GENERAL_FILENAME_BASE_ALLOCATION("general.filename.base.allocation", "100", PropertyType.COUNT,
       "The minimum number of filenames that will be allocated from Zookeeper at a time.", "2.1.3"),
   GENERAL_FILENAME_JITTER_ALLOCATION("general.filename.jitter.allocation", "100",
@@ -1839,7 +1837,8 @@ public enum Property {
         || key.startsWith(Property.MASTER_PREFIX.getKey())
         || key.startsWith(Property.GC_PREFIX.getKey())
         || key.startsWith(Property.GENERAL_ARBITRARY_PROP_PREFIX.getKey())
-        || key.startsWith(Property.GENERAL_FILENAME_PREFIX.getKey())
+        || key.equals(Property.GENERAL_FILENAME_BASE_ALLOCATION.getKey())
+        || key.equals(Property.GENERAL_FILENAME_JITTER_ALLOCATION.getKey())
         || key.startsWith(VFS_CONTEXT_CLASSPATH_PROPERTY.getKey())
         || key.startsWith(REPLICATION_PREFIX.getKey());
   }
