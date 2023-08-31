4c484bb7ce3f15d34ae466d23e6a196063e469ce
Dave Marion <dlmarion@apache.org>
2023-08-31 18:59:01 +0000

    Fix IT

--- test/src/main/java/org/apache/accumulo/test/functional/ThriftMaxFrameSizeIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/ThriftMaxFrameSizeIT.java
@@ -19,8 +19,13 @@
 package org.apache.accumulo.test.functional;
 
 import static org.apache.accumulo.test.functional.ConfigurableMacBase.configureForSsl;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.accumulo.core.client.Accumulo;
 import org.apache.accumulo.core.client.AccumuloClient;
@@ -32,20 +37,19 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.thrift.TConfiguration;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 public class ThriftMaxFrameSizeIT extends AccumuloClusterHarness {
 
   private ThriftServerType serverType;
 
   @Override
-  protected Duration defaultTimeout() {
-    return Duration.ofMinutes(1);
-  }
-
-  @Override
   public void configureMiniCluster(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
+    cfg.setNumTservers(1);
     cfg.setProperty(Property.GENERAL_RPC_SERVER_TYPE, serverType.name());
     cfg.setProperty(Property.GENERAL_MAX_MESSAGE_SIZE,
+        Integer.toString(TConfiguration.DEFAULT_MAX_FRAME_SIZE));
+    cfg.setProperty(Property.TSERV_MAX_MESSAGE_SIZE,
         Integer.toString(TConfiguration.DEFAULT_MAX_FRAME_SIZE));
     if (serverType == ThriftServerType.SSL) {
       configureForSsl(cfg,
@@ -58,12 +62,30 @@ public class ThriftMaxFrameSizeIT extends AccumuloClusterHarness {
     TestDefault() {
       serverType = ThriftServerType.getDefault();
     }
+
+    @Test
+    public void testDefaultServerFrameSize() throws Exception {
+      AtomicBoolean succeeded = new AtomicBoolean(false);
+      assertThrows(AssertionFailedError.class,
+          () -> assertTimeoutPreemptively(Duration.ofSeconds(30),
+              () -> testMaxFrameSizeLargerThanDefault(succeeded)));
+      assertFalse(succeeded.get());
+    }
   }
 
   @Nested
   class TestThreadedSelector extends TestMaxFrameSize {
     TestThreadedSelector() {
       serverType = ThriftServerType.THREADED_SELECTOR;
+    }
+
+    @Test
+    public void testThreadedSelectorServerFrameSize() throws Exception {
+      AtomicBoolean succeeded = new AtomicBoolean(false);
+      assertThrows(AssertionFailedError.class,
+          () -> assertTimeoutPreemptively(Duration.ofSeconds(30),
+              () -> testMaxFrameSizeLargerThanDefault(succeeded)));
+      assertFalse(succeeded.get());
     }
   }
 
@@ -72,6 +94,15 @@ public class ThriftMaxFrameSizeIT extends AccumuloClusterHarness {
     TestCustomHsHa() {
       serverType = ThriftServerType.CUSTOM_HS_HA;
     }
+
+    @Test
+    public void testCustomHsHaServerFrameSize() throws Exception {
+      AtomicBoolean succeeded = new AtomicBoolean(false);
+      assertThrows(AssertionFailedError.class,
+          () -> assertTimeoutPreemptively(Duration.ofSeconds(30),
+              () -> testMaxFrameSizeLargerThanDefault(succeeded)));
+      assertFalse(succeeded.get());
+    }
   }
 
   @Nested
@@ -79,19 +110,34 @@ public class ThriftMaxFrameSizeIT extends AccumuloClusterHarness {
     TestThreadPool() {
       serverType = ThriftServerType.THREADPOOL;
     }
+
+    @Test
+    public void testThreadPoolServerFrameSize() throws Exception {
+      AtomicBoolean succeeded = new AtomicBoolean(false);
+      assertThrows(AssertionFailedError.class,
+          () -> assertTimeoutPreemptively(Duration.ofSeconds(30),
+              () -> testMaxFrameSizeLargerThanDefault(succeeded)));
+      assertFalse(succeeded.get());
+    }
   }
 
   @Nested
   class TestSsl extends TestMaxFrameSize {
     TestSsl() {
-      serverType = ThriftServerType.THREADPOOL;
+      serverType = ThriftServerType.SSL;
+    }
+
+    @Test
+    public void testSslServerFrameSize() throws Exception {
+      AtomicBoolean succeeded = new AtomicBoolean(false);
+      testMaxFrameSizeLargerThanDefault(succeeded);
+      assertTrue(succeeded.get());
     }
   }
 
   protected abstract class TestMaxFrameSize {
 
-    @Test
-    public void testMaxFrameSizeLargerThanDefault() throws Exception {
+    public void testMaxFrameSizeLargerThanDefault(AtomicBoolean success) throws Exception {
 
       int maxSize = TConfiguration.DEFAULT_MAX_FRAME_SIZE;
       // make sure we go even bigger than that
@@ -100,10 +146,11 @@ public class ThriftMaxFrameSizeIT extends AccumuloClusterHarness {
       // Ingest with a value width greater than the thrift default size to verify our setting works
       // for max frame size
       try (AccumuloClient accumuloClient = Accumulo.newClient().from(getClientProps()).build()) {
-        String table = getUniqueNames(1)[0];
+        String table = getUniqueNames(1)[0] + serverType.name();
         ReadWriteIT.ingest(accumuloClient, 1, 1, ourSize, 0, table);
         ReadWriteIT.verify(accumuloClient, 1, 1, ourSize, 0, table);
       }
+      success.set(true);
     }
   }
 
