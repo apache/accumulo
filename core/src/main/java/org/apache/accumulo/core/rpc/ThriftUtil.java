2975f1f62a1fe50b5eb84588737546b753c60eb7
Christopher Tubbs <ctubbsii@apache.org>
2023-09-28 01:50:16 -0400

    Clean up Thrift SSL code
    * Remove unneeded ProtocolOverridingSSLSocketFactory that was used to
      support older JDK versions that we no longer support
    * Clean up SslConnectionParams class to remove a workaround for a bug
      that was patched upstream in Thrift
    * Use Thrift's provided TSSLTransportFactory.getClientSocket method that
      allows us to provide custom TSSLTransportParameters and delete the
      private methods copied from Thrift 0.9.1 that were used to implement
      this ourselves (methods which were not kept up to date with the
      improvements in upstream Thrift)
    * Rename SslConnectionParams.getTTransportParams to
      .getTSSLTransportParameters, because the old name for the getter
      method did not reflect the actual type that it returned, and the
      old name did not make it clear it was returning a parameters object
      specifically for SSL / TLS

--- core/src/main/java/org/apache/accumulo/core/rpc/ThriftUtil.java
+++ core/src/main/java/org/apache/accumulo/core/rpc/ThriftUtil.java
@@ -18,21 +18,13 @@
  */
 package org.apache.accumulo.core.rpc;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.InetAddress;
 import java.nio.channels.ClosedByInterruptException;
-import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
 
 import org.apache.accumulo.core.clientImpl.ClientContext;
 import org.apache.accumulo.core.rpc.SaslConnectionParams.SaslMechanism;
@@ -51,8 +43,6 @@ import org.apache.thrift.transport.TTransportException;
 import org.apache.thrift.transport.TTransportFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /**
  * Factory methods for creating Thrift client objects
@@ -220,26 +210,10 @@ public class ThriftUtil {
           transport =
               TSSLTransportFactory.getClientSocket(address.getHost(), address.getPort(), timeout);
         } else {
-          // JDK6's factory doesn't appear to pass the protocol onto the Socket properly so we have
-          // to do some magic to make sure that happens. Not an issue in JDK7
-
-          // Taken from thrift-0.9.1 to make the SSLContext
-          SSLContext sslContext = createSSLContext(sslParams);
-
-          // Create the factory from it
-          SSLSocketFactory sslSockFactory = sslContext.getSocketFactory();
-
-          // Wrap the real factory with our own that will set the protocol on the Socket before
-          // returning it
-          ProtocolOverridingSSLSocketFactory wrappingSslSockFactory =
-              new ProtocolOverridingSSLSocketFactory(sslSockFactory,
-                  new String[] {sslParams.getClientProtocol()});
-
-          // Create the TSocket from that
-          transport =
-              createClient(wrappingSslSockFactory, address.getHost(), address.getPort(), timeout);
-          // TSSLTransportFactory leaves transports open, so no need to open here
+          transport = TSSLTransportFactory.getClientSocket(address.getHost(), address.getPort(),
+              timeout, sslParams.getTSSLTransportParameters());
         }
+        // TSSLTransportFactory leaves transports open, so no need to open here
 
         transport = ThriftUtil.transportFactory().getTransport(transport);
       } else if (saslParams != null) {
@@ -338,12 +312,12 @@ public class ThriftUtil {
         transport = ThriftUtil.transportFactory().getTransport(transport);
       }
       success = true;
+      return transport;
     } finally {
       if (!success && transport != null) {
         transport.close();
       }
     }
-    return transport;
   }
 
   /**
@@ -397,80 +371,6 @@ public class ThriftUtil {
       // IO-like Exception.
       log.warn("Failed to check (and/or perform) Kerberos client re-login", e);
       throw new RuntimeException(e);
-    }
-  }
-
-  /**
-   * Lifted from TSSLTransportFactory in Thrift-0.9.1. The method to create a client socket with an
-   * SSLContextFactory object is not visible to us. Have to use SslConnectionParams instead of
-   * TSSLTransportParameters because no getters exist on TSSLTransportParameters.
-   *
-   * @param params Parameters to use to create the SSLContext
-   */
-  @SuppressFBWarnings(value = "PATH_TRAVERSAL_IN",
-      justification = "code runs in same security context as user who providing the keystore files")
-  private static SSLContext createSSLContext(SslConnectionParams params)
-      throws TTransportException {
-    try {
-      SSLContext ctx = SSLContext.getInstance(params.getClientProtocol());
-      TrustManagerFactory tmf = null;
-      KeyManagerFactory kmf = null;
-
-      if (params.isTrustStoreSet()) {
-        tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        KeyStore ts = KeyStore.getInstance(params.getTrustStoreType());
-        try (FileInputStream fis = new FileInputStream(params.getTrustStorePath())) {
-          ts.load(fis, params.getTrustStorePass().toCharArray());
-        }
-        tmf.init(ts);
-      }
-
-      if (params.isKeyStoreSet()) {
-        kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        KeyStore ks = KeyStore.getInstance(params.getKeyStoreType());
-        try (FileInputStream fis = new FileInputStream(params.getKeyStorePath())) {
-          ks.load(fis, params.getKeyStorePass().toCharArray());
-        }
-        kmf.init(ks, params.getKeyStorePass().toCharArray());
-      }
-
-      if (params.isKeyStoreSet() && params.isTrustStoreSet()) {
-        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-      } else if (params.isKeyStoreSet()) {
-        ctx.init(kmf.getKeyManagers(), null, null);
-      } else {
-        ctx.init(null, tmf.getTrustManagers(), null);
-      }
-      return ctx;
-    } catch (Exception e) {
-      throw new TTransportException("Error creating the transport", e);
-    }
-  }
-
-  /**
-   * Lifted from Thrift-0.9.1 because it was private. Create an SSLSocket with the given factory,
-   * host:port, and timeout.
-   *
-   * @param factory Factory to create the socket from
-   * @param host Destination host
-   * @param port Destination port
-   * @param timeout Socket timeout
-   */
-  private static TSocket createClient(SSLSocketFactory factory, String host, int port, int timeout)
-      throws TTransportException {
-    SSLSocket socket = null;
-    try {
-      socket = (SSLSocket) factory.createSocket(host, port);
-      socket.setSoTimeout(timeout);
-      return new TSocket(socket);
-    } catch (Exception e) {
-      try {
-        if (socket != null) {
-          socket.close();
-        }
-      } catch (IOException ioe) {}
-
-      throw new TTransportException("Could not connect to " + host + " on port " + port, e);
     }
   }
 
