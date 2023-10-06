c90531cc15cba441f016bffff3df9882865337b3
Dave Marion <dlmarion@apache.org>
2023-10-06 17:35:21 +0000

    Handle empty ALLOWED array

--- src/build/ci/find-unapproved-junit.sh
+++ src/build/ci/find-unapproved-junit.sh
@@ -36,8 +36,10 @@ function findalljunitproblems() {
   fi
   # find any new classes using something other than the jupiter API, except those allowed
   grep "$opts" --include='*.java' 'org[.]junit[.](?!jupiter)' | grep -Pv "^(${ALLOWED_PIPE_SEP//./[.]})\$"
-  # find any uses of the jupiter API in the allowed vintage classes
-  grep "$opts" 'org[.]junit[.]jupiter' "${ALLOWED[@]}"
+  if ((${#ALLOWED[@]} != 0)); then
+    # find any uses of the jupiter API in the allowed vintage classes
+    grep "$opts" 'org[.]junit[.]jupiter' "${ALLOWED[@]}"
+  fi
 }
 
 function comparecounts() {
