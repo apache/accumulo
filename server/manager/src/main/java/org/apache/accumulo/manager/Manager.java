apache/accumulo

commit 4e06b2f05423469bab2c6d7b4ed27f147c5ead3c

Avoids full table scans in TabletGroupWatcher in some situations. (#3705)
The manager has an internal class called EventCoordinator that is used
to signal that something happened in the manager.  When anything
signaled an event happened this would cause the tablet group watcher to
scan the entire metadata table.  This commit makes a few changes to
improve this.

First it added the ability to EventCoordinator to signal with more
specific information, like a something happened with a table or a
tablet.

Second the TabletGroupWatcher was adapted to react to this more specific
signaling with narrower scans of the metadata table.  If a signal is
made that something changed with a table, then only that tables metadata
will be scanned.  The TabletGroupWatcher will still do full scans
periodically (based on configuration) or when a something signals a full
scan is needed.

A new IT was added that creates a 1 million splits in a table and then
reads and writes to around 100 random tablets.  The tablets are ondemand
so must be brought online for the read and write.  If doing bringing
them onling takes too long (because a full tables scan is done) then the
test might timeout and fail.

server/manager/src/main/java/org/apache/accumulo/manager/Manager.java
@@ -543,7 +543,8 @@ public class Manager extends AbstractServer
       }
       mergeLock.notifyAll();
     }
-    nextEvent.event("Merge state of %s set to %s", info.getExtent(), state);
+    nextEvent.event(info.getExtent().tableId(), "Merge state of %s set to %s", info.getExtent(),
+        state);
   }
 
   public void clearMergeState(TableId tableId) throws KeeperException, InterruptedException {
@@ -552,7 +553,7 @@ public class Manager extends AbstractServer
       getContext().getZooReaderWriter().recursiveDelete(path, NodeMissingPolicy.SKIP);
       mergeLock.notifyAll();
     }
-    nextEvent.event("Merge state of %s cleared", tableId);
+    nextEvent.event(tableId, "Merge state of %s cleared", tableId);
   }
 
   void setManagerGoalState(ManagerGoalState state) {
@@ -614,7 +615,7 @@ public class Manager extends AbstractServer
       return txids;
     });
 
-    nextEvent.event("Unassignment requested %s", tablet);
+    nextEvent.event(tablet, "Unassignment requested %s", tablet);
   }
 
   public void cancelUnassignmentRequest(KeyExtent tablet, long fateTxid) {
@@ -627,7 +628,7 @@ public class Manager extends AbstractServer
       return v;
     });
 
-    nextEvent.event("Unassignment request canceled %s", tablet);
+    nextEvent.event(tablet, "Unassignment request canceled %s", tablet);
   }
 
   public boolean isUnassignmentRequested(KeyExtent extent) {
@@ -856,7 +857,7 @@ public class Manager extends AbstractServer
 
     @Override
     public void run() {
-      EventCoordinator.Listener eventListener = nextEvent.getListener();
+      EventCoordinator.Tracker eventTracker = nextEvent.getTracker();
       while (stillManager()) {
         long wait = DEFAULT_WAIT_FOR_WATCHER;
         try {
@@ -935,7 +936,7 @@ public class Manager extends AbstractServer
         Span span = TraceUtil.startSpan(this.getClass(), "run::updateStatus");
         try (Scope scope = span.makeCurrent()) {
           wait = updateStatus();
-          eventListener.waitForEvents(wait);
+          eventTracker.waitForEvents(wait);
         } catch (Exception t) {
           TraceUtil.setException(span, t, false);
           log.error("Error balancing tablets, will wait for {} (seconds) and then retry ",
@@ -1172,7 +1173,7 @@ public class Manager extends AbstractServer
     fateServiceHandler = new FateServiceHandler(this);
     managerClientHandler = new ManagerClientServiceHandler(this);
     compactionCoordinator =
-        new CompactionCoordinator(context, tserverSet, security, compactionJobQueues);
+        new CompactionCoordinator(context, tserverSet, security, compactionJobQueues, nextEvent);
     // Start the Manager's Client service
     // Ensure that calls before the manager gets the lock fail
     ManagerClientService.Iface haProxy =
@@ -1697,7 +1698,7 @@ public class Manager extends AbstractServer
 
   @Override
   public void stateChanged(TableId tableId, TableState state) {
-    nextEvent.event("Table state in zookeeper changed for %s to %s", tableId, state);
+    nextEvent.event(tableId, "Table state in zookeeper changed for %s to %s", tableId, state);
     if (state == TableState.OFFLINE) {
       clearMigrations(tableId);
     }

core/src/main/java/org/apache/accumulo/core/logging/TabletLogger.java
@@ -93,7 +93,8 @@ public class TabletLogger {
   }
 
   public static void split(KeyExtent parent, SortedSet<Text> splits) {
-    locLog.debug("Split {} into {}", parent, splits);
+    locLog.debug("Split {} into {} tablets", parent, splits.size() + 1);
+    locLog.trace("Split {} into {}", parent, splits);
   }
 
   /**

server/base/src/main/java/org/apache/accumulo/server/manager/state/ClosableIterable.java
@@ -18,27 +18,7 @@
  */
 package org.apache.accumulo.server.manager.state;
 
-import java.util.List;
-
-import org.apache.accumulo.core.clientImpl.ClientContext;
-import org.apache.accumulo.core.data.Range;
-import org.apache.accumulo.core.manager.state.TabletManagement;
-import org.apache.accumulo.core.metadata.RootTable;
-import org.apache.accumulo.core.metadata.schema.Ample.DataLevel;
-
-class RootTabletStateStore extends MetaDataStateStore {
-
-  RootTabletStateStore(DataLevel level, ClientContext context, CurrentState state) {
-    super(level, context, state, RootTable.NAME);
-  }
-
+public interface ClosableIterable<T> extends Iterable<T> {
   @Override
-  public ClosableIterator<TabletManagement> iterator(List<Range> ranges) {
-    return new TabletManagementScanner(context, ranges, state, RootTable.NAME);
-  }
-
-  @Override
-  public String name() {
-    return "Metadata Tablets";
-  }
+  ClosableIterator<T> iterator();
 }

server/base/src/main/java/org/apache/accumulo/server/manager/state/LoggingTabletStateStore.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.logging.TabletLogger;
 import org.apache.accumulo.core.manager.state.TabletManagement;
 import org.apache.accumulo.core.metadata.TServerInstance;
@@ -54,13 +55,8 @@ class LoggingTabletStateStore implements TabletStateStore {
   }
 
   @Override
-  public ClosableIterator<TabletManagement> iterator() {
-    return wrapped.iterator();
-  }
-
-  @Override
-  public boolean addTabletStateChange(TabletManagement tablet) {
-    return this.wrapped.addTabletStateChange(tablet);
+  public ClosableIterator<TabletManagement> iterator(List<Range> ranges) {
+    return wrapped.iterator(ranges);
   }
 
   @Override

server/base/src/main/java/org/apache/accumulo/server/manager/state/MetaDataStateStore.java
@@ -19,15 +19,15 @@
 package org.apache.accumulo.server.manager.state;
 
 import java.util.Collection;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.List;
 
 import org.apache.accumulo.core.clientImpl.ClientContext;
+import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.manager.state.TabletManagement;
 import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.accumulo.core.metadata.schema.Ample;
 import org.apache.accumulo.core.metadata.schema.Ample.ConditionalResult.Status;
 import org.apache.accumulo.core.metadata.schema.Ample.DataLevel;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
 import org.apache.accumulo.core.metadata.schema.TabletMetadata;
 
 class MetaDataStateStore extends AbstractTabletStateStore implements TabletStateStore {
@@ -37,8 +37,6 @@ class MetaDataStateStore extends AbstractTabletStateStore implements TabletState
   private final String targetTableName;
   private final Ample ample;
   private final DataLevel level;
-  protected final ArrayBlockingQueue<TabletManagement> knownStateChanges =
-      new ArrayBlockingQueue<>(1_000);
 
   protected MetaDataStateStore(DataLevel level, ClientContext context, CurrentState state,
       String targetTableName) {
@@ -60,14 +58,8 @@ class MetaDataStateStore extends AbstractTabletStateStore implements TabletState
   }
 
   @Override
-  public ClosableIterator<TabletManagement> iterator() {
-    return new TabletManagementScanner(context, TabletsSection.getRange(), state, targetTableName,
-        knownStateChanges);
-  }
-
-  @Override
-  public boolean addTabletStateChange(TabletManagement tablet) {
-    return this.knownStateChanges.offer(tablet);
+  public ClosableIterator<TabletManagement> iterator(List<Range> ranges) {
+    return new TabletManagementScanner(context, ranges, state, targetTableName);
   }
 
   @Override

server/base/src/main/java/org/apache/accumulo/server/manager/state/TabletManagementIterator.java
@@ -275,7 +275,7 @@ public class TabletManagementIterator extends SkippingIterator {
     final long sumOfFileSizes =
         tm.getFilesMap().values().stream().mapToLong(DataFileValue::getSize).sum();
     final boolean shouldSplit = sumOfFileSizes > splitThreshold;
-    LOG.debug("{} should split? sum: {}, threshold: {}, result: {}", tm.getExtent(), sumOfFileSizes,
+    LOG.trace("{} should split? sum: {}, threshold: {}, result: {}", tm.getExtent(), sumOfFileSizes,
         splitThreshold, shouldSplit);
     return shouldSplit;
   }
@@ -292,8 +292,8 @@ public class TabletManagementIterator extends SkippingIterator {
         onlineTables.contains(tm.getTableId()) && tm.getOperationId() == null;
 
     TabletState state = TabletState.compute(tm, current, balancer, tserverResourceGroups);
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("{} is {}. Table is {}line. Tablet hosting goal is {}, hostingRequested: {}",
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("{} is {}. Table is {}line. Tablet hosting goal is {}, hostingRequested: {}",
           tm.getExtent(), state, (shouldBeOnline ? "on" : "off"), tm.getHostingGoal(),
           tm.getHostingRequested());
     }
@@ -402,7 +402,6 @@ public class TabletManagementIterator extends SkippingIterator {
     balancer = Property.createInstanceFromPropertyName(conf, Property.MANAGER_TABLET_BALANCER,
         TabletBalancer.class, new SimpleLoadBalancer());
     balancer.init(benv);
-    LOG.debug("Balancer is set to {}", balancer.getClass().getSimpleName());
   }
 
   @Override

server/base/src/main/java/org/apache/accumulo/server/manager/state/TabletManagementScanner.java
@@ -20,15 +20,12 @@ package org.apache.accumulo.server.manager.state;
 
 import java.io.IOException;
 import java.lang.ref.Cleaner.Cleanable;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
-import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.accumulo.core.client.AccumuloException;
@@ -60,11 +57,10 @@ public class TabletManagementScanner implements ClosableIterator<TabletManagemen
   private final BatchScanner mdScanner;
   private final Iterator<Entry<Key,Value>> iter;
   private final AtomicBoolean closed = new AtomicBoolean(false);
-  private final Queue<TabletManagement> knownTabletModifications;
 
   // This constructor is called from TabletStateStore implementations
-  public TabletManagementScanner(ClientContext context, Range range, CurrentState state,
-      String tableName, Queue<TabletManagement> knownTabletModifications) {
+  public TabletManagementScanner(ClientContext context, List<Range> ranges, CurrentState state,
+      String tableName) {
     // scan over metadata table, looking for tablets in the wrong state based on the live servers
     // and online tables
     try {
@@ -95,14 +91,13 @@ public class TabletManagementScanner implements ClosableIterator<TabletManagemen
     }
     cleanable = CleanerUtil.unclosed(this, TabletManagementScanner.class, closed, log, mdScanner);
     TabletManagementIterator.configureScanner(mdScanner, state);
-    mdScanner.setRanges(Collections.singletonList(range));
+    mdScanner.setRanges(ranges);
     iter = mdScanner.iterator();
-    this.knownTabletModifications = knownTabletModifications;
   }
 
   // This constructor is called from utilities and tests
   public TabletManagementScanner(ClientContext context, Range range, String tableName) {
-    this(context, range, null, tableName, new ArrayBlockingQueue<>(1_000));
+    this(context, List.of(range), null, tableName);
   }
 
   @Override
@@ -120,9 +115,7 @@ public class TabletManagementScanner implements ClosableIterator<TabletManagemen
     if (closed.get()) {
       return false;
     }
-    if (!knownTabletModifications.isEmpty()) {
-      return true;
-    }
+
     boolean result = iter.hasNext();
     if (!result) {
       close();
@@ -135,13 +128,7 @@ public class TabletManagementScanner implements ClosableIterator<TabletManagemen
     if (closed.get()) {
       throw new NoSuchElementException(this.getClass().getSimpleName() + " is closed");
     }
-    if (!knownTabletModifications.isEmpty()) {
-      TabletManagement tm = knownTabletModifications.poll();
-      log.trace("Returning known tablet modification, extent: {}, hostingGoal: {}, actions: {}",
-          tm.getTabletMetadata().getExtent(), tm.getTabletMetadata().getHostingGoal(),
-          tm.getActions());
-      return tm;
-    }
+
     Entry<Key,Value> e = iter.next();
     try {
       TabletManagement tm = TabletManagementIterator.decode(e);

commit 29c15e5d6fdd82e5baea2fb600ea7000f06fb67e

fixes merge
merge was broken by #3705

server/manager/src/main/java/org/apache/accumulo/manager/Manager.java
@@ -749,7 +749,7 @@ public class Manager extends AbstractServer
         final boolean overlaps = mergeInfo.overlaps(extent);
 
         if (overlaps) {
-          log.debug("mergeInfo overlaps: {} true", extent);
+          log.debug("mergeInfo overlaps: {} true {}", extent, mergeInfo.getState());
           switch (mergeInfo.getState()) {
             case NONE:
             case COMPLETE:

server/manager/src/main/java/org/apache/accumulo/manager/TabletGroupWatcher.java
@@ -327,6 +327,8 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
   private static class TableMgmtStats {
     int[] counts = new int[TabletState.values().length];
     private int totalUnloaded;
+
+    Map<TableId,MergeStats> mergeStatsCache = new HashMap<>();
   }
 
   private TableMgmtStats manageTablets(Iterator<TabletManagement> iter,
@@ -337,7 +339,6 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
     TableMgmtStats tableMgmtStats = new TableMgmtStats();
     int unloaded = 0;
 
-    Map<TableId,MergeStats> mergeStatsCache = new HashMap<>();
     Map<TableId,MergeStats> currentMerges = new HashMap<>();
     for (MergeInfo merge : manager.merges()) {
       if (merge.getExtent() != null) {
@@ -396,7 +397,7 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
 
       final TableConfiguration tableConf = manager.getContext().getTableConfiguration(tableId);
 
-      final MergeStats mergeStats = mergeStatsCache.computeIfAbsent(tableId, k -> {
+      final MergeStats mergeStats = tableMgmtStats.mergeStatsCache.computeIfAbsent(tableId, k -> {
         var mStats = currentMerges.get(k);
         return mStats != null ? mStats : new MergeStats(new MergeInfo());
       });
@@ -634,8 +635,7 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
               tabletMgmtStats.totalUnloaded);
         }
 
-        // TODO
-        // updateMergeState(mergeStatsCache);
+        updateMergeState(tabletMgmtStats.mergeStatsCache);
 
         synchronized (this) {
           lastScanServers = ImmutableSortedSet.copyOf(currentTServers.keySet());