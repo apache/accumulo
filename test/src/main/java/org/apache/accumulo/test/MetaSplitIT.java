1adcb7f21fca2468ca1ea4c9b60d26ce060f4b89
Christopher L. Shannon (cshannon) <christopher.l.shannon@gmail.com>
2023-11-04 12:09:19 -0400

    Update MetaSplitIT to verify fenced files
    This test already splits and merges the metadata table so the updates
    here add some extra checks to verify ranged files have been created when
    they should and that all data can still be scanned after ranged files
    were created on merge.
    
    This addresses part of #3766

--- test/src/main/java/org/apache/accumulo/test/MetaSplitIT.java
+++ test/src/main/java/org/apache/accumulo/test/MetaSplitIT.java
@@ -18,8 +18,10 @@
  */
 package org.apache.accumulo.test;
 
+import static org.apache.accumulo.test.util.FileMetadataUtil.countFencedFiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -33,9 +35,12 @@ import org.apache.accumulo.core.client.AccumuloClient;
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.admin.CompactionConfig;
 import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.accumulo.core.metadata.RootTable;
+import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
 import org.apache.hadoop.io.Text;
 import org.junit.jupiter.api.AfterEach;
@@ -111,29 +116,63 @@ public class MetaSplitIT extends AccumuloClusterHarness {
   @Test
   public void testMetadataTableSplit() throws Exception {
     try (AccumuloClient client = Accumulo.newClient().from(getClientProps()).build()) {
+      // disable compactions
+      client.tableOperations().setProperty(MetadataTable.NAME, Property.TABLE_MAJC_RATIO.getKey(),
+          "9999");
+
       TableOperations opts = client.tableOperations();
       for (int i = 1; i <= 10; i++) {
         opts.create(Integer.toString(i));
       }
       try {
+        assertEquals(0, countFencedFiles(getServerContext(), MetadataTable.NAME));
+        verifyMetadataTableScan(client);
         opts.merge(MetadataTable.NAME, new Text("01"), new Text("02"));
         checkMetadataSplits(1, opts);
+        verifyMetadataTableScan(client);
         addSplits(opts, "4 5 6 7 8".split(" "));
         checkMetadataSplits(6, opts);
+        verifyMetadataTableScan(client);
+
         opts.merge(MetadataTable.NAME, new Text("6"), new Text("9"));
         checkMetadataSplits(4, opts);
+        // Merging tablets should produce fenced files because of no-chop merge
+        assertTrue(countFencedFiles(getServerContext(), MetadataTable.NAME) > 0);
+        verifyMetadataTableScan(client);
+
         addSplits(opts, "44 55 66 77 88".split(" "));
         checkMetadataSplits(9, opts);
+        assertTrue(countFencedFiles(getServerContext(), MetadataTable.NAME) > 0);
+        verifyMetadataTableScan(client);
+
         opts.merge(MetadataTable.NAME, new Text("5"), new Text("7"));
         checkMetadataSplits(6, opts);
+        assertTrue(countFencedFiles(getServerContext(), MetadataTable.NAME) > 0);
+        verifyMetadataTableScan(client);
+
         opts.merge(MetadataTable.NAME, null, null);
         checkMetadataSplits(0, opts);
+        assertTrue(countFencedFiles(getServerContext(), MetadataTable.NAME) > 0);
+        verifyMetadataTableScan(client);
+
+        opts.compact(MetadataTable.NAME, new CompactionConfig());
+        // Should be no more fenced files after compaction
+        assertEquals(0, countFencedFiles(getServerContext(), MetadataTable.NAME));
       } finally {
         for (int i = 1; i <= 10; i++) {
           opts.delete(Integer.toString(i));
         }
       }
     }
+  }
+
+  // Count the number of entries that can be read in the Metadata table
+  // This verifies all the entries can still be read after splits/merges
+  // when ranged files are used
+  private void verifyMetadataTableScan(AccumuloClient client) throws Exception {
+    // There should be 50 entries, 5 entries per Tablet
+    assertEquals(50,
+        client.createScanner(MetadataTable.NAME, Authorizations.EMPTY).stream().count());
   }
 
   private static void checkMetadataSplits(int numSplits, TableOperations opts)
