ac657ed6f8ecac295bf72f02a25c6a7549b8233c
Keith Turner <kturner@apache.org>
2024-02-06 22:46:31 +0000

    clean up test and add todos

--- server/base/src/test/java/org/apache/accumulo/server/SetEqualityIteratorTest.java
+++ server/base/src/test/java/org/apache/accumulo/server/SetEqualityIteratorTest.java
@@ -18,26 +18,23 @@
  */
 package org.apache.accumulo.server;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Mutation;
-import org.apache.accumulo.core.data.PartialKey;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.dataImpl.KeyExtent;
 import org.apache.accumulo.core.iteratorsImpl.system.SortedMapIterator;
 import org.apache.accumulo.core.metadata.ReferencedTabletFile;
-import org.apache.accumulo.core.metadata.TServerInstance;
+import org.apache.accumulo.core.metadata.StoredTabletFile;
 import org.apache.accumulo.core.metadata.schema.DataFileValue;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.FutureLocationColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema;
 import org.apache.accumulo.core.metadata.schema.TabletMetadata;
 import org.apache.accumulo.server.metadata.iterators.SetEqualityIterator;
 import org.apache.hadoop.fs.Path;
@@ -45,38 +42,33 @@ import org.apache.hadoop.io.Text;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.google.common.net.HostAndPort;
-
 public class SetEqualityIteratorTest {
 
   private SetEqualityIterator setEqualityIterator;
   private SortedMapIterator sortedMapIterator;
 
+  private KeyExtent extent = new KeyExtent(TableId.of("5"), new Text("df"), new Text("da"));
+
+  private StoredTabletFile file1 =
+      new ReferencedTabletFile(new Path("dfs://nn1/acc/tables/1/t-0001/sf1.rf")).insert();
+  private StoredTabletFile file2 =
+      new ReferencedTabletFile(new Path("dfs://nn1/acc/tables/1/t-0001/sf2.rf")).insert();
+  private StoredTabletFile file3 =
+      new ReferencedTabletFile(new Path("dfs://nn1/acc/tables/1/t-0001/sf3.rf")).insert();
+
   @BeforeEach
   public void setUp() throws IOException {
-    // Create a TServerInstance for server1
-    TServerInstance ser1 = new TServerInstance(HostAndPort.fromParts("server1", 8555), "s001");
 
-    // Create a TabletMetadata with sample data
-    KeyExtent extent = new KeyExtent(TableId.of("5"), new Text("df"), new Text("da"));
-
-    // Create a mutation to represent the assignment of a tablet to a server
-    Mutation assignmentMutation = TabletColumnFamily.createPrevRowMutation(extent);
-    assignmentMutation.at().family(FutureLocationColumnFamily.NAME).qualifier(ser1.getSession())
-        .put(ser1.getHostPort());
-
-    // Add the mutation to the key values using putFile with ReferencedTabletFile
-    ReferencedTabletFile mutationFile = new ReferencedTabletFile(new Path("mutation_file"));
-    TabletMetadata tm =
-        TabletMetadata.builder(extent).putFile(mutationFile, new DataFileValue(0, 0))
-            .putFile(new ReferencedTabletFile(new Path("hdfs://nn1/acc/tables/1/t-0001/sf1.rf"))
-                .insert(), new DataFileValue(555, 23))
-            .putFile(new ReferencedTabletFile(new Path("hdfs://nn1/acc/tables/1/t-0001/sf2.rf"))
-                .insert(), new DataFileValue(234, 13))
-            .putFlushId(6).build();
+    TabletMetadata tm = TabletMetadata.builder(extent).putFile(file1, new DataFileValue(0, 0))
+        .putFile(file2, new DataFileValue(555, 23)).putFile(file3, new DataFileValue(234, 13))
+        .putFlushId(6).build();
 
     // Convert TabletMetadata to a SortedMap
     SortedMap<Key,Value> sortedMap = new TreeMap<>(tm.getKeyValues());
+
+    var extent2 = new KeyExtent(extent.tableId(), null, extent.endRow());
+    // TODO create another tablet metadata using extent2 w/ diff files and add it to sortedMap. This
+    // will add another row to the test data which ensures that iterator does not go to another row.
 
     // Create a SortedMapIterator using the SortedMap
     sortedMapIterator = new SortedMapIterator(sortedMap);
@@ -87,19 +79,27 @@ public class SetEqualityIteratorTest {
   }
 
   @Test
-  public void testSeek() throws IOException {
+  public void testTabletWithMultipleFiles() throws IOException {
     // Creating a test range
-    Text tabletRow = new Text("row");
-    Text family = new Text("family");
-    Key startKey = new Key(tabletRow, family);
-    Key endKey = new Key(tabletRow, family).followingKey(PartialKey.ROW_COLFAM);
-    Range range = new Range(startKey, true, endKey, false);
+    Text tabletRow = new Text(extent.toMetaRow());
+    Text family = MetadataSchema.TabletsSection.DataFileColumnFamily.NAME;
+
+    Range range = Range.exact(tabletRow, family);
 
     // Invoking the seek method
     setEqualityIterator.seek(range, Collections.emptyList(), false);
 
     // Asserting the result
-    assertEquals(new Key("row1", "family"), setEqualityIterator.getTopKey());
-    // assertEquals(new Value("..."), setEqualityIterator.getTopValue());
+    assertEquals(new Key(tabletRow, family), setEqualityIterator.getTopKey());
+    // The iterator should produce a value that is equal to the expected value on the condition
+    var condition = SetEqualityIterator.createCondition(Set.of(file1, file2, file3),
+        storedTabletFile -> storedTabletFile.getMetadata().getBytes(UTF_8), family);
+    assertArrayEquals(condition.getValue().toArray(), setEqualityIterator.getTopValue().get());
+
   }
+
+  // TODO test tablets with no files and tablet with one file.. will probably need to refactor code
+  // in setUp so that it can be used to create tablet metadata with different sets of files and then
+  // create an iterator over that tablet
+
 }
