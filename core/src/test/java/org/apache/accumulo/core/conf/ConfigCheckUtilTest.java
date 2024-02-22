af50af77cea24a8c4c8cfe62c09b4238577e1034
Dave Marion <dlmarion@apache.org>
2024-02-22 12:19:36 -0500

    Added validation of configuration during upgrade (#4289)
    Backported second parameter to CheckConfigUtil.validate and updated
    all call locations to supply the second parameter. Added method to
    UpgradeCoordinator to call CheckConfigUtil.validate after the
    upgradeMetadata method is called on all Upgraders
    
    Fixes #3972

--- core/src/test/java/org/apache/accumulo/core/conf/ConfigCheckUtilTest.java
+++ core/src/test/java/org/apache/accumulo/core/conf/ConfigCheckUtilTest.java
@@ -40,51 +40,51 @@ public class ConfigCheckUtilTest {
     m.put(Property.MANAGER_TABLET_BALANCER.getKey(),
         "org.apache.accumulo.server.manager.balancer.TableLoadBalancer");
     m.put(Property.MANAGER_BULK_RETRIES.getKey(), "3");
-    ConfigCheckUtil.validate(m.entrySet());
+    ConfigCheckUtil.validate(m.entrySet(), "test");
   }
 
   @Test
   public void testPass_Empty() {
-    ConfigCheckUtil.validate(m.entrySet());
+    ConfigCheckUtil.validate(m.entrySet(), "test");
   }
 
   @Test
   public void testPass_UnrecognizedValidProperty() {
     m.put(Property.MANAGER_CLIENTPORT.getKey(), "9999");
     m.put(Property.MANAGER_PREFIX.getKey() + "something", "abcdefg");
-    ConfigCheckUtil.validate(m.entrySet());
+    ConfigCheckUtil.validate(m.entrySet(), "test");
   }
 
   @Test
   public void testPass_UnrecognizedProperty() {
     m.put(Property.MANAGER_CLIENTPORT.getKey(), "9999");
     m.put("invalid.prefix.value", "abcdefg");
-    ConfigCheckUtil.validate(m.entrySet());
+    ConfigCheckUtil.validate(m.entrySet(), "test");
   }
 
   @Test
   public void testFail_Prefix() {
     m.put(Property.MANAGER_CLIENTPORT.getKey(), "9999");
     m.put(Property.MANAGER_PREFIX.getKey(), "oops");
-    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet()));
+    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet(), "test"));
   }
 
   @Test
   public void testFail_InstanceZkTimeoutOutOfRange() {
     m.put(Property.INSTANCE_ZK_TIMEOUT.getKey(), "10ms");
-    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet()));
+    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet(), "test"));
   }
 
   @Test
   public void testFail_badCryptoFactory() {
     m.put(Property.INSTANCE_CRYPTO_FACTORY.getKey(), "DoesNotExistCryptoFactory");
-    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet()));
+    assertThrows(ConfigCheckException.class, () -> ConfigCheckUtil.validate(m.entrySet(), "test"));
   }
 
   @Test
   public void testPass_defaultCryptoFactory() {
     m.put(Property.INSTANCE_CRYPTO_FACTORY.getKey(),
         Property.INSTANCE_CRYPTO_FACTORY.getDefaultValue());
-    ConfigCheckUtil.validate(m.entrySet());
+    ConfigCheckUtil.validate(m.entrySet(), "test");
   }
 }
