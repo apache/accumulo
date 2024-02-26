6c972dafbba1d4a547ff8d5e5d4550b284cc4409
arbaazkhan1 <bazzy_94@yahoo.com>
2024-02-26 16:31:56 -0500

    Added changes

--- server/manager/src/main/java/org/apache/accumulo/manager/TabletGroupWatcher.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/TabletGroupWatcher.java
@@ -682,7 +682,19 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
 
         iter = store.iterator(tableMgmtParams);
         var tabletMgmtStats = manageTablets(iter, tableMgmtParams, currentTServers, true);
-        lookForTabletsNeedingVolReplacement = tabletMgmtStats.totalVolumeReplacements != 0 || tabletMgmtStats.tabletsWithErrors != 0;
+
+        // If currently looking for volume replacements, determine if the next round needs to look.
+        if (lookForTabletsNeedingVolReplacement) {
+          // Continue to look for tablets needing volume replacement if there was an error
+          // processing
+          // tablets in the call to manageTablets() or if we are still performing volume
+          // replacement.
+          // We only want to stop looking for tablets that need volume replacement when we have
+          // successfully
+          // processed all tablet metadata and no more volume replacements are being performed.
+          lookForTabletsNeedingVolReplacement = tabletMgmtStats.totalVolumeReplacements != 0
+              || tabletMgmtStats.tabletsWithErrors != 0;
+        }
 
         // provide stats after flushing changes to avoid race conditions w/ delete table
         stats.end(managerState);
