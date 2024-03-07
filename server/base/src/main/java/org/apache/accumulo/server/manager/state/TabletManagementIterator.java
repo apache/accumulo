8e57faad5b1ebe2adc50ef2b798a3f032ff28ec1
Keith Turner <kturner@apache.org>
2024-03-06 21:55:48 -0500

    partially avoids splitting and compacting offline tablets
    Modifies TabletManagementIterator so that it does not return tablets
    that need split of compaction of if the table is offline.  This
    partially prevents those tablets from splitting.  Still need to handle
    things that were queued for split of compaction when a table is taken
    offline.  Once those other changes are made this change will still avoid
    queuing and processing tablets for work that does not need to be done.

--- server/base/src/main/java/org/apache/accumulo/server/manager/state/TabletManagementIterator.java
+++ server/base/src/main/java/org/apache/accumulo/server/manager/state/TabletManagementIterator.java
@@ -252,7 +252,7 @@ public class TabletManagementIterator extends SkippingIterator {
       reasonsToReturnThisTablet.add(ManagementAction.NEEDS_LOCATION_UPDATE);
     }
 
-    if (tm.getOperationId() == null
+    if (tm.getOperationId() == null && tabletMgmtParams.isTableOnline(tm.getTableId())
         && Collections.disjoint(REASONS_NOT_TO_SPLIT_OR_COMPACT, reasonsToReturnThisTablet)) {
       try {
         final long splitThreshold =
