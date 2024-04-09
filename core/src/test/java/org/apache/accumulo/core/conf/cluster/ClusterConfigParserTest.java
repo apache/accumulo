02d0676cbf9e011bbdd0e08461ea93e9898ce9ac
Keith Turner <kturner@apache.org>
2024-04-09 20:44:30 +0000

    WIP adds compactors per host per queue to accumulo-cluster

--- core/src/test/java/org/apache/accumulo/core/conf/cluster/ClusterConfigParserTest.java
+++ core/src/test/java/org/apache/accumulo/core/conf/cluster/ClusterConfigParserTest.java
@@ -85,7 +85,7 @@ public class ClusterConfigParserTest {
     Map<String,String> contents =
         ClusterConfigParser.parseConfiguration(new File(configFile.toURI()).getAbsolutePath());
 
-    assertEquals(13, contents.size());
+    assertEquals(14, contents.size());
     assertTrue(contents.containsKey("manager"));
     assertEquals("localhost1 localhost2", contents.get("manager"));
     assertTrue(contents.containsKey("monitor"));
@@ -113,8 +113,10 @@ public class ClusterConfigParserTest {
     assertEquals("2", contents.get("tservers_per_host"));
     assertTrue(contents.containsKey("sservers_per_host"));
     assertEquals("2", contents.get("sservers_per_host"));
-    assertTrue(contents.containsKey("compactors_per_host"));
-    assertEquals("3", contents.get("compactors_per_host"));
+    assertTrue(contents.containsKey("compactors_per_host.q1"));
+    assertEquals("3", contents.get("compactors_per_host.q1"));
+    assertTrue(contents.containsKey("compactors_per_host.q2"));
+    assertEquals("1", contents.get("compactors_per_host.q2"));
   }
 
   @Test
@@ -173,7 +175,6 @@ public class ClusterConfigParserTest {
     expected.put("TSERVER_HOSTS", "localhost1 localhost2 localhost3 localhost4");
     expected.put("NUM_TSERVERS", "${NUM_TSERVERS:=1}");
     expected.put("NUM_SSERVERS", "${NUM_SSERVERS:=1}");
-    expected.put("NUM_COMPACTORS", "${NUM_COMPACTORS:=1}");
 
     expected.replaceAll((k, v) -> '"' + v + '"');
 
@@ -232,7 +233,8 @@ public class ClusterConfigParserTest {
     expected.put("SSERVER_HOSTS_cheap", "burstyvm1 burstyvm2");
     expected.put("NUM_TSERVERS", "${NUM_TSERVERS:=2}");
     expected.put("NUM_SSERVERS", "${NUM_SSERVERS:=2}");
-    expected.put("NUM_COMPACTORS", "${NUM_COMPACTORS:=3}");
+    expected.put("NUM_COMPACTORS_q1", "3");
+    expected.put("NUM_COMPACTORS_q2", "1");
 
     expected.replaceAll((k, v) -> {
       return '"' + v + '"';
