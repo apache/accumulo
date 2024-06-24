4f4c182c705b18c036a4a287d6c4491531705ce5
Keith Turner <kturner@apache.org>
2024-06-24 11:03:27 -0400

    adds trace logging of tserver set diffs to TGW
    This logging may be useful when debugging issues with
    balancing in the manager.

--- server/manager/src/main/java/org/apache/accumulo/manager/TabletGroupWatcher.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/TabletGroupWatcher.java
@@ -109,6 +109,7 @@ import org.slf4j.event.Level;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Sets;
 
 abstract class TabletGroupWatcher extends AccumuloDaemonThread {
 
@@ -143,7 +144,16 @@ abstract class TabletGroupWatcher extends AccumuloDaemonThread {
    * time an assignment scan was started.
    */
   synchronized boolean isSameTserversAsLastScan(Set<TServerInstance> candidates) {
-    return candidates.equals(lastScanServers);
+    boolean same = candidates.equals(lastScanServers);
+    if (!same && Manager.log.isTraceEnabled()) {
+      Manager.log.trace("{} set difference candidates-lastScanServers : {}", store.name(),
+          Sets.difference(candidates, lastScanServers));
+      Manager.log.trace("{} set difference lastScanServers-candidates : {}", store.name(),
+          Sets.difference(lastScanServers, candidates));
+      Manager.log.trace("{} set intersection(lastScanServers,candidates) size : {}", store.name(),
+          Sets.intersection(lastScanServers, candidates).size());
+    }
+    return same;
   }
 
   /**
