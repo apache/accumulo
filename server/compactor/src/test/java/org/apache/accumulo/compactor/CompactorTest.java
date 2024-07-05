06ba905fc146cbf1ed76b0b7585b07c15523f922
Dom Garguilo <domgarguilo@apache.org>
2024-07-05 15:27:49 -0400

    Fix mocking in compactorTest

--- server/compactor/src/test/java/org/apache/accumulo/compactor/CompactorTest.java
+++ server/compactor/src/test/java/org/apache/accumulo/compactor/CompactorTest.java
@@ -57,6 +57,7 @@ import org.apache.accumulo.server.ServerContext;
 import org.apache.accumulo.server.compaction.FileCompactor;
 import org.apache.accumulo.server.compaction.RetryableThriftCall.RetriesExceededException;
 import org.apache.accumulo.server.fs.VolumeManagerImpl;
+import org.apache.accumulo.server.metrics.ProcessMetrics;
 import org.apache.accumulo.server.rpc.ServerAddress;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
@@ -190,6 +191,7 @@ public class CompactorTest {
     private volatile boolean completedCalled = false;
     private volatile boolean failedCalled = false;
     private TCompactionStatusUpdate latestState = null;
+    private ProcessMetrics processMetrics;
 
     SuccessfulCompactor(Supplier<UUID> uuid, ServerAddress address, TExternalCompactionJob job,
         ServerContext context, ExternalCompactionId eci) {
@@ -199,6 +201,7 @@ public class CompactorTest {
       this.job = job;
       this.context = context;
       this.eci = eci;
+      this.processMetrics = new ProcessMetrics();
     }
 
     @Override
@@ -323,6 +326,7 @@ public class CompactorTest {
     PowerMock.resetAll();
     PowerMock.suppress(PowerMock.methods(Halt.class, "halt"));
     PowerMock.suppress(PowerMock.constructor(AbstractServer.class));
+    PowerMock.suppress(PowerMock.methods(AbstractServer.class, "idleProcessCheck"));
 
     ServerAddress client = PowerMock.createNiceMock(ServerAddress.class);
     HostAndPort address = HostAndPort.fromString("localhost:10240");
@@ -373,6 +377,7 @@ public class CompactorTest {
     PowerMock.resetAll();
     PowerMock.suppress(PowerMock.methods(Halt.class, "halt"));
     PowerMock.suppress(PowerMock.constructor(AbstractServer.class));
+    PowerMock.suppress(PowerMock.methods(AbstractServer.class, "idleProcessCheck"));
 
     ServerAddress client = PowerMock.createNiceMock(ServerAddress.class);
     HostAndPort address = HostAndPort.fromString("localhost:10240");
@@ -424,6 +429,7 @@ public class CompactorTest {
     PowerMock.resetAll();
     PowerMock.suppress(PowerMock.methods(Halt.class, "halt"));
     PowerMock.suppress(PowerMock.constructor(AbstractServer.class));
+    PowerMock.suppress(PowerMock.methods(AbstractServer.class, "idleProcessCheck"));
 
     ServerAddress client = PowerMock.createNiceMock(ServerAddress.class);
     HostAndPort address = HostAndPort.fromString("localhost:10240");
