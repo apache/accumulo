e8053fe39a520fe4df194085ba5f518cc5839ee0
meatballspaghetti <69256191+meatballspaghetti@users.noreply.github.com>
2024-07-24 19:41:03 +0000

    Add bounds checking to reset method
    - Add bounds checking to reset method in ArrayByteSequence.
      Repushed.
    
    Fixes #4738

--- core/src/main/java/org/apache/accumulo/core/data/ArrayByteSequence.java
+++ core/src/main/java/org/apache/accumulo/core/data/ArrayByteSequence.java
@@ -160,6 +160,10 @@ public class ArrayByteSequence extends ByteSequence implements Serializable {
    * @since 3.1.0
    */
   public void reset(byte[] data, int offset, int length) {
+    if (offset < 0 || offset > data.length || length < 0 || (offset + length) > data.length) {
+      throw new IllegalArgumentException(" Bad offset and/or length data.length = " + data.length
+          + " offset = " + offset + " length = " + length);
+    }
     this.data = data;
     this.offset = offset;
     this.length = length;
@@ -190,20 +194,5 @@ public class ArrayByteSequence extends ByteSequence implements Serializable {
   @Override
   public String toString() {
     return new String(data, offset, length, UTF_8);
-  }
-
-  /**
-   * Resets data, offset, and length of a backing array for this byte sequence object.
-   *
-   * @since 3.1.0
-   */
-  public void reset(byte[] data, int offset, int length) {
-    if (offset < 0 || offset > data.length || length < 0 || (offset + length) > data.length) {
-      throw new IllegalArgumentException(" Bad offset and/or length data.length = " + data.length
-          + " offset = " + offset + " length = " + length);
-    }
-    this.data = data;
-    this.offset = offset;
-    this.length = length;
   }
 }
