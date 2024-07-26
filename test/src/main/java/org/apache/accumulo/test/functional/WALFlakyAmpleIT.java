12c6433acb4b734e654594e76293710a5e4bd17e
Keith Turner <kturner@apache.org>
2024-07-26 08:33:38 -0700

    tests log recovery using FlakyAmple (#4762)
    Modfied a log recovery test to run using FlakyAmple
    This will provide coverage for the case of flay
    conditional mutations that are written to tablets
    as part log recovery.
    
    Most of the code in WalSunnyDayIT was moved to
    WalSunnyDatBaseIT w/o modification.  So even
    though this commit has lots of diff, there are
    very little actual changes.
    
    This change is for #4710

--- test/src/main/java/org/apache/accumulo/test/functional/WALFlakyAmpleIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/WALFlakyAmpleIT.java
@@ -18,17 +18,17 @@
  */
 package org.apache.accumulo.test.functional;
 
-import static org.apache.accumulo.harness.AccumuloITBase.SUNNY_DAY;
+import org.apache.accumulo.minicluster.ServerType;
+import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
+import org.apache.accumulo.test.ample.FlakyAmpleManager;
+import org.apache.accumulo.test.ample.FlakyAmpleTserver;
+import org.apache.hadoop.conf.Configuration;
 
-import org.junit.jupiter.api.Tag;
-
-/*
- * This class is empty because of the SUNNY_DAY tag.  WALSunnyDayBaseIT exists to share code with
- * WALFlakyAmpleIT.  Ideally the code in WALSunnyDayBaseIT could be moved into this class and
- * WALFlakyAmpleIT could extend this class.  However, if it did then WALFlakyAmpleIT would inherit
- * the SUNNY_DAY tag which is not desirable.
- */
-@Tag(SUNNY_DAY)
-public class WALSunnyDayIT extends WALSunnyDayBaseIT {
-
+public class WALFlakyAmpleIT extends WALSunnyDayBaseIT {
+  @Override
+  protected void configure(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
+    super.configure(cfg, hadoopCoreSite);
+    cfg.setServerClass(ServerType.TABLET_SERVER, FlakyAmpleTserver.class);
+    cfg.setServerClass(ServerType.MANAGER, FlakyAmpleManager.class);
+  }
 }
