a369a0ca57792f633c751162659cb24944cca515
Kevin Rathbun <kevinrr888@gmail.com>
2024-08-13 15:44:01 -0400

    Impl structure of new 'admin check' cmd
    (Part of https://github.com/apache/accumulo/issues/4687)
    - Added new `admin check` command
    - `admin check list` prints the checks that can be run along with their descriptions and dependencies
    - `admin check run` runs all the checks or a specified list of checks (explicitly provide each check, provide a regex of the checks to run, or just run all if no further args are provided). The dependencies are run first and if a checks dependency fails, the check is skipped
    - Current impl of `admin check run` does not do any actual work besides printing that the check is running (to ensure correct run order) and returning an `OK` status
    - New IT AdminCheckIT
    	- This IT includes a verion of the Checks where no work is actually done (right now, this is equivalent to the actual implementation). This is done so correct run order, correct checks run, etc. can be
    	verified without actually running the checks which may take a long time. More tests can be added later to test the actual check functionality when that is implemented.
    - No existing functionality was changed

---
+++ server/base/src/main/java/org/apache/accumulo/server/util/checkCommand/UserFilesCheckRunner.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.server.util.checkCommand;
+
+import static org.apache.accumulo.server.util.Admin.CheckCommand.Check;
+
+import org.apache.accumulo.server.util.Admin;
+
+public class UserFilesCheckRunner implements CheckRunner {
+  private static final Check check = Check.USER_FILES;
+
+  @Override
+  public Admin.CheckCommand.CheckStatus runCheck() {
+    Admin.CheckCommand.CheckStatus status = Admin.CheckCommand.CheckStatus.OK;
+
+    System.out.println("Running check " + check);
+    // work
+    System.out.println("Check " + check + " completed with status " + status);
+    return status;
+  }
+
+  @Override
+  public Admin.CheckCommand.Check getCheck() {
+    return check;
+  }
+}
