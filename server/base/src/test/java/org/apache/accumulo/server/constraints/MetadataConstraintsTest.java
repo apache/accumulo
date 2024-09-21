b5dde78c72db7eb5efb235bc82c88f0062c8b78f
Christopher L. Shannon <cshannon@apache.org>
2024-09-21 13:31:32 -0400

    Validate the format of a directory column (#4913)
    MetadataConstraints has been updated to validate the format of the
    directory column

--- server/base/src/test/java/org/apache/accumulo/server/constraints/MetadataConstraintsTest.java
+++ server/base/src/test/java/org/apache/accumulo/server/constraints/MetadataConstraintsTest.java
@@ -269,7 +269,7 @@ public class MetadataConstraintsTest {
         BulkFileColumnFamily.NAME, StoredTabletFile
             .of(new Path("hdfs://1.2.3.4/accumulo/tables/2a/t-0003/someFile")).getMetadataText(),
         new Value(fateId1.canonical()));
-    ServerColumnFamily.DIRECTORY_COLUMN.put(m, new Value("/t1"));
+    ServerColumnFamily.DIRECTORY_COLUMN.put(m, new Value("t-000009x"));
     violations = mc.check(createEnv(), m);
     assertTrue(violations.isEmpty());
 
@@ -606,6 +606,24 @@ public class MetadataConstraintsTest {
     assertFalse(violations.isEmpty());
     assertEquals(1, violations.size());
     assertEquals(Short.valueOf((short) 15), violations.get(0));
+  }
+
+  @Test
+  public void testDirectoryColumn() {
+    MetadataConstraints mc = new MetadataConstraints();
+    Mutation m;
+    List<Short> violations;
+    m = new Mutation(new Text("0;foo"));
+    ServerColumnFamily.DIRECTORY_COLUMN.put(m, new Value("t-000009x"));
+    violations = mc.check(createEnv(), m);
+    assertTrue(violations.isEmpty());
+
+    m = new Mutation(new Text("0;foo"));
+    ServerColumnFamily.DIRECTORY_COLUMN.put(m, new Value("/invalid"));
+    violations = mc.check(createEnv(), m);
+    assertFalse(violations.isEmpty());
+    assertEquals(1, violations.size());
+    assertEquals((short) 17, violations.get(0));
   }
 
   // Encode a row how it would appear in Json
