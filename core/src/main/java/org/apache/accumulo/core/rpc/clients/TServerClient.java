0c4c31625b2eb6680eef4818b61b324a1914a794
Dave Marion <dlmarion@apache.org>
2024-10-03 16:12:00 -0400

    Enable user to specify specific server for Thrift client calls (#4880)
    Allow the user to set a system property to the address of a server
    to use when making calls to the Client Thrift API. Example:
    org.apache.accumulo.client.rpc.debug.host="localhost:1234"
    
    Closes #4823

--- core/src/main/java/org/apache/accumulo/core/rpc/clients/TServerClient.java
+++ core/src/main/java/org/apache/accumulo/core/rpc/clients/TServerClient.java
@@ -23,6 +23,8 @@ import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterrup
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.apache.accumulo.core.util.LazySingletons.RANDOM;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -54,6 +56,8 @@ import com.google.common.net.HostAndPort;
 
 public interface TServerClient<C extends TServiceClient> {
 
+  static final String DEBUG_HOST = "org.apache.accumulo.client.rpc.debug.host";
+
   Pair<String,C> getThriftServerConnection(ClientContext context, boolean preferCachedConnections)
       throws TTransportException;
 
@@ -62,7 +66,9 @@ public interface TServerClient<C extends TServiceClient> {
       ThriftService service) throws TTransportException {
     checkArgument(context != null, "context is null");
 
-    if (preferCachedConnections) {
+    final String debugHost = System.getProperty(DEBUG_HOST, null);
+
+    if (preferCachedConnections && debugHost == null) {
       Pair<String,TTransport> cachedTransport =
           context.getTransportPool().getAnyCachedTransport(type);
       if (cachedTransport != null) {
@@ -79,28 +85,40 @@ public interface TServerClient<C extends TServiceClient> {
     final ZooCache zc = context.getZooCache();
 
     final List<String> serverPaths = new ArrayList<>();
-    zc.getChildren(tserverZooPath).forEach(tserverAddress -> {
-      serverPaths.add(tserverZooPath + "/" + tserverAddress);
-    });
-    if (type == ThriftClientTypes.CLIENT) {
-      zc.getChildren(sserverZooPath).forEach(sserverAddress -> {
-        serverPaths.add(sserverZooPath + "/" + sserverAddress);
-      });
+    if (type == ThriftClientTypes.CLIENT && debugHost != null) {
+      // add all three paths to the set even though they may not be correct.
+      // The entire set will be checked in the code below to validate
+      // that the path is correct and the lock is held and will return the
+      // correct one.
+      serverPaths.add(tserverZooPath + "/" + debugHost);
+      serverPaths.add(sserverZooPath + "/" + debugHost);
       zc.getChildren(compactorZooPath).forEach(compactorGroup -> {
-        zc.getChildren(compactorZooPath + "/" + compactorGroup).forEach(compactorAddress -> {
-          serverPaths.add(compactorZooPath + "/" + compactorGroup + "/" + compactorAddress);
-        });
+        serverPaths.add(compactorZooPath + "/" + compactorGroup + "/" + debugHost);
       });
+    } else {
+      zc.getChildren(tserverZooPath).forEach(tserverAddress -> {
+        serverPaths.add(tserverZooPath + "/" + tserverAddress);
+      });
+      if (type == ThriftClientTypes.CLIENT) {
+        zc.getChildren(sserverZooPath).forEach(sserverAddress -> {
+          serverPaths.add(sserverZooPath + "/" + sserverAddress);
+        });
+        zc.getChildren(compactorZooPath).forEach(compactorGroup -> {
+          zc.getChildren(compactorZooPath + "/" + compactorGroup).forEach(compactorAddress -> {
+            serverPaths.add(compactorZooPath + "/" + compactorGroup + "/" + compactorAddress);
+          });
+        });
+      }
+      if (serverPaths.isEmpty()) {
+        if (warned.compareAndSet(false, true)) {
+          LOG.warn(
+              "There are no servers serving the {} api: check that zookeeper and accumulo are running.",
+              type);
+        }
+        throw new TTransportException("There are no servers for type: " + type);
+      }
     }
 
-    if (serverPaths.isEmpty()) {
-      if (warned.compareAndSet(false, true)) {
-        LOG.warn(
-            "There are no servers serving the {} api: check that zookeeper and accumulo are running.",
-            type);
-      }
-      throw new TTransportException("There are no servers for type: " + type);
-    }
     Collections.shuffle(serverPaths, RANDOM.get());
 
     for (String serverPath : serverPaths) {
@@ -113,10 +131,19 @@ public interface TServerClient<C extends TServiceClient> {
             TTransport transport = context.getTransportPool().getTransport(type,
                 tserverClientAddress, rpcTimeout, context, preferCachedConnections);
             C client = ThriftUtil.createClient(type, transport);
+            if (type == ThriftClientTypes.CLIENT && debugHost != null) {
+              LOG.info("Connecting to debug host: {}", debugHost);
+            }
             warned.set(false);
             return new Pair<String,C>(tserverClientAddress.toString(), client);
           } catch (TTransportException e) {
-            LOG.trace("Error creating transport to {}", tserverClientAddress);
+            if (type == ThriftClientTypes.CLIENT && debugHost != null) {
+              LOG.error(
+                  "Error creating transport to debug host: {}. If this server is down, then you will need to remove or change the system property {}.",
+                  debugHost, DEBUG_HOST);
+            } else {
+              LOG.trace("Error creating transport to {}", tserverClientAddress);
+            }
             continue;
           }
         }
@@ -127,7 +154,15 @@ public interface TServerClient<C extends TServiceClient> {
       LOG.warn("Failed to find an available server in the list of servers: {} for API type: {}",
           serverPaths, type);
     }
-    throw new TTransportException("Failed to connect to any server for API type " + type);
+    // Need to throw a different exception, when a TTransportException is
+    // thrown below, then the operation will be retried endlessly.
+    if (type == ThriftClientTypes.CLIENT && debugHost != null) {
+      throw new UncheckedIOException("Error creating transport to debug host: " + debugHost
+          + ". If this server is down, then you will need to remove or change the system property "
+          + DEBUG_HOST + ".", new IOException(""));
+    } else {
+      throw new TTransportException("Failed to connect to any server for API type " + type);
+    }
   }
 
   default <R> R execute(Logger LOG, ClientContext context, Exec<R,C> exec)
