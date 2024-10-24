f404f6834cb280bac256a89a4852a41f4e9be24e
Kevin Rathbun <krathbun@apache.org>
2024-10-24 13:16:01 -0400

    Refactors MultipleStoresIT (#4975)
    * Refactors MultipleStoresIT
    
    Refactors MultipleStoresIT to function more similarly to how other fate
    tests are run (the two store types are tested in their own separate
    concrete classes). MultipleStoresIT is now an abstract class
    implemented by two new classes MetaMultipleStoresIT and
    UserMultipleStoresIT.
    
    closes #4903

---
+++ test/src/main/java/org/apache/accumulo/test/fate/meta/MetaMultipleStoresIT.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.test.fate.meta;
+
+import java.io.File;
+import java.util.function.Predicate;
+
+import org.apache.accumulo.core.Constants;
+import org.apache.accumulo.core.fate.FateStore;
+import org.apache.accumulo.core.fate.zookeeper.MetaFateStore;
+import org.apache.accumulo.core.fate.zookeeper.ZooReaderWriter;
+import org.apache.accumulo.core.fate.zookeeper.ZooUtil;
+import org.apache.accumulo.test.fate.MultipleStoresIT;
+import org.apache.accumulo.test.zookeeper.ZooKeeperTestingServer;
+import org.apache.zookeeper.KeeperException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.io.TempDir;
+
+public class MetaMultipleStoresIT extends MultipleStoresIT {
+  @TempDir
+  private static File TEMP_DIR;
+  private static ZooKeeperTestingServer SZK;
+  private static ZooReaderWriter ZK;
+  private static String FATE_DIR;
+
+  @BeforeAll
+  public static void setup() throws Exception {
+    SZK = new ZooKeeperTestingServer(TEMP_DIR);
+    ZK = SZK.getZooReaderWriter();
+    FATE_DIR = Constants.ZFATE;
+  }
+
+  @AfterAll
+  public static void teardown() throws Exception {
+    SZK.close();
+  }
+
+  @Override
+  protected void executeSleepingEnvTest(MultipleStoresTestExecutor<SleepingTestEnv> testMethod)
+      throws Exception {
+    testMethod.execute(new SleepingEnvMetaStoreFactory());
+  }
+
+  @Override
+  protected void executeLatchEnvTest(MultipleStoresTestExecutor<LatchTestEnv> testMethod)
+      throws Exception {
+    testMethod.execute(new LatchEnvMetaStoreFactory());
+  }
+
+  static class SleepingEnvMetaStoreFactory implements TestStoreFactory<SleepingTestEnv> {
+    @Override
+    public FateStore<SleepingTestEnv> create(ZooUtil.LockID lockID,
+        Predicate<ZooUtil.LockID> isLockHeld) throws InterruptedException, KeeperException {
+      return new MetaFateStore<>(FATE_DIR, ZK, lockID, isLockHeld);
+    }
+  }
+
+  static class LatchEnvMetaStoreFactory implements TestStoreFactory<LatchTestEnv> {
+    @Override
+    public FateStore<LatchTestEnv> create(ZooUtil.LockID lockID,
+        Predicate<ZooUtil.LockID> isLockHeld) throws InterruptedException, KeeperException {
+      return new MetaFateStore<>(FATE_DIR, ZK, lockID, isLockHeld);
+    }
+  }
+}
