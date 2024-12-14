d5fad56d0d6c25022ca91f2fb4e5565a02e8a10a
Keith Turner <kturner@apache.org>
2024-12-14 16:17:59 -0500

    Update core/src/main/java/org/apache/accumulo/core/conf/Property.java
    Co-authored-by: Christopher L. Shannon <cshannon@apache.org>

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -774,7 +774,7 @@ public enum Property {
       PropertyType.COUNT, "Numbers of threads for executing conditional updates on the metadata table.",
       "4.0.0"),
   TSERV_CONDITIONAL_UPDATE_THREADS_USER("tserver.conditionalupdate.threads.user", "16",
-      PropertyType.COUNT, "Numbers of threads for executing conditional updates on the root table.",
+      PropertyType.COUNT, "Numbers of threads for executing conditional updates on the user table.",
       "4.0.0"),
 
   // accumulo garbage collector properties
