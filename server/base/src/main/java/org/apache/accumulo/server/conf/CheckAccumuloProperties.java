apache/accumulo

commit 7b8b21335ecd6adc168e9fb99aae58cbff2b75dd

Adds new accumulo command (#5073)
* Adds new accumulo command:

Adds new `accumulo check-accumulo-properties` command which checks the provided Accumulo configuration file for errors. Only checks the file contents, and not any running instance, so useful for verifying config prior to init. Performs a subset of the checks that `accumulo check-server-config` does. Useful for (at least mostly) validating the `accumulo.properties` file without a running instance.

Co-authored-by: Christopher Tubbs <ctubbsii@apache.org>

server/base/src/main/java/org/apache/accumulo/server/conf/CheckAccumuloProperties.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.server.conf;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.accumulo.core.conf.SiteConfiguration;
+import org.apache.accumulo.server.ServerDirs;
+import org.apache.accumulo.server.fs.VolumeManagerImpl;
+import org.apache.accumulo.start.spi.KeywordExecutable;
+import org.apache.hadoop.conf.Configuration;
+
+import com.google.auto.service.AutoService;
+import com.google.common.base.Preconditions;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@AutoService(KeywordExecutable.class)
+public class CheckAccumuloProperties implements KeywordExecutable {
+
+  @SuppressFBWarnings(value = "PATH_TRAVERSAL_IN", justification = "intentional user-provided path")
+  public static void main(String[] args) throws IOException {
+    Preconditions.checkArgument(args.length == 1,
+        "Expected 1 argument (the properties file path), got " + args.length);
+    var hadoopConfig = new Configuration();
+    var siteConfig = SiteConfiguration.fromFile(new File(args[0])).build();
+
+    VolumeManagerImpl.get(siteConfig, hadoopConfig);
+    new ServerDirs(siteConfig, hadoopConfig);
+  }
+
+  @Override
+  public String keyword() {
+    return "check-accumulo-properties";
+  }
+
+  @Override
+  public String description() {
+    return "Checks the provided Accumulo configuration file for errors. "
+        + "This only checks the contents of the file and not any running Accumulo system, "
+        + "so it can be used prior to init, but only performs a subset of the checks done by "
+        + (new CheckServerConfig().keyword());
+  }
+
+  @Override
+  public void execute(String[] args) throws Exception {
+    main(args);
+  }
+}

commit 7b746cd08fc18ea3e75ea5f5180c1a6661367db5

Fix KeywordStartIT broken by #5073
* Remove main method and move implementation to the execute method in
  the new CheckAccumuloProperties command
* Add the new KeywordExecutable class to the IT

server/base/src/main/java/org/apache/accumulo/server/conf/CheckAccumuloProperties.java
@@ -35,17 +35,6 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 @AutoService(KeywordExecutable.class)
 public class CheckAccumuloProperties implements KeywordExecutable {
 
-  @SuppressFBWarnings(value = "PATH_TRAVERSAL_IN", justification = "intentional user-provided path")
-  public static void main(String[] args) throws IOException {
-    Preconditions.checkArgument(args.length == 1,
-        "Expected 1 argument (the properties file path), got " + args.length);
-    var hadoopConfig = new Configuration();
-    var siteConfig = SiteConfiguration.fromFile(new File(args[0])).build();
-
-    VolumeManagerImpl.get(siteConfig, hadoopConfig);
-    new ServerDirs(siteConfig, hadoopConfig);
-  }
-
   @Override
   public String keyword() {
     return "check-accumulo-properties";
@@ -59,8 +48,16 @@ public class CheckAccumuloProperties implements KeywordExecutable {
         + (new CheckServerConfig().keyword());
   }
 
+  @SuppressFBWarnings(value = "PATH_TRAVERSAL_IN", justification = "intentional user-provided path")
   @Override
-  public void execute(String[] args) throws Exception {
-    main(args);
+  public void execute(String[] args) throws IOException {
+    Preconditions.checkArgument(args.length == 1,
+        "Expected 1 argument (the properties file path), got " + args.length);
+    var hadoopConfig = new Configuration();
+    var siteConfig = SiteConfiguration.fromFile(new File(args[0])).build();
+
+    VolumeManagerImpl.get(siteConfig, hadoopConfig);
+    new ServerDirs(siteConfig, hadoopConfig);
   }
+
 }

test/src/main/java/org/apache/accumulo/test/start/KeywordStartIT.java
@@ -53,6 +53,7 @@ import org.apache.accumulo.minicluster.MiniAccumuloRunner;
 import org.apache.accumulo.miniclusterImpl.MiniClusterExecutable;
 import org.apache.accumulo.monitor.Monitor;
 import org.apache.accumulo.monitor.MonitorExecutable;
+import org.apache.accumulo.server.conf.CheckAccumuloProperties;
 import org.apache.accumulo.server.conf.CheckCompactionConfig;
 import org.apache.accumulo.server.conf.CheckServerConfig;
 import org.apache.accumulo.server.conf.util.ConfigPropertyUpgrader;
@@ -131,6 +132,7 @@ public class KeywordStartIT {
     expectSet.put("admin", Admin.class);
     expectSet.put("check-compaction-config", CheckCompactionConfig.class);
     expectSet.put("check-server-config", CheckServerConfig.class);
+    expectSet.put("check-accumulo-properties", CheckAccumuloProperties.class);
     expectSet.put("compaction-coordinator", CoordinatorExecutable.class);
     expectSet.put("compactor", CompactorExecutable.class);
     expectSet.put("config-upgrade", ConfigPropertyUpgrader.class);