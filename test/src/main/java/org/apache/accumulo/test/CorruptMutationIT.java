1aef946fd5ca72aff76afba48cf55fbacb90261b
Keith Turner <kturner@apache.org>
2025-01-07 22:00:38 +0000

    fix imports

--- test/src/main/java/org/apache/accumulo/test/CorruptMutationIT.java
+++ test/src/main/java/org/apache/accumulo/test/CorruptMutationIT.java
@@ -18,7 +18,11 @@
  */
 package org.apache.accumulo.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Set;
