7bf0ecfc5d022a9c48dca21dd1b70f32f3abab89
Keith Turner <kturner@apache.org>
2025-02-03 17:16:34 -0500

    Disables HTML escaping in LoadPlan json encoding. (#5299)
    The encoding in LoadPlan was using GSon default settings which encoded
    some HTML chars like `=` as unicode escape sequences.  This did not
    cause any problems because GSon would reverse this encoding.  However it
    was not documented in the javadoc for LoadPlan and it seems unnecessary.
    
    Disabled the HTML escaping so the code aligns w/ the javadoc.  Also
    updated the unit test to produce `=` in the base64 and see how that
    behaves in the json.  This allows the test to better detect changes in
    the way the code persist data.

--- core/src/main/java/org/apache/accumulo/core/data/LoadPlan.java
+++ core/src/main/java/org/apache/accumulo/core/data/LoadPlan.java
@@ -279,7 +279,8 @@ public class LoadPlan {
 
   }
 
-  private static final Gson gson = new GsonBuilder().disableJdkUnsafe().serializeNulls().create();
+  private static final Gson gson =
+      new GsonBuilder().disableHtmlEscaping().disableJdkUnsafe().serializeNulls().create();
 
   /**
    * Serializes the load plan to json that looks like the following. The values of startRow and
