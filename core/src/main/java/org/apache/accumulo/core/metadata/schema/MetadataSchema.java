apache/accumulo

commit 80a48cdc60d4c5839763068f988b383878908844

Filter based on columns declared in TabletMetadataCheck (#5335)
Adds filtering to TabletMetadataCheckIterator that will use matching
families for the provided columns in order to reduce the amount of data
that is fetched. A ResolvedColumns instance is returned by each
TabletMetadataCheck instance that contains the set of columns and set of
matching families to read.

CompactionReservationCheck caches the ResolvedColumns
instance statically to avoid having to recreate the set of columns and
familes for each scan.

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletMetadata.java
@@ -18,15 +18,28 @@
  */
 package org.apache.accumulo.core.metadata.schema;
 
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.MergedColumnFamily.MERGED_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_NONCE_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_NONCE_QUAL;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.OPID_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.OPID_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.SELECTED_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.SELECTED_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.TIME_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.TIME_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.SplitColumnFamily.UNSPLITTABLE_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.SuspendLocationColumn.SUSPEND_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.AVAILABILITY_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.AVAILABILITY_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.MERGEABILITY_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.MERGEABILITY_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.PREV_ROW_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.PREV_ROW_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.REQUESTED_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.REQUESTED_QUAL;
 
 import java.util.Collection;
@@ -40,10 +53,12 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.accumulo.core.client.admin.TabletAvailability;
 import org.apache.accumulo.core.clientImpl.ClientContext;
 import org.apache.accumulo.core.clientImpl.TabletAvailabilityUtil;
+import org.apache.accumulo.core.data.ArrayByteSequence;
 import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.TableId;
@@ -58,6 +73,7 @@ import org.apache.accumulo.core.metadata.AccumuloTable;
 import org.apache.accumulo.core.metadata.StoredTabletFile;
 import org.apache.accumulo.core.metadata.SuspendingTServer;
 import org.apache.accumulo.core.metadata.TServerInstance;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ClonedColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.CompactedColumnFamily;
@@ -75,6 +91,7 @@ import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.Su
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.UserCompactionRequestedColumnFamily;
 import org.apache.accumulo.core.tabletserver.log.LogEntry;
+import org.apache.accumulo.core.util.ColumnFQ;
 import org.apache.accumulo.core.zookeeper.ZcStat;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -202,7 +219,152 @@ public class TabletMetadata {
     COMPACTED,
     USER_COMPACTION_REQUESTED,
     UNSPLITTABLE,
-    MERGEABILITY
+    MERGEABILITY;
+
+    public static final Map<ColumnType,Set<Text>> COLUMNS_TO_FAMILIES;
+    public static final Map<ColumnType,ColumnFQ> COLUMNS_TO_QUALIFIERS;
+
+    static {
+      ImmutableMap.Builder<ColumnType,Set<Text>> colsToFamilies = ImmutableMap.builder();
+      ImmutableMap.Builder<ColumnType,ColumnFQ> colsToQualifiers = ImmutableMap.builder();
+
+      for (ColumnType column : EnumSet.allOf(ColumnType.class)) {
+        switch (column) {
+          case CLONED:
+            colsToFamilies.put(column, Set.of(ClonedColumnFamily.NAME));
+            break;
+          case DIR:
+          case FLUSH_ID:
+          case TIME:
+          case OPID:
+          case SELECTED:
+          case FLUSH_NONCE:
+            colsToFamilies.put(column, Set.of(ServerColumnFamily.NAME));
+            break;
+          case FILES:
+            colsToFamilies.put(column, Set.of(DataFileColumnFamily.NAME));
+            break;
+          case AVAILABILITY:
+          case HOSTING_REQUESTED:
+          case PREV_ROW:
+          case MERGEABILITY:
+            colsToFamilies.put(column, Set.of(TabletColumnFamily.NAME));
+            break;
+          case LAST:
+            colsToFamilies.put(column, Set.of(LastLocationColumnFamily.NAME));
+            break;
+          case LOADED:
+            colsToFamilies.put(column, Set.of(BulkFileColumnFamily.NAME));
+            break;
+          case LOCATION:
+            colsToFamilies.put(column,
+                Set.of(CurrentLocationColumnFamily.NAME, FutureLocationColumnFamily.NAME));
+            break;
+          case LOGS:
+            colsToFamilies.put(column, Set.of(LogColumnFamily.NAME));
+            break;
+          case SCANS:
+            colsToFamilies.put(column, Set.of(ScanFileColumnFamily.NAME));
+            break;
+          case SUSPEND:
+            colsToFamilies.put(column, Set.of(SuspendLocationColumn.NAME));
+            break;
+          case ECOMP:
+            colsToFamilies.put(column, Set.of(ExternalCompactionColumnFamily.NAME));
+            break;
+          case MERGED:
+            colsToFamilies.put(column, Set.of(MergedColumnFamily.NAME));
+            break;
+          case COMPACTED:
+            colsToFamilies.put(column, Set.of(CompactedColumnFamily.NAME));
+            break;
+          case USER_COMPACTION_REQUESTED:
+            colsToFamilies.put(column, Set.of(UserCompactionRequestedColumnFamily.NAME));
+            break;
+          case UNSPLITTABLE:
+            colsToFamilies.put(column, Set.of(SplitColumnFamily.NAME));
+            break;
+          default:
+            throw new IllegalArgumentException("Unknown col type " + column);
+        }
+      }
+
+      for (ColumnType column : EnumSet.allOf(ColumnType.class)) {
+        switch (column) {
+          case CLONED:
+          case COMPACTED:
+          case ECOMP:
+          case FILES:
+          case LAST:
+          case LOADED:
+          case LOCATION:
+          case LOGS:
+          case SCANS:
+          case USER_COMPACTION_REQUESTED:
+            break;
+          case DIR:
+            colsToQualifiers.put(column, DIRECTORY_COLUMN);
+            break;
+          case FLUSH_ID:
+            colsToQualifiers.put(column, FLUSH_COLUMN);
+            break;
+          case TIME:
+            colsToQualifiers.put(column, TIME_COLUMN);
+            break;
+          case OPID:
+            colsToQualifiers.put(column, OPID_COLUMN);
+            break;
+          case SELECTED:
+            colsToQualifiers.put(column, SELECTED_COLUMN);
+            break;
+          case FLUSH_NONCE:
+            colsToQualifiers.put(column, FLUSH_NONCE_COLUMN);
+            break;
+          case AVAILABILITY:
+            colsToQualifiers.put(column, AVAILABILITY_COLUMN);
+            break;
+          case HOSTING_REQUESTED:
+            colsToQualifiers.put(column, REQUESTED_COLUMN);
+            break;
+          case PREV_ROW:
+            colsToQualifiers.put(column, PREV_ROW_COLUMN);
+            break;
+          case MERGEABILITY:
+            colsToQualifiers.put(column, MERGEABILITY_COLUMN);
+            break;
+          case SUSPEND:
+            colsToQualifiers.put(column, SUSPEND_COLUMN);
+            break;
+          case MERGED:
+            colsToQualifiers.put(column, MERGED_COLUMN);
+            break;
+          case UNSPLITTABLE:
+            colsToQualifiers.put(column, UNSPLITTABLE_COLUMN);
+            break;
+          default:
+            throw new IllegalArgumentException("Unknown col type " + column);
+        }
+      }
+
+      COLUMNS_TO_FAMILIES = colsToFamilies.build();
+      COLUMNS_TO_QUALIFIERS = colsToQualifiers.build();
+    }
+
+    public static Set<ByteSequence> resolveFamilies(Set<ColumnType> columns) {
+      return columns.stream()
+          .flatMap(cf -> COLUMNS_TO_FAMILIES.get(cf).stream()
+              .map(family -> new ArrayByteSequence(family.copyBytes())))
+          .collect(Collectors.toUnmodifiableSet());
+    }
+
+    public static Set<Text> resolveFamiliesAsText(ColumnType column) {
+      return COLUMNS_TO_FAMILIES.get(column);
+    }
+
+    public static ColumnFQ resolveQualifier(ColumnType columnType) {
+      return COLUMNS_TO_QUALIFIERS.get(columnType);
+    }
+
   }
 
   public static class Location {

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletsMetadata.java
@@ -21,12 +21,7 @@ package org.apache.accumulo.core.metadata.schema;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_COLUMN;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.OPID_COLUMN;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.SELECTED_COLUMN;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.TIME_COLUMN;
-import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily.PREV_ROW_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.PREV_ROW;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -64,21 +59,6 @@ import org.apache.accumulo.core.iterators.user.WholeRowIterator;
 import org.apache.accumulo.core.metadata.AccumuloTable;
 import org.apache.accumulo.core.metadata.schema.Ample.DataLevel;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ClonedColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.CompactedColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.CurrentLocationColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.DataFileColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ExternalCompactionColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.FutureLocationColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.LastLocationColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.LogColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.MergedColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ScanFileColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.SplitColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.SuspendLocationColumn;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.UserCompactionRequestedColumnFamily;
 import org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType;
 import org.apache.accumulo.core.metadata.schema.filters.TabletMetadataFilter;
 import org.apache.accumulo.core.security.Authorizations;
@@ -325,84 +305,15 @@ public class TabletsMetadata implements Iterable<TabletMetadata>, AutoCloseable 
     @Override
     public Options fetch(ColumnType... colsToFetch) {
       Preconditions.checkArgument(colsToFetch.length > 0);
-
-      for (ColumnType colToFetch : colsToFetch) {
-
-        fetchedCols.add(colToFetch);
-
-        switch (colToFetch) {
-          case CLONED:
-            families.add(ClonedColumnFamily.NAME);
-            break;
-          case DIR:
-            qualifiers.add(DIRECTORY_COLUMN);
-            break;
-          case FILES:
-            families.add(DataFileColumnFamily.NAME);
-            break;
-          case FLUSH_ID:
-            qualifiers.add(FLUSH_COLUMN);
-            break;
-          case AVAILABILITY:
-            qualifiers.add(TabletsSection.TabletColumnFamily.AVAILABILITY_COLUMN);
-            break;
-          case HOSTING_REQUESTED:
-            qualifiers.add(TabletsSection.TabletColumnFamily.REQUESTED_COLUMN);
-            break;
-          case LAST:
-            families.add(LastLocationColumnFamily.NAME);
-            break;
-          case LOADED:
-            families.add(BulkFileColumnFamily.NAME);
-            break;
-          case LOCATION:
-            families.add(CurrentLocationColumnFamily.NAME);
-            families.add(FutureLocationColumnFamily.NAME);
-            break;
-          case LOGS:
-            families.add(LogColumnFamily.NAME);
-            break;
-          case PREV_ROW:
-            qualifiers.add(PREV_ROW_COLUMN);
-            break;
-          case SCANS:
-            families.add(ScanFileColumnFamily.NAME);
-            break;
-          case SUSPEND:
-            families.add(SuspendLocationColumn.SUSPEND_COLUMN.getColumnFamily());
-            break;
-          case TIME:
-            qualifiers.add(TIME_COLUMN);
-            break;
-          case ECOMP:
-            families.add(ExternalCompactionColumnFamily.NAME);
-            break;
-          case MERGED:
-            families.add(MergedColumnFamily.NAME);
-            break;
-          case OPID:
-            qualifiers.add(OPID_COLUMN);
-            break;
-          case SELECTED:
-            qualifiers.add(SELECTED_COLUMN);
-            break;
-          case COMPACTED:
-            families.add(CompactedColumnFamily.NAME);
-            break;
-          case USER_COMPACTION_REQUESTED:
-            families.add(UserCompactionRequestedColumnFamily.NAME);
-            break;
-          case UNSPLITTABLE:
-            qualifiers.add(SplitColumnFamily.UNSPLITTABLE_COLUMN);
-            break;
-          case MERGEABILITY:
-            qualifiers.add(TabletColumnFamily.MERGEABILITY_COLUMN);
-            break;
-          default:
-            throw new IllegalArgumentException("Unknown col type " + colToFetch);
+      for (var col : fetchedCols) {
+        fetchedCols.add(col);
+        var qualifier = ColumnType.resolveQualifier(col);
+        if (qualifier != null) {
+          qualifiers.add(qualifier);
+        } else {
+          families.addAll(ColumnType.resolveFamiliesAsText(col));
         }
       }
-
       return this;
     }
 

commit e821659abbc95f41e8bed511416180dab4a90eab

Fixed bug in TabletsMetadata.fetch
A bug was introduced in #5335 to TabletsMetadata.fetch where the
columns being passed as arguments to the function where not being
used. This has the side effect of fetching all columns in the
TabletMetadata which was causing some ITs to time out. The ITs
in these cases were fetching and counting specific columns in
the TabletMetadata.

This bug also highlighted another bug in CloneIT where an invalid
directory was being inserted into the TabletMetadata which would
then subsequently fail when fetched. The directory column name
was being validated on most, but not all, places where it was being
inserted into the TabletMetadata. I added validation to the
ServerColumnFamily.DIRECTORY_COLUMN.put method and fixed CloneIT.

core/src/main/java/org/apache/accumulo/core/metadata/schema/MetadataSchema.java
@@ -203,7 +203,13 @@ public class MetadataSchema {
        * Holds the location of the tablet in the DFS file system
        */
       public static final String DIRECTORY_QUAL = "dir";
-      public static final ColumnFQ DIRECTORY_COLUMN = new ColumnFQ(NAME, new Text(DIRECTORY_QUAL));
+      public static final ColumnFQ DIRECTORY_COLUMN = new ColumnFQ(NAME, new Text(DIRECTORY_QUAL)) {
+        @Override
+        public void put(Mutation m, Value v) {
+          isValidDirCol(v.toString());
+          super.put(m, v);
+        }
+      };
       /**
        * Initial tablet directory name for the default tablet in all tables
        */

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletMetadata.java
@@ -73,7 +73,6 @@ import org.apache.accumulo.core.metadata.AccumuloTable;
 import org.apache.accumulo.core.metadata.StoredTabletFile;
 import org.apache.accumulo.core.metadata.SuspendingTServer;
 import org.apache.accumulo.core.metadata.TServerInstance;
-import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ClonedColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.CompactedColumnFamily;

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletsMetadata.java
@@ -21,7 +21,6 @@ package org.apache.accumulo.core.metadata.schema;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
-import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.PREV_ROW;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -305,7 +304,7 @@ public class TabletsMetadata implements Iterable<TabletMetadata>, AutoCloseable 
     @Override
     public Options fetch(ColumnType... colsToFetch) {
       Preconditions.checkArgument(colsToFetch.length > 0);
-      for (var col : fetchedCols) {
+      for (var col : colsToFetch) {
         fetchedCols.add(col);
         var qualifier = ColumnType.resolveQualifier(col);
         if (qualifier != null) {

test/src/main/java/org/apache/accumulo/test/CloneIT.java
@@ -66,7 +66,7 @@ public class CloneIT extends AccumuloClusterHarness {
       Mutation mut = TabletColumnFamily.createPrevRowMutation(ke);
 
       ServerColumnFamily.TIME_COLUMN.put(mut, new Value("M0"));
-      ServerColumnFamily.DIRECTORY_COLUMN.put(mut, new Value("/default_tablet"));
+      ServerColumnFamily.DIRECTORY_COLUMN.put(mut, new Value("default_tablet"));
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName)) {
         bw1.addMutation(mut);
@@ -95,7 +95,7 @@ public class CloneIT extends AccumuloClusterHarness {
       Mutation mut = TabletColumnFamily.createPrevRowMutation(ke);
 
       ServerColumnFamily.TIME_COLUMN.put(mut, new Value("M0"));
-      ServerColumnFamily.DIRECTORY_COLUMN.put(mut, new Value("/default_tablet"));
+      ServerColumnFamily.DIRECTORY_COLUMN.put(mut, new Value("default_tablet"));
       mut.put(DataFileColumnFamily.NAME.toString(),
           getMetadata(filePrefix + "/default_tablet/0_0.rf", range1),
           new DataFileValue(1, 200).encodeAsString());
@@ -155,17 +155,17 @@ public class CloneIT extends AccumuloClusterHarness {
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName);
           BatchWriter bw2 = client.createBatchWriter(tableName)) {
-        bw1.addMutation(createTablet("0", null, null, "/default_tablet",
+        bw1.addMutation(createTablet("0", null, null, "default_tablet",
             filePrefix + "/default_tablet/0_0.rf", range));
 
         bw1.flush();
 
         MetadataTableUtil.initializeClone(tableName, TableId.of("0"), TableId.of("1"), client, bw2);
 
-        bw1.addMutation(createTablet("0", "m", null, "/default_tablet",
+        bw1.addMutation(createTablet("0", "m", null, "default_tablet",
             filePrefix + "/default_tablet/0_0.rf", range));
         bw1.addMutation(
-            createTablet("0", null, "m", "/t-1", filePrefix + "/default_tablet/0_0.rf", range));
+            createTablet("0", null, "m", "t-1", filePrefix + "/default_tablet/0_0.rf", range));
 
         bw1.flush();
 
@@ -203,17 +203,17 @@ public class CloneIT extends AccumuloClusterHarness {
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName);
           BatchWriter bw2 = client.createBatchWriter(tableName)) {
-        bw1.addMutation(createTablet("0", null, null, "/default_tablet",
+        bw1.addMutation(createTablet("0", null, null, "default_tablet",
             filePrefix + "/default_tablet/0_0.rf", range));
 
         bw1.flush();
 
         MetadataTableUtil.initializeClone(tableName, TableId.of("0"), TableId.of("1"), client, bw2);
 
-        bw1.addMutation(createTablet("0", "m", null, "/default_tablet",
+        bw1.addMutation(createTablet("0", "m", null, "default_tablet",
             filePrefix + "/default_tablet/1_0.rf", range));
         Mutation mut3 =
-            createTablet("0", null, "m", "/t-1", filePrefix + "/default_tablet/1_0.rf", range);
+            createTablet("0", null, "m", "t-1", filePrefix + "/default_tablet/1_0.rf", range);
         mut3.putDelete(DataFileColumnFamily.NAME.toString(),
             getMetadata(filePrefix + "/default_tablet/0_0.rf", range));
         bw1.addMutation(mut3);
@@ -285,17 +285,17 @@ public class CloneIT extends AccumuloClusterHarness {
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName);
           BatchWriter bw2 = client.createBatchWriter(tableName)) {
-        bw1.addMutation(createTablet("0", "m", null, "/d1", filePrefix + "/d1/file1.rf", range1));
-        bw1.addMutation(createTablet("0", null, "m", "/d2", filePrefix + "/d2/file2.rf", range2));
+        bw1.addMutation(createTablet("0", "m", null, "d1", filePrefix + "/d1/file1.rf", range1));
+        bw1.addMutation(createTablet("0", null, "m", "d2", filePrefix + "/d2/file2.rf", range2));
 
         bw1.flush();
 
         MetadataTableUtil.initializeClone(tableName, TableId.of("0"), TableId.of("1"), client, bw2);
 
-        bw1.addMutation(createTablet("0", "f", null, "/d1", filePrefix + "/d1/file3.rf", range3));
-        bw1.addMutation(createTablet("0", "m", "f", "/d3", filePrefix + "/d1/file1.rf", range1));
-        bw1.addMutation(createTablet("0", "s", "m", "/d2", filePrefix + "/d2/file2.rf", range2));
-        bw1.addMutation(createTablet("0", null, "s", "/d4", filePrefix + "/d2/file2.rf", range2));
+        bw1.addMutation(createTablet("0", "f", null, "d1", filePrefix + "/d1/file3.rf", range3));
+        bw1.addMutation(createTablet("0", "m", "f", "d3", filePrefix + "/d1/file1.rf", range1));
+        bw1.addMutation(createTablet("0", "s", "m", "d2", filePrefix + "/d2/file2.rf", range2));
+        bw1.addMutation(createTablet("0", null, "s", "d4", filePrefix + "/d2/file2.rf", range2));
 
         bw1.flush();
 
@@ -335,8 +335,8 @@ public class CloneIT extends AccumuloClusterHarness {
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName);
           BatchWriter bw2 = client.createBatchWriter(tableName)) {
-        bw1.addMutation(createTablet("0", "m", null, "/d1", filePrefix + "/d1/file1.rf", range1));
-        bw1.addMutation(createTablet("0", null, "m", "/d2", filePrefix + "/d2/file2.rf", range2));
+        bw1.addMutation(createTablet("0", "m", null, "d1", filePrefix + "/d1/file1.rf", range1));
+        bw1.addMutation(createTablet("0", null, "m", "d2", filePrefix + "/d2/file2.rf", range2));
 
         bw1.flush();
 
@@ -347,10 +347,10 @@ public class CloneIT extends AccumuloClusterHarness {
 
         bw1.flush();
 
-        bw1.addMutation(createTablet("0", "f", null, "/d1", filePrefix + "/d1/file3.rf", range3));
-        bw1.addMutation(createTablet("0", "m", "f", "/d3", filePrefix + "/d1/file1.rf", range1));
-        bw1.addMutation(createTablet("0", "s", "m", "/d2", filePrefix + "/d2/file3.rf", range3));
-        bw1.addMutation(createTablet("0", null, "s", "/d4", filePrefix + "/d4/file3.rf", range3));
+        bw1.addMutation(createTablet("0", "f", null, "d1", filePrefix + "/d1/file3.rf", range3));
+        bw1.addMutation(createTablet("0", "m", "f", "d3", filePrefix + "/d1/file1.rf", range1));
+        bw1.addMutation(createTablet("0", "s", "m", "d2", filePrefix + "/d2/file3.rf", range3));
+        bw1.addMutation(createTablet("0", null, "s", "d4", filePrefix + "/d4/file3.rf", range3));
 
         bw1.flush();
 
@@ -363,7 +363,7 @@ public class CloneIT extends AccumuloClusterHarness {
 
         bw1.flush();
 
-        bw1.addMutation(createTablet("0", "m", "f", "/d3", filePrefix + "/d1/file3.rf", range3));
+        bw1.addMutation(createTablet("0", "m", "f", "d3", filePrefix + "/d1/file3.rf", range3));
 
         bw1.flush();
 
@@ -405,15 +405,15 @@ public class CloneIT extends AccumuloClusterHarness {
 
       try (BatchWriter bw1 = client.createBatchWriter(tableName);
           BatchWriter bw2 = client.createBatchWriter(tableName)) {
-        bw1.addMutation(createTablet("0", "m", null, "/d1", filePrefix + "/d1/file1.rf", range1));
-        bw1.addMutation(createTablet("0", null, "m", "/d2", filePrefix + "/d2/file2.rf", range2));
+        bw1.addMutation(createTablet("0", "m", null, "d1", filePrefix + "/d1/file1.rf", range1));
+        bw1.addMutation(createTablet("0", null, "m", "d2", filePrefix + "/d2/file2.rf", range2));
 
         bw1.flush();
 
         MetadataTableUtil.initializeClone(tableName, TableId.of("0"), TableId.of("1"), client, bw2);
 
         bw1.addMutation(deleteTablet("0", "m", null, filePrefix + "/d1/file1.rf", range1));
-        Mutation mut = createTablet("0", null, null, "/d2", filePrefix + "/d2/file2.rf", range2);
+        Mutation mut = createTablet("0", null, null, "d2", filePrefix + "/d2/file2.rf", range2);
         mut.put(DataFileColumnFamily.NAME.toString(),
             getMetadata(filePrefix + "/d1/file1.rf", range1),
             new DataFileValue(10, 200).encodeAsString());