e8839be30a9163265e777c79fc6174b537b49fab
Daniel Roberts ddanielr <ddanielr@gmail.com>
2025-02-21 10:57:21 +0000

    Added trace logging for determining tablet lookup

--- server/manager/src/main/java/org/apache/accumulo/manager/tableOps/bulkVer2/LoadFiles.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/tableOps/bulkVer2/LoadFiles.java
@@ -325,6 +325,9 @@ class LoadFiles extends ManagerRepo {
 
     Text startRow = loadMapEntry.getKey().prevEndRow();
 
+    String fmtTid = FateTxId.formatTid(tid);
+    log.trace("{}: Starting bulk load at row: {}", fmtTid, startRow);
+
     Iterator<TabletMetadata> tabletIter =
         TabletsMetadata.builder(manager.getContext()).forTable(tableId).overlapping(startRow, null)
             .checkConsistency().fetch(PREV_ROW, LOCATION, LOADED).build().iterator();
@@ -341,15 +344,21 @@ class LoadFiles extends ManagerRepo {
     long t1 = System.currentTimeMillis();
     while (lmi.hasNext()) {
       loadMapEntry = lmi.next();
-      List<TabletMetadata> tablets = findOverlappingTablets(loadMapEntry.getKey(), tabletIter);
+      List<TabletMetadata> tablets =
+          findOverlappingTablets(fmtTid, loadMapEntry.getKey(), tabletIter);
       loader.load(tablets, loadMapEntry.getValue());
     }
 
+    log.trace("{}: Completed Finding Overlapping Tablets", fmtTid);
+
     long sleepTime = loader.finish();
     if (sleepTime > 0) {
-      long scanTime = Math.min(System.currentTimeMillis() - t1, 30000);
+      log.trace("{}: Tablet Max Sleep is {}", fmtTid, sleepTime);
+      long scanTime = Math.min(System.currentTimeMillis() - t1, 30_000);
+      log.trace("{}: Scan time is {}", fmtTid, scanTime);
       sleepTime = Math.max(sleepTime, scanTime * 2);
     }
+    log.trace("{}: Sleeping for {}ms", fmtTid, sleepTime);
     return sleepTime;
   }
 
@@ -359,7 +368,7 @@ class LoadFiles extends ManagerRepo {
   /**
    * Find all the tablets within the provided bulk load mapping range.
    */
-  private List<TabletMetadata> findOverlappingTablets(KeyExtent loadRange,
+  private List<TabletMetadata> findOverlappingTablets(String fmtTid, KeyExtent loadRange,
       Iterator<TabletMetadata> tabletIter) {
 
     TabletMetadata currTablet = null;
@@ -368,11 +377,13 @@ class LoadFiles extends ManagerRepo {
 
       List<TabletMetadata> tablets = new ArrayList<>();
       currTablet = tabletIter.next();
+      log.trace("{}: Finding Overlapping Tablets for row: {}", fmtTid, currTablet.getExtent());
 
       int cmp;
 
       // skip tablets until we find the prevEndRow of loadRange
       while ((cmp = PREV_COMP.compare(currTablet.getPrevEndRow(), loadRange.prevEndRow())) < 0) {
+        log.trace("{}: Skipping tablet: {}", fmtTid, currTablet.getExtent());
         currTablet = tabletIter.next();
       }
 
@@ -388,6 +399,7 @@ class LoadFiles extends ManagerRepo {
       // adding tablets to the list until the endRow matches the loadRange
       while ((cmp = END_COMP.compare(currTablet.getEndRow(), loadRange.endRow())) < 0) {
         currTablet = tabletIter.next();
+        log.trace("{}: Adding tablet: {} to overlapping list", fmtTid, currTablet.getExtent());
         tablets.add(currTablet);
       }
 
