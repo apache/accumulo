6dddc6ef10500285c1d261981a6d0ba9a26433a3
Daniel Roberts ddanielr <ddanielr@gmail.com>
2025-02-25 19:54:45 +0000

    Match the default value of the balancer conf prop
    The default value of MANAGER_TABLET_BALANCER is the TableLoadBalancer
    Updates the default value here to match.

--- server/manager/src/main/java/org/apache/accumulo/manager/Manager.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/Manager.java
@@ -99,7 +99,7 @@ import org.apache.accumulo.core.metrics.MetricsProducer;
 import org.apache.accumulo.core.process.thrift.ServerProcessService;
 import org.apache.accumulo.core.replication.thrift.ReplicationCoordinator;
 import org.apache.accumulo.core.spi.balancer.BalancerEnvironment;
-import org.apache.accumulo.core.spi.balancer.SimpleLoadBalancer;
+import org.apache.accumulo.core.spi.balancer.TableLoadBalancer;
 import org.apache.accumulo.core.spi.balancer.TabletBalancer;
 import org.apache.accumulo.core.spi.balancer.data.TServerStatus;
 import org.apache.accumulo.core.spi.balancer.data.TabletMigration;
@@ -1948,7 +1948,7 @@ public class Manager extends AbstractServer implements LiveTServerSet.Listener, 
 
   void initializeBalancer() {
     var localTabletBalancer = Property.createInstanceFromPropertyName(getConfiguration(),
-        Property.MANAGER_TABLET_BALANCER, TabletBalancer.class, new SimpleLoadBalancer());
+        Property.MANAGER_TABLET_BALANCER, TabletBalancer.class, new TableLoadBalancer());
     localTabletBalancer.init(balancerEnvironment);
     tabletBalancer = localTabletBalancer;
   }
