c6ab0db894bd47a2c3cb458e9753c29d9ea33e52
Christopher Tubbs <ctubbsii@apache.org>
2025-03-17 15:35:42 -0400

    Fix ZooInfoViewer acl read

--- server/base/src/main/java/org/apache/accumulo/server/conf/util/ZooInfoViewer.java
+++ server/base/src/main/java/org/apache/accumulo/server/conf/util/ZooInfoViewer.java
@@ -231,11 +231,13 @@ public class ZooInfoViewer implements KeywordExecutable {
     var conf = opts.getSiteConfiguration();
     try (var zk = new ZooSession(getClass().getSimpleName(), conf)) {
 
+      String instanceRoot = ZooUtil.getRoot(iid);
+
       final Stat stat = new Stat();
 
       recursiveAclRead(zk, ZROOT + ZINSTANCES, stat, aclMap);
 
-      recursiveAclRead(zk, "/", stat, aclMap);
+      recursiveAclRead(zk, instanceRoot, stat, aclMap);
 
       // print formatting
       aclMap.forEach((path, acl) -> {
