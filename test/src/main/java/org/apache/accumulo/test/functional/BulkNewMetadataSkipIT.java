98483b1001e76bb6d15a00e61e0808615efff543
Dave Marion <dlmarion@apache.org>
2025-03-26 14:58:16 +0000

    Reworked IT to add splits to metadata table

--- test/src/main/java/org/apache/accumulo/test/functional/BulkNewMetadataSkipIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/BulkNewMetadataSkipIT.java
@@ -43,14 +43,15 @@ import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.LoadPlan;
 import org.apache.accumulo.core.data.LoadPlan.RangeType;
+import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.file.FileOperations;
 import org.apache.accumulo.core.file.FileSKVWriter;
 import org.apache.accumulo.core.file.rfile.RFile;
+import org.apache.accumulo.core.metadata.MetadataTable;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.spi.crypto.NoCryptoServiceFactory;
-import org.apache.accumulo.harness.MiniClusterConfigurationCallback;
-import org.apache.accumulo.harness.SharedMiniClusterBase;
+import org.apache.accumulo.harness.AccumuloClusterHarness;
 import org.apache.accumulo.minicluster.MemoryUnit;
 import org.apache.accumulo.minicluster.ServerType;
 import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
@@ -59,37 +60,30 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RawLocalFileSystem;
 import org.apache.hadoop.io.Text;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class BulkNewMetadataSkipIT extends SharedMiniClusterBase {
+/**
+ * This test creates a table with 1000 splits and then imports files into a sparse set of the
+ * tablets. This test also splits the metadata table such that the tablet metadata for each tablet
+ * of the test table is in its own metadata tablet. The test then runs with different values for the
+ * TABLE_BULK_SKIP_THRESHOLD property starting with zero (disabled) then increasing.
+ *
+ * This test uses AccumuloClusterHarness instead of SharedMiniClusterBase so that we don't have to
+ * re-merge the metadata table and delete the test table. Doing these two things, and then waiting
+ * for balancing, takes a long time. It's faster to just start with a clean instance for each test
+ * run.
+ */
+public class BulkNewMetadataSkipIT extends AccumuloClusterHarness {
 
-  private static class ConfigurationCallback implements MiniClusterConfigurationCallback {
-    @Override
-    public void configureMiniCluster(MiniAccumuloConfigImpl cfg, Configuration conf) {
-      cfg.setMemory(ServerType.TABLET_SERVER, 512, MemoryUnit.MEGABYTE);
-
-      // use raw local file system
-      conf.set("fs.file.impl", RawLocalFileSystem.class.getName());
-    }
-  }
-
-  @BeforeAll
-  public static void setup() throws Exception {
-    SharedMiniClusterBase.startMiniClusterWithConfig(new ConfigurationCallback());
-  }
-
-  @AfterAll
-  public static void teardown() {
-    SharedMiniClusterBase.stopMiniCluster();
-  }
-
-  private String getDir(FileSystem fs, String rootPath, String testName) throws Exception {
-    String dir = rootPath + testName + getUniqueNames(1)[0];
-    fs.delete(new Path(dir), true);
-    return dir;
+  @Override
+  public void configureMiniCluster(MiniAccumuloConfigImpl cfg, Configuration hadoopCoreSite) {
+    cfg.setMemory(ServerType.TABLET_SERVER, 512, MemoryUnit.MEGABYTE);
+    cfg.setProperty(Property.MANAGER_TABLET_GROUP_WATCHER_INTERVAL, "3s");
+    cfg.setProperty(Property.TSERV_ASSIGNMENT_MAXCONCURRENT, "25");
+    cfg.setNumTservers(1);
+    hadoopCoreSite.set("fs.file.impl", RawLocalFileSystem.class.getName());
   }
 
   public static String writeNonContiguousData(FileSystem fs, String file,
@@ -106,66 +100,61 @@ public class BulkNewMetadataSkipIT extends SharedMiniClusterBase {
     return hash(filename);
   }
 
+  @BeforeEach
+  @Override
+  public void setupCluster() throws Exception {
+    super.setupCluster();
+    // prime the zk connection
+    try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {}
+  }
+
   @ParameterizedTest
-  @ValueSource(ints = {0, 2, 4, 8, 16, 32, 64, 128})
+  @ValueSource(ints = {0, 0, 2, 4, 8, 16, 32, 64, 128})
   public void test(int skipDistance) throws Exception {
 
-    String tableName = getUniqueNames(1)[0];
+    final String tableName = getUniqueNames(1)[0] + "_" + skipDistance;
+    final AccumuloConfiguration aconf = getCluster().getServerContext().getConfiguration();
+    final FileSystem fs = getCluster().getFileSystem();
+    final String rootPath = getCluster().getTemporaryPath().toString();
+    final String dir = rootPath + "/" + tableName;
 
-    try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
+    fs.delete(new Path(dir), true);
 
-      try {
-        AccumuloConfiguration aconf = getCluster().getServerContext().getConfiguration();
-        FileSystem fs = getCluster().getFileSystem();
-        String rootPath = getCluster().getTemporaryPath().toString();
+    final SortedSet<Text> splits = new TreeSet<>();
+    IntStream.rangeClosed(0, 1000).forEach(i -> splits.add(new Text(String.format("%04d", i))));
 
-        SortedSet<Text> splits = new TreeSet<>();
-        IntStream.rangeClosed(0, 1000).forEach(i -> splits.add(new Text(String.format("%04d", i))));
-        NewTableConfiguration ntc = new NewTableConfiguration();
-        ntc.setProperties(
-            Map.of(Property.TABLE_BULK_SKIP_THRESHOLD.getKey(), Integer.toString(skipDistance)));
-        ntc.withSplits(splits);
-        c.tableOperations().create(tableName, ntc);
+    final NewTableConfiguration ntc = new NewTableConfiguration();
+    ntc.setProperties(
+        Map.of(Property.TABLE_BULK_SKIP_THRESHOLD.getKey(), Integer.toString(skipDistance)));
+    ntc.withSplits(splits);
 
-        String dir = getDir(fs, rootPath, "/testMetadataSkipIT-");
+    final Map<String,Set<String>> hashes = new HashMap<>();
+    IntStream.rangeClosed(0, 1000).forEach(i -> hashes.put(row(i) + "", new HashSet<>()));
+    hashes.put("null", new HashSet<>());
 
-        Map<String,Set<String>> hashes = new HashMap<>();
-        IntStream.rangeClosed(0, 1000).forEach(i -> hashes.put(row(i) + "", new HashSet<>()));
-        hashes.put("null", new HashSet<>());
+    String h1 = writeData(fs, dir + "/f1.", aconf, 0, 11);
+    IntStream.rangeClosed(0, 11).forEach(i -> hashes.get(row(i)).add(h1));
 
-        String h1 = writeData(fs, dir + "/f1.", aconf, 0, 11);
-        hashes.get(row(0)).add(h1);
-        hashes.get(row(1)).add(h1);
-        hashes.get(row(2)).add(h1);
-        hashes.get(row(3)).add(h1);
-        hashes.get(row(4)).add(h1);
-        hashes.get(row(5)).add(h1);
-        hashes.get(row(6)).add(h1);
-        hashes.get(row(7)).add(h1);
-        hashes.get(row(8)).add(h1);
-        hashes.get(row(9)).add(h1);
-        hashes.get(row(10)).add(h1);
-        hashes.get(row(11)).add(h1);
+    int[] h2Rows = new int[] {11, 199, 200, 204};
+    String h2 = writeNonContiguousData(fs, dir + "/f2.", aconf, h2Rows);
+    for (int i : h2Rows) {
+      hashes.get(row(i)).add(h2);
+    }
 
-        String h2 = writeNonContiguousData(fs, dir + "/f2.", aconf, new int[] {11, 199, 200, 204});
-        hashes.get(row(11)).add(h2);
-        hashes.get(row(199)).add(h2);
-        hashes.get(row(200)).add(h2);
-        hashes.get(row(204)).add(h2);
+    int[] h3Rows = new int[] {13, 200, 272, 273};
+    String h3 = writeNonContiguousData(fs, dir + "/f3.", aconf, h3Rows);
+    for (int i : h3Rows) {
+      hashes.get(row(i)).add(h3);
+    }
 
-        String h3 = writeNonContiguousData(fs, dir + "/f3.", aconf, new int[] {13, 200, 272, 273});
-        hashes.get(row(13)).add(h3);
-        hashes.get(row(200)).add(h3);
-        hashes.get(row(272)).add(h3);
-        hashes.get(row(273)).add(h3);
+    int[] h4Rows = new int[] {300, 301, 672, 998};
+    String h4 = writeNonContiguousData(fs, dir + "/f4.", aconf, h4Rows);
+    for (int i : h4Rows) {
+      hashes.get(row(i)).add(h4);
+    }
 
-        String h4 = writeNonContiguousData(fs, dir + "/f4.", aconf, new int[] {300, 301, 672, 998});
-        hashes.get(row(300)).add(h4);
-        hashes.get(row(301)).add(h4);
-        hashes.get(row(672)).add(h4);
-        hashes.get(row(998)).add(h4);
-
-        LoadPlan loadPlan = LoadPlan.builder().loadFileTo("f1.rf", RangeType.FILE, row(0), row(11))
+    final LoadPlan loadPlan =
+        LoadPlan.builder().loadFileTo("f1.rf", RangeType.FILE, row(0), row(11))
             .loadFileTo("f2.rf", RangeType.TABLE, row(10), row(11))
             .loadFileTo("f2.rf", RangeType.FILE, row(199), row(200))
             .loadFileTo("f2.rf", RangeType.TABLE, row(203), row(204))
@@ -176,14 +165,27 @@ public class BulkNewMetadataSkipIT extends SharedMiniClusterBase {
             .loadFileTo("f4.rf", RangeType.TABLE, row(671), row(672))
             .loadFileTo("f4.rf", RangeType.TABLE, row(997), row(998)).build();
 
-        c.tableOperations().importDirectory(dir).to(tableName).plan(loadPlan).load();
+    try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
 
-        verifyData(c, tableName, new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 199, 200, 204,
-            272, 273, 300, 301, 672, 998}, false);
-        verifyMetadata(c, tableName, hashes);
-      } finally {
-        c.tableOperations().delete(tableName);
-      }
+      c.tableOperations().create(tableName, ntc);
+      TableId tid = TableId.of(c.tableOperations().tableIdMap().get(tableName));
+
+      final SortedSet<Text> metadataSplits = new TreeSet<>();
+      Scanner s = c.createScanner(MetadataTable.NAME, Authorizations.EMPTY);
+      final String mdTablePrefix = tid.canonical() + ";";
+      s.forEach(e -> {
+        final String row = e.getKey().getRow().toString();
+        if (row.startsWith(mdTablePrefix)) {
+          metadataSplits.add(new Text(row + "\\x00"));
+        }
+      });
+      c.tableOperations().addSplits(MetadataTable.NAME, metadataSplits);
+
+      c.tableOperations().importDirectory(dir).to(tableName).plan(loadPlan).load();
+
+      verifyData(c, tableName, new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 199, 200, 204,
+          272, 273, 300, 301, 672, 998}, false);
+      verifyMetadata(c, tableName, hashes);
     }
   }
 
