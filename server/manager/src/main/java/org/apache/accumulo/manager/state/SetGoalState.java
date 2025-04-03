f837afb1d1ae0507881fd919e4e44f2ffd3f8de1
Dave Marion <dlmarion@apache.org>
2025-04-03 15:11:56 -0400

    Update server/manager/src/main/java/org/apache/accumulo/manager/state/SetGoalState.java
    Co-authored-by: Christopher Tubbs <ctubbsii@apache.org>

--- server/manager/src/main/java/org/apache/accumulo/manager/state/SetGoalState.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/state/SetGoalState.java
@@ -36,13 +36,8 @@ public class SetGoalState {
    * Utility program that will change the goal state for the manager from the command line.
    */
   public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.err.println(
-          "Usage: accumulo " + SetGoalState.class.getName() + " [NORMAL|SAFE_MODE|CLEAN_STOP]");
-      System.exit(-1);
-    }
-
     try {
+      Preconditions.checkArgument(args.length != 1);
       ManagerGoalState.valueOf(args[0]);
     } catch (IllegalArgumentException e) {
       System.err.println(
