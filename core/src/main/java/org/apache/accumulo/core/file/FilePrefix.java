51383e18d8ffac8e3a5e61d3186c2946a446f5fb
Dave Marion <dlmarion@apache.org>
2025-04-15 12:34:07 +0000

    Merged FileTypePrefix into FilePrefix
    Closes #5476

--- core/src/main/java/org/apache/accumulo/core/file/FilePrefix.java
+++ core/src/main/java/org/apache/accumulo/core/file/FilePrefix.java
@@ -18,11 +18,21 @@
  */
 package org.apache.accumulo.core.file;
 
-import java.util.stream.Stream;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import com.google.common.base.Preconditions;
 
 public enum FilePrefix {
 
-  BULK_IMPORT("I"), MINOR_COMPACTION("F"), MAJOR_COMPACTION("C"), MAJOR_COMPACTION_ALL_FILES("A");
+  ALL("*"),
+  MINOR_COMPACTION("F"),
+  BULK_IMPORT("I"),
+  MAJOR_COMPACTION("C"),
+  MAJOR_COMPACTION_ALL_FILES("A"),
+  MERGING_MINOR_COMPACTION("M");
 
   final String prefix;
 
@@ -30,13 +40,68 @@ public enum FilePrefix {
     this.prefix = prefix;
   }
 
-  public static FilePrefix fromPrefix(String prefix) {
-    return Stream.of(FilePrefix.values()).filter(p -> p.prefix.equals(prefix)).findAny()
-        .orElseThrow(() -> new IllegalArgumentException("Unknown prefix type: " + prefix));
-  }
-
   public String toPrefix() {
     return this.prefix;
+  }
+
+  public String createFileName(String fileName) {
+    Objects.requireNonNull(fileName, "filename must be supplied");
+    Preconditions.checkArgument(!fileName.isBlank(), "Empty filename supplied");
+    if (this == ALL || this == MERGING_MINOR_COMPACTION) {
+      throw new IllegalStateException(
+          "Unable to create filename with ALL, MERGING_MINOR_COMPACTION, or UNKNOWN prefix");
+    }
+    return prefix + fileName;
+  }
+
+  public static FilePrefix fromPrefix(String prefix) {
+    Objects.requireNonNull(prefix, "prefix must be supplied");
+    Preconditions.checkArgument(!prefix.isBlank(), "Empty prefix supplied");
+    Preconditions.checkArgument(prefix.length() == 1, "Invalid prefix supplied: " + prefix);
+    switch (prefix.toUpperCase()) {
+      case "A":
+        return MAJOR_COMPACTION_ALL_FILES;
+      case "C":
+        return MAJOR_COMPACTION;
+      case "F":
+        return MINOR_COMPACTION;
+      case "I":
+        return BULK_IMPORT;
+      case "M":
+        return MERGING_MINOR_COMPACTION;
+      default:
+        throw new IllegalArgumentException("Unknown prefix type: " + prefix);
+    }
+  }
+
+  public static FilePrefix fromFileName(String fileName) {
+    Objects.requireNonNull(fileName, "file name must be supplied");
+    Preconditions.checkArgument(!fileName.isBlank(), "Empty filename supplied");
+    String firstChar = fileName.substring(0, 1);
+    if (!firstChar.equals(firstChar.toUpperCase())) {
+      throw new IllegalArgumentException(
+          "Expected first character of file name to be upper case, name: " + fileName);
+    }
+    return fromPrefix(firstChar);
+  }
+
+  public static EnumSet<FilePrefix> typesFromList(String list) {
+    final EnumSet<FilePrefix> dropCacheFilePrefixes;
+    if (!list.isBlank()) {
+      if (list.contains("*")) {
+        dropCacheFilePrefixes = EnumSet.of(FilePrefix.ALL);
+      } else {
+        Set<FilePrefix> set = new HashSet<>();
+        String[] prefixes = list.trim().split(",");
+        for (String p : prefixes) {
+          set.add(FilePrefix.fromPrefix(p.trim().toUpperCase()));
+        }
+        dropCacheFilePrefixes = EnumSet.copyOf(set);
+      }
+    } else {
+      dropCacheFilePrefixes = EnumSet.noneOf(FilePrefix.class);
+    }
+    return dropCacheFilePrefixes;
   }
 
 }
