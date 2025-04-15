c1073aa65809307183caaa0fdc2b23f035211111
Dave Marion <dlmarion@apache.org>
2025-04-15 18:14:57 +0000

    Updated BlockCacheManager to use NoopCache when size is zero

--- core/src/main/java/org/apache/accumulo/core/spi/cache/BlockCacheManager.java
+++ core/src/main/java/org/apache/accumulo/core/spi/cache/BlockCacheManager.java
@@ -20,9 +20,11 @@ package org.apache.accumulo.core.spi.cache;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.file.blockfile.cache.impl.BlockCacheConfiguration;
+import org.apache.accumulo.core.file.blockfile.cache.impl.NoopCache;
 
 /**
  * @since 2.0.0
@@ -111,10 +113,12 @@ public abstract class BlockCacheManager {
    * @param conf accumulo configuration
    */
   public void start(Configuration conf) {
+    Supplier<NoopCache> noop = () -> new NoopCache();
     for (CacheType type : CacheType.values()) {
       if (conf.getMaxSize(type) > 0) {
-        BlockCache cache = this.createCache(conf, type);
-        this.caches.put(type, cache);
+        this.caches.put(type, createCache(conf, type));
+      } else {
+        this.caches.put(type, noop.get());
       }
     }
   }
