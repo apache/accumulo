apache/accumulo

commit b1132ce8713f59fb6d4e8120843889e04855e323

Moves migrations from Manager mem to metadata (#5416)
Moves the Managers in-memory migrations data structure into the metadata stores (root meta/root table/metadata table).

Migration cleanup strategy was changed here as well. Previously, there were many places where migrations were deleted. Instead, only delete migrations where necessary and prefer most of the deletion to be done in the Managers MigrationCleanupThread which periodically cleans up migrations. When migrations are read, we now filter out those that will eventually be cleaned up.

---------

Co-authored-by: Keith Turner <kturner@apache.org>

core/src/main/java/org/apache/accumulo/core/metadata/schema/Ample.java
@@ -401,6 +401,10 @@ public interface Ample {
      * false.
      */
     T automaticallyPutServerLock(boolean b);
+
+    T putMigration(TServerInstance tserver);
+
+    T deleteMigration();
   }
 
   interface TabletMutator extends TabletUpdates<TabletMutator> {
@@ -500,6 +504,8 @@ public interface Ample {
      */
     ConditionalTabletMutator requireLocation(Location location);
 
+    ConditionalTabletMutator requireCurrentLocationNotEqualTo(TServerInstance tsi);
+
     /**
      * Requires the tablet to have the specified tablet availability before any changes are made.
      */
@@ -539,6 +545,11 @@ public interface Ample {
      * updated.
      */
     ConditionalTabletMutator requireCheckSuccess(TabletMetadataCheck check);
+
+    /**
+     * Requires that a tablet be migrating to the given tserver
+     */
+    ConditionalTabletMutator requireMigration(TServerInstance tserver);
 
     /**
      * <p>

core/src/main/java/org/apache/accumulo/core/metadata/schema/MetadataSchema.java
@@ -309,6 +309,14 @@ public class MetadataSchema {
        */
       public static final String SELECTED_QUAL = "selected";
       public static final ColumnFQ SELECTED_COLUMN = new ColumnFQ(NAME, new Text(SELECTED_QUAL));
+
+      /**
+       * This column is used to indicate that a tablet is in the process of being migrated from one
+       * tablet server to another. The destination of the migration is the value. The tserver being
+       * migrated is the row this is set on.
+       */
+      public static final String MIGRATION_QUAL = "migration";
+      public static final ColumnFQ MIGRATION_COLUMN = new ColumnFQ(NAME, new Text(MIGRATION_QUAL));
     }
 
     /**

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletMetadata.java
@@ -25,6 +25,8 @@ import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSec
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_NONCE_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_NONCE_QUAL;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_QUAL;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.MIGRATION_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.MIGRATION_QUAL;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.OPID_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.OPID_QUAL;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.SELECTED_COLUMN;
@@ -142,6 +144,7 @@ public class TabletMetadata {
   private final UnSplittableMetadata unSplittableMetadata;
   private final TabletMergeabilityMetadata mergeability;
   private final Supplier<Long> fileSize;
+  private final TServerInstance migration;
 
   private TabletMetadata(Builder tmBuilder) {
     this.tableId = tmBuilder.tableId;
@@ -185,6 +188,7 @@ public class TabletMetadata {
     });
     this.extent =
         Suppliers.memoize(() -> new KeyExtent(getTableId(), getEndRow(), getPrevEndRow()));
+    this.migration = tmBuilder.migration;
   }
 
   public static TabletMetadataBuilder builder(KeyExtent extent) {
@@ -218,7 +222,8 @@ public class TabletMetadata {
     COMPACTED,
     USER_COMPACTION_REQUESTED,
     UNSPLITTABLE,
-    MERGEABILITY;
+    MERGEABILITY,
+    MIGRATION;
 
     public static final Map<ColumnType,Set<Text>> COLUMNS_TO_FAMILIES;
     public static final Map<ColumnType,ColumnFQ> COLUMNS_TO_QUALIFIERS;
@@ -238,6 +243,7 @@ public class TabletMetadata {
           case OPID:
           case SELECTED:
           case FLUSH_NONCE:
+          case MIGRATION:
             colsToFamilies.put(column, Set.of(ServerColumnFamily.NAME));
             break;
           case FILES:
@@ -339,6 +345,9 @@ public class TabletMetadata {
             break;
           case UNSPLITTABLE:
             colsToQualifiers.put(column, UNSPLITTABLE_COLUMN);
+            break;
+          case MIGRATION:
+            colsToQualifiers.put(column, MIGRATION_COLUMN);
             break;
           default:
             throw new IllegalArgumentException("Unknown col type " + column);
@@ -608,6 +617,11 @@ public class TabletMetadata {
     return mergeability;
   }
 
+  public TServerInstance getMigration() {
+    ensureFetched(ColumnType.MIGRATION);
+    return migration;
+  }
+
   @Override
   public String toString() {
     return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("tableId", tableId)
@@ -623,7 +637,7 @@ public class TabletMetadata {
         .append("futureAndCurrentLocationSet", futureAndCurrentLocationSet)
         .append("userCompactionsRequested", userCompactionsRequested)
         .append("unSplittableMetadata", unSplittableMetadata).append("mergeability", mergeability)
-        .toString();
+        .append("migration", migration).toString();
   }
 
   public List<Entry<Key,Value>> getKeyValues() {
@@ -726,6 +740,8 @@ public class TabletMetadata {
             case SELECTED_QUAL:
               tmBuilder.selectedFiles(SelectedFiles.from(val));
               break;
+            case MIGRATION_QUAL:
+              tmBuilder.migration(new TServerInstance(val));
           }
           break;
         case DataFileColumnFamily.STR_NAME:
@@ -863,6 +879,7 @@ public class TabletMetadata {
     private final ImmutableSet.Builder<FateId> userCompactionsRequested = ImmutableSet.builder();
     private UnSplittableMetadata unSplittableMetadata;
     private TabletMergeabilityMetadata mergeability = TabletMergeabilityMetadata.never();
+    private TServerInstance migration;
 
     void table(TableId tableId) {
       this.tableId = tableId;
@@ -906,6 +923,10 @@ public class TabletMetadata {
 
     void selectedFiles(SelectedFiles selectedFiles) {
       this.selectedFiles = selectedFiles;
+    }
+
+    void migration(TServerInstance tserver) {
+      this.migration = tserver;
     }
 
     void location(String val, String qual, LocationType lt, boolean suppressError) {

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletMetadataBuilder.java
@@ -32,6 +32,7 @@ import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.LOGS;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.MERGEABILITY;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.MERGED;
+import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.MIGRATION;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.OPID;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.PREV_ROW;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.SCANS;
@@ -323,6 +324,18 @@ public class TabletMetadataBuilder implements Ample.TabletUpdates<TabletMetadata
 
   @Override
   public TabletMetadataBuilder automaticallyPutServerLock(boolean b) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public TabletMetadataBuilder putMigration(TServerInstance tserver) {
+    fetched.add(MIGRATION);
+    internalBuilder.putMigration(tserver);
+    return this;
+  }
+
+  @Override
+  public TabletMetadataBuilder deleteMigration() {
     throw new UnsupportedOperationException();
   }
 

core/src/main/java/org/apache/accumulo/core/metadata/schema/TabletMutatorBase.java
@@ -395,6 +395,18 @@ public abstract class TabletMutatorBase<T extends Ample.TabletUpdates<T>>
     return getThis();
   }
 
+  @Override
+  public T putMigration(TServerInstance tserver) {
+    ServerColumnFamily.MIGRATION_COLUMN.put(mutation, new Value(tserver.getHostPortSession()));
+    return getThis();
+  }
+
+  @Override
+  public T deleteMigration() {
+    ServerColumnFamily.MIGRATION_COLUMN.putDelete(mutation);
+    return getThis();
+  }
+
   public void setCloseAfterMutate(AutoCloseable closeable) {
     this.closeAfterMutate = closeable;
   }

core/src/test/java/org/apache/accumulo/core/metadata/schema/TabletMetadataTest.java
@@ -25,6 +25,7 @@ import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSec
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.DIRECTORY_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.FLUSH_NONCE_COLUMN;
+import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.MIGRATION_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ServerColumnFamily.TIME_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.SuspendLocationColumn.SUSPEND_COLUMN;
 import static org.apache.accumulo.core.metadata.schema.TabletMetadata.ColumnType.AVAILABILITY;
@@ -166,6 +167,9 @@ public class TabletMetadataTest {
             CompactorGroupId.of("Q1"), true, FateId.from(FateInstanceType.USER, UUID.randomUUID()));
     mutation.put(ExternalCompactionColumnFamily.STR_NAME, ecid.canonical(), ecMeta.toJson());
 
+    TServerInstance tsi = new TServerInstance("localhost:9997", 5000L);
+    MIGRATION_COLUMN.put(mutation, new Value(tsi.getHostPortSession()));
+
     SortedMap<Key,Value> rowMap = toRowMap(mutation);
 
     TabletMetadata tm = TabletMetadata.convertRow(rowMap.entrySet().iterator(),
@@ -203,6 +207,7 @@ public class TabletMetadataTest {
     assertEquals(unsplittableMeta, tm.getUnSplittable());
     assertEquals(ecMeta.toJson(), tm.getExternalCompactions().get(ecid).toJson());
     assertEquals(10, tm.getFlushNonce().getAsLong());
+    assertEquals(tsi, tm.getMigration());
   }
 
   @Test
@@ -639,6 +644,8 @@ public class TabletMetadataTest {
     FateId compactFateId1 = FateId.from(type, UUID.randomUUID());
     FateId compactFateId2 = FateId.from(type, UUID.randomUUID());
 
+    TServerInstance migration = new TServerInstance("localhost:9999", 1000L);
+
     TabletMetadata tm = TabletMetadata.builder(extent)
         .putTabletAvailability(TabletAvailability.UNHOSTED).putLocation(Location.future(ser1))
         .putFile(sf1, dfv1).putFile(sf2, dfv2).putBulkFile(rf1, loadedFateId1)
@@ -646,6 +653,7 @@ public class TabletMetadataTest {
         .putCompacted(compactFateId1).putCompacted(compactFateId2).putCloned()
         .putTabletMergeability(
             TabletMergeabilityMetadata.always(SteadyTime.from(1, TimeUnit.SECONDS)))
+        .putMigration(migration)
         .build(ECOMP, HOSTING_REQUESTED, MERGED, USER_COMPACTION_REQUESTED, UNSPLITTABLE);
 
     assertEquals(extent, tm.getExtent());
@@ -667,6 +675,7 @@ public class TabletMetadataTest {
     assertEquals("OK", tm.getCloned());
     assertEquals(TabletMergeabilityMetadata.always(SteadyTime.from(1, TimeUnit.SECONDS)),
         tm.getTabletMergeability());
+    assertEquals(migration, tm.getMigration());
     assertThrows(IllegalStateException.class, tm::getOperationId);
     assertThrows(IllegalStateException.class, tm::getSuspend);
     assertThrows(IllegalStateException.class, tm::getTime);
@@ -694,6 +703,7 @@ public class TabletMetadataTest {
     assertThrows(IllegalStateException.class, tm2::getUserCompactionsRequested);
     assertThrows(IllegalStateException.class, tm2::getUnSplittable);
     assertThrows(IllegalStateException.class, tm2::getTabletAvailability);
+    assertThrows(IllegalStateException.class, tm2::getMigration);
 
     var ecid1 = ExternalCompactionId.generate(UUID.randomUUID());
     CompactionMetadata ecm =

server/base/src/main/java/org/apache/accumulo/server/constraints/MetadataConstraints.java
@@ -39,6 +39,7 @@ import org.apache.accumulo.core.lock.ServiceLock;
 import org.apache.accumulo.core.metadata.StoredTabletFile;
 import org.apache.accumulo.core.metadata.SuspendingTServer;
 import org.apache.accumulo.core.metadata.SystemTables;
+import org.apache.accumulo.core.metadata.TServerInstance;
 import org.apache.accumulo.core.metadata.schema.DataFileValue;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.BulkFileColumnFamily;
 import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.ChoppedColumnFamily;
@@ -94,6 +95,7 @@ public class MetadataConstraints implements Constraint {
           ServerColumnFamily.FLUSH_COLUMN,
           ServerColumnFamily.FLUSH_NONCE_COLUMN,
           ServerColumnFamily.OPID_COLUMN,
+          ServerColumnFamily.MIGRATION_COLUMN,
           TabletColumnFamily.AVAILABILITY_COLUMN,
           TabletColumnFamily.REQUESTED_COLUMN,
           ServerColumnFamily.SELECTED_COLUMN,
@@ -301,6 +303,8 @@ public class MetadataConstraints implements Constraint {
         return "Malformed availability value";
       case 4006:
         return "Malformed mergeability value";
+      case 4007:
+        return "Malformed migration value";
 
     }
     return null;
@@ -440,6 +444,12 @@ public class MetadataConstraints implements Constraint {
           addViolation(violations, 4001);
         }
         break;
+      case ServerColumnFamily.MIGRATION_QUAL:
+        try {
+          new TServerInstance(new String(columnUpdate.getValue(), UTF_8));
+        } catch (Exception e) {
+          addViolation(violations, 4007);
+        }
     }
   }
 

server/base/src/main/java/org/apache/accumulo/server/manager/LiveTServerSet.java
@@ -221,7 +221,7 @@ public class LiveTServerSet implements ZooCacheWatcher {
     scanServers();
 
     ThreadPools.watchCriticalScheduledTask(this.context.getScheduledExecutor()
-        .scheduleWithFixedDelay(this::scanServers, 0, 5000, TimeUnit.MILLISECONDS));
+        .scheduleWithFixedDelay(this::scanServers, 5000, 5000, TimeUnit.MILLISECONDS));
   }
 
   public void tabletServerShuttingDown(String server) {
@@ -250,7 +250,6 @@ public class LiveTServerSet implements ZooCacheWatcher {
         checkServer(updates, doomed, tserverPath);
       }
 
-      // log.debug("Current: " + current.keySet());
       this.cback.update(this, doomed, updates);
     } catch (Exception ex) {
       log.error("{}", ex.getMessage(), ex);

commit be620ee08b660270629c068b91377ebc61835e55

Added RootRecoveryIT that exhibits bug in root table assignment
Created IT that exhibits an issue introduced with #5416 that
moved migrations from an in-memory object to a entries in the
root and metadata table.

Closes #5577

test/src/main/java/org/apache/accumulo/test/RootRecoveryIT.java
@@ -1,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.test;
+
+import java.time.Duration;
+
+import org.apache.accumulo.core.client.Accumulo;
+import org.apache.accumulo.core.client.AccumuloClient;
+import org.apache.accumulo.core.client.BatchWriter;
+import org.apache.accumulo.core.client.MutationsRejectedException;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.admin.servers.ServerId.Type;
+import org.apache.accumulo.core.conf.Property;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.metadata.SystemTables;
+import org.apache.accumulo.core.metadata.schema.MetadataSchema.TabletsSection.TabletColumnFamily;
+import org.apache.accumulo.harness.MiniClusterConfigurationCallback;
+import org.apache.accumulo.harness.SharedMiniClusterBase;
+import org.apache.accumulo.minicluster.ServerType;
+import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
+import org.apache.accumulo.test.util.Wait;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Iterables;
+
+public class RootRecoveryIT extends SharedMiniClusterBase {
+
+  private static final int ZK_TIMEOUT_MS = 5000;
+  private static final Logger LOG = LoggerFactory.getLogger(RootRecoveryIT.class);
+  
+  private static class RootRecoveryITConfigurationCallback implements MiniClusterConfigurationCallback {
+    @Override
+    public void configureMiniCluster(MiniAccumuloConfigImpl cfg, Configuration coreSite) {
+      cfg.setProperty(Property.INSTANCE_ZK_TIMEOUT, ZK_TIMEOUT_MS+"ms");
+    } 
+  }
+
+  @Override
+  protected Duration defaultTimeout() {
+    return Duration.ofMinutes(3);
+  }
+
+  @BeforeAll
+  public static void start() throws Exception {
+    SharedMiniClusterBase.startMiniClusterWithConfig(new RootRecoveryITConfigurationCallback());
+  }
+
+  @AfterAll
+  public static void stop() throws Exception {
+    SharedMiniClusterBase.stopMiniCluster();
+  }
+
+  @Test
+  public void testRootRecovery() throws Exception {
+
+    // Insert some delete mutations into the metadata table
+    try (BatchWriter writer =
+        getCluster().getServerContext().createBatchWriter(SystemTables.METADATA.tableName())) {
+      var mutation = new Mutation("3;<");
+      mutation.putDelete(TabletColumnFamily.AVAILABILITY_COLUMN.getColumnFamily(),
+          TabletColumnFamily.AVAILABILITY_COLUMN.getColumnQualifier());
+      writer.addMutation(mutation);
+      writer.flush();
+    } catch (TableNotFoundException | MutationsRejectedException e) {
+      throw new RuntimeException(e);
+    }
+
+    // Compact the metadata table, which will end up writing mutations
+    // to the root tablet for new metadata files
+    try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
+      c.tableOperations().compact(SystemTables.METADATA.tableName(), null, null, true, true);
+    }
+
+    getCluster().getClusterControl().stopAllServers(ServerType.MANAGER);
+    getCluster().getClusterControl().stopAllServers(ServerType.TABLET_SERVER);
+
+    // Let the ZooKeeper locks expire
+    Thread.sleep(ZK_TIMEOUT_MS * 2);
+
+    getCluster().getClusterControl().startAllServers(ServerType.TABLET_SERVER);
+    getCluster().getClusterControl().startAllServers(ServerType.MANAGER);
+
+    // This scan should not time out
+    try (AccumuloClient c = Accumulo.newClient().from(getClientProps()).build()) {
+      Wait.waitFor(() -> c.instanceOperations().getServers(Type.MANAGER).size() == 1);
+      Wait.waitFor(() -> c.instanceOperations().getServers(Type.TABLET_SERVER).size() == 2);
+
+      LOG.info("Scanning root table");
+      Scanner s = c.createScanner(SystemTables.ROOT.tableName());
+      @SuppressWarnings("unused")
+      int ignoredSize = Iterables.size(s);
+    }
+
+  }
+
+}