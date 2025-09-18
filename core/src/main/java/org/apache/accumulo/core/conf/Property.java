e66de10643abe915c2879aa35ef2414a8a9df431
Keith Turner <kturner@apache.org>
2025-09-18 22:00:21 +0000

    removes newlines in default fate config json
    ExistingMacIT was failing w/ the following error.  Seemed that a newline
    in the default fate config json was causing apache commons config to
    only see up to the first newline for the value.  Removed the newlines in
    the default config to fix the issue.
    
    ```
    org.apache.accumulo.core.conf.ConfigCheckUtil$ConfigCheckException: BAD CONFIG improperly formatted value for key (manager.fate.user.config, type=fate user config) : { for site config
    	at org.apache.accumulo.core.conf.ConfigCheckUtil.fatal(ConfigCheckUtil.java:150)
    	at org.apache.accumulo.core.conf.ConfigCheckUtil.validate(ConfigCheckUtil.java:62)
    	at org.apache.accumulo.core.conf.SiteConfiguration.<init>(SiteConfiguration.java:206)
    	at org.apache.accumulo.core.conf.SiteConfiguration$Builder.build(SiteConfiguration.java:171)
    	at org.apache.accumulo.miniclusterImpl.MiniAccumuloClusterImpl.<init>(MiniAccumuloClusterImpl.java:271)
    	at org.apache.accumulo.test.ExistingMacIT.testExistingInstance(ExistingMacIT.java:138)
    	at java.base/java.lang.reflect.Method.invoke(Method.java:569)
    	at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
    	at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1136)
    	at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:635)
    	at java.base/java.lang.Thread.run(Thread.java:840)
    ```

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -472,14 +472,14 @@ public enum Property {
       "1.9.3"),
   MANAGER_FATE_USER_CONFIG("manager.fate.user.config",
       """
-          {
+          {\
             'general': {'TABLE_CREATE,TABLE_DELETE,TABLE_RENAME,TABLE_ONLINE,TABLE_OFFLINE,NAMESPACE_CREATE,\
           NAMESPACE_DELETE,NAMESPACE_RENAME,TABLE_TABLET_AVAILABILITY,SHUTDOWN_TSERVER,\
           TABLE_BULK_IMPORT2,TABLE_COMPACT,TABLE_CANCEL_COMPACT,TABLE_MERGE,TABLE_DELETE_RANGE,\
-          TABLE_SPLIT,TABLE_CLONE,TABLE_IMPORT,TABLE_EXPORT,SYSTEM_MERGE': 4},
-            'commit': {'COMMIT_COMPACTION': 4},
-            'split': {'SYSTEM_SPLIT': 4}
-          }
+          TABLE_SPLIT,TABLE_CLONE,TABLE_IMPORT,TABLE_EXPORT,SYSTEM_MERGE': 4},\
+            'commit': {'COMMIT_COMPACTION': 4},\
+            'split': {'SYSTEM_SPLIT': 4}\
+          }\
           """,
       PropertyType.FATE_USER_CONFIG, """
           The number of threads used to run fault-tolerant executions (FATE) on user \
@@ -490,14 +490,14 @@ public enum Property {
           """, "4.0.0"),
   MANAGER_FATE_META_CONFIG("manager.fate.meta.config",
       """
-          {
+          {\
             'general': {'TABLE_CREATE,TABLE_DELETE,TABLE_RENAME,TABLE_ONLINE,TABLE_OFFLINE,NAMESPACE_CREATE,\
           NAMESPACE_DELETE,NAMESPACE_RENAME,TABLE_TABLET_AVAILABILITY,SHUTDOWN_TSERVER,\
           TABLE_BULK_IMPORT2,TABLE_COMPACT,TABLE_CANCEL_COMPACT,TABLE_MERGE,TABLE_DELETE_RANGE,\
-          TABLE_SPLIT,TABLE_CLONE,TABLE_IMPORT,TABLE_EXPORT,SYSTEM_MERGE': 4},
-            'commit': {'COMMIT_COMPACTION': 4},
-            'split': {'SYSTEM_SPLIT': 4}
-          }
+          TABLE_SPLIT,TABLE_CLONE,TABLE_IMPORT,TABLE_EXPORT,SYSTEM_MERGE': 4},\
+            'commit': {'COMMIT_COMPACTION': 4},\
+            'split': {'SYSTEM_SPLIT': 4}\
+          }\
           """,
       PropertyType.FATE_META_CONFIG, """
           The number of threads used to run fault-tolerant executions (FATE) on Accumulo system \
