ac7fc4d27e08b5f5cbbf287f107018c094eebf05
Dave Marion <dlmarion@apache.org>
2025-09-23 18:05:05 +0000

    Log details of key violating DefaultKeySizeConstraint
    Closes #5915

--- core/src/main/java/org/apache/accumulo/core/data/constraints/DefaultKeySizeConstraint.java
+++ core/src/main/java/org/apache/accumulo/core/data/constraints/DefaultKeySizeConstraint.java
@@ -22,7 +22,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.accumulo.core.data.ColumnUpdate;
+import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A constraints that limits the size of keys to 1mb.
@@ -34,6 +37,7 @@ import org.apache.accumulo.core.data.Mutation;
  */
 public class DefaultKeySizeConstraint implements Constraint {
 
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultKeySizeConstraint.class);
   protected static final short MAX__KEY_SIZE_EXCEEDED_VIOLATION = 1;
   protected static final long maxSize = 1048576; // 1MB default size
 
@@ -68,6 +72,8 @@ public class DefaultKeySizeConstraint implements Constraint {
 
       if (size > maxSize) {
         violations.add(MAX__KEY_SIZE_EXCEEDED_VIOLATION);
+        LOG.info("Constraint violation. Key starting with row '{}' (size = {}) larger than 1MB",
+            Key.toPrintableString(mutation.getRow(), 0, mutation.getRow().length, 32), size);
       }
     }
 
