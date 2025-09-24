f6d590a125ae3429b0254e3b9ef3e603424052e3
Keith Turner <kturner@apache.org>
2025-09-24 19:52:46 +0000

    remove import

--- test/src/main/java/org/apache/accumulo/test/functional/OnDemandTabletUnloadingIT.java
+++ test/src/main/java/org/apache/accumulo/test/functional/OnDemandTabletUnloadingIT.java
@@ -62,7 +62,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 
 public class OnDemandTabletUnloadingIT extends SharedMiniClusterBase {
 
