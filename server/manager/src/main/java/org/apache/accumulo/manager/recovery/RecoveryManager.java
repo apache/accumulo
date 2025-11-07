7ca1f51777bebc0ce6f1d0755cc8cca46f879f73
Keith Turner <kturner@apache.org>
2025-11-07 21:15:47 +0000

    Avoids redundant work in manager for log recovery
    fixes #5969

--- server/manager/src/main/java/org/apache/accumulo/manager/recovery/RecoveryManager.java
+++ server/manager/src/main/java/org/apache/accumulo/manager/recovery/RecoveryManager.java
@@ -36,7 +36,6 @@ import java.util.concurrent.TimeUnit;
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.conf.AccumuloConfiguration;
 import org.apache.accumulo.core.conf.Property;
-import org.apache.accumulo.core.dataImpl.KeyExtent;
 import org.apache.accumulo.core.fate.zookeeper.ZooCache;
 import org.apache.accumulo.core.util.threads.ThreadPools;
 import org.apache.accumulo.manager.Manager;
@@ -150,78 +149,98 @@ public class RecoveryManager {
     }
   }
 
-  public boolean recoverLogs(KeyExtent extent, Collection<Collection<String>> walogs)
-      throws IOException {
-    boolean recoveryNeeded = false;
+  // caches per log recovery decisions for its lifetime
+  public class RecoverySession {
 
-    for (Collection<String> logs : walogs) {
-      for (String walog : logs) {
+    private HashMap<String,Boolean> needsRecovery = new HashMap<>();
 
-        Path switchedWalog = VolumeUtil.switchVolume(walog, FileType.WAL,
-            manager.getContext().getVolumeReplacements());
-        if (switchedWalog != null) {
-          // replaces the volume used for sorting, but do not change entry in metadata table. When
-          // the tablet loads it will change the metadata table entry. If
-          // the tablet has the same replacement config, then it will find the sorted log.
-          log.info("Volume replaced {} -> {}", walog, switchedWalog);
-          walog = switchedWalog.toString();
-        }
+    public boolean recoverLogs(Collection<Collection<String>> walogs) throws IOException {
+      boolean recoveryNeeded = false;
 
-        String[] parts = walog.split("/");
-        String sortId = parts[parts.length - 1];
-        String filename = new Path(walog).toString();
-        String dest = RecoveryPath.getRecoveryPath(new Path(filename)).toString();
-
-        boolean sortQueued;
-        synchronized (this) {
-          sortQueued = sortsQueued.contains(sortId);
-        }
-
-        if (sortQueued
-            && zooCache.get(manager.getZooKeeperRoot() + Constants.ZRECOVERY + "/" + sortId)
-                == null) {
-          synchronized (this) {
-            sortsQueued.remove(sortId);
+      for (Collection<String> logs : walogs) {
+        for (String walog : logs) {
+          var logNeedsRecovery = needsRecovery.get(walog);
+          if (logNeedsRecovery == null) {
+            logNeedsRecovery = recoverLog(walog);
+            needsRecovery.put(walog, logNeedsRecovery);
           }
-        }
-
-        if (exists(SortedLogState.getFinishedMarkerPath(dest))) {
-          synchronized (this) {
-            closeTasksQueued.remove(sortId);
-            recoveryDelay.remove(sortId);
-            sortsQueued.remove(sortId);
-          }
-          continue;
-        }
-
-        recoveryNeeded = true;
-        synchronized (this) {
-          if (!closeTasksQueued.contains(sortId) && !sortsQueued.contains(sortId)) {
-            AccumuloConfiguration aconf = manager.getConfiguration();
-            @SuppressWarnings("deprecation")
-            LogCloser closer = Property.createInstanceFromPropertyName(aconf,
-                aconf.resolve(Property.MANAGER_WAL_CLOSER_IMPLEMENTATION,
-                    Property.MANAGER_WALOG_CLOSER_IMPLEMETATION),
-                LogCloser.class, new HadoopLogCloser());
-            Long delay = recoveryDelay.get(sortId);
-            if (delay == null) {
-              delay = aconf.getTimeInMillis(Property.MANAGER_RECOVERY_DELAY);
-            } else {
-              delay = Math.min(2 * delay, 1000 * 60 * 5L);
-            }
-
-            log.info("Starting recovery of {} (in : {}s), tablet {} holds a reference", filename,
-                (delay / 1000), extent);
-
-            ScheduledFuture<?> future = executor.schedule(
-                new LogSortTask(closer, filename, dest, sortId), delay, TimeUnit.MILLISECONDS);
-            ThreadPools.watchNonCriticalScheduledTask(future);
-            closeTasksQueued.add(sortId);
-            recoveryDelay.put(sortId, delay);
-          }
+          recoveryNeeded |= logNeedsRecovery;
         }
       }
+
+      return recoveryNeeded;
     }
+  }
+
+  public RecoverySession newRecoverySession() {
+    return new RecoverySession();
+  }
+
+  private boolean recoverLog(String walog) throws IOException {
+    boolean recoveryNeeded = false;
+
+    Path switchedWalog =
+        VolumeUtil.switchVolume(walog, FileType.WAL, manager.getContext().getVolumeReplacements());
+    if (switchedWalog != null) {
+      // replaces the volume used for sorting, but do not change entry in metadata table. When
+      // the tablet loads it will change the metadata table entry. If
+      // the tablet has the same replacement config, then it will find the sorted log.
+      log.info("Volume replaced {} -> {}", walog, switchedWalog);
+      walog = switchedWalog.toString();
+    }
+
+    String[] parts = walog.split("/");
+    String sortId = parts[parts.length - 1];
+    String filename = new Path(walog).toString();
+    String dest = RecoveryPath.getRecoveryPath(new Path(filename)).toString();
+
+    boolean sortQueued;
+    synchronized (this) {
+      sortQueued = sortsQueued.contains(sortId);
+    }
+
+    if (sortQueued
+        && zooCache.get(manager.getZooKeeperRoot() + Constants.ZRECOVERY + "/" + sortId) == null) {
+      synchronized (this) {
+        sortsQueued.remove(sortId);
+      }
+    }
+
+    if (exists(SortedLogState.getFinishedMarkerPath(dest))) {
+      synchronized (this) {
+        closeTasksQueued.remove(sortId);
+        recoveryDelay.remove(sortId);
+        sortsQueued.remove(sortId);
+      }
+      return false;
+    }
+
+    recoveryNeeded = true;
+    synchronized (this) {
+      if (!closeTasksQueued.contains(sortId) && !sortsQueued.contains(sortId)) {
+        AccumuloConfiguration aconf = manager.getConfiguration();
+        @SuppressWarnings("deprecation")
+        LogCloser closer = Property.createInstanceFromPropertyName(aconf,
+            aconf.resolve(Property.MANAGER_WAL_CLOSER_IMPLEMENTATION,
+                Property.MANAGER_WALOG_CLOSER_IMPLEMETATION),
+            LogCloser.class, new HadoopLogCloser());
+        Long delay = recoveryDelay.get(sortId);
+        if (delay == null) {
+          delay = aconf.getTimeInMillis(Property.MANAGER_RECOVERY_DELAY);
+        } else {
+          delay = Math.min(2 * delay, 1000 * 60 * 5L);
+        }
+
+        log.info("Starting recovery of {} (in : {}s)", filename, (delay / 1000));
+
+        ScheduledFuture<?> future = executor.schedule(
+            new LogSortTask(closer, filename, dest, sortId), delay, TimeUnit.MILLISECONDS);
+        ThreadPools.watchNonCriticalScheduledTask(future);
+        closeTasksQueued.add(sortId);
+        recoveryDelay.put(sortId, delay);
+      }
+    }
+
     return recoveryNeeded;
   }
 }
