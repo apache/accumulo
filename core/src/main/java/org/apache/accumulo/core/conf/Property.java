3d11ae443e4b243bbab42d7c2b0a04ce644b3f58
Keith Turner <kturner@apache.org>
2025-12-04 21:13:31 +0000

    code review update

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -282,10 +282,10 @@ public enum Property {
   GENERAL_OPENTELEMETRY_ENABLED("general.opentelemetry.enabled", "false", PropertyType.BOOLEAN,
       "Enables tracing for all spans in the process using OpenTelemetry (assuming OpenTelemetry is "
           + "configured). When true, if a thread with no active trace encounters a span, then a new trace with "
-          + "a new id is started. When this property is false, RPC request that arrive at a process with tracing "
-          + "set will still emit traces for all spans as long as OpenTelemetry is properly configured. For example "
+          + "a new id is started. When this property is false, RPC requests that arrive at a process with tracing "
+          + "set will still emit traces for all spans as long as OpenTelemetry is properly configured. For example, "
           + "if this property is only set for the Accumulo GC server, then all RPCs initiated by the GC would emit "
-          + "trace data across the cluster. However threads on non gc server processes with no active trace would "
+          + "trace data across the cluster. However, threads on non gc server processes with no active trace would "
           + "not emit anything when they encounter a span.",
       "2.1.0"),
   GENERAL_THREADPOOL_SIZE("general.server.threadpool.size", "1", PropertyType.COUNT,
