f2aa47dbbc0bc817f73a36fd726cec79218f78c7
Daniel Roberts <ddanielr@gmail.com>
2025-12-04 13:38:58 -0500

    Use TabletLogger.renamed and cleanup log messages (#5993)
    Use the TabletLogger.renamed method and switch some log messages to
    using positional arguments.

--- server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Tablet.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/Tablet.java
@@ -1581,11 +1581,12 @@ public class Tablet extends TabletBase {
           }
           attemptedRename = true;
           ScanfileManager.rename(vm, tmpDatafile.getPath(), newDatafile.getPath());
+          TabletLogger.renamed(getExtent(), tmpDatafile, newDatafile);
         }
         break;
       } catch (IOException ioe) {
-        log.warn("Tablet " + getExtent() + " failed to rename " + newDatafile
-            + " after MinC, will retry in 60 secs...", ioe);
+        log.warn("Tablet {} failed to rename {} after MinC, will retry in 60 secs...", getExtent(),
+            newDatafile, ioe);
         sleepUninterruptibly(1, TimeUnit.MINUTES);
       }
     } while (true);
@@ -1630,7 +1631,7 @@ public class Tablet extends TabletBase {
               commitSession.getWALogSeq() + 2);
           break;
         } catch (IOException e) {
-          log.error("Failed to write to write-ahead log " + e.getMessage() + " will retry", e);
+          log.error("Failed to write to write-ahead log {} will retry", e.getMessage(), e);
           sleepUninterruptibly(1, TimeUnit.SECONDS);
         }
       } while (true);
