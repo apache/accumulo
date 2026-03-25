e786409dc520ed956c223fd7bb1f25105b047c3c
Keith Turner <kturner@apache.org>
2026-03-25 10:03:51 -0700

    Removes coordinator cancel RPC (#6243)
    Modified the command that calls this to instead reach out directly to
    the compactor.  This change is made in support of #6217, it removes a
    useage of the running cahce and it simplifies the coordinators RPCs.

--- core/src/main/thrift/compaction-coordinator.thrift
+++ core/src/main/thrift/compaction-coordinator.thrift
@@ -147,15 +147,6 @@ service CompactionCoordinatorService {
      2:client.ThriftNotActiveServiceException tnase
   )
 
-  void cancel(
-    1:client.TInfo tinfo
-    2:security.TCredentials credentials
-    3:string externalCompactionId
-  )throws(
-    1:client.ThriftSecurityException sec
-    2:client.ThriftNotActiveServiceException tnase
-  )
-
   oneway void recordCompletion(
     1:client.TInfo tinfo
     2:security.TCredentials credentials
