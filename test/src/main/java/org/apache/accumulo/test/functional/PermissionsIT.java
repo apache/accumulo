apache/accumulo

commit 328b72a7deb7dc7f77b38fbf6987e5cd53b59700

Give ITs read permissions on Fate and ScanRef tables (#6253)
This change gives the majority of ITs read permissions
on the Fate and ScanRef tables. It's still possible
that additional work may need to be done in specific
ITs that need write or other permission, are for classes
that are not subclasses of the classes modified in
this commit.

test/src/main/java/org/apache/accumulo/harness/AccumuloClusterHarness.java
@@ -165,6 +165,10 @@ public abstract class AccumuloClusterHarness extends AccumuloITBase
 
     if (type.isDynamic()) {
       cluster.start();
+      try (AccumuloClient ac = Accumulo.newClient().from(getClientProps()).build()) {
+        AccumuloITBase.setSystemTablePermsForITs(ac,
+            cluster.getServerContext().securityOperations());
+      }
     } else {
       log.info("Removing tables which appear to be from a previous test run");
       cleanupTables();

test/src/main/java/org/apache/accumulo/harness/AccumuloITBase.java
@@ -30,9 +30,15 @@ import java.time.Duration;
 import java.util.Collection;
 import java.util.Map.Entry;
 
+import org.apache.accumulo.core.client.AccumuloClient;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.metadata.SystemTables;
+import org.apache.accumulo.core.security.TablePermission;
 import org.apache.accumulo.test.util.Wait;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -159,5 +165,15 @@ public class AccumuloITBase extends WithTestNames {
     jar.deleteOnExit();
 
     return jar;
+  }
+
+  // ITs may use the client properties to create an AccumuloClient
+  // and then interact with the Fate and ScanRef tables. By default
+  // only the system user can interact with these tables.
+  public static void setSystemTablePermsForITs(AccumuloClient client, SecurityOperations ops)
+      throws AccumuloException, AccumuloSecurityException {
+    ops.grantTablePermission(client.whoami(), SystemTables.FATE.tableName(), TablePermission.READ);
+    ops.grantTablePermission(client.whoami(), SystemTables.SCAN_REF.tableName(),
+        TablePermission.READ);
   }
 }

test/src/main/java/org/apache/accumulo/harness/SharedMiniClusterBase.java
@@ -136,7 +136,9 @@ public abstract class SharedMiniClusterBase extends AccumuloITBase implements Cl
     cluster = harness.create(getTestClassName(), SharedMiniClusterBase.class.getSimpleName(), token,
         miniClusterCallback, krb);
     cluster.start();
-
+    try (AccumuloClient ac = Accumulo.newClient().from(getClientProps()).build()) {
+      AccumuloITBase.setSystemTablePermsForITs(ac, cluster.getServerContext().securityOperations());
+    }
   }
 
   private static String getTestClassName() {

test/src/main/java/org/apache/accumulo/test/functional/ConfigurableMacBase.java
@@ -30,6 +30,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.accumulo.core.client.Accumulo;
+import org.apache.accumulo.core.client.AccumuloClient;
 import org.apache.accumulo.core.conf.ClientProperty;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
@@ -136,6 +138,10 @@ public class ConfigurableMacBase extends AccumuloITBase {
     for (int i = 0; i < 3; i++) {
       try {
         cluster.start();
+        try (AccumuloClient ac = Accumulo.newClient().from(getClientProperties()).build()) {
+          AccumuloITBase.setSystemTablePermsForITs(ac,
+              cluster.getServerContext().securityOperations());
+        }
         return;
       } catch (ZooKeeperBindException e) {
         lastException = e;

commit 463e62841b4eb3eeefc8211ee9458447fcc27cdf

Fix PermissionsIT, broken by #6253
PR #6253 fixed an issue where the fate and scanref
tables could not be scanned by the ITs. The fix in
in PermissionsIT which tests that non-system users
don't have the ability to scan the fate and scanref
tables. This commit overrides the behavior added in

test/src/main/java/org/apache/accumulo/harness/AccumuloClusterHarness.java
@@ -33,6 +33,8 @@ import org.apache.accumulo.cluster.ClusterUsers;
 import org.apache.accumulo.cluster.standalone.StandaloneAccumuloCluster;
 import org.apache.accumulo.core.client.Accumulo;
 import org.apache.accumulo.core.client.AccumuloClient;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.client.security.tokens.AuthenticationToken;
@@ -166,8 +168,7 @@ public abstract class AccumuloClusterHarness extends AccumuloITBase
     if (type.isDynamic()) {
       cluster.start();
       try (AccumuloClient ac = Accumulo.newClient().from(getClientProps()).build()) {
-        AccumuloITBase.setSystemTablePermsForITs(ac,
-            cluster.getServerContext().securityOperations());
+        setSystemTablePerms(ac, cluster.getServerContext().securityOperations());
       }
     } else {
       log.info("Removing tables which appear to be from a previous test run");
@@ -176,6 +177,12 @@ public abstract class AccumuloClusterHarness extends AccumuloITBase
       cleanupUsers();
     }
 
+  }
+
+  protected void setSystemTablePerms(AccumuloClient client, SecurityOperations sops)
+      throws AccumuloException, AccumuloSecurityException {
+    AccumuloITBase.setSystemTablePermsForITs(client,
+        cluster.getServerContext().securityOperations());
   }
 
   public void cleanupTables() throws Exception {

test/src/main/java/org/apache/accumulo/test/functional/PermissionsIT.java
@@ -44,6 +44,7 @@ import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.MutationsRejectedException;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.security.SecurityErrorCode;
 import org.apache.accumulo.core.client.security.tokens.AuthenticationToken;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
@@ -75,6 +76,13 @@ public class PermissionsIT extends AccumuloClusterHarness {
   @Override
   protected Duration defaultTimeout() {
     return Duration.ofSeconds(90);
+  }
+
+  @Override
+  protected void setSystemTablePerms(AccumuloClient client, SecurityOperations sops)
+      throws AccumuloException, AccumuloSecurityException {
+    // overridden to do nothing. The parent class gives read permissions to the
+    // system tables for the ITs. We want to test the default behavior
   }
 
   @BeforeEach