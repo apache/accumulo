apache/accumulo

commit a420842a121ded35d006c3dd3cf36a28438795d8

Modified InstanceOperations.getServer(MANAGER) to return all (#6304)
This used to only return the primary manager.

core/src/main/java/org/apache/accumulo/core/rpc/clients/ManagerClient.java
@@ -21,10 +21,11 @@ package org.apache.accumulo.core.rpc.clients;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.net.UnknownHostException;
-import java.util.Set;
+import java.util.Optional;
 
-import org.apache.accumulo.core.client.admin.servers.ServerId;
 import org.apache.accumulo.core.clientImpl.ClientContext;
+import org.apache.accumulo.core.lock.ServiceLockData;
+import org.apache.accumulo.core.lock.ServiceLockPaths;
 import org.apache.accumulo.core.rpc.ThriftUtil;
 import org.apache.thrift.TServiceClient;
 import org.apache.thrift.transport.TTransportException;
@@ -37,14 +38,21 @@ public interface ManagerClient<C extends TServiceClient> {
   default C getManagerConnection(Logger log, ThriftClientTypes<C> type, ClientContext context) {
     checkArgument(context != null, "context is null");
 
-    Set<ServerId> managers = context.instanceOperations().getServers(ServerId.Type.MANAGER);
+    // obtain the primary manager location
+    String managerLocation = null;
+    ServiceLockPaths.ServiceLockPath m = context.getServerPaths().getManager(true);
+    if (m != null) {
+      Optional<ServiceLockData> sld = context.getZooCache().getLockData(m);
+      if (sld.isPresent()) {
+        managerLocation = sld.orElseThrow().getAddressString(ServiceLockData.ThriftService.MANAGER);
+      }
+    }
 
-    if (managers == null || managers.isEmpty()) {
+    if (managerLocation == null) {
       log.debug("No managers...");
       return null;
     }
 
-    final String managerLocation = managers.iterator().next().toHostPortString();
     if (managerLocation.equals("0.0.0.0:0")) {
       // The Manager creates the lock with an initial address of 0.0.0.0:0, then
       // later updates the lock contents with the actual address after everything

core/src/main/java/org/apache/accumulo/core/clientImpl/InstanceOperationsImpl.java
@@ -233,7 +233,7 @@ public class InstanceOperationsImpl implements InstanceOperations {
     if (managers == null || managers.isEmpty()) {
       return List.of();
     } else {
-      return List.of(managers.iterator().next().toHostPortString());
+      return managers.stream().map(ServerId::toHostPortString).toList();
     }
   }
 
@@ -561,18 +561,8 @@ public class InstanceOperationsImpl implements InstanceOperations {
             .forEach(c -> results.add(createServerId(type, c)));
         break;
       case MANAGER:
-        ServiceLockPath m = context.getServerPaths().getManager(true);
-        if (m != null) {
-          Optional<ServiceLockData> sld = context.getZooCache().getLockData(m);
-          String location = null;
-          if (sld.isPresent()) {
-            location = sld.orElseThrow().getAddressString(ThriftService.MANAGER);
-            if (location != null && addressSelector.getPredicate().test(location)) {
-              HostAndPort hp = HostAndPort.fromString(location);
-              results.add(new ServerId(type, ResourceGroupId.DEFAULT, hp.getHost(), hp.getPort()));
-            }
-          }
-        }
+        context.getServerPaths().getAssistantManagers(addressSelector, true)
+            .forEach(s -> results.add(createServerId(type, s)));
         break;
       case MONITOR:
         ServiceLockPath mon = context.getServerPaths().getMonitor(true);

server/base/src/main/java/org/apache/accumulo/server/util/Info.java
@@ -57,13 +57,9 @@ public class Info extends ServerKeywordExecutable<ServerOpts> {
   public void execute(JCommander cl, ServerOpts options) throws Exception {
     ServerContext context = getServerContext();
     Set<ServerId> managers = context.instanceOperations().getServers(ServerId.Type.MANAGER);
-    String manager = null;
-    if (managers != null && !managers.isEmpty()) {
-      manager = managers.iterator().next().getHost();
-    }
 
     System.out.println("monitor: " + MonitorUtil.getLocation(context));
-    System.out.println("managers: " + manager);
+    System.out.println("managers: " + managers);
     System.out.println("zookeepers: " + context.getZooKeepers());
   }
 }

server/base/src/main/java/org/apache/accumulo/server/util/checkCommand/SystemConfigCheckRunner.java
@@ -76,8 +76,6 @@ public class SystemConfigCheckRunner implements CheckRunner {
       var servers = context.instanceOperations().getServers(serverType);
 
       switch (serverType) {
-        case MANAGER:
-          // essential process
         case GARBAGE_COLLECTOR:
           // essential process
           if (servers.size() != 1) {
@@ -101,6 +99,8 @@ public class SystemConfigCheckRunner implements CheckRunner {
             log.trace("Verified ZooKeeper lock for {}", servers);
           }
           break;
+        case MANAGER:
+          // essential process(es)
         case TABLET_SERVER:
           // essential process(es)
         case COMPACTOR:

test/src/main/java/org/apache/accumulo/test/InstanceOperationsIT.java
@@ -31,12 +31,13 @@ import java.util.Set;
 import org.apache.accumulo.core.client.Accumulo;
 import org.apache.accumulo.core.client.AccumuloClient;
 import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.admin.InstanceOperations;
 import org.apache.accumulo.core.client.admin.servers.ServerId;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.data.ResourceGroupId;
 import org.apache.accumulo.harness.AccumuloClusterHarness;
+import org.apache.accumulo.minicluster.ServerType;
+import org.apache.accumulo.miniclusterImpl.MiniAccumuloClusterImpl;
 import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
 import org.apache.accumulo.test.util.Wait;
 import org.apache.hadoop.conf.Configuration;
@@ -60,7 +61,7 @@ public class InstanceOperationsIT extends AccumuloClusterHarness {
    * Verify that we get the same servers from getServers() and getServer()
    */
   @Test
-  public void testGetServer() {
+  public void testGetServer() throws Exception {
     try (AccumuloClient client = Accumulo.newClient().from(getClientProps()).build()) {
       final InstanceOperations iops = client.instanceOperations();
 
@@ -108,6 +109,20 @@ public class InstanceOperationsIT extends AccumuloClusterHarness {
         assertEquals(expectedServerId, actualServerId);
       });
 
+      var cluster = (MiniAccumuloClusterImpl) getCluster();
+      cluster.getConfig().getClusterServerConfiguration().setNumManagers(3);
+      cluster.getClusterControl().start(ServerType.MANAGER);
+      Wait.waitFor(() -> iops.getServers(ServerId.Type.MANAGER).size() == 3);
+      final Set<ServerId> managers3 = iops.getServers(ServerId.Type.MANAGER);
+      assertEquals(3, managers3.size());
+      managers3.forEach(expectedServerId -> {
+        ServerId actualServerId =
+            iops.getServer(expectedServerId.getType(), expectedServerId.getResourceGroup(),
+                expectedServerId.getHost(), expectedServerId.getPort());
+        assertNotNull(actualServerId, "Expected to find manager " + expectedServerId);
+        assertEquals(expectedServerId, actualServerId);
+      });
+
       // verify GC
       final Set<ServerId> gcs = iops.getServers(ServerId.Type.GARBAGE_COLLECTOR);
       assertEquals(1, gcs.size()); // Assuming there is only one garbage collector
@@ -128,7 +143,7 @@ public class InstanceOperationsIT extends AccumuloClusterHarness {
 
   @SuppressWarnings("deprecation")
   @Test
-  public void testGetServers() throws AccumuloException, AccumuloSecurityException {
+  public void testGetServers() throws Exception {
     try (AccumuloClient client = Accumulo.newClient().from(getClientProps()).build()) {
       InstanceOperations iops = client.instanceOperations();
 
@@ -146,6 +161,13 @@ public class InstanceOperationsIT extends AccumuloClusterHarness {
 
       assertEquals(1, iops.getServers(ServerId.Type.MANAGER).size());
       assertEquals(1, iops.getManagerLocations().size());
+      validateAddresses(iops.getManagerLocations(), iops.getServers(ServerId.Type.MANAGER));
+      var cluster = (MiniAccumuloClusterImpl) getCluster();
+      cluster.getConfig().getClusterServerConfiguration().setNumManagers(3);
+      cluster.getClusterControl().start(ServerType.MANAGER);
+      Wait.waitFor(() -> iops.getServers(ServerId.Type.MANAGER).size() == 3);
+      assertEquals(3, iops.getServers(ServerId.Type.MANAGER).size());
+      assertEquals(3, iops.getManagerLocations().size());
       validateAddresses(iops.getManagerLocations(), iops.getServers(ServerId.Type.MANAGER));
 
       assertEquals(1, iops.getServers(ServerId.Type.GARBAGE_COLLECTOR).size());

commit 69012e4f1b38610c669d60c3e09cd781952b75df

fixes MultipleManagerStartupWaitIT
This test was broken by changes in #6304 which made the way it got the
primary manager addr incorrect. Modified the test to get the primary
manager addr in another way.

core/src/main/java/org/apache/accumulo/core/rpc/clients/ManagerClient.java
@@ -35,10 +35,7 @@ import com.google.common.net.HostAndPort;
 
 public interface ManagerClient<C extends TServiceClient> {
 
-  default C getManagerConnection(Logger log, ThriftClientTypes<C> type, ClientContext context) {
-    checkArgument(context != null, "context is null");
-
-    // obtain the primary manager location
+  public static String getPrimaryManagerLocation(ClientContext context) {
     String managerLocation = null;
     ServiceLockPaths.ServiceLockPath m = context.getServerPaths().getManager(true);
     if (m != null) {
@@ -47,6 +44,14 @@ public interface ManagerClient<C extends TServiceClient> {
         managerLocation = sld.orElseThrow().getAddressString(ServiceLockData.ThriftService.MANAGER);
       }
     }
+    return managerLocation;
+  }
+
+  default C getManagerConnection(Logger log, ThriftClientTypes<C> type, ClientContext context) {
+    checkArgument(context != null, "context is null");
+
+    // obtain the primary manager location
+    String managerLocation = getPrimaryManagerLocation(context);
 
     if (managerLocation == null) {
       log.debug("No managers...");

test/src/main/java/org/apache/accumulo/test/MultipleManagerStartupWaitIT.java
@@ -26,9 +26,9 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.apache.accumulo.core.client.admin.servers.ServerId;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.lock.ServiceLockPaths.AddressSelector;
+import org.apache.accumulo.core.rpc.clients.ManagerClient;
 import org.apache.accumulo.minicluster.ServerType;
 import org.apache.accumulo.miniclusterImpl.MiniAccumuloConfigImpl;
 import org.apache.accumulo.test.functional.ConfigurableMacBase;
@@ -104,12 +104,10 @@ public class MultipleManagerStartupWaitIT extends ConfigurableMacBase {
     // There is a time period where the primary manager has the lock but has not yet set the address
     // on the lock, when the address is not set an empty set would be returned. So this waits for
     // the address to be set.
-    Wait.waitFor(() -> !getCluster().getServerContext().instanceOperations()
-        .getServers(ServerId.Type.MANAGER).isEmpty());
-    Set<ServerId> primary =
-        getCluster().getServerContext().instanceOperations().getServers(ServerId.Type.MANAGER);
-    assertEquals(1, primary.size());
-    assertTrue(managerHosts.contains(primary.iterator().next().toHostPortString()));
+    Wait.waitFor(() -> ManagerClient.getPrimaryManagerLocation(cluster.getServerContext()) != null);
+    var primaryAddr = ManagerClient.getPrimaryManagerLocation(cluster.getServerContext());
+    assertTrue(managerHosts.contains(primaryAddr),
+        () -> "mangerHosts:" + managerHosts + " primaryAddr:" + primaryAddr);
     assertNull(startError.get());
     clusterThread.join();
   }