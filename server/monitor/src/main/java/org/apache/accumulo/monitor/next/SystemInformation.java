da4ff8c98d0c11f3b94784d4a113824297beea72
Keith Turner <kturner@apache.org>
2026-04-16 07:02:59 -0700

    Removes unused monitor rest endpoint '/ec' (#6317)

--- server/monitor/src/main/java/org/apache/accumulo/monitor/next/SystemInformation.java
+++ server/monitor/src/main/java/org/apache/accumulo/monitor/next/SystemInformation.java
@@ -71,7 +71,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.benmanes.caffeine.cache.Cache;
-import com.google.common.net.HostAndPort;
 
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.Meter.Id;
@@ -394,7 +393,6 @@ public class SystemInformation {
   // Compaction Information
   private final Map<String,List<FMetric>> queueMetrics = new ConcurrentHashMap<>();
   private volatile Set<ServerId> registeredCompactors = Set.of();
-  private volatile HostAndPort coordinatorHost;
 
   protected final Map<String,TimeOrderedRunningCompactionSet> longRunningCompactionsByRg =
       new ConcurrentHashMap<>();
@@ -443,7 +441,6 @@ public class SystemInformation {
     rgTServerMetrics.clear();
     queueMetrics.clear();
     registeredCompactors = Set.of();
-    coordinatorHost = null;
     longRunningCompactionsByRg.clear();
     tables.clear();
     tablets.clear();
@@ -559,13 +556,12 @@ public class SystemInformation {
         .add(new RunningCompactionInfo(tec));
   }
 
-  public void processExternalCompactionInventory(Set<ServerId> compactors, HostAndPort host) {
+  public void processExternalCompactionInventory(Set<ServerId> compactors) {
     if (compactors == null) {
       registeredCompactors = Set.of();
     } else {
       registeredCompactors = Set.copyOf(compactors);
     }
-    coordinatorHost = host;
   }
 
   public void processTabletInformation(TableId tableId, String tableName, TabletInformation info) {
@@ -754,10 +750,6 @@ public class SystemInformation {
 
   public Set<ServerId> getCompactorServers() {
     return registeredCompactors;
-  }
-
-  public HostAndPort getCoordinatorHost() {
-    return coordinatorHost;
   }
 
   public Map<String,TimeOrderedRunningCompactionSet> getTopRunningCompactions() {
