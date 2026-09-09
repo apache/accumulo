72f453b1f601e4b933103e81806c9884374cbc29
Daniel Roberts <ddanielr@gmail.com>
2026-09-09 09:33:06 -0400

    Update core/src/main/java/org/apache/accumulo/core/conf/Property.java
    Co-authored-by: Dave Marion <dlmarion@apache.org>

--- core/src/main/java/org/apache/accumulo/core/conf/Property.java
+++ core/src/main/java/org/apache/accumulo/core/conf/Property.java
@@ -261,7 +261,7 @@ public enum Property {
           + "org.apache.accumulo.server.rpc.ThriftServerType for more information. "
           + "Only useful for benchmarking thrift servers.",
       "1.7.0"),
-  GENERAL_RPC_SERVER_SELECTOR_THREADS("general.rpc.server.selector.threads", "2",
+  GENERAL_RPC_SERVER_SELECTOR_THREADS("general.rpc.server.threaded.selector.threads", "2",
       PropertyType.COUNT,
       "The number of selector threads to use for the TThreadedSelectorServer. "
           + " If the value ends with C, then it will be multiplied by the number of cores on the system.",
